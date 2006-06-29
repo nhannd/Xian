ca2ea29b012714e89381227ea51317e02b5d9d6d
jonathan <devnull@localhost>
2006-06-29 21:44:19 +0000

    Requirement: R151 - forgot this file in last commit.

---
+++ Desktop/View/WinForms/ApplicationComponentView.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClearCanvas.Desktop.View.WinForms
+{
+    public abstract class ApplicationComponentView : WinFormsView, IApplicationComponentView
+    {
+        private IApplicationComponent _component;
+
+        protected IApplicationComponent Component
+        {
+            get { return _component; }
+        }
+
+        #region IApplicationComponentView Members
+
+        public void SetComponent(IApplicationComponent component)
+        {
+            _component = component;
+        }
+
+        #endregion
+    }
+}
