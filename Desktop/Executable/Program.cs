32ccd0483ed6b5352751e21cf2e6a8fcd81c457a
jonathan <devnull@localhost>
2006-07-13 21:37:42 +0000

    Requirement: R151- Modified Executable to accept command line arguments.  The first argument is expected to be a class name, which is treated as the name of the application root class to launch.  Subsequent arguments are forwarded to the application root.  This is intentionally simplistic, and will probably need to change in future.

--- Desktop/Executable/Program.cs
+++ Desktop/Executable/Program.cs
@@ -20,8 +20,24 @@ namespace ClearCanvas.Desktop.Executable
 #if !MONO && !DEBUG
 			SplashScreen.ShowSplashScreen();
 #endif
-			Platform.PluginManager.PluginLoaded += new EventHandler<PluginLoadedEventArgs>(OnPluginProgress);
-			Platform.StartApp();
+            Platform.PluginManager.PluginLoaded += new EventHandler<PluginLoadedEventArgs>(OnPluginProgress);
+
+            // check for command line arguments
+            if (args.Length > 0)
+            {
+                // for the sake of simplicity, this is a naive implementation (probably needs to change in future)
+                // if there is > 0 arguments, assume the first argument is a class name
+                // and bundle the subsequent arguments into a secondary array which is 
+                // forwarded to the application root class
+                string[] args1 = new string[args.Length - 1];
+                Array.Copy(args, 1, args1, 0, args1.Length);
+
+                Platform.StartApp(new ClassNameExtensionFilter(args[0]), args1);
+            }
+            else
+            {
+                Platform.StartApp();
+            }
 		}
 
 		private static void OnPluginProgress(object sender, PluginLoadedEventArgs e)
