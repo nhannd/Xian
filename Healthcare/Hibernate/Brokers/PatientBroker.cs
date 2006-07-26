a049a1444b6235163b2ab34686628fa49c7e0f50
jonathan <devnull@localhost>
2006-07-26 15:28:32 +0000

    Requirement: R151 - Modified PatientBroker.Find() method to filter out duplicate results created by joining on patient identifiers.  This is unfortunate but by far the lesser evil (not joining would lead to terrible performance).

--- Healthcare/Hibernate/Brokers/PatientBroker.cs
+++ Healthcare/Hibernate/Brokers/PatientBroker.cs
@@ -22,13 +22,26 @@ namespace ClearCanvas.Healthcare.Hibernate.Brokers
             // note use of "join fetch" - this is important to ensure that the patient identifiers are fetched
             // along with the patient in a single select statement, rather than having to execute a subsequent
             // select statement
+            
             HqlQuery query = HqlQuery.FromSearchCriteria(
                 "from Patient p left join fetch p.Identifiers i",
                 new string[] { "p", "i" },
                 new SearchCriteria[] { criteria, criteria.Identifiers },
                 page);
-            
-            return MakeTypeSafe(ExecuteHql(query));
+
+            IList<Patient> results = MakeTypeSafe(ExecuteHql(query));
+
+            // due to use of "join fetch", the result set may contain duplicates
+            // (this is a fundamental limitation of SQL)
+            // these should be removed
+            List<Patient> filtered = new List<Patient>();
+            foreach(Patient p in results)
+            {
+                if(!filtered.Contains(p))
+                    filtered.Add(p);
+            }
+
+            return filtered;
         }
     }
 }
