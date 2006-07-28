a274cd5a576422c7fa0bf2169f2715e607911e6c
rob <devnull@localhost>
2006-07-28 17:51:55 +0000

    Requirement: R151 - Layout changes to AddressEditorControl

--- Ris/Client/Admin/View/WinForms/AddressesEditorControl.Designer.cs
+++ Ris/Client/Admin/View/WinForms/AddressesEditorControl.Designer.cs
@@ -38,132 +38,182 @@ namespace ClearCanvas.Ris.Client.Admin.View.WinForms
             this._street = new ClearCanvas.Controls.WinForms.TextField();
             this._province = new ClearCanvas.Controls.WinForms.ComboBoxField();
             this._country = new ClearCanvas.Controls.WinForms.ComboBoxField();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.flowLayoutPanel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // _acceptButton
             // 
-            this._acceptButton.Location = new System.Drawing.Point(466, 219);
+            this._acceptButton.Location = new System.Drawing.Point(392, 3);
             this._acceptButton.Name = "_acceptButton";
             this._acceptButton.Size = new System.Drawing.Size(75, 23);
-            this._acceptButton.TabIndex = 8;
+            this._acceptButton.TabIndex = 0;
             this._acceptButton.Text = "Accept";
             this._acceptButton.UseVisualStyleBackColor = true;
             this._acceptButton.Click += new System.EventHandler(this._acceptButton_Click);
             // 
             // _cancelButton
             // 
-            this._cancelButton.Location = new System.Drawing.Point(563, 219);
+            this._cancelButton.Location = new System.Drawing.Point(473, 3);
             this._cancelButton.Name = "_cancelButton";
             this._cancelButton.Size = new System.Drawing.Size(75, 23);
-            this._cancelButton.TabIndex = 9;
+            this._cancelButton.TabIndex = 1;
             this._cancelButton.Text = "Cancel";
             this._cancelButton.UseVisualStyleBackColor = true;
             this._cancelButton.Click += new System.EventHandler(this._cancelButton_Click);
             // 
             // _postalCode
             // 
+            this._postalCode.Dock = System.Windows.Forms.DockStyle.Fill;
             this._postalCode.LabelText = "Postal Code";
-            this._postalCode.Location = new System.Drawing.Point(535, 148);
-            this._postalCode.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this._postalCode.Location = new System.Drawing.Point(280, 92);
+            this._postalCode.Margin = new System.Windows.Forms.Padding(2);
             this._postalCode.Name = "_postalCode";
-            this._postalCode.Size = new System.Drawing.Size(102, 41);
-            this._postalCode.TabIndex = 5;
+            this._postalCode.Size = new System.Drawing.Size(135, 41);
+            this._postalCode.TabIndex = 6;
             this._postalCode.Value = null;
             // 
             // _validFrom
             // 
+            this._validFrom.Dock = System.Windows.Forms.DockStyle.Fill;
             this._validFrom.LabelText = "Valid From";
-            this._validFrom.Location = new System.Drawing.Point(314, 24);
-            this._validFrom.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this._validFrom.Location = new System.Drawing.Point(280, 2);
+            this._validFrom.Margin = new System.Windows.Forms.Padding(2);
             this._validFrom.Name = "_validFrom";
             this._validFrom.Nullable = true;
-            this._validFrom.Size = new System.Drawing.Size(150, 41);
-            this._validFrom.TabIndex = 6;
+            this._validFrom.Size = new System.Drawing.Size(135, 41);
+            this._validFrom.TabIndex = 1;
             this._validFrom.Value = new System.DateTime(2006, 7, 26, 16, 37, 8, 953);
             this._validFrom.Visible = false;
             // 
             // _validUntil
             // 
+            this._validUntil.Dock = System.Windows.Forms.DockStyle.Fill;
             this._validUntil.LabelText = "Valid Until";
-            this._validUntil.Location = new System.Drawing.Point(488, 24);
-            this._validUntil.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this._validUntil.Location = new System.Drawing.Point(419, 2);
+            this._validUntil.Margin = new System.Windows.Forms.Padding(2);
             this._validUntil.Name = "_validUntil";
             this._validUntil.Nullable = true;
-            this._validUntil.Size = new System.Drawing.Size(150, 41);
-            this._validUntil.TabIndex = 7;
+            this._validUntil.Size = new System.Drawing.Size(136, 41);
+            this._validUntil.TabIndex = 2;
             this._validUntil.Value = new System.DateTime(2006, 7, 26, 16, 37, 6, 765);
             this._validUntil.Visible = false;
             // 
             // _type
             // 
             this._type.DataSource = null;
+            this._type.Dock = System.Windows.Forms.DockStyle.Fill;
             this._type.LabelText = "Type";
-            this._type.Location = new System.Drawing.Point(25, 24);
-            this._type.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this._type.Location = new System.Drawing.Point(2, 2);
+            this._type.Margin = new System.Windows.Forms.Padding(2);
             this._type.Name = "_type";
-            this._type.Size = new System.Drawing.Size(150, 41);
+            this._type.Size = new System.Drawing.Size(135, 41);
             this._type.TabIndex = 0;
             this._type.Value = null;
             // 
             // _city
             // 
+            this._city.Dock = System.Windows.Forms.DockStyle.Fill;
             this._city.LabelText = "City";
-            this._city.Location = new System.Drawing.Point(25, 148);
-            this._city.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this._city.Location = new System.Drawing.Point(2, 92);
+            this._city.Margin = new System.Windows.Forms.Padding(2);
             this._city.Name = "_city";
-            this._city.Size = new System.Drawing.Size(102, 41);
-            this._city.TabIndex = 2;
+            this._city.Size = new System.Drawing.Size(135, 41);
+            this._city.TabIndex = 4;
             this._city.Value = null;
             // 
             // _street
             // 
+            this.tableLayoutPanel1.SetColumnSpan(this._street, 4);
+            this._street.Dock = System.Windows.Forms.DockStyle.Fill;
             this._street.LabelText = "Street";
-            this._street.Location = new System.Drawing.Point(25, 90);
-            this._street.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this._street.Location = new System.Drawing.Point(2, 47);
+            this._street.Margin = new System.Windows.Forms.Padding(2);
             this._street.Name = "_street";
-            this._street.Size = new System.Drawing.Size(613, 41);
-            this._street.TabIndex = 1;
+            this._street.Size = new System.Drawing.Size(553, 41);
+            this._street.TabIndex = 3;
             this._street.Value = null;
             // 
             // _province
             // 
             this._province.DataSource = null;
+            this._province.Dock = System.Windows.Forms.DockStyle.Fill;
             this._province.LabelText = "Province";
-            this._province.Location = new System.Drawing.Point(169, 144);
-            this._province.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this._province.Location = new System.Drawing.Point(141, 92);
+            this._province.Margin = new System.Windows.Forms.Padding(2);
             this._province.Name = "_province";
-            this._province.Size = new System.Drawing.Size(150, 41);
-            this._province.TabIndex = 3;
+            this._province.Size = new System.Drawing.Size(135, 41);
+            this._province.TabIndex = 5;
             this._province.Value = null;
             // 
             // _country
             // 
             this._country.DataSource = null;
+            this._country.Dock = System.Windows.Forms.DockStyle.Fill;
             this._country.LabelText = "Country";
-            this._country.Location = new System.Drawing.Point(351, 144);
-            this._country.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this._country.Location = new System.Drawing.Point(419, 92);
+            this._country.Margin = new System.Windows.Forms.Padding(2);
             this._country.Name = "_country";
-            this._country.Size = new System.Drawing.Size(150, 41);
-            this._country.TabIndex = 4;
+            this._country.Size = new System.Drawing.Size(136, 41);
+            this._country.TabIndex = 7;
             this._country.Value = null;
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.AutoSize = true;
+            this.tableLayoutPanel1.ColumnCount = 4;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.Controls.Add(this.flowLayoutPanel1, 0, 3);
+            this.tableLayoutPanel1.Controls.Add(this._type, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this._country, 3, 2);
+            this.tableLayoutPanel1.Controls.Add(this._street, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this._validUntil, 3, 0);
+            this.tableLayoutPanel1.Controls.Add(this._city, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this._validFrom, 2, 0);
+            this.tableLayoutPanel1.Controls.Add(this._postalCode, 2, 2);
+            this.tableLayoutPanel1.Controls.Add(this._province, 1, 2);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 4;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(557, 171);
+            this.tableLayoutPanel1.TabIndex = 10;
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.AutoSize = true;
+            this.flowLayoutPanel1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.tableLayoutPanel1.SetColumnSpan(this.flowLayoutPanel1, 4);
+            this.flowLayoutPanel1.Controls.Add(this._cancelButton);
+            this.flowLayoutPanel1.Controls.Add(this._acceptButton);
+            this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.flowLayoutPanel1.FlowDirection = System.Windows.Forms.FlowDirection.RightToLeft;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(3, 138);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(551, 30);
+            this.flowLayoutPanel1.TabIndex = 8;
             // 
             // AddressesEditorControl
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Controls.Add(this._country);
-            this.Controls.Add(this._province);
-            this.Controls.Add(this._postalCode);
-            this.Controls.Add(this._validFrom);
-            this.Controls.Add(this._validUntil);
-            this.Controls.Add(this._cancelButton);
-            this.Controls.Add(this._acceptButton);
-            this.Controls.Add(this._type);
-            this.Controls.Add(this._city);
-            this.Controls.Add(this._street);
+            this.Controls.Add(this.tableLayoutPanel1);
             this.Name = "AddressesEditorControl";
-            this.Size = new System.Drawing.Size(678, 273);
+            this.Size = new System.Drawing.Size(557, 171);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.flowLayoutPanel1.ResumeLayout(false);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -179,5 +229,7 @@ namespace ClearCanvas.Ris.Client.Admin.View.WinForms
         private ClearCanvas.Controls.WinForms.TextField _postalCode;
         private ClearCanvas.Controls.WinForms.ComboBoxField _province;
         private ClearCanvas.Controls.WinForms.ComboBoxField _country;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
     }
 }
