4b904521b091318b198918be0207b41bb0f9be35
clinton <devnull@localhost>
2006-08-16 15:24:03 +0000

    Requirement: R5 It's weird, but I can't seem to commit both the deletion and addition of the DataStore folders at the same time. This is the deletion of the folder in Services.

--- Dicom/Services/DataStore/Hibernate/CreateIndices.sql
+++
@@ -1,3 +0,0 @@
-create index SopInstanceUidIndex_ on SopInstance_ (SopInstanceUid_);
-create index SeriesInstanceUidIndex_ on Series_ (SeriesInstanceUid_);
-create index StudyInstanceUidIndex_ on Study_ (StudyInstanceUid_);
