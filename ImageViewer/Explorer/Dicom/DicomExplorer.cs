b6c3cd7409383b550f9e162ea62b5eef0e8e00bc
norman <devnull@localhost>
2006-08-31 07:18:39 +0000

    Requirement: R293 - Added some properties to StudyBrowserComponent.

--- ImageViewer/Explorer/Dicom/DicomExplorer.cs
+++ ImageViewer/Explorer/Dicom/DicomExplorer.cs
@@ -11,6 +11,8 @@ namespace ClearCanvas.ImageViewer.Explorer.Dicom
 	public class DicomExplorer : IHealthcareArtifactExplorer
 	{
 		private SplitComponentContainer _splitComponentContainer;
+		private AENavigatorComponent _aeNavigator;
+		private StudyBrowserComponent _studyBrowser;
 
 		public DicomExplorer()
 		{
@@ -30,10 +32,14 @@ namespace ClearCanvas.ImageViewer.Explorer.Dicom
 			{
 				if (_splitComponentContainer == null)
 				{
-					AENavigatorComponent aeNavigator = new AENavigatorComponent();
-					StudyBrowserComponent studyBrowser = new StudyBrowserComponent();
-					SplitPane leftPane = new SplitPane("AE Navigator", aeNavigator);
-					SplitPane rightPane = new SplitPane("Study Browser", studyBrowser);
+					if (_aeNavigator == null)
+						_aeNavigator = new AENavigatorComponent();
+
+					if (_studyBrowser == null)
+						_studyBrowser = new StudyBrowserComponent();
+
+					SplitPane leftPane = new SplitPane("AE Navigator", _aeNavigator);
+					SplitPane rightPane = new SplitPane("Study Browser", _studyBrowser);
 					_splitComponentContainer = new SplitComponentContainer(leftPane, rightPane);
 				}
 
--- ImageViewer/Explorer/Dicom/StudyBrowserComponent.cs
+++ ImageViewer/Explorer/Dicom/StudyBrowserComponent.cs
@@ -16,6 +16,65 @@ namespace ClearCanvas.ImageViewer.Explorer.Dicom
 	[AssociateView(typeof(StudyBrowserComponentViewExtensionPoint))]
 	public class StudyBrowserComponent : ApplicationComponent
 	{
+		private IStudyFinder _studyFinder;
+		private IStudyLoader _studyLoader;
 
+		private string _title;
+
+		private string _lastName;
+		private string _firstName;
+		private string _patientID;
+		private string _accessionNumber;
+
+		public IStudyFinder StudyFinder
+		{
+			get { return _studyFinder; }
+			set { _studyFinder = value; }
+		}
+
+		public IStudyLoader StudyLoader
+		{
+			get { return _studyLoader; }
+			set { _studyLoader = value; }
+		}
+		
+		public string Title
+		{
+			get { return _title; }
+			set { _title = value; }
+		}
+
+		public string AccessionNumber
+		{
+			get { return _accessionNumber; }
+			set { _accessionNumber = value; }
+		}
+
+
+		public string PatientID
+		{
+			get { return _patientID; }
+			set { _patientID = value; }
+		}
+
+		public string FirstName
+		{
+			get { return _firstName; }
+			set { _firstName = value; }
+		}
+
+		public string LastName
+		{
+			get { return _lastName; }
+			set { _lastName = value; }
+		}
+
+		public void Search()
+		{
+		}
+
+		public void Clear()
+		{
+		}
 	}
 }
