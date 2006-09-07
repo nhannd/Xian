7a1a908be9b1ff702c7e2122faa77b5451d99da5
stewart <devnull@localhost>
2006-09-07 12:08:28 +0000

    R383 - missed a file in my previous checkin for the text overlay.

---
+++ ImageViewer/DicomAnnotationConfiguration.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using ClearCanvas.Dicom;
+using ClearCanvas.ImageViewer.Imaging;
+
+namespace ClearCanvas.ImageViewer
+{
+	public class DicomAnnotationConfiguration : AnnotationConfiguration
+	{
+		private string _modality;
+		private static readonly string _identifierPrefix = "DICOM.";
+
+		public DicomAnnotationConfiguration(string modality, AnnotationLayout annotationLayout)
+			: base(_identifierPrefix + modality, modality, annotationLayout)
+		{
+			_modality = modality;
+		}
+
+		protected override bool MeetsCriteria(PresentationImage presentationImage)
+		{
+			DicomPresentationImage dicomImage = (DicomPresentationImage)presentationImage;
+			if (dicomImage == null)
+				return false;
+
+			try
+			{
+				if (dicomImage.ImageSop.Modality == _modality)
+					return true;
+			}
+			catch
+			{
+				try
+				{
+					string modality = String.Empty;
+					bool success = false;
+					dicomImage.ImageSop.GetTag(Dcm.Modality, out modality, out success);
+					if (modality == _modality)
+						return true;
+				}
+				catch
+				{
+				}
+			}
+
+			return false;
+		}
+	}
+}
