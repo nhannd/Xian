74e4ca21e67a1f3ff05f19c7a923fa895ae54291
jonathan <devnull@localhost>
2006-09-08 20:27:55 +0000

    Requirement: R421
    Changed TableView class so that it allows writable columns.

--- Desktop/View/WinForms/TableView.Designer.cs
+++ Desktop/View/WinForms/TableView.Designer.cs
@@ -28,14 +28,14 @@ namespace ClearCanvas.Desktop.View.WinForms
 		/// </summary>
 		private void InitializeComponent()
 		{
-            this._components = new System.ComponentModel.Container();
+            this.components = new System.ComponentModel.Container();
             System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
             System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
             this._dataGridView = new System.Windows.Forms.DataGridView();
+            this._contextMenu = new System.Windows.Forms.ContextMenuStrip(this.components);
             this._toolStrip = new System.Windows.Forms.ToolStrip();
             this._tableLayoutPanel = new System.Windows.Forms.TableLayoutPanel();
-            this._bindingSource = new System.Windows.Forms.BindingSource(this._components);
-            this._contextMenu = new System.Windows.Forms.ContextMenuStrip(this._components);
+            this._bindingSource = new System.Windows.Forms.BindingSource(this.components);
             ((System.ComponentModel.ISupportInitialize)(this._dataGridView)).BeginInit();
             this._tableLayoutPanel.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this._bindingSource)).BeginInit();
@@ -69,13 +69,21 @@ namespace ClearCanvas.Desktop.View.WinForms
             this._dataGridView.Location = new System.Drawing.Point(0, 25);
             this._dataGridView.Margin = new System.Windows.Forms.Padding(0);
             this._dataGridView.Name = "_dataGridView";
-            this._dataGridView.ReadOnly = true;
             this._dataGridView.RowHeadersVisible = false;
             this._dataGridView.RowTemplate.Height = 24;
             this._dataGridView.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
             this._dataGridView.Size = new System.Drawing.Size(720, 256);
             this._dataGridView.TabIndex = 0;
             this._dataGridView.CellDoubleClick += new System.Windows.Forms.DataGridViewCellEventHandler(this._dataGridView_CellDoubleClick);
+            // 
+            // _contextMenu
+            // 
+            this._contextMenu.Name = "_contextMenu";
+            this._contextMenu.Size = new System.Drawing.Size(61, 4);
+            this._contextMenu.Opened += new System.EventHandler(this._contextMenu_Opened);
+            this._contextMenu.Closed += new System.Windows.Forms.ToolStripDropDownClosedEventHandler(this._contextMenu_Closed);
+            this._contextMenu.Opening += new System.ComponentModel.CancelEventHandler(this._contextMenu_Opening);
+            this._contextMenu.Closing += new System.Windows.Forms.ToolStripDropDownClosingEventHandler(this._contextMenu_Closing);
             // 
             // _toolStrip
             // 
@@ -96,7 +104,7 @@ namespace ClearCanvas.Desktop.View.WinForms
             this._tableLayoutPanel.Controls.Add(this._dataGridView, 0, 1);
             this._tableLayoutPanel.Dock = System.Windows.Forms.DockStyle.Fill;
             this._tableLayoutPanel.Location = new System.Drawing.Point(0, 0);
-            this._tableLayoutPanel.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this._tableLayoutPanel.Margin = new System.Windows.Forms.Padding(4);
             this._tableLayoutPanel.Name = "_tableLayoutPanel";
             this._tableLayoutPanel.RowCount = 2;
             this._tableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle());
@@ -104,21 +112,12 @@ namespace ClearCanvas.Desktop.View.WinForms
             this._tableLayoutPanel.Size = new System.Drawing.Size(720, 281);
             this._tableLayoutPanel.TabIndex = 2;
             // 
-            // _contextMenu
-            // 
-            this._contextMenu.Name = "_contextMenu";
-            this._contextMenu.Size = new System.Drawing.Size(153, 26);
-            this._contextMenu.Opened += new System.EventHandler(this._contextMenu_Opened);
-            this._contextMenu.Closed += new System.Windows.Forms.ToolStripDropDownClosedEventHandler(this._contextMenu_Closed);
-            this._contextMenu.Opening += new System.ComponentModel.CancelEventHandler(this._contextMenu_Opening);
-            this._contextMenu.Closing += new System.Windows.Forms.ToolStripDropDownClosingEventHandler(this._contextMenu_Closing);
-            // 
             // TableView
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.Controls.Add(this._tableLayoutPanel);
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "TableView";
             this.Size = new System.Drawing.Size(720, 281);
             ((System.ComponentModel.ISupportInitialize)(this._dataGridView)).EndInit();
@@ -136,5 +135,6 @@ namespace ClearCanvas.Desktop.View.WinForms
 		private System.Windows.Forms.TableLayoutPanel _tableLayoutPanel;
         private System.Windows.Forms.BindingSource _bindingSource;
         private System.Windows.Forms.ContextMenuStrip _contextMenu;
+        private System.ComponentModel.IContainer components;
 	}
 }
