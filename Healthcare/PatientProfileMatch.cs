5401b16a0b7f4096d92c17a4b6250198dbda693f
rob <devnull@localhost>
2006-09-08 04:21:13 +0000

    Requirement: R421  Missed this file in previous commit

--- Healthcare/PatientProfileMatch.cs
+++ Healthcare/PatientProfileMatch.cs
@@ -6,14 +6,14 @@ namespace ClearCanvas.Healthcare
 {
     public class PatientProfileMatch
     {
-        public PatientProfileMatch(PatientProfile patientProfile, int score)
+        public PatientProfileMatch(PatientProfile patientProfile, ScoreValue score)
         {
             _patientProfile = patientProfile;
             _score = score;
         }
 
         private PatientProfile _patientProfile;
-        private int _score;
+        private ScoreValue _score;
 
         public PatientProfile PatientProfile
         {
@@ -21,10 +21,61 @@ namespace ClearCanvas.Healthcare
             set { _patientProfile = value; }
         }
 	
-        public int Score
+        public ScoreValue Score
         {
             get { return _score; }
             set { _score = value; }
+        }
+
+        public enum ScoreValue
+        {
+            High = 2,
+            Moderate = 1
+        }
+
+        public static IList<PatientProfileMatch> CreateList(PatientProfile self, IList<PatientProfile> profileList, ScoreValue score)
+        {
+            IList<PatientProfileMatch> matchList = new List<PatientProfileMatch>();
+            foreach (PatientProfile profile in profileList)
+            {
+                if (!profile.Equals(self))
+                {
+                    matchList.Add(new PatientProfileMatch(profile, score));
+                }
+            }
+            return matchList;
+        }
+
+        public static IList<PatientProfileMatch> Join(IList<PatientProfileMatch> leftSet, IList<PatientProfileMatch> rightSet)
+        {
+            IList<PatientProfileMatch> result = new List<PatientProfileMatch>();
+
+            foreach (PatientProfileMatch left in leftSet)
+            {
+                result.Add(left);
+            }
+            foreach (PatientProfileMatch right in rightSet)
+            {
+                bool found = false;
+                foreach (PatientProfileMatch alreadyAdded in result)
+                {
+                    if (right.PatientProfile.Equals(alreadyAdded.PatientProfile))
+                    {
+                        found = true;
+                        if (right.Score > alreadyAdded.Score)
+                        {
+                            alreadyAdded.Score = right.Score;
+                        }
+                        break;
+                    }
+                }
+                if (found == false)
+                {
+                    result.Add(right);
+                }
+            }
+
+            return result;
         }
     }
 }
