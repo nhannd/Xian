140f5600174c3112d7ac277027f596d8a1afe741
norman <devnull@localhost>
2006-09-09 03:55:08 +0000

    Ticket #113: Fixed.  Study list now resizes properly.

--- ImageViewer/Explorer/Dicom/View/WinForms/StudyBrowserControl.Designer.cs
+++ ImageViewer/Explorer/Dicom/View/WinForms/StudyBrowserControl.Designer.cs
@@ -28,66 +28,55 @@ namespace ClearCanvas.ImageViewer.Explorer.Dicom.View.WinForms
 		/// </summary>
 		private void InitializeComponent()
 		{
-            this._titleBar = new Crownwood.DotNetMagic.Controls.TitleBar();
-            this._studySearchForm = new ClearCanvas.ImageViewer.Explorer.Dicom.View.WinForms.StudySearchForm();
-            this._studyTableView = new ClearCanvas.Desktop.View.WinForms.TableView();
-            this.titleBar1 = new Crownwood.DotNetMagic.Controls.TitleBar();
-            this.SuspendLayout();
-            // 
-            // _titleBar
-            // 
-            this._titleBar.Dock = System.Windows.Forms.DockStyle.Top;
-            this._titleBar.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this._titleBar.Location = new System.Drawing.Point(0, 0);
-            this._titleBar.MouseOverColor = System.Drawing.Color.Empty;
-            this._titleBar.Name = "_titleBar";
-            this._titleBar.Size = new System.Drawing.Size(661, 26);
-            this._titleBar.TabIndex = 1;
-            this._titleBar.Text = "Search";
-            // 
-            // _studySearchForm
-            // 
-            this._studySearchForm.Dock = System.Windows.Forms.DockStyle.Top;
-            this._studySearchForm.Location = new System.Drawing.Point(0, 26);
-            this._studySearchForm.MinimumSize = new System.Drawing.Size(494, 159);
-            this._studySearchForm.Name = "_studySearchForm";
-            this._studySearchForm.Size = new System.Drawing.Size(661, 159);
-            this._studySearchForm.TabIndex = 2;
-            // 
-            // _studyTableView
-            // 
-            this._studyTableView.DataSource = null;
-            this._studyTableView.Dock = System.Windows.Forms.DockStyle.Top;
-            this._studyTableView.Location = new System.Drawing.Point(0, 185);
-            this._studyTableView.MenuModel = null;
-            this._studyTableView.Name = "_studyTableView";
-            this._studyTableView.Size = new System.Drawing.Size(661, 413);
-            this._studyTableView.TabIndex = 0;
-            this._studyTableView.ToolbarModel = null;
-            this._studyTableView.ToolStripRightToLeft = System.Windows.Forms.RightToLeft.No;
-            // 
-            // titleBar1
-            // 
-            this.titleBar1.Location = new System.Drawing.Point(425, 16);
-            this.titleBar1.MouseOverColor = System.Drawing.Color.Empty;
-            this.titleBar1.Name = "titleBar1";
-            this.titleBar1.Size = new System.Drawing.Size(75, 23);
-            this.titleBar1.TabIndex = 3;
-            this.titleBar1.Text = "titleBar1";
-            // 
-            // StudyBrowserControl
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.Controls.Add(this.titleBar1);
-            this.Controls.Add(this._studyTableView);
-            this.Controls.Add(this._studySearchForm);
-            this.Controls.Add(this._titleBar);
-            this.Margin = new System.Windows.Forms.Padding(2);
-            this.Name = "StudyBrowserControl";
-            this.Size = new System.Drawing.Size(661, 601);
-            this.ResumeLayout(false);
+			this._titleBar = new Crownwood.DotNetMagic.Controls.TitleBar();
+			this._studyTableView = new ClearCanvas.Desktop.View.WinForms.TableView();
+			this._studySearchForm = new ClearCanvas.ImageViewer.Explorer.Dicom.View.WinForms.StudySearchForm();
+			this.SuspendLayout();
+			// 
+			// _titleBar
+			// 
+			this._titleBar.Dock = System.Windows.Forms.DockStyle.Top;
+			this._titleBar.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this._titleBar.Location = new System.Drawing.Point(0, 0);
+			this._titleBar.MouseOverColor = System.Drawing.Color.Empty;
+			this._titleBar.Name = "_titleBar";
+			this._titleBar.Size = new System.Drawing.Size(661, 23);
+			this._titleBar.TabIndex = 1;
+			this._titleBar.Text = "Search";
+			// 
+			// _studyTableView
+			// 
+			this._studyTableView.DataSource = null;
+			this._studyTableView.Dock = System.Windows.Forms.DockStyle.Fill;
+			this._studyTableView.Location = new System.Drawing.Point(0, 182);
+			this._studyTableView.MenuModel = null;
+			this._studyTableView.Name = "_studyTableView";
+			this._studyTableView.Size = new System.Drawing.Size(661, 419);
+			this._studyTableView.TabIndex = 0;
+			this._studyTableView.ToolbarModel = null;
+			this._studyTableView.ToolStripRightToLeft = System.Windows.Forms.RightToLeft.No;
+			// 
+			// _studySearchForm
+			// 
+			this._studySearchForm.Dock = System.Windows.Forms.DockStyle.Top;
+			this._studySearchForm.Location = new System.Drawing.Point(0, 23);
+			this._studySearchForm.MinimumSize = new System.Drawing.Size(494, 159);
+			this._studySearchForm.Name = "_studySearchForm";
+			this._studySearchForm.Size = new System.Drawing.Size(661, 159);
+			this._studySearchForm.TabIndex = 2;
+			// 
+			// StudyBrowserControl
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.Controls.Add(this._studyTableView);
+			this.Controls.Add(this._studySearchForm);
+			this.Controls.Add(this._titleBar);
+			this.Margin = new System.Windows.Forms.Padding(2);
+			this.Name = "StudyBrowserControl";
+			this.Size = new System.Drawing.Size(661, 601);
+			this.ResumeLayout(false);
 
 		}
 
@@ -96,7 +85,6 @@ namespace ClearCanvas.ImageViewer.Explorer.Dicom.View.WinForms
 		private ClearCanvas.Desktop.View.WinForms.TableView _studyTableView;
 		private Crownwood.DotNetMagic.Controls.TitleBar _titleBar;
 		private StudySearchForm _studySearchForm;
-        private Crownwood.DotNetMagic.Controls.TitleBar titleBar1;
 
 
 	}
