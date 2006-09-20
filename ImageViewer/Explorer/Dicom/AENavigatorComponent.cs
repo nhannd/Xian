3f26c343e1a3e83e461a1dbf4c707ae45284e551
guo <devnull@localhost>
2006-09-20 14:06:42 +0000

    Requirement: R289 (Design a UI to manage the remote Aes) - Revise codes in AENavigatorComponent and AENavigatorControl

--- ImageViewer/Explorer/Dicom/AENavigatorComponent.cs
+++ ImageViewer/Explorer/Dicom/AENavigatorComponent.cs
@@ -166,17 +166,6 @@ namespace ClearCanvas.ImageViewer.Explorer.Dicom
             return NewServerName+tstamp;
         }
 
-        public void DataStoreEvent()
-        {
-            //UpdateServerSetting();
-            _serverPool.Currentserver = null;
-            _serverPool.Currentserverid = -1;
-            _serverSelected = null;
-            LoadServerSetting();
-            EventsHelper.Fire(_dataStoreSelected, this, EventArgs.Empty);
-            EventsHelper.Fire(_selectedServerChanged, this, EventArgs.Empty);
-        }
-
         public bool DeleteServer()
         {
             if (_serverPool.Currentserver == null || _serverPool.Currentserverid < 0 || _serverPool.Currentserverid >= _serverPool.Serverlist.Count)
@@ -260,48 +249,7 @@ namespace ClearCanvas.ImageViewer.Explorer.Dicom
 
         public void ServerSettingError(ServerSettingItem sitem, int errorvalue)
         {
-            if(errorvalue == -1)
-            {
-                StringBuilder msgText = new StringBuilder();
-                msgText.AppendFormat("The value of {0} should not be blank. \r\n\r\nPlease try again.", sitem.ToString());
-                MessageBox.Show(msgText.ToString(), "Blank Value Error");
-            }
-            else if (errorvalue == -2)
-            {
-                StringBuilder msgText = new StringBuilder();
-                msgText.AppendFormat("The Port value should be a integer. \r\n\r\nPlease input an integer data.");
-                MessageBox.Show(msgText.ToString(), "Port Value Error");
-            }
-            else if (errorvalue <= -1000000)
-            {
-                int i = (-1 * errorvalue) - 1000000;
-                if (i < 0 || i >= _serverPool.Serverlist.Count || i == _serverPool.Currentserverid)
-                    return;
-                StringBuilder msgText = new StringBuilder();
-                switch (sitem)
-                {
-                    case ServerSettingItem.ServerName:
-                        msgText.AppendFormat("The Saver Name ({0}) exists under {1}\r\n\r\nServer Name: {2}\r\nAE Title: {3}\r\nHost: {4}\r\n\r\nPlease choose another server name.",
-                            _serverPool.Serverlist[i].Servername, _serverPool.Serverlist[i].Serverpath, _serverPool.Serverlist[i].Servername,
-                            _serverPool.Serverlist[i].AE, _serverPool.Serverlist[i].Host);
-                        MessageBox.Show(msgText.ToString(), "Server Name Error");
-                        break;
-                    case ServerSettingItem.AE:
-                        msgText.AppendFormat("The AE Title ({0}) exists under {1}\r\n\r\nServer Name: {2}\r\nAE Title: {3}\r\nHost: {4}\r\n\r\nPlease choose another server name.",
-                            _serverPool.Serverlist[i].AE, _serverPool.Serverlist[i].Serverpath, _serverPool.Serverlist[i].Servername,
-                            _serverPool.Serverlist[i].AE, _serverPool.Serverlist[i].Host);
-                        MessageBox.Show(msgText.ToString(), "AE Title Error");
-                        break;
-                    case ServerSettingItem.Host:
-                        msgText.AppendFormat("The Saver Host ({0}) exists under {1}\r\n\r\nServer Name: {2}\r\nAE Title: {3}\r\nHost: {4}\r\n\r\nPlease choose another server name.",
-                            _serverPool.Serverlist[i].Host, _serverPool.Serverlist[i].Serverpath, _serverPool.Serverlist[i].Servername,
-                            _serverPool.Serverlist[i].AE, _serverPool.Serverlist[i].Host);
-                        MessageBox.Show(msgText.ToString(), "Server Host Error");
-                        break;
-                    default:
-                        break;
-                }
-            }
+            // to do
         }
 
         public bool ServerDeleteConfirm()
@@ -314,91 +262,8 @@ namespace ClearCanvas.ImageViewer.Explorer.Dicom
 
         public int IsServerSettingValid(ServerSettingItem sitem, string svalue, bool chkchangeonly)
         {
-            if (_serverPool.Currentserver == null)
-                return 1;
-
-            if (svalue == null || svalue.Equals(""))
-            {
-                switch (sitem)
-                {
-                    case ServerSettingItem.ServerName:
-                        this.ServerName = _serverPool.Currentserver.Servername;
-                        break;
-                    case ServerSettingItem.AE:
-                        this.ServerAE = _serverPool.Currentserver.AE;
-                        break;
-                    case ServerSettingItem.Host:
-                        this.ServerHost = _serverPool.Currentserver.Host;
-                        break;
-                    case ServerSettingItem.Port:
-                        this.ServerPort = _serverPool.Currentserver.Port.ToString();
-                        break;
-                    default:
-                        throw new System.Exception("Unexpected condition in figuring out which server setting item was updated");
-                        //return false;
-                }
-                return -1;
-            }
-
-            switch (sitem)
-            {
-                case ServerSettingItem.ServerName:
-                    if (chkchangeonly && this.ServerName.Equals(_serverPool.Currentserver.Servername))
-                        return 2;
-                    for (int i = 0; i < _serverPool.Serverlist.Count; i++)
-                    {
-                        if (i == _serverPool.Currentserverid || _serverPool.Serverlist[i].Servername.Equals(EmptyNodeName))
-                            continue;
-                        if (this.ServerName.Equals(_serverPool.Serverlist[i].Servername))
-                        {
-                            this.ServerName = _serverPool.Currentserver.Servername;
-                            return -1000000 - i;
-                        }
-                    }
-                    return 3;
-                case ServerSettingItem.AE:
-                    if (chkchangeonly && this.ServerAE.Equals(_serverPool.Currentserver.AE))
-                        return 4;
-                    for (int i = 0; i < _serverPool.Serverlist.Count; i++)
-                    {
-                        if (i == _serverPool.Currentserverid || _serverPool.Serverlist[i].Servername.Equals(EmptyNodeName))
-                            continue;
-                        if (this.ServerAE.Equals(_serverPool.Serverlist[i].AE))
-                        {
-                            this.ServerAE = _serverPool.Currentserver.AE;
-                            return -1000000 - i;
-                        }
-                    }
-                    return 5;
-                case ServerSettingItem.Host:
-                    if (chkchangeonly && this.ServerHost.Equals(_serverPool.Currentserver.Host))
-                        return 6;
-                    for (int i = 0; i < _serverPool.Serverlist.Count; i++)
-                    {
-                        if (i == _serverPool.Currentserverid || _serverPool.Serverlist[i].Servername.Equals(EmptyNodeName))
-                            continue;
-                        if (this.ServerHost.Equals(_serverPool.Serverlist[i].Host))
-                        {
-                            this.ServerHost = _serverPool.Currentserver.Host;
-                            return -1000000 - i;
-                        }
-                    }
-                    return 7;
-                case ServerSettingItem.Port:
-                    try
-                    {
-                        int.Parse(this.ServerPort);
-                    }
-                    catch
-                    {
-                        this.ServerPort = _serverPool.Currentserver.Port.ToString();
-                        return -2;
-                    }
-                    return 8;
-                default: 
-                    //throw new System.Exception("Unexpected condition in figuring out which server setting item was updated");
-                    return -3;
-            }
+            // to do
+            return 0;
         }
 
         #region IApplicationComponent overrides
--- ImageViewer/Explorer/Dicom/View/WinForms/AENavigatorControl.cs
+++ ImageViewer/Explorer/Dicom/View/WinForms/AENavigatorControl.cs
@@ -14,7 +14,6 @@ namespace ClearCanvas.ImageViewer.Explorer.Dicom.View.WinForms
 	{
         private AENavigatorComponent _aenavigatorComponent;
         private TreeNode _lastClickedNode;
-        private BindingSource _bindingSource;
 
         public event EventHandler AddClicked
         {
@@ -34,193 +33,41 @@ namespace ClearCanvas.ImageViewer.Explorer.Dicom.View.WinForms
             InitializeComponent();
 
             _aenavigatorComponent = component;
-            //AddClicked += new EventHandler(OnAddClicked);
-            //DeleteClicked += new EventHandler(OnDeleteClicked);
-
-            _bindingSource = new BindingSource();
-            _bindingSource.DataSource = _aenavigatorComponent;
+            AddClicked += new EventHandler(OnAddClicked);
+            DeleteClicked += new EventHandler(OnDeleteClicked);
 
             _aenavigatorComponent.SelectedServerChanged += new EventHandler(ServerSettingChanged);
-            //_aeserverTreeForm1.LostFocus += new EventHandler(_aeserverTreeForm1_LostFocus);
-            //_aeserverTreeForm1.ServerName.LostFocus += new EventHandler(ServerName_LostFocus);
-            //_aeserverTreeForm1.ServerAE.LostFocus += new EventHandler(ServerAE_LostFocus);
-            //_aeserverTreeForm1.ServerHost.LostFocus += new EventHandler(ServerHost_LostFocus);
-            //_aeserverTreeForm1.ServerPort.LostFocus += new EventHandler(ServerPort_LostFocus);
 
-            _aeserverTreeForm1.ServerName.DataBindings.Add("Text", _bindingSource, "ServerName", true, DataSourceUpdateMode.OnPropertyChanged);
-            _aeserverTreeForm1.ServerDesc.DataBindings.Add("Text", _bindingSource, "ServerDesc", true, DataSourceUpdateMode.OnPropertyChanged);
-            _aeserverTreeForm1.ServerAE.DataBindings.Add("Text", _bindingSource, "ServerAE", true, DataSourceUpdateMode.OnPropertyChanged);
-            _aeserverTreeForm1.ServerHost.DataBindings.Add("Text", _bindingSource, "ServerHost", true, DataSourceUpdateMode.OnPropertyChanged);
-            _aeserverTreeForm1.ServerPort.DataBindings.Add("Text", _bindingSource, "ServerPort", true, DataSourceUpdateMode.OnPropertyChanged);
-
-            //_aeserverTreeForm1.AeserverTree.BeforeExpand += new TreeViewCancelEventHandler(TreeView_BeforeExpand);
-            _aeserverTreeForm1.AeserverTree.Click += new EventHandler(AeserverTree_Click);
-            //_aeserverTreeForm1.AeserverTree.BeforeSelect += new TreeViewCancelEventHandler(AeserverTree_BeforeSelect);
-            BuildServerTreeView(_aeserverTreeForm1.AeserverTree, _aenavigatorComponent.ServerTreeView);
+            _aeTreeView.Click += new EventHandler(AeserverTree_Click);
+            BuildServerTreeView(_aeTreeView, _aenavigatorComponent.ServerTreeView);
         }
 
         void ServerSettingChanged(object sender, EventArgs e)
         {
-//            _bindingSource.ResetBindings(false);
-        }
-
-        void _aeserverTreeForm1_LostFocus(object sender, EventArgs e)
-        {
-            //to do
-        }
-
-        void ServerName_LostFocus(object sender, EventArgs e)
-        {
-            int chknum = _aenavigatorComponent.IsServerSettingValid(ServerSettingItem.ServerName, _aeserverTreeForm1.ServerName.Text, true);
-            if (chknum < 0)
-            {
-                _bindingSource.ResetBindings(false);
-                _aenavigatorComponent.ServerSettingError(ServerSettingItem.ServerName, chknum);
-            }
-            else
-            {
-                UpdateServerNodeName();
-            }
-        }
-
-        void ServerAE_LostFocus(object sender, EventArgs e)
-        {
-            int chknum = _aenavigatorComponent.IsServerSettingValid(ServerSettingItem.AE, _aeserverTreeForm1.ServerAE.Text, true);
-            if (chknum < 0)
-            {
-                _bindingSource.ResetBindings(false);
-                _aenavigatorComponent.ServerSettingError(ServerSettingItem.AE, chknum);
-            }
-        }
-
-        void ServerHost_LostFocus(object sender, EventArgs e)
-        {
-            int chknum = _aenavigatorComponent.IsServerSettingValid(ServerSettingItem.Host, _aeserverTreeForm1.ServerHost.Text, true);
-            if (chknum < 0)
-            {
-                _bindingSource.ResetBindings(false);
-                _aenavigatorComponent.ServerSettingError(ServerSettingItem.Host, chknum);
-            }
-        }
-
-        void ServerPort_LostFocus(object sender, EventArgs e)
-        {
-            int chknum = _aenavigatorComponent.IsServerSettingValid(ServerSettingItem.Port, _aeserverTreeForm1.ServerPort.Text, true);
-            if (chknum < 0)
-            {
-                _bindingSource.ResetBindings(false);
-                _aenavigatorComponent.ServerSettingError(ServerSettingItem.Port, chknum);
-            }
+            // to do
         }
 
         void OnAddClicked(object sender, EventArgs e)
         {
-            string svrname = _aenavigatorComponent.AddServer();
-            if (svrname == null || svrname.Equals(""))
-                return;
-            TreeNode newNode = new TreeNode(svrname);
-            _aeserverTreeForm1.AeserverTree.Nodes[1].Nodes.Add(newNode);
-            _aeserverTreeForm1.AeserverTree.SelectedNode = newNode;
-
-            _lastClickedNode = newNode;
-            //UpdateServerNodeName();
-
+            // to do
         }
 
         void OnDeleteClicked(object sender, EventArgs e)
         {
-            using (new CursorManager(this, Cursors.WaitCursor))
-            {
-                if (_lastClickedNode == null || _lastClickedNode.Text.Equals(AENavigatorComponent.MyDatastoreTitle)
-                    || _lastClickedNode.Text.Equals(AENavigatorComponent.MyServersTitle))
-                    return;
-                if (_aenavigatorComponent.ServerDeleteConfirm())
-                {
-                    if (_aenavigatorComponent.DeleteServer() && _lastClickedNode != null)
-                    {
-                        _aeserverTreeForm1.AeserverTree.SelectedNode = _lastClickedNode.Parent;
-                        _lastClickedNode.Remove();
-                    }
-                }
-            }
-        }
-
-        void AeserverTree_BeforeSelect(object sender, TreeViewCancelEventArgs e)
-        {
-            if (!CheckServerSettingData())
-                e.Cancel = true; ;
+            // to do
         }
 
         void AeserverTree_Click(object sender, EventArgs e)
         {
             //if (!CheckServerSettingData())
             //{ return; }
-            _lastClickedNode = _aeserverTreeForm1.AeserverTree.GetNodeAt(((MouseEventArgs)e).X, ((MouseEventArgs)e).Y);
+            _lastClickedNode = _aeTreeView.GetNodeAt(((MouseEventArgs)e).X, ((MouseEventArgs)e).Y);
             if (_lastClickedNode == null)
                 return;
             //if (_lastClickedNode.Text.Equals(AENavigatorComponent.MyDatastoreTitle))
             //    _aenavigatorComponent.DataStoreEvent();
             //else
                 _aenavigatorComponent.SelectChanged(_lastClickedNode.Text);
-        }
-
-        private void TreeView_BeforeExpand(object sender, TreeViewCancelEventArgs e)
-        {
-            TreeNode expandingNode = e.Node;
-            foreach (TreeNode subNode in expandingNode.Nodes)
-            {
-                // only build the next level if it hasn't been built previously
-                if (subNode.Nodes.Count == 0)
-                {
-                    BuildNextTreeLevel(subNode);
-                }
-            }
-        }
-
-        public bool CheckServerSettingData()
-        {
-            int chknum = _aenavigatorComponent.IsServerSettingValid(ServerSettingItem.ServerName, _aeserverTreeForm1.ServerName.Text, false);
-            if (chknum < 0)
-            {
-                _bindingSource.ResetBindings(false);
-                if (_lastClickedNode != null && _aeserverTreeForm1.AeserverTree.SelectedNode != null
-                    && _aeserverTreeForm1.AeserverTree.SelectedNode.Text.Equals(_lastClickedNode.Text))
-                    _aeserverTreeForm1.AeserverTree.SelectedNode = _lastClickedNode;
-                _aenavigatorComponent.ServerSettingError(ServerSettingItem.ServerName, chknum);
-                return false;
-            }
-            chknum = _aenavigatorComponent.IsServerSettingValid(ServerSettingItem.AE, _aeserverTreeForm1.ServerAE.Text, false);
-            if (chknum < 0)
-            {
-                _bindingSource.ResetBindings(false);
-                if (_lastClickedNode != null && _aeserverTreeForm1.AeserverTree.SelectedNode != null
-                    && _aeserverTreeForm1.AeserverTree.SelectedNode.Text.Equals(_lastClickedNode.Text))
-                    _aeserverTreeForm1.AeserverTree.SelectedNode = _lastClickedNode;
-                _aenavigatorComponent.ServerSettingError(ServerSettingItem.AE, chknum);
-                return false;
-            }
-            chknum = _aenavigatorComponent.IsServerSettingValid(ServerSettingItem.Host, _aeserverTreeForm1.ServerHost.Text, false);
-            if (chknum < 0)
-            {
-                _bindingSource.ResetBindings(false);
-                if (_lastClickedNode != null && _aeserverTreeForm1.AeserverTree.SelectedNode != null
-                    && _aeserverTreeForm1.AeserverTree.SelectedNode.Text.Equals(_lastClickedNode.Text))
-                    _aeserverTreeForm1.AeserverTree.SelectedNode = _lastClickedNode;
-                _aenavigatorComponent.ServerSettingError(ServerSettingItem.Host, chknum);
-                return false;
-            }
-            chknum = _aenavigatorComponent.IsServerSettingValid(ServerSettingItem.Port, _aeserverTreeForm1.ServerPort.Text, false);
-            if (chknum < 0)
-            {
-                _bindingSource.ResetBindings(false);
-                if (_lastClickedNode != null && _aeserverTreeForm1.AeserverTree.SelectedNode != null
-                    && _aeserverTreeForm1.AeserverTree.SelectedNode.Text.Equals(_lastClickedNode.Text))
-                    _aeserverTreeForm1.AeserverTree.SelectedNode = _lastClickedNode;
-                _aenavigatorComponent.ServerSettingError(ServerSettingItem.Port, chknum);
-                return false;
-            }
-            return true;
-
         }
 
         /// <summary>
@@ -256,16 +103,6 @@ namespace ClearCanvas.ImageViewer.Explorer.Dicom.View.WinForms
                 treeChild.Tag = dataChild;
                 treeChild.ToolTipText = dataChild.Details;
                 treeNode.Nodes.Add(treeChild);
-            }
-        }
-
-        private void UpdateServerNodeName()
-        {
-            if (_lastClickedNode != null && !_lastClickedNode.Text.Equals(AENavigatorComponent.MyDatastoreTitle)
-                && !_lastClickedNode.Text.Equals(AENavigatorComponent.MyServersTitle)
-                && !_aeserverTreeForm1.ServerName.Text.Equals("") && !_lastClickedNode.Text.Equals(_aeserverTreeForm1.ServerName.Text))
-            {
-                _lastClickedNode.Text = _aeserverTreeForm1.ServerName.Text;
             }
         }
 
