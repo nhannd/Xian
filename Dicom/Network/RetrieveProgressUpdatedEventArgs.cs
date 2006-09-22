9d3862c3e65e525e4763e8d85ad93dfb6496b095
clinton <devnull@localhost>
2006-09-22 20:37:58 +0000

    Requirement: R179 Forgot to add this file in the previous commit.

---
+++ Dicom/Network/RetrieveProgressUpdatedEventArgs.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClearCanvas.Dicom.Network
+{
+    public class RetrieveProgressUpdatedEventArgs : EventArgs
+    {
+
+        public RetrieveProgressUpdatedEventArgs(int completedSuboperations, int failedSuboperations, int remainingSuboperations)
+        {
+            _completedSuboperations = completedSuboperations;
+            _failedSuboperations = failedSuboperations;
+            _remainingSuboperations = remainingSuboperations;
+        }
+
+        private int _completedSuboperations;
+        private int _failedSuboperations;
+        private int _remainingSuboperations;
+
+        public int RemainingSuboperations
+        {
+            get { return _remainingSuboperations; }
+            set { _remainingSuboperations = value; }
+        }
+	
+        public int FailedSuboperations
+        {
+            get { return _failedSuboperations; }
+            set { _failedSuboperations = value; }
+        }
+	
+        public int CompletedSuboperations
+        {
+            get { return _completedSuboperations; }
+            set { _completedSuboperations = value; }
+        }
+	
+    }
+}
