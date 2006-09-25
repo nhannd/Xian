b4a2961756b73d58268cfaaddffb3920f079f6af
jonathan <devnull@localhost>
2006-09-25 21:53:19 +0000

    Requirement: R787
    Added ClearCanvas.Jscript plugin.  This plugin implements ClearCanvas.Common.ScriptEngineExtensionPoint for the JScript.NET language.  The plugin is itself written in JScript.NET, and therefore it cannot be built with VisualStudio but must be built with msbuild using the included Build.proj file.

---
+++ Jscript/ClearCanvas.Jscript.js
@@ -0,0 +1,40 @@
+import System;
+import System.Collections;
+
+import ClearCanvas.Common;
+import ClearCanvas.Common.Scripting;
+
+[assembly: ClearCanvas.Common.Plugin()]
+
+package ClearCanvas.Jscript
+{
+    /// This class provides an implementation of ClearCanvas.Common.Scripting.ScriptEngineExtensionPoint
+    /// for JScript
+    ///
+    /// The context IDictionary is accessible to the script as "this.Context".  For example, if
+    /// the dictionary contains "Name" => "JoJo", then from the script,
+    ///
+    ///     this.Context['Name'] => "JoJo"
+    ///     or
+    ///     this.Context.Name => "JoJo"     (the entries are directly accessible as properties of the Context object)
+    /// 
+    public 
+    ClearCanvas.Common.ExtensionOf(ScriptEngineExtensionPoint)
+    ClearCanvas.Common.Scripting.LanguageSupport("jscript")
+    expando class Engine implements IScriptEngine
+    {
+        var Context: Object;
+    
+	    function Run(script: String, context: IDictionary)
+	    {
+	        this.Context = new Object();
+	        
+	        for(var entry in context)
+	        {
+	            this.Context[entry.Key] = entry.Value;
+	        }
+	        
+		    return eval(script);
+	    }
+    }
+}
