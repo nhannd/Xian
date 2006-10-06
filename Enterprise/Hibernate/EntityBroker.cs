a0c85a98e1344fe1ff67efdc7cfca952723c9f84
jonathan <devnull@localhost>
2006-10-06 19:43:50 +0000

    Requirement: R787
    Forgot to commit this - just deleting some unused files from the project.

--- Enterprise/Hibernate/EntityBroker.cs
+++ Enterprise/Hibernate/EntityBroker.cs
@@ -6,6 +6,7 @@ using System.Text;
 using NHibernate;
 using NHibernate.Expression;
 using ClearCanvas.Enterprise.Hibernate.Hql;
+using ClearCanvas.Common.Utilities;
 
 namespace ClearCanvas.Enterprise.Hibernate
 {
