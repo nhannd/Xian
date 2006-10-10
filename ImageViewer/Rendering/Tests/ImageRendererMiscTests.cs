a937a0af65eb2d33fa797f675be22d6bb6972755
norman <devnull@localhost>
2006-10-10 21:17:03 +0000

    Requirement: R593 - Forgot to add unit tests

---
+++ ImageViewer/Rendering/Tests/ImageRendererMiscTests.cs
@@ -0,0 +1,106 @@
+#if	UNIT_TESTS
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Drawing;
+using System.Drawing.Imaging;
+using NUnit.Framework;
+using ClearCanvas.Common;
+using ClearCanvas.ImageViewer.Imaging;
+using ClearCanvas.ImageViewer.Layers;
+using ClearCanvas.ImageViewer.Mathematics;
+using ClearCanvas.ImageViewer.Rendering;
+
+namespace ClearCanvas.ImageViewer.Rendering.Tests
+{
+
+	[TestFixture]
+	public class ImageRendererMiscTests
+	{
+		public ImageRendererMiscTests()
+		{
+		}
+
+		[TestFixtureSetUp]
+		public void Init()
+		{
+		}
+	
+		[TestFixtureTearDown]
+		public void Cleanup()
+		{
+		}
+
+		[Test]
+		public void RenderInvalidRectangle()
+		{
+
+		}
+
+		[Test]
+		public void TestScaleAndRotateTransform()
+		{
+			Rectangle clientRectangle = new Rectangle(0, 0, 100, 200);
+			SpatialTransform transform = new SpatialTransform();
+			transform.Rotation = -90;
+			transform.Scale = 1.5F;
+			transform.ScaleToFit = false;
+			transform.SourceRectangle = new Rectangle(0, 0, 100, 200);
+			transform.DestinationRectangle = new Rectangle(0, 0, 100, 200);
+			transform.Calculate();
+
+			RectangleF srcRectangleF = transform.SourceRectangle;
+			RectangleF dstRectangleF = transform.ConvertToDestination(srcRectangleF);
+
+			Assert.IsTrue(FloatComparer.AreEqual(dstRectangleF.Left, -100));
+			Assert.IsTrue(FloatComparer.AreEqual(dstRectangleF.Top, 175));
+			Assert.IsTrue(FloatComparer.AreEqual(dstRectangleF.Right, 200));
+			Assert.IsTrue(FloatComparer.AreEqual(dstRectangleF.Bottom, 25));
+
+			RectangleF dstViewableRectangleF = RectangleUtilities.Intersect(dstRectangleF, clientRectangle);
+            RectangleF srcViewableRectangleF = transform.ConvertToSource(dstViewableRectangleF);
+
+			Rectangle dstViewableRectangle = Rectangle.Round(dstViewableRectangleF);
+
+			Assert.AreEqual(0, dstViewableRectangle.Left);
+			Assert.AreEqual(175, dstViewableRectangle.Top);
+			Assert.AreEqual(100, dstViewableRectangle.Right);
+			Assert.AreEqual(25, dstViewableRectangle.Bottom);
+
+			Rectangle srcViewableRectangle = Rectangle.Round(srcViewableRectangleF);
+
+			Assert.AreEqual(0, srcViewableRectangle.Left);
+			Assert.AreEqual(67, srcViewableRectangle.Top);
+			Assert.AreEqual(100, srcViewableRectangle.Right);
+			Assert.AreEqual(134, srcViewableRectangle.Bottom);
+		}
+
+		[Test]
+		public void TestFlipHorizontalTransform()
+		{
+			Rectangle clientRectangle = new Rectangle(0, 0, 100, 200);
+			SpatialTransform transform = new SpatialTransform();
+			transform.FlipHorizontal = true;
+			transform.Scale = 1.0F;
+			transform.ScaleToFit = false;
+			transform.SourceRectangle = new Rectangle(0, 0, 100, 200);
+			transform.DestinationRectangle = new Rectangle(0, 0, 100, 200);
+			transform.Calculate();
+
+			RectangleF srcRectangleF = transform.SourceRectangle;
+			RectangleF dstRectangleF = transform.ConvertToDestination(srcRectangleF);
+			RectangleF dstViewableRectangleF = RectangleUtilities.Intersect(dstRectangleF, clientRectangle);
+			RectangleF srcViewableRectangleF = transform.ConvertToSource(dstViewableRectangleF);
+
+			Rectangle dstViewableRectangle = Rectangle.Round(dstViewableRectangleF);
+			Rectangle srcViewableRectangle = Rectangle.Round(srcViewableRectangleF);
+
+			Assert.AreEqual(0, srcViewableRectangle.Left);
+			Assert.AreEqual(0, srcViewableRectangle.Top);
+			Assert.AreEqual(100, srcViewableRectangle.Right);
+			Assert.AreEqual(200, srcViewableRectangle.Bottom);
+		}
+	}
+}
+#endif
---
+++ ImageViewer/Rendering/Tests/ImageRendererRGBTripletOrientationTests.cs
@@ -0,0 +1,202 @@
+#if	UNIT_TESTS
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Drawing;
+using System.Drawing.Imaging;
+using NUnit.Framework;
+using ClearCanvas.Common;
+using ClearCanvas.ImageViewer.Imaging;
+using ClearCanvas.ImageViewer.Layers;
+using ClearCanvas.ImageViewer.Rendering;
+
+namespace ClearCanvas.ImageViewer.Rendering.Tests
+{
+	[TestFixture]
+	public class ImageRendererRGBTripletOrientationTests
+	{
+		LayerGroup _layerGroup;
+		MockImageLayer _layer;
+		int _srcWidth, _srcHeight;
+		int _dstWidth, _dstHeight;
+		Rectangle _srcRect, _dstRect;
+		Color _leftTop, _rightTop, _leftBottom, _rightBottom;
+
+		public ImageRendererRGBTripletOrientationTests()
+		{
+		}
+
+		[TestFixtureSetUp]
+		public void Init()
+		{
+			CreateRGBTripletImageLayer();
+			CreatePhantom();
+		}
+
+		[TestFixtureTearDown]
+		public void Cleanup()
+		{
+		}
+
+		[Test]
+		public void RenderOrientation1()
+		{
+			_layer.SpatialTransform.Initialize();
+			_layer.SpatialTransform.Calculate();
+
+			Bitmap bitmap = ImageRendererTestUtilities.RenderLayer(_layer, _dstWidth, _dstHeight);
+
+			ImageRendererTestUtilities.VerifyRGBPixelValue(_dstRect.Left, _dstRect.Top, _leftTop, bitmap);
+			ImageRendererTestUtilities.VerifyRGBPixelValue(_dstRect.Right - 1, _dstRect.Top, _rightTop, bitmap);
+			ImageRendererTestUtilities.VerifyRGBPixelValue(_dstRect.Left, _dstRect.Bottom - 1, _leftBottom, bitmap);
+			ImageRendererTestUtilities.VerifyRGBPixelValue(_dstRect.Right - 1, _dstRect.Bottom - 1, _rightBottom, bitmap);
+		}
+
+		[Test]
+		public void RenderOrientation2()
+		{
+			_layer.SpatialTransform.Initialize();
+			_layer.SpatialTransform.FlipVertical = true;
+			_layer.SpatialTransform.Calculate();
+
+			Bitmap bitmap = ImageRendererTestUtilities.RenderLayer(_layer, _dstWidth, _dstHeight);
+
+			ImageRendererTestUtilities.VerifyRGBPixelValue(_dstRect.Left, _dstRect.Top, _leftBottom, bitmap);
+			ImageRendererTestUtilities.VerifyRGBPixelValue(_dstRect.Right - 1, _dstRect.Top, _rightBottom, bitmap);
+			ImageRendererTestUtilities.VerifyRGBPixelValue(_dstRect.Left, _dstRect.Bottom - 1, _leftTop, bitmap);
+			ImageRendererTestUtilities.VerifyRGBPixelValue(_dstRect.Right - 1, _dstRect.Bottom - 1, _rightTop, bitmap);
+		}
+
+		[Test]
+		public void RenderOrientation3()
+		{
+			_layer.SpatialTransform.Initialize();
+			_layer.SpatialTransform.FlipHorizontal = true;
+			_layer.SpatialTransform.Calculate();
+
+			Bitmap bitmap = ImageRendererTestUtilities.RenderLayer(_layer, _dstWidth, _dstHeight);
+
+			ImageRendererTestUtilities.VerifyRGBPixelValue(_dstRect.Left, _dstRect.Top, _rightTop, bitmap);
+			ImageRendererTestUtilities.VerifyRGBPixelValue(_dstRect.Right - 1, _dstRect.Top, _leftTop, bitmap);
+			ImageRendererTestUtilities.VerifyRGBPixelValue(_dstRect.Left, _dstRect.Bottom - 1, _rightBottom, bitmap);
+			ImageRendererTestUtilities.VerifyRGBPixelValue(_dstRect.Right - 1, _dstRect.Bottom - 1, _leftBottom, bitmap);
+		}
+
+		[Test]
+		public void RenderOrientation4()
+		{
+			_layer.SpatialTransform.Initialize();
+			_layer.SpatialTransform.FlipHorizontal = true;
+			_layer.SpatialTransform.FlipVertical = true;
+			_layer.SpatialTransform.Calculate();
+
+			Bitmap bitmap = ImageRendererTestUtilities.RenderLayer(_layer, _dstWidth, _dstHeight);
+
+			ImageRendererTestUtilities.VerifyRGBPixelValue(_dstRect.Left, _dstRect.Top, _rightBottom, bitmap);
+			ImageRendererTestUtilities.VerifyRGBPixelValue(_dstRect.Right - 1, _dstRect.Top, _leftBottom, bitmap);
+			ImageRendererTestUtilities.VerifyRGBPixelValue(_dstRect.Left, _dstRect.Bottom - 1, _rightTop, bitmap);
+			ImageRendererTestUtilities.VerifyRGBPixelValue(_dstRect.Right - 1, _dstRect.Bottom - 1, _leftTop, bitmap);
+		}
+
+		[Test]
+		public void RenderOrientation5()
+		{
+			_layer.SpatialTransform.Initialize();
+			_layer.SpatialTransform.Rotation = -90; // CCW rotation
+			_layer.SpatialTransform.Calculate();
+
+			Bitmap bitmap = ImageRendererTestUtilities.RenderLayer(_layer, _dstWidth, _dstHeight);
+
+			ImageRendererTestUtilities.VerifyRGBPixelValue(_dstRect.Left, _dstRect.Top, _rightTop, bitmap);
+			ImageRendererTestUtilities.VerifyRGBPixelValue(_dstRect.Right - 1, _dstRect.Top, _rightBottom, bitmap);
+			ImageRendererTestUtilities.VerifyRGBPixelValue(_dstRect.Left, _dstRect.Bottom - 1, _leftTop, bitmap);
+			ImageRendererTestUtilities.VerifyRGBPixelValue(_dstRect.Right - 1, _dstRect.Bottom - 1, _leftBottom, bitmap);
+		}
+
+		[Test]
+		public void RenderOrientation6()
+		{
+			_layer.SpatialTransform.Initialize();
+			_layer.SpatialTransform.Rotation = -90;
+			_layer.SpatialTransform.FlipHorizontal = true;
+			_layer.SpatialTransform.Calculate();
+
+			Bitmap bitmap = ImageRendererTestUtilities.RenderLayer(_layer, _dstWidth, _dstHeight);
+
+			ImageRendererTestUtilities.VerifyRGBPixelValue(_dstRect.Left, _dstRect.Top, _leftTop, bitmap);
+			ImageRendererTestUtilities.VerifyRGBPixelValue(_dstRect.Right - 1, _dstRect.Top, _leftBottom, bitmap);
+			ImageRendererTestUtilities.VerifyRGBPixelValue(_dstRect.Left, _dstRect.Bottom - 1, _rightTop, bitmap);
+			ImageRendererTestUtilities.VerifyRGBPixelValue(_dstRect.Right - 1, _dstRect.Bottom - 1, _rightBottom, bitmap);
+		}
+
+		[Test]
+		public void RenderOrientation7()
+		{
+			_layer.SpatialTransform.Initialize();
+			_layer.SpatialTransform.Rotation = -90; // CCW rotation
+			_layer.SpatialTransform.FlipVertical = true;
+			_layer.SpatialTransform.Calculate();
+
+			Bitmap bitmap = ImageRendererTestUtilities.RenderLayer(_layer, _dstWidth, _dstHeight);
+
+			ImageRendererTestUtilities.VerifyRGBPixelValue(_dstRect.Left, _dstRect.Top, _rightBottom, bitmap);
+			ImageRendererTestUtilities.VerifyRGBPixelValue(_dstRect.Right - 1, _dstRect.Top, _rightTop, bitmap);
+			ImageRendererTestUtilities.VerifyRGBPixelValue(_dstRect.Left, _dstRect.Bottom - 1, _leftBottom, bitmap);
+			ImageRendererTestUtilities.VerifyRGBPixelValue(_dstRect.Right - 1, _dstRect.Bottom - 1, _leftTop, bitmap);
+		}
+
+		[Test]
+		public void RenderOrientation8()
+		{
+			_layer.SpatialTransform.Initialize();
+			_layer.SpatialTransform.Rotation = -90; // CCW rotation
+			_layer.SpatialTransform.FlipVertical = true;
+			_layer.SpatialTransform.FlipHorizontal = true;
+			_layer.SpatialTransform.Calculate();
+
+			Bitmap bitmap = ImageRendererTestUtilities.RenderLayer(_layer, _dstWidth, _dstHeight);
+
+			ImageRendererTestUtilities.VerifyRGBPixelValue(_dstRect.Left, _dstRect.Top, _leftBottom, bitmap);
+			ImageRendererTestUtilities.VerifyRGBPixelValue(_dstRect.Right - 1, _dstRect.Top, _leftTop, bitmap);
+			ImageRendererTestUtilities.VerifyRGBPixelValue(_dstRect.Left, _dstRect.Bottom - 1, _rightBottom, bitmap);
+			ImageRendererTestUtilities.VerifyRGBPixelValue(_dstRect.Right - 1, _dstRect.Bottom - 1, _rightTop, bitmap);
+		}
+
+		private void CreateRGBTripletImageLayer()
+		{
+			_srcWidth = 10;
+			_srcHeight = 10;
+			_dstWidth = 23;
+			_dstHeight = 23;
+
+			_layer = ImageLayerFactory.CreateRGBTripletImageLayer(_srcWidth, _srcHeight);
+
+			_layerGroup = new LayerGroup();
+			_layerGroup.Layers.Add(_layer);
+
+			_srcRect = new Rectangle(0, 0, _srcWidth, _srcHeight);
+			_dstRect = new Rectangle(0, 0, _dstWidth, _dstHeight);
+
+			_layer.SpatialTransform.SourceRectangle = _srcRect;
+			_layer.SpatialTransform.DestinationRectangle = _dstRect;
+
+			_layer.NormalInterpolationMethod = ImageLayer.InterpolationMethods.NEAREST_NEIGHBOURS;
+		}
+
+		private void CreatePhantom()
+		{
+			_leftTop = Color.FromArgb(10, 20, 30);
+			_rightTop = Color.FromArgb(40, 50, 60);
+			_leftBottom = Color.FromArgb(70, 80, 90);
+			_rightBottom = Color.FromArgb(100, 110, 120);
+
+			// Set a pixel with a different value at each corner of the image
+			_layer.PixelDataWrapper.SetPixelRGB(_srcRect.Left, _srcRect.Top, _leftTop);
+			_layer.PixelDataWrapper.SetPixelRGB(_srcRect.Right - 1, _srcRect.Top, _rightTop);
+			_layer.PixelDataWrapper.SetPixelRGB(_srcRect.Left, _srcRect.Bottom - 1, _leftBottom);
+			_layer.PixelDataWrapper.SetPixelRGB(_srcRect.Right - 1, _srcRect.Bottom - 1, _rightBottom);
+		}
+	}
+}
+#endif
