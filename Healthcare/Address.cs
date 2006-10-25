4df03fedb4976084002fbe0cb5634af858fb8c6f
rob <devnull@localhost>
2006-10-25 20:39:43 +0000

    Requirement: R1019
    1. Updated mappings for several HL7 message fields.  Also updated the default values for several fields.
    
    2. Added an IsEquivalentTo method to both Address and TelephoneNumber classes.  This method compares two objects for equality, ignoring the validity range of the objects.
    
    3. Updated HL7 processor to assign default validity ranges to both Addresses and TelephoneNumbers.  The processor will also set the ValidUntil property for any existing addresses/phone numbers of the same type.

--- Healthcare/Address.cs
+++ Healthcare/Address.cs
@@ -46,5 +46,22 @@ namespace ClearCanvas.Healthcare {
         {
             return string.Format("{0}, {1} {2} {3}", _street, _city, _province, _postalCode);
         }
+
+        /// <summary>
+        /// Equivalence comparison which ignores validity range
+        /// </summary>
+        /// <param name="that">The Address to compare to</param>
+        /// <returns>True if all fields other than the validity range are the same, False otherwise</returns>
+        public bool IsEquivalentTo(Address that)
+        {
+            return (that != null) &&
+                ((this._street == default(string)) ? (that._street == default(string)) : this._street.Equals(that._street)) &&
+                ((this._city == default(string)) ? (that._city == default(string)) : this._city.Equals(that._city)) &&
+                ((this._province == default(string)) ? (that._province == default(string)) : this._province.Equals(that._province)) &&
+                ((this._postalCode == default(string)) ? (that._postalCode == default(string)) : this._postalCode.Equals(that._postalCode)) &&
+                ((this._country == default(string)) ? (that._country == default(string)) : this._country.Equals(that._country)) &&
+                ((this._type == default(AddressType)) ? (that._type == default(AddressType)) : this._type.Equals(that._type))  &&
+                true;
+        }
 	}
 }
--- Healthcare/TelephoneNumber.cs
+++ Healthcare/TelephoneNumber.cs
@@ -33,6 +33,7 @@ namespace ClearCanvas.Healthcare {
             _areaCode = source.AreaCode;
             _number = source.Number;
             _extension = source.Extension;
+            _validRange = (source._validRange == null) ? null : new DateTimeRange(source._validRange.From, source._validRange.Until);
         }
 
         public bool IsCurrent
@@ -45,6 +46,24 @@ namespace ClearCanvas.Healthcare {
             return _extension == null ?
                 string.Format("{0} ({1}) {2}", _countryCode, _areaCode, _number) :
                 string.Format("{0} ({1}) {2} x{3}", _countryCode, _areaCode, _number, _extension);
+        }
+
+        /// <summary>
+        /// Equivalence comparison which ignores ValidRange
+        /// </summary>
+        /// <param name="that">The TelephoneNumber to compare to</param>
+        /// <returns>True if all fields other than the validity range are the same, False otherwise</returns>
+        public bool IsEquivalentTo(TelephoneNumber that)
+        {
+            return (that != null) &&
+                ((this._countryCode == default(string)) ? (that._countryCode == default(string)) : this._countryCode.Equals(that._countryCode)) &&
+                ((this._areaCode == default(string)) ? (that._areaCode == default(string)) : this._areaCode.Equals(that._areaCode)) &&
+                ((this._number == default(string)) ? (that._number == default(string)) : this._number.Equals(that._number)) &&
+                ((this._extension == default(string)) ? (that._extension == default(string)) : this._extension.Equals(that._extension)) &&
+                ((this._use == default(TelephoneUse)) ? (that._use == default(TelephoneUse)) : this._use.Equals(that._use)) &&
+                ((this._equipment == default(TelephoneEquipment)) ? (that._equipment == default(TelephoneEquipment)) : this._equipment.Equals(that._equipment)) &&
+                true;
+
         }
 
 	}
