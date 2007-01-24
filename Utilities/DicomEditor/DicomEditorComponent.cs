cdc640439f7b3032db584d466836a67d57596a8e
henry <devnull@localhost>
2007-01-24 21:56:39 +0000

    Requirement: R1559
    Added progress bar to dump tool.

--- Utilities/DicomEditor/DicomEditorComponent.cs
+++ Utilities/DicomEditor/DicomEditorComponent.cs
@@ -312,17 +312,36 @@ namespace ClearCanvas.Utilities.DicomEditor
 
         #endregion
 
-        public IEnumerable<FileDicomImage> Files
+        //public IEnumerable<FileDicomImage> Files
+        public ICollection<FileDicomImage> Files
         {
             set 
             {
                 _loadedDicomDumps.Clear();
                 DicomFileAccessor accessor = new DicomFileAccessor();
+                IEnumerator<FileDicomImage> enumerator = value.GetEnumerator();
 
-                foreach (FileDicomImage file in value)
-                {
-                    _loadedDicomDumps.Add(accessor.LoadDicomDump(file));
-                }
+                BackgroundTask task = new BackgroundTask(delegate(IBackgroundTaskContext backgroundcontext)
+                    {
+                        int i = 0;
+                        
+                        while (enumerator.MoveNext())
+                        {
+                            if (backgroundcontext.CancelRequested)
+                            {
+                                backgroundcontext.Cancel();
+                                return;
+                            }
+                            _loadedDicomDumps.Add(accessor.LoadDicomDump(enumerator.Current));
+                            backgroundcontext.ReportProgress(new BackgroundTaskProgress((int) (( (double)(i + 1) / (double) value.Count) * 100.0), "Processing Files..."));
+                            i++;
+                        }
+
+                        backgroundcontext.Complete(null);
+                    }, false);
+
+                ProgressDialog.Show(task, true, this.Host.DesktopWindow);
+
                 _position = 0;
 
                 this.UpdateComponent();
