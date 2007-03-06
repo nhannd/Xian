cda38fe9663d2cd2ca05697188fc5f854e32daa2
norman <devnull@localhost>
2007-03-06 22:59:30 +0000

    Bug: #315
    Currently, the RIS and ImageViewer both have PostBuild.bat files in the same directory as their solution files.
    If you want to have more than one solution file in the directory (for different solution configurations), this is a problem, since there will be a name clash.  To fix this, I've changed the calling PostBuild.bat (located in Executable) so that it calls a batch file with the same name of the solution.  For example, in the ImageViewer directory, I can now have something like this:
    
    ImageViewer.sln
    ImageViewer.bat
    
    ImageViewerVolume.sln
    ImageViewerVolume.bat
    
    The ImageViewer.sln is the standard configuration of the viewer and ImageViewer.bat copies over the standard files.
    
    ImageViewerVolume.sln has the additional volume projects and ImageViewerVolume.bat copies over the standard files plus the additional volume dlls.
    
    I've made the appropriate change in /RIS by renaming the PostBuild.bat in that directory to Ris.bat.

--- Desktop/Executable/PostBuild.bat
+++ Desktop/Executable/PostBuild.bat
@@ -3,8 +3,8 @@
 :: then runs a solution specific batch file to copy files specific to that solution.
 :: 
 :: Assumptions:
-:: 1) The name of the solution specific batch file is PostBuild.bat
-:: 2) PostBuild.bat is located in the same folder as the solution file
+:: 1) The name of the solution specific batch file is %2.bat, where %2 is the name of the solution
+:: 2) %2.bat is located in the same folder as the solution file
 
 echo Executing ClearCanvas post-build step
 
@@ -21,16 +21,16 @@ md ".\datastore"
 copy "..\..\Logging.config" "."
 
 :: Copy shared libraries
-copy "..\..\..\..\Common\bin\%2\*.*" ".\Common"
-copy "..\..\..\..\Controls\WinForms\bin\%2\ClearCanvas.Controls.WinForms.dll" ".\Common"
+copy "..\..\..\..\Common\bin\%3\*.*" ".\Common"
+copy "..\..\..\..\Controls\WinForms\bin\%3\ClearCanvas.Controls.WinForms.dll" ".\Common"
 copy "..\..\..\..\ReferencedAssemblies\nunit.framework.dll" ".\Common" 
 copy "..\..\..\..\ReferencedAssemblies\log4net.dll" ".\Common"
 copy "..\..\..\..\ReferencedAssemblies\DotNetMagic2005.DLL" ".\Common"
 copy "..\..\..\..\ReferencedAssemblies\Iesi.Collections.dll" ".\Common"
 copy "..\..\..\..\ReferencedAssemblies\Nhibernate.dll" ".\Common"
 copy "..\..\..\..\ReferencedAssemblies\Castle.DynamicProxy.dll" ".\Common"
-copy "..\..\..\..\Dicom\DataStore\bin\%2\ClearCanvas.Dicom.DataStore.dll" ".\Common"
-copy "..\..\..\..\Dicom\DataStore\NHibernateDriver\bin\%2\ClearCanvas.Dicom.DataStore.NHibernateDriver.dll" ".\Common"
+copy "..\..\..\..\Dicom\DataStore\bin\%3\ClearCanvas.Dicom.DataStore.dll" ".\Common"
+copy "..\..\..\..\Dicom\DataStore\NHibernateDriver\bin\%3\ClearCanvas.Dicom.DataStore.NHibernateDriver.dll" ".\Common"
 copy "..\..\..\..\Dicom\DataStore\AuxiliaryFiles\sqlceca30.dll" ".\Common"
 copy "..\..\..\..\Dicom\DataStore\AuxiliaryFiles\sqlcecompact30.dll" ".\Common"
 copy "..\..\..\..\Dicom\DataStore\AuxiliaryFiles\sqlceer30EN.dll" ".\Common"
@@ -41,16 +41,16 @@ copy "..\..\..\..\Dicom\DataStore\AuxiliaryFiles\sqlcese30.dll" ".\Common"
 copy "..\..\..\..\Dicom\DataStore\AuxiliaryFiles\System.Data.SqlServerCe.dll" ".\Common"
 
 :: Copy shared plugins
-copy "..\..\..\bin\%2\ClearCanvas.Desktop.dll" ".\plugins"
-copy "..\..\..\Configuration\bin\%2\ClearCanvas.Desktop.Configuration.dll" .\plugins
-copy "..\..\..\Configuration\View\WinForms\bin\%2\ClearCanvas.Desktop.Configuration.View.Winforms.dll" .\plugins
-copy "..\..\..\View\WinForms\bin\%2\ClearCanvas.Desktop.View.WinForms.dll" ".\plugins"
-copy "..\..\..\Help\bin\%2\ClearCanvas.Desktop.Help.dll" ".\plugins"
-copy "..\..\..\ExtensionBrowser\bin\%2\ClearCanvas.Desktop.ExtensionBrowser.dll" ".\plugins"
-copy "..\..\..\ExtensionBrowser\View\WinForms\bin\%2\ClearCanvas.Desktop.ExtensionBrowser.View.WinForms.dll" ".\plugins"
+copy "..\..\..\bin\%3\ClearCanvas.Desktop.dll" ".\plugins"
+copy "..\..\..\Configuration\bin\%3\ClearCanvas.Desktop.Configuration.dll" .\plugins
+copy "..\..\..\Configuration\View\WinForms\bin\%3\ClearCanvas.Desktop.Configuration.View.Winforms.dll" .\plugins
+copy "..\..\..\View\WinForms\bin\%3\ClearCanvas.Desktop.View.WinForms.dll" ".\plugins"
+copy "..\..\..\Help\bin\%3\ClearCanvas.Desktop.Help.dll" ".\plugins"
+copy "..\..\..\ExtensionBrowser\bin\%3\ClearCanvas.Desktop.ExtensionBrowser.dll" ".\plugins"
+copy "..\..\..\ExtensionBrowser\View\WinForms\bin\%3\ClearCanvas.Desktop.ExtensionBrowser.View.WinForms.dll" ".\plugins"
 
 :: Copy config files
 copy "..\..\..\Actions\actionmodels.xml "."
 
 :: Run the solution specific batch file
-call ""%1\PostBuild.bat"" %2
+call ""%1\%2.bat"" %3
---
+++ ImageViewer/ImageViewer.bat
@@ -0,0 +1,48 @@
+:: ImageViewer specific postbuild step
+
+echo Executing ImageViewer post-build step
+
+:: Copy database file
+
+IF NOT EXIST ".\datastore\viewer.sdf" copy "..\..\..\..\Dicom\DataStore\AuxiliaryFiles\empty_viewer.sdf" ".\datastore\viewer.sdf"
+
+:: Copy Hibernate configuration file
+copy "..\..\..\..\Dicom\DataStore\AuxiliaryFiles\ClearCanvas.Dicom.DataStore.cfg.xml" "."
+copy "..\..\..\..\Dicom\DataStore\AuxiliaryFiles\ClearCanvas.Dicom.DataStore.cfg.xml" "ClearCanvas.Dicom.Services.cfg.xml"
+copy "..\..\..\..\ImageViewer\Tools\Standard\bin\%1\LutPresets\DefaultLutPresets.xml" "."
+copy "..\..\..\..\ImageViewer\Tools\Standard\bin\%1\LutPresets\DefaultLutPresetKeyAssignments.xml" "."
+
+:: Copy database rebuild utility
+copy "..\..\..\..\Utilities\RebuildDatabase\bin\%1\ClearCanvas.Utilities.RebuildDatabase.exe" "."
+copy "..\..\..\..\Utilities\RebuildDatabase\app.config" ".\ClearCanvas.Utilities.RebuildDatabase.exe.config"
+
+:: Copy Dicom assemblies	
+copy "..\..\..\..\Dicom\bin\%1\ClearCanvas.Dicom.dll" ".\Common"
+copy "..\..\..\..\Dicom\OffisWrapper\csharp\%1\ClearCanvas.Dicom.OffisWrapper.dll" ".\Common"
+copy "..\..\..\..\Dicom\OffisWrapper\cppwrapper\%1\OffisDcm.dll" ".\Common"
+copy "..\..\..\..\Dicom\DataStore\bin\%1\ClearCanvas.Dicom.DataStore.dll" ".\Common"
+
+:: Copy ImageViewer plugins
+copy "..\..\..\..\Codecs\bin\%1\ClearCanvas.Codecs.dll" ".\plugins"
+copy "..\..\..\Explorer\bin\%1\ClearCanvas.Desktop.Explorer.dll" ".\plugins"
+copy "..\..\..\..\ImageViewer\bin\%1\ClearCanvas.ImageViewer.dll" ".\plugins"
+copy "..\..\..\..\ImageViewer\View\WinForms\bin\%1\ClearCanvas.ImageViewer.View.WinForms.dll" ".\plugins"
+copy "..\..\..\..\ImageViewer\Tools\Measurement\bin\%1\ClearCanvas.ImageViewer.Tools.Measurement.dll" ".\plugins"
+copy "..\..\..\..\ImageViewer\Tools\Standard\bin\%1\ClearCanvas.ImageViewer.Tools.Standard.dll" ".\plugins"
+copy "..\..\..\Edit\bin\%1\ClearCanvas.Desktop.Edit.dll" ".\plugins"
+copy "..\..\..\..\ImageViewer\Explorer\Local\bin\%1\ClearCanvas.ImageViewer.Explorer.Local.dll" ".\plugins"
+copy "..\..\..\..\ImageViewer\Explorer\Local\Tools\bin\%1\ClearCanvas.ImageViewer.Explorer.Local.Tools.dll" ".\plugins"
+copy "..\..\..\..\ImageViewer\Explorer\Local\View\WinForms\bin\%1\ClearCanvas.ImageViewer.Explorer.Local.View.WinForms.dll" ".\plugins"
+copy "..\..\..\..\ImageViewer\Explorer\Dicom\bin\%1\ClearCanvas.ImageViewer.Explorer.Dicom.dll" ".\plugins"
+copy "..\..\..\..\ImageViewer\Explorer\Dicom\View\WinForms\bin\%1\ClearCanvas.ImageViewer.Explorer.Dicom.View.WinForms.dll" ".\plugins"
+copy "..\..\..\..\ImageViewer\Layout\Basic\bin\%1\ClearCanvas.ImageViewer.Layout.Basic.dll" ".\plugins"
+copy "..\..\..\..\ImageViewer\Layout\Basic\View\WinForms\bin\%1\ClearCanvas.ImageViewer.Layout.Basic.View.WinForms.dll" ".\plugins"
+copy "..\..\..\..\ImageViewer\Rendering\BilinearInterpolation\bin\%1\ClearCanvas.ImageViewer.Rendering.BilinearInterpolation.dll" ".\plugins"
+copy "..\..\..\..\ImageViewer\StudyFinders\Remote\bin\%1\ClearCanvas.ImageViewer.StudyFinders.Remote.dll" ".\plugins"
+copy "..\..\..\..\ImageViewer\StudyFinders\LocalDataStore\bin\%1\ClearCanvas.ImageViewer.StudyFinders.LocalDataStore.dll" ".\plugins"
+copy "..\..\..\..\ImageViewer\StudyLoaders\LocalDataStore\bin\%1\ClearCanvas.ImageViewer.StudyLoaders.LocalDataStore.dll" ".\plugins"
+copy "..\..\..\..\ImageViewer\AnnotationProviders\bin\%1\ClearCanvas.ImageViewer.AnnotationProviders.dll" ".\plugins"
+copy "..\..\..\..\Dicom\Services\bin\%1\ClearCanvas.Dicom.Services.dll" ".\plugins"
+copy "..\..\..\..\Dicom\Services\View\WinForms\bin\%1\ClearCanvas.Dicom.Services.View.WinForms.dll" ".\plugins"
+copy "..\..\..\..\Utilities\DicomEditor\bin\%1\ClearCanvas.Utilities.DicomEditor.dll" ".\plugins"
+copy "..\..\..\..\Utilities\DicomEditor\View\WinForms\bin\%1\ClearCanvas.Utilities.DicomEditor.View.WinForms.dll" ".\plugins"
