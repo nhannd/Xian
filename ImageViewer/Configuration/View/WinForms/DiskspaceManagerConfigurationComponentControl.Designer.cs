9ddd92d5b264fd9d262ad7ffee61bc51b642a0bf
guo <devnull@localhost>
2007-03-21 19:51:44 +0000

    Requirement: R2381 (Add Disk Management UI) -- Add Drive name on UI

--- ImageViewer/Configuration/View/WinForms/DiskspaceManagerConfigurationComponentControl.Designer.cs
+++ ImageViewer/Configuration/View/WinForms/DiskspaceManagerConfigurationComponentControl.Designer.cs
@@ -42,51 +42,55 @@ namespace ClearCanvas.ImageViewer.Configuration.View.WinForms
             this.label6 = new System.Windows.Forms.Label();
             this.label7 = new System.Windows.Forms.Label();
             this._txtStatus = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this._lbDriveName = new System.Windows.Forms.ListBox();
             ((System.ComponentModel.ISupportInitialize)(this._tbLowWatermark)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this._tbHighWatermark)).BeginInit();
             this.SuspendLayout();
             // 
             // _tbLowWatermark
             // 
-            this._tbLowWatermark.Location = new System.Drawing.Point(95, 137);
-            this._tbLowWatermark.Maximum = 100;
+            this._tbLowWatermark.Location = new System.Drawing.Point(95, 193);
+            this._tbLowWatermark.Maximum = 10000;
             this._tbLowWatermark.Name = "_tbLowWatermark";
             this._tbLowWatermark.Size = new System.Drawing.Size(267, 53);
             this._tbLowWatermark.TabIndex = 0;
+            this._tbLowWatermark.TickFrequency = 1000;
             this._tbLowWatermark.TickStyle = System.Windows.Forms.TickStyle.TopLeft;
             // 
             // _tbHighWatermark
             // 
-            this._tbHighWatermark.Location = new System.Drawing.Point(95, 36);
-            this._tbHighWatermark.Maximum = 100;
+            this._tbHighWatermark.Location = new System.Drawing.Point(95, 92);
+            this._tbHighWatermark.Maximum = 10000;
             this._tbHighWatermark.Name = "_tbHighWatermark";
             this._tbHighWatermark.Size = new System.Drawing.Size(267, 53);
             this._tbHighWatermark.TabIndex = 1;
+            this._tbHighWatermark.TickFrequency = 1000;
             // 
             // _pbUsedSpace
             // 
-            this._pbUsedSpace.Location = new System.Drawing.Point(104, 94);
+            this._pbUsedSpace.Location = new System.Drawing.Point(104, 151);
             this._pbUsedSpace.Name = "_pbUsedSpace";
             this._pbUsedSpace.Size = new System.Drawing.Size(246, 23);
             this._pbUsedSpace.TabIndex = 2;
             // 
             // _txtHighWatermark
             // 
-            this._txtHighWatermark.Location = new System.Drawing.Point(368, 50);
+            this._txtHighWatermark.Location = new System.Drawing.Point(368, 107);
             this._txtHighWatermark.Name = "_txtHighWatermark";
             this._txtHighWatermark.Size = new System.Drawing.Size(57, 22);
             this._txtHighWatermark.TabIndex = 3;
             // 
             // _txtUsedSpace
             // 
-            this._txtUsedSpace.Location = new System.Drawing.Point(368, 95);
+            this._txtUsedSpace.Location = new System.Drawing.Point(368, 151);
             this._txtUsedSpace.Name = "_txtUsedSpace";
             this._txtUsedSpace.Size = new System.Drawing.Size(57, 22);
             this._txtUsedSpace.TabIndex = 4;
             // 
             // _txtLowWatermark
             // 
-            this._txtLowWatermark.Location = new System.Drawing.Point(368, 137);
+            this._txtLowWatermark.Location = new System.Drawing.Point(368, 193);
             this._txtLowWatermark.Name = "_txtLowWatermark";
             this._txtLowWatermark.Size = new System.Drawing.Size(57, 22);
             this._txtLowWatermark.TabIndex = 5;
@@ -94,7 +98,7 @@ namespace ClearCanvas.ImageViewer.Configuration.View.WinForms
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(15, 36);
+            this.label1.Location = new System.Drawing.Point(15, 95);
             this.label1.MaximumSize = new System.Drawing.Size(80, 0);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(77, 34);
@@ -105,8 +109,8 @@ namespace ClearCanvas.ImageViewer.Configuration.View.WinForms
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(32, 83);
-            this.label2.MaximumSize = new System.Drawing.Size(80, 0);
+            this.label2.Location = new System.Drawing.Point(32, 145);
+            this.label2.MaximumSize = new System.Drawing.Size(60, 0);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(48, 34);
             this.label2.TabIndex = 7;
@@ -116,7 +120,7 @@ namespace ClearCanvas.ImageViewer.Configuration.View.WinForms
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(15, 131);
+            this.label3.Location = new System.Drawing.Point(15, 196);
             this.label3.MaximumSize = new System.Drawing.Size(80, 0);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(77, 34);
@@ -127,7 +131,7 @@ namespace ClearCanvas.ImageViewer.Configuration.View.WinForms
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(431, 53);
+            this.label4.Location = new System.Drawing.Point(431, 110);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(20, 17);
             this.label4.TabIndex = 9;
@@ -136,7 +140,7 @@ namespace ClearCanvas.ImageViewer.Configuration.View.WinForms
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(431, 98);
+            this.label5.Location = new System.Drawing.Point(431, 154);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(20, 17);
             this.label5.TabIndex = 10;
@@ -145,7 +149,7 @@ namespace ClearCanvas.ImageViewer.Configuration.View.WinForms
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(431, 140);
+            this.label6.Location = new System.Drawing.Point(431, 196);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(20, 17);
             this.label6.TabIndex = 11;
@@ -154,7 +158,7 @@ namespace ClearCanvas.ImageViewer.Configuration.View.WinForms
             // label7
             // 
             this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(32, 197);
+            this.label7.Location = new System.Drawing.Point(32, 252);
             this.label7.Name = "label7";
             this.label7.Size = new System.Drawing.Size(48, 17);
             this.label7.TabIndex = 12;
@@ -162,15 +166,35 @@ namespace ClearCanvas.ImageViewer.Configuration.View.WinForms
             // 
             // _txtStatus
             // 
-            this._txtStatus.Location = new System.Drawing.Point(104, 197);
+            this._txtStatus.Location = new System.Drawing.Point(104, 252);
             this._txtStatus.Name = "_txtStatus";
             this._txtStatus.Size = new System.Drawing.Size(321, 22);
             this._txtStatus.TabIndex = 13;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(51, 44);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(41, 17);
+            this.label8.TabIndex = 14;
+            this.label8.Text = "Drive";
+            // 
+            // _lbDriveName
+            // 
+            this._lbDriveName.FormattingEnabled = true;
+            this._lbDriveName.ItemHeight = 16;
+            this._lbDriveName.Location = new System.Drawing.Point(104, 44);
+            this._lbDriveName.Name = "_lbDriveName";
+            this._lbDriveName.Size = new System.Drawing.Size(65, 20);
+            this._lbDriveName.TabIndex = 15;
             // 
             // DiskspaceManagerConfigurationComponentControl
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this._lbDriveName);
+            this.Controls.Add(this.label8);
             this.Controls.Add(this._txtStatus);
             this.Controls.Add(this.label7);
             this.Controls.Add(this.label6);
@@ -186,7 +210,7 @@ namespace ClearCanvas.ImageViewer.Configuration.View.WinForms
             this.Controls.Add(this._tbHighWatermark);
             this.Controls.Add(this._tbLowWatermark);
             this.Name = "DiskspaceManagerConfigurationComponentControl";
-            this.Size = new System.Drawing.Size(492, 247);
+            this.Size = new System.Drawing.Size(492, 312);
             ((System.ComponentModel.ISupportInitialize)(this._tbLowWatermark)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this._tbHighWatermark)).EndInit();
             this.ResumeLayout(false);
@@ -210,5 +234,7 @@ namespace ClearCanvas.ImageViewer.Configuration.View.WinForms
         private System.Windows.Forms.Label label6;
         private System.Windows.Forms.Label label7;
         private System.Windows.Forms.TextBox _txtStatus;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.ListBox _lbDriveName;
     }
 }
