378760e58471b3b4a5f7b6d51e2ab05b8ad7353f
jonathan <devnull@localhost>
2007-03-22 19:11:25 +0000

    Requirement: R2207
    Commented out most of PatientOverviewComponent since it didn't really do anything anyways.
    Removed PatientProfilePreviewComponent from project since it is no longer used.

--- Ris/Client/Adt/PatientOverviewComponent.cs
+++ Ris/Client/Adt/PatientOverviewComponent.cs
@@ -8,6 +8,7 @@ using ClearCanvas.Desktop.Tools;
 using ClearCanvas.Desktop.Actions;
 using ClearCanvas.Ris.Application.Common;
 using ClearCanvas.Enterprise.Common;
+using ClearCanvas.Ris.Application.Common.Admin.PatientAdmin;
 
 namespace ClearCanvas.Ris.Client.Adt
 {
@@ -26,7 +27,7 @@ namespace ClearCanvas.Ris.Client.Adt
 
     public interface IPatientOverviewToolContext : IToolContext
     {
-        EntityRef<PatientProfile> PatientProfile { get; }
+        EntityRef PatientProfile { get; }
         IDesktopWindow DesktopWindow { get; }
     }
 
@@ -35,7 +36,7 @@ namespace ClearCanvas.Ris.Client.Adt
     /// PatientComponent class
     /// </summary>
 //    [AssociateView(typeof(PatientComponentViewExtensionPoint))]
-    public class PatientOverviewComponent : SplitComponentContainer
+    public class PatientOverviewComponent : ApplicationComponent
     {
         class PatientOverviewToolContext : ToolContext, IPatientOverviewToolContext
         {
@@ -46,7 +47,7 @@ namespace ClearCanvas.Ris.Client.Adt
                 _component = component;
             }
 
-            public EntityRef<PatientProfile> PatientProfile
+            public EntityRef PatientProfile
             {
                 get { return _component._patientProfileRef; }
             }
@@ -58,41 +59,28 @@ namespace ClearCanvas.Ris.Client.Adt
         }
 
 
-        private PatientProfilePreviewComponent _preview;
-        private PatientProfilePreviewComponent _detail;
-
-        private EntityRef<PatientProfile> _patientProfileRef;
-        private PatientProfile _patientProfile;
+        private EntityRef _patientProfileRef;
 
         private ToolSet _toolSet;
-        private IAdtService _adtService;
 
 
         /// <summary>
         /// Constructor
         /// </summary>
-        public PatientOverviewComponent(EntityRef<PatientProfile> profileRef)
-            :base(SplitOrientation.Vertical)
+        public PatientOverviewComponent(EntityRef profileRef)
         {
             _patientProfileRef = profileRef;
         }
 
         public override void Start()
         {
-            _adtService = ApplicationContext.GetService<IAdtService>();
-            _adtService.PatientProfileChanged += PatientProfileChangedEventHandler;
-
-
-            this.Pane1 = new SplitPane(SR.TitlePreview, _preview = new PatientProfilePreviewComponent(), 1);
-            this.Pane2 = new SplitPane(SR.TitleDetail, _detail = new PatientProfilePreviewComponent(), 1);
-
             _toolSet = new ToolSet(new PatientOverviewToolExtensionPoint(), new PatientOverviewToolContext(this));
 
             Refresh();
 
             base.Start();
         }
-
+/*
         private void PatientProfileChangedEventHandler(object sender, EntityChangeEventArgs e)
         {
             // is the patient profile that changed the same one we are looking at?
@@ -101,11 +89,11 @@ namespace ClearCanvas.Ris.Client.Adt
                 Refresh();
             }
         }
-
+*/
         public override void Stop()
         {
             _toolSet.Dispose();
-            _adtService.PatientProfileChanged -= PatientProfileChangedEventHandler;
+ //           _adtService.PatientProfileChanged -= PatientProfileChangedEventHandler;
 
             base.Stop();
         }
@@ -117,12 +105,9 @@ namespace ClearCanvas.Ris.Client.Adt
 
         private void Refresh()
         {
-            _patientProfile = _adtService.LoadPatientProfile(_patientProfileRef, false);
             this.Host.SetTitle(string.Format(SR.TitlePatientComponent,
-                Format.Custom(_patientProfile.Name),
-                Format.Custom(_patientProfile.Mrn)));
-
-            _preview.PatientProfileRef = _patientProfileRef;
+                "Name",
+                "MRN"));
         }
     }
 }
