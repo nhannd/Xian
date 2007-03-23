7281fe99b50db1624386a3c3f9fecc667f8c76ce
norman <devnull@localhost>
2007-03-23 20:16:51 +0000

    Bug: #355 - Changed "Back" to "Previous" - Adjusted the size of the buttons and the position of the splitter while I was at it.

--- Desktop/View/WinForms/NavigatorComponentContainerControl.Designer.cs
+++ Desktop/View/WinForms/NavigatorComponentContainerControl.Designer.cs
@@ -59,8 +59,8 @@ namespace ClearCanvas.Desktop.View.WinForms
 			// splitContainer1.Panel2
 			// 
 			this.splitContainer1.Panel2.Controls.Add(this.tableLayoutPanel1);
-			this.splitContainer1.Size = new System.Drawing.Size(540, 377);
-			this.splitContainer1.SplitterDistance = 106;
+			this.splitContainer1.Size = new System.Drawing.Size(567, 377);
+			this.splitContainer1.SplitterDistance = 144;
 			this.splitContainer1.SplitterWidth = 3;
 			this.splitContainer1.TabIndex = 0;
 			// 
@@ -71,7 +71,7 @@ namespace ClearCanvas.Desktop.View.WinForms
 			this._treeView.Location = new System.Drawing.Point(0, 0);
 			this._treeView.Margin = new System.Windows.Forms.Padding(2);
 			this._treeView.Name = "_treeView";
-			this._treeView.Size = new System.Drawing.Size(106, 377);
+			this._treeView.Size = new System.Drawing.Size(144, 377);
 			this._treeView.TabIndex = 0;
 			this._treeView.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this._treeView_AfterSelect);
 			// 
@@ -90,16 +90,16 @@ namespace ClearCanvas.Desktop.View.WinForms
 			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
 			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
 			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
-			this.tableLayoutPanel1.Size = new System.Drawing.Size(431, 377);
+			this.tableLayoutPanel1.Size = new System.Drawing.Size(420, 377);
 			this.tableLayoutPanel1.TabIndex = 0;
 			// 
 			// _contentPanel
 			// 
 			this._contentPanel.Dock = System.Windows.Forms.DockStyle.Fill;
-			this._contentPanel.Location = new System.Drawing.Point(2, 38);
+			this._contentPanel.Location = new System.Drawing.Point(2, 33);
 			this._contentPanel.Margin = new System.Windows.Forms.Padding(2);
 			this._contentPanel.Name = "_contentPanel";
-			this._contentPanel.Size = new System.Drawing.Size(427, 305);
+			this._contentPanel.Size = new System.Drawing.Size(416, 303);
 			this._contentPanel.TabIndex = 1;
 			// 
 			// flowLayoutPanel1
@@ -110,18 +110,18 @@ namespace ClearCanvas.Desktop.View.WinForms
 			this.flowLayoutPanel1.Controls.Add(this._nextButton);
 			this.flowLayoutPanel1.Controls.Add(this._backButton);
 			this.flowLayoutPanel1.FlowDirection = System.Windows.Forms.FlowDirection.RightToLeft;
-			this.flowLayoutPanel1.Location = new System.Drawing.Point(165, 347);
+			this.flowLayoutPanel1.Location = new System.Drawing.Point(80, 340);
 			this.flowLayoutPanel1.Margin = new System.Windows.Forms.Padding(2);
 			this.flowLayoutPanel1.Name = "flowLayoutPanel1";
-			this.flowLayoutPanel1.Size = new System.Drawing.Size(264, 28);
+			this.flowLayoutPanel1.Size = new System.Drawing.Size(338, 35);
 			this.flowLayoutPanel1.TabIndex = 2;
 			// 
 			// _cancelButton
 			// 
-			this._cancelButton.Location = new System.Drawing.Point(206, 2);
+			this._cancelButton.Location = new System.Drawing.Point(261, 2);
 			this._cancelButton.Margin = new System.Windows.Forms.Padding(2);
 			this._cancelButton.Name = "_cancelButton";
-			this._cancelButton.Size = new System.Drawing.Size(56, 19);
+			this._cancelButton.Size = new System.Drawing.Size(75, 23);
 			this._cancelButton.TabIndex = 3;
 			this._cancelButton.Text = "Cancel";
 			this._cancelButton.UseVisualStyleBackColor = true;
@@ -129,10 +129,10 @@ namespace ClearCanvas.Desktop.View.WinForms
 			// 
 			// _okButton
 			// 
-			this._okButton.Location = new System.Drawing.Point(146, 2);
+			this._okButton.Location = new System.Drawing.Point(182, 2);
 			this._okButton.Margin = new System.Windows.Forms.Padding(2);
 			this._okButton.Name = "_okButton";
-			this._okButton.Size = new System.Drawing.Size(56, 19);
+			this._okButton.Size = new System.Drawing.Size(75, 23);
 			this._okButton.TabIndex = 2;
 			this._okButton.Text = "OK";
 			this._okButton.UseVisualStyleBackColor = true;
@@ -140,10 +140,10 @@ namespace ClearCanvas.Desktop.View.WinForms
 			// 
 			// _nextButton
 			// 
-			this._nextButton.Location = new System.Drawing.Point(86, 2);
+			this._nextButton.Location = new System.Drawing.Point(103, 2);
 			this._nextButton.Margin = new System.Windows.Forms.Padding(2);
 			this._nextButton.Name = "_nextButton";
-			this._nextButton.Size = new System.Drawing.Size(56, 19);
+			this._nextButton.Size = new System.Drawing.Size(75, 23);
 			this._nextButton.TabIndex = 1;
 			this._nextButton.Text = "Next >";
 			this._nextButton.UseVisualStyleBackColor = true;
@@ -151,24 +151,24 @@ namespace ClearCanvas.Desktop.View.WinForms
 			// 
 			// _backButton
 			// 
-			this._backButton.Location = new System.Drawing.Point(26, 2);
+			this._backButton.Location = new System.Drawing.Point(24, 2);
 			this._backButton.Margin = new System.Windows.Forms.Padding(2);
 			this._backButton.Name = "_backButton";
-			this._backButton.Size = new System.Drawing.Size(56, 19);
+			this._backButton.Size = new System.Drawing.Size(75, 23);
 			this._backButton.TabIndex = 0;
-			this._backButton.Text = "< Back";
+			this._backButton.Text = "< Previous";
 			this._backButton.UseVisualStyleBackColor = true;
 			this._backButton.Click += new System.EventHandler(this._backButton_Click);
 			// 
 			// _titleBar
 			// 
 			this._titleBar.Dock = System.Windows.Forms.DockStyle.Top;
-			this._titleBar.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this._titleBar.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
 			this._titleBar.GradientColoring = Crownwood.DotNetMagic.Controls.GradientColoring.LightBackToDarkBack;
 			this._titleBar.Location = new System.Drawing.Point(3, 3);
 			this._titleBar.MouseOverColor = System.Drawing.Color.Empty;
 			this._titleBar.Name = "_titleBar";
-			this._titleBar.Size = new System.Drawing.Size(425, 30);
+			this._titleBar.Size = new System.Drawing.Size(414, 25);
 			this._titleBar.Style = Crownwood.DotNetMagic.Common.VisualStyle.Office2003;
 			this._titleBar.TabIndex = 3;
 			this._titleBar.Text = "titleBar1";
@@ -182,7 +182,7 @@ namespace ClearCanvas.Desktop.View.WinForms
 			this.Controls.Add(this.splitContainer1);
 			this.Margin = new System.Windows.Forms.Padding(2);
 			this.Name = "NavigatorComponentContainerControl";
-			this.Size = new System.Drawing.Size(540, 377);
+			this.Size = new System.Drawing.Size(567, 377);
 			this.splitContainer1.Panel1.ResumeLayout(false);
 			this.splitContainer1.Panel2.ResumeLayout(false);
 			this.splitContainer1.ResumeLayout(false);
