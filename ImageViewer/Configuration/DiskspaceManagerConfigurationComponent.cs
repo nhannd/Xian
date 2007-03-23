19bb3e6f0e6d51a72a5675cacf28214d0ee5daa8
guo <devnull@localhost>
2007-03-23 21:33:37 +0000

    Requirement: R2381 (Add Disk Management UI) -- Data binding for the UI

--- ImageViewer/Configuration/DiskspaceManagerConfigurationComponent.cs
+++ ImageViewer/Configuration/DiskspaceManagerConfigurationComponent.cs
@@ -5,6 +5,7 @@ using System.Text;
 using ClearCanvas.Common;
 using ClearCanvas.Desktop;
 using ClearCanvas.Desktop.Configuration;
+using ClearCanvas.ImageViewer.Services.DiskspaceManager;
 
 namespace ClearCanvas.ImageViewer.Configuration
 {
@@ -23,9 +24,13 @@ namespace ClearCanvas.ImageViewer.Configuration
     public class DiskspaceManagerConfigurationComponent : ConfigurationApplicationComponent
     {
         private string _driveName;
+        private List<string> _availableDrives;
+        private string _status;
         private float _lowWatermark;
         private float _highWatermark;
         private float _spaceUsed;
+
+        private DiskspaceManagerServiceClient _serviceClient;
 
         /// <summary>
         /// Constructor
@@ -37,6 +42,7 @@ namespace ClearCanvas.ImageViewer.Configuration
         public override void Start()
         {
             // TODO prepare the component for its live phase
+            ConnectToClient();
             base.Start();
         }
 
@@ -47,10 +53,59 @@ namespace ClearCanvas.ImageViewer.Configuration
             base.Stop();
         }
 
+        public void ConnectToClient()
+        {
+            try
+            {
+                if (_serviceClient != null)
+                    _serviceClient.Close();
+
+                _serviceClient = new DiskspaceManagerServiceClient();
+                GetServerSettingResponse response = _serviceClient.GetServerSetting();
+
+                _availableDrives = new List<string>();
+                _driveName = response.DriveName;
+                _availableDrives.Add(_driveName);
+                _status = response.Status;
+                _lowWatermark = response.LowWatermark;
+                _highWatermark = response.HighWatermark;
+                _spaceUsed = response.UsedSpace;
+
+            }
+            catch (Exception e)
+            {
+                _driveName = "";
+                _status = "";
+                _lowWatermark = 0.0F;
+                _highWatermark = 0.0F;
+                _spaceUsed = 0.0F;
+                _serviceClient = null;
+
+            }
+
+        }
+
         public override void Save()
         {
-            // to do
+            if (_serviceClient != null)
+            {
+                try
+                {
+                    UpdateServerSettingRequest request = new UpdateServerSettingRequest();
+                    request.DriveName = _driveName;
+                    request.Status = _status;
+                    request.LowWatermark = _lowWatermark;
+                    request.HighWatermark = _highWatermark;
+                    request.UsedSpace = _spaceUsed;
+                    _serviceClient.UpdateServerSetting(request);
+                }
+                catch (Exception e)
+                {
+                    ExceptionHandler.Report(e, SR.ExceptionFailedToUpdateServerSettings, this.Host.DesktopWindow);
+                }
+            }
         }
+
 
         #region Properties
 
@@ -60,6 +115,26 @@ namespace ClearCanvas.ImageViewer.Configuration
             set
             {
                 _driveName = value;
+                this.Modified = true;
+            }
+        }
+
+        public List<string> AvailableDrives
+        {
+            get { return _availableDrives; }
+            set
+            {
+                _availableDrives = value;
+                this.Modified = true;
+            }
+        }
+
+        public string Status
+        {
+            get { return _status; }
+            set
+            {
+                _status = value;
                 this.Modified = true;
             }
         }
@@ -80,6 +155,7 @@ namespace ClearCanvas.ImageViewer.Configuration
             set
             {
                 _highWatermark = value;
+                //NotifyPropertyChanged("HighWatermarkString");
                 this.Modified = true;
             }
         }
@@ -92,6 +168,11 @@ namespace ClearCanvas.ImageViewer.Configuration
                 _spaceUsed = value;
                 this.Modified = true;
             }
+        }
+
+        public bool Enabled
+        {
+            get { return _serviceClient != null; }
         }
 
         #endregion
--- ImageViewer/Configuration/View/WinForms/DiskspaceManagerConfigurationComponentControl.cs
+++ ImageViewer/Configuration/View/WinForms/DiskspaceManagerConfigurationComponentControl.cs
@@ -6,12 +6,15 @@ using System.Data;
 using System.Text;
 using System.Windows.Forms;
 
+using ClearCanvas.Desktop.View.WinForms;
+using System.IO;
+
 namespace ClearCanvas.ImageViewer.Configuration.View.WinForms
 {
     /// <summary>
     /// Provides a Windows Forms user-interface for <see cref="DiskspaceManagerConfigurationComponent"/>
     /// </summary>
-    public partial class DiskspaceManagerConfigurationComponentControl : UserControl
+    public partial class DiskspaceManagerConfigurationComponentControl : ApplicationComponentUserControl
     {
         private DiskspaceManagerConfigurationComponent _component;
 
@@ -19,12 +22,30 @@ namespace ClearCanvas.ImageViewer.Configuration.View.WinForms
         /// Constructor
         /// </summary>
         public DiskspaceManagerConfigurationComponentControl(DiskspaceManagerConfigurationComponent component)
+            : base(component)
         {
             InitializeComponent();
 
             _component = component;
 
-            // TODO add .NET databindings to _component
+            _lbDriveName.DataSource = _component.AvailableDrives;
+            _tbLowWatermark.DataBindings.Add("Value", _component, "LowWatermark", true, DataSourceUpdateMode.OnPropertyChanged);
+            _tbHighWatermark.DataBindings.Add("Value", _component, "HighWatermark", true, DataSourceUpdateMode.OnPropertyChanged);
+            _pbUsedSpace.DataBindings.Add("Value", _component, "SpaceUsed", true, DataSourceUpdateMode.OnPropertyChanged);
+            _txtLowWatermark.DataBindings.Add("Text", _component, "LowWatermark", true, DataSourceUpdateMode.OnPropertyChanged);
+            _txtHighWatermark.DataBindings.Add("Text", _component, "HighWatermark", true, DataSourceUpdateMode.OnPropertyChanged);
+            _txtUsedSpace.DataBindings.Add("Text", _component, "SpaceUsed", true, DataSourceUpdateMode.OnPropertyChanged);
+            _txtStatus.DataBindings.Add("Text", _component, "Status", true, DataSourceUpdateMode.OnPropertyChanged);
+
+            _lbDriveName.DataBindings.Add("Enabled", _component, "Enabled", true, DataSourceUpdateMode.OnPropertyChanged);
+            _tbLowWatermark.DataBindings.Add("Enabled", _component, "Enabled", true, DataSourceUpdateMode.OnPropertyChanged);
+            _tbHighWatermark.DataBindings.Add("Enabled", _component, "Enabled", true, DataSourceUpdateMode.OnPropertyChanged);
+            _pbUsedSpace.DataBindings.Add("Enabled", _component, "Enabled", true, DataSourceUpdateMode.OnPropertyChanged);
+            _txtLowWatermark.DataBindings.Add("Enabled", _component, "Enabled", true, DataSourceUpdateMode.OnPropertyChanged);
+            _txtHighWatermark.DataBindings.Add("Enabled", _component, "Enabled", true, DataSourceUpdateMode.OnPropertyChanged);
+            _txtUsedSpace.DataBindings.Add("Enabled", _component, "Enabled", true, DataSourceUpdateMode.OnPropertyChanged);
+            _txtStatus.DataBindings.Add("Enabled", _component, "Enabled", true, DataSourceUpdateMode.OnPropertyChanged);
+
         }
     }
 }
