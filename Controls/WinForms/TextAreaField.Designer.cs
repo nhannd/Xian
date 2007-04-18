b349dafc0050708574eb327586e34fa03b97bfce
stewart <devnull@localhost>
2007-04-18 16:18:32 +0000

    Bug: #348 - Top edges of edit boxes cut off in settings management edit value dialog - fixed by "Sending to Back" on the label

--- Controls/WinForms/TextAreaField.Designer.cs
+++ Controls/WinForms/TextAreaField.Designer.cs
@@ -28,40 +28,43 @@ namespace ClearCanvas.Controls.WinForms
         /// </summary>
         private void InitializeComponent()
         {
-            this._textBox = new System.Windows.Forms.TextBox();
-            this._label = new System.Windows.Forms.Label();
-            this.SuspendLayout();
-            // 
-            // _textBox
-            // 
-            this._textBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
-                        | System.Windows.Forms.AnchorStyles.Left)
-                        | System.Windows.Forms.AnchorStyles.Right)));
-            this._textBox.Location = new System.Drawing.Point(3, 20);
-            this._textBox.Multiline = true;
-            this._textBox.Name = "_textBox";
-            this._textBox.Size = new System.Drawing.Size(244, 128);
-            this._textBox.TabIndex = 0;
-            // 
-            // _label
-            // 
-            this._label.AutoSize = true;
-            this._label.Location = new System.Drawing.Point(3, 0);
-            this._label.Name = "_label";
-            this._label.Size = new System.Drawing.Size(38, 17);
-            this._label.TabIndex = 1;
-            this._label.Text = "label";
-            // 
-            // TextArea
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Controls.Add(this._label);
-            this.Controls.Add(this._textBox);
-            this.Name = "TextArea";
-            this.Size = new System.Drawing.Size(250, 151);
-            this.ResumeLayout(false);
-            this.PerformLayout();
+			this._textBox = new System.Windows.Forms.TextBox();
+			this._label = new System.Windows.Forms.Label();
+			this.SuspendLayout();
+			// 
+			// _textBox
+			// 
+			this._textBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+						| System.Windows.Forms.AnchorStyles.Left)
+						| System.Windows.Forms.AnchorStyles.Right)));
+			this._textBox.Location = new System.Drawing.Point(2, 16);
+			this._textBox.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+			this._textBox.Multiline = true;
+			this._textBox.Name = "_textBox";
+			this._textBox.Size = new System.Drawing.Size(184, 105);
+			this._textBox.TabIndex = 0;
+			// 
+			// _label
+			// 
+			this._label.AutoSize = true;
+			this._label.Location = new System.Drawing.Point(2, 0);
+			this._label.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+			this._label.Name = "_label";
+			this._label.Size = new System.Drawing.Size(29, 13);
+			this._label.TabIndex = 1;
+			this._label.Text = "label";
+			// 
+			// TextAreaField
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.Controls.Add(this._textBox);
+			this.Controls.Add(this._label);
+			this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+			this.Name = "TextAreaField";
+			this.Size = new System.Drawing.Size(188, 123);
+			this.ResumeLayout(false);
+			this.PerformLayout();
 
         }
 
