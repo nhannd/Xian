58349f09214aa4a9832f62151a1aa6e3d6a41645
jay <devnull@localhost>
2007-05-01 16:25:21 +0000

    Requirement: R2521
    somehow some classes went missing... and broke the build in the last CheckIn, this should fix it.

--- Healthcare/Workflow/Operation.cs
+++ Healthcare/Workflow/Operation.cs
@@ -7,8 +7,36 @@ using ClearCanvas.Healthcare.Brokers;
 
 namespace ClearCanvas.Healthcare.Workflow
 {
-    public abstract class Operation
+    public class Operations
     {
+        public abstract class Operation
+        {
 
+        }
+
+        public class CheckIn : Operation
+        {
+            public void Execute(EntityRef rpRef, Staff currentUserStaff, IPersistenceContext context)
+            {
+                RequestedProcedure rp = context.GetBroker<IRequestedProcedureBroker>().Load(rpRef);
+
+                CheckInProcedureStep cps = new CheckInProcedureStep(rp);
+                cps.Start(currentUserStaff);
+                cps.Complete(currentUserStaff);
+
+                rp.CheckInProcedureSteps.Add(cps);
+                context.Lock(rp, DirtyState.Dirty);
+            }
+        }
+
+        public class Cancel : Operation
+        {
+            public void Execute(EntityRef orderRef, OrderCancelReason reason, IPersistenceContext context)
+            {
+                Order order = context.GetBroker<IOrderBroker>().Load(orderRef);
+                order.Cancel(reason);
+                context.Lock(order, DirtyState.Dirty);
+            }
+        }
     }
 }
