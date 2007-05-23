c8482c4911854de7bd408b40d6f36ceb36fed887
rob <devnull@localhost>
2007-05-23 21:37:30 +0000

    Requirement: R2593
    -Added a Diagnostic Service Tree to the model
    -Imported existing RIS's procedure tree via checked-in sql script

---
+++ Healthcare/DiagnosticServiceTreeNode.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections;
+using System.Text;
+
+using ClearCanvas.Enterprise.Core;
+
+namespace ClearCanvas.Healthcare {
+
+
+    /// <summary>
+    /// DiagnosticServiceTreeNode entity
+    /// </summary>
+	public partial class DiagnosticServiceTreeNode : ClearCanvas.Enterprise.Core.Entity
+	{
+	
+		/// <summary>
+		/// This method is called from the constructor.  Use this method to implement any custom
+		/// object initialization.
+		/// </summary>
+		private void CustomInitialize()
+		{
+		}
+		
+		#region Object overrides
+		
+		public override bool Equals(object that)
+		{
+			// TODO: implement a test for business-key equality
+			return base.Equals(that);
+		}
+		
+		public override int GetHashCode()
+		{
+			// TODO: implement a hash-code based on the business-key used in the Equals() method
+			return base.GetHashCode();
+		}
+		
+		#endregion
+
+	}
+}
