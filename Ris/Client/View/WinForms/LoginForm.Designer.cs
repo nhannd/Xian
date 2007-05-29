b963b2ddfb2a19f48a0a191d754cffa36dad0edf
norman <devnull@localhost>
2007-05-29 20:25:28 +0000

    Bug: #128 - Updated login dialog to use CC icon.

--- Ris/Client/View/WinForms/LoginForm.Designer.cs
+++ Ris/Client/View/WinForms/LoginForm.Designer.cs
@@ -28,65 +28,72 @@ namespace ClearCanvas.Ris.Client.View.WinForms
         /// </summary>
         private void InitializeComponent()
         {
-            this._userName = new System.Windows.Forms.TextBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this._cancelButton = new System.Windows.Forms.Button();
-            this._loginButton = new System.Windows.Forms.Button();
-            this.SuspendLayout();
-            // 
-            // _userName
-            // 
-            this._userName.Location = new System.Drawing.Point(27, 38);
-            this._userName.Name = "_userName";
-            this._userName.Size = new System.Drawing.Size(293, 22);
-            this._userName.TabIndex = 0;
-            this._userName.TextChanged += new System.EventHandler(this._userName_TextChanged);
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(24, 18);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(79, 17);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "User Name";
-            // 
-            // _cancelButton
-            // 
-            this._cancelButton.Location = new System.Drawing.Point(245, 166);
-            this._cancelButton.Name = "_cancelButton";
-            this._cancelButton.Size = new System.Drawing.Size(75, 23);
-            this._cancelButton.TabIndex = 2;
-            this._cancelButton.Text = "Cancel";
-            this._cancelButton.UseVisualStyleBackColor = true;
-            this._cancelButton.Click += new System.EventHandler(this._cancelButton_Click);
-            // 
-            // _loginButton
-            // 
-            this._loginButton.Enabled = false;
-            this._loginButton.Location = new System.Drawing.Point(168, 166);
-            this._loginButton.Name = "_loginButton";
-            this._loginButton.Size = new System.Drawing.Size(75, 24);
-            this._loginButton.TabIndex = 3;
-            this._loginButton.Text = "Login";
-            this._loginButton.UseVisualStyleBackColor = true;
-            this._loginButton.Click += new System.EventHandler(this._loginButton_Click);
-            // 
-            // LoginForm
-            // 
-            this.AcceptButton = this._cancelButton;
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(345, 202);
-            this.Controls.Add(this._loginButton);
-            this.Controls.Add(this._cancelButton);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this._userName);
-            this.Name = "LoginForm";
-            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "ClearCanvas Login";
-            this.ResumeLayout(false);
-            this.PerformLayout();
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(LoginForm));
+			this._userName = new System.Windows.Forms.TextBox();
+			this.label1 = new System.Windows.Forms.Label();
+			this._cancelButton = new System.Windows.Forms.Button();
+			this._loginButton = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// _userName
+			// 
+			this._userName.Location = new System.Drawing.Point(20, 31);
+			this._userName.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+			this._userName.Name = "_userName";
+			this._userName.Size = new System.Drawing.Size(221, 20);
+			this._userName.TabIndex = 0;
+			this._userName.TextChanged += new System.EventHandler(this._userName_TextChanged);
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(18, 15);
+			this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(60, 13);
+			this.label1.TabIndex = 1;
+			this.label1.Text = "User Name";
+			// 
+			// _cancelButton
+			// 
+			this._cancelButton.Location = new System.Drawing.Point(184, 135);
+			this._cancelButton.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+			this._cancelButton.Name = "_cancelButton";
+			this._cancelButton.Size = new System.Drawing.Size(56, 19);
+			this._cancelButton.TabIndex = 2;
+			this._cancelButton.Text = "Cancel";
+			this._cancelButton.UseVisualStyleBackColor = true;
+			this._cancelButton.Click += new System.EventHandler(this._cancelButton_Click);
+			// 
+			// _loginButton
+			// 
+			this._loginButton.Enabled = false;
+			this._loginButton.Location = new System.Drawing.Point(126, 135);
+			this._loginButton.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+			this._loginButton.Name = "_loginButton";
+			this._loginButton.Size = new System.Drawing.Size(56, 20);
+			this._loginButton.TabIndex = 3;
+			this._loginButton.Text = "Login";
+			this._loginButton.UseVisualStyleBackColor = true;
+			this._loginButton.Click += new System.EventHandler(this._loginButton_Click);
+			// 
+			// LoginForm
+			// 
+			this.AcceptButton = this._cancelButton;
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(259, 164);
+			this.Controls.Add(this._loginButton);
+			this.Controls.Add(this._cancelButton);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this._userName);
+			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+			this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+			this.Name = "LoginForm";
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+			this.Text = "ClearCanvas Login";
+			this.ResumeLayout(false);
+			this.PerformLayout();
 
         }
 
