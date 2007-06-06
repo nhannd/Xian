032e8257c8734075e238f7f6684aca818a028b58
norman <devnull@localhost>
2007-06-06 21:54:22 +0000

    Requirement: R2377 - Renamed Browser folder to WebBrowser so as to be consistent with other names within projects

--- ImageViewer/ImageViewerSamples.bat
+++ ImageViewer/ImageViewerSamples.bat
@@ -4,5 +4,5 @@ call "..\..\..\..\ImageViewer\ImageViewer.bat" %1
 
 echo Executing ImageViewerVolume post-build step
 
-copy "..\..\..\..\ImageViewer\Samples\Browser\bin\%1\ClearCanvas.ImageViewer.Samples.WebBrowser.dll" ".\plugins"
-copy "..\..\..\..\ImageViewer\Samples\Browser\View\WinForms\bin\%1\ClearCanvas.ImageViewer.Samples.WebBrowser.View.WinForms.dll" ".\plugins"
+copy "..\..\..\..\ImageViewer\Samples\WebBrowser\bin\%1\ClearCanvas.ImageViewer.Samples.WebBrowser.dll" ".\plugins"
+copy "..\..\..\..\ImageViewer\Samples\WebBrowser\View\WinForms\bin\%1\ClearCanvas.ImageViewer.Samples.WebBrowser.View.WinForms.dll" ".\plugins"
