207a7304b621172f01899ed4c509b1aaf639bce4
norman <devnull@localhost>
2007-07-01 06:51:32 +0000

    Requirement: R2377 - Added a path profile component that shows the pixel values as a function of the points along a ruler tool.

--- ImageViewer/ImageViewerSamples.bat
+++ ImageViewer/ImageViewerSamples.bat
@@ -11,8 +11,8 @@ copy "..\..\..\..\ImageViewer\Tools\ImageProcessing\Filter\bin\%1\ClearCanvas.Im
 copy "..\..\..\..\ImageViewer\Tools\ImageProcessing\Filter\View\WinForms\bin\%1\ClearCanvas.ImageViewer.Tools.ImageProcessing.Filter.View.WinForms.dll" ".\plugins"
 
 copy "..\..\..\..\ReferencedAssemblies\NPlot.dll" ".\plugins"
-copy "..\..\..\..\ImageViewer\Tools\ImageProcessing\RoiHistogram\bin\%1\ClearCanvas.ImageViewer.Tools.ImageProcessing.RoiHistogram.dll" ".\plugins"
-copy "..\..\..\..\ImageViewer\Tools\ImageProcessing\RoiHistogram\View\WinForms\bin\%1\ClearCanvas.ImageViewer.Tools.ImageProcessing.RoiHistogram.View.WinForms.dll" ".\plugins"
+copy "..\..\..\..\ImageViewer\Tools\ImageProcessing\RoiAnalysis\bin\%1\ClearCanvas.ImageViewer.Tools.ImageProcessing.RoiAnalysis.dll" ".\plugins"
+copy "..\..\..\..\ImageViewer\Tools\ImageProcessing\RoiAnalysis\View\WinForms\bin\%1\ClearCanvas.ImageViewer.Tools.ImageProcessing.RoiAnalysis.View.WinForms.dll" ".\plugins"
 
 copy "..\..\..\..\ImageViewer\Tools\Volume\bin\%1\ClearCanvas.ImageViewer.Tools.Volume.dll" ".\plugins"
 copy "..\..\..\..\ImageViewer\Tools\Volume\View\WinForms\bin\%1\ClearCanvas.ImageViewer.Tools.Volume.View.WinForms.dll" ".\plugins"
