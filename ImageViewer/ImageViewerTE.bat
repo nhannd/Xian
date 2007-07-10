a1440db04984441b5604d8fbef3389a554d4a7e1
norman <devnull@localhost>
2007-07-10 19:50:02 +0000

    Requirement: R2647 - Removed .bat files and replaced them with msbuild .proj files....forgot this one.

--- ImageViewer/ImageViewerTE.bat
+++
@@ -1,7 +0,0 @@
-:: ImageViewerVolume specific postbuild step
-
-call "..\..\..\..\ImageViewer\ImageViewer.bat" %1
-
-echo Executing ImageViewerTE post-build step
-
-copy "..\..\..\..\ImageViewer\Tools\ImageProcessing\DynamicTe\bin\%1\ClearCanvas.ImageViewer.Tools.ImageProcessing.DynamicTe.dll" ".\plugins"
