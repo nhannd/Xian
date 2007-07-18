3828225c87fe91440cb73e1f4b11449f36244be0
rob <devnull@localhost>
2007-07-18 15:19:49 +0000

    Requirement: R641 - Administratively define worklist instances based on requested procedure group
    - Added IEquatable<> methods to EnumValueInfo

--- Ris/Application/Common/EnumValueInfo.cs
+++ Ris/Application/Common/EnumValueInfo.cs
@@ -1,13 +1,11 @@
 using System;
-using System.Collections.Generic;
-using System.Text;
 using System.Runtime.Serialization;
 using ClearCanvas.Enterprise.Common;
 
 namespace ClearCanvas.Ris.Application.Common
 {
     [DataContract]
-    public class EnumValueInfo : DataContractBase, ICloneable
+    public class EnumValueInfo : DataContractBase, ICloneable, IEquatable<EnumValueInfo>
     {
         public EnumValueInfo(string code, string value)
             : this(code, value, "")
@@ -39,6 +37,33 @@ namespace ClearCanvas.Ris.Application.Common
         public object Clone()
         {
             return new EnumValueInfo(this.Code, this.Value, this.Description);
+        }
+
+        #endregion
+
+        #region IEquatable<EnumValueInfo>
+
+        public bool Equals(EnumValueInfo enumValueInfo)
+        {
+            if (enumValueInfo == null) return false;
+            if (!Equals(Code, enumValueInfo.Code)) return false;
+            if (!Equals(Value, enumValueInfo.Value)) return false;
+            if (!Equals(Description, enumValueInfo.Description)) return false;
+            return true;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(this, obj)) return true;
+            return Equals(obj as EnumValueInfo);
+        }
+
+        public override int GetHashCode()
+        {
+            int result = Code.GetHashCode();
+            result = 29*result + Value.GetHashCode();
+            result = 29*result + (Description != null ? Description.GetHashCode() : 0);
+            return result;
         }
 
         #endregion
