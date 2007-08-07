1accf7e1802befcbeb417b621fce805c4fe869c8
jonathan <devnull@localhost>
2007-08-07 14:28:06 +0000

    Requirement: #780 automated RIS install
    Deleted default-authority-groups.xml, since default authority groups are now hard-coded rather than supplied in xml.

--- Ris/Server/setup/import-authority-groups.bat
+++ Ris/Server/setup/import-authority-groups.bat
@@ -1,6 +1,6 @@
-:: Imports authority groups from the specified xml file
+:: Imports authority groups from the installed plugins
 :: Note that authority tokens must already be imported
 
-ClearCanvas.Ris.Server.Executable.exe ClearCanvas.Enterprise.Authentication.AuthorityGroupImporter default-authority-groups.xml
+ClearCanvas.Ris.Server.Executable.exe ClearCanvas.Enterprise.Authentication.AuthorityGroupImporter
 
 pause
