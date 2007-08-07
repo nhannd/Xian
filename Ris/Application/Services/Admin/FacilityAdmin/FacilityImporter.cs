de424fa2ce71c28ea2b82bc62141a3339e7dd2f6
jonathan <devnull@localhost>
2007-08-07 22:15:13 +0000

    Requirement: #780 automated RIS install
    Added FacilityImporter, and sample data facilities.csv.

---
+++ Ris/Application/Services/Admin/FacilityAdmin/FacilityImporter.cs
@@ -0,0 +1,58 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using ClearCanvas.Healthcare;
+using ClearCanvas.Enterprise.Core;
+using ClearCanvas.Common.Utilities;
+using ClearCanvas.Healthcare.Brokers;
+using ClearCanvas.Common;
+
+namespace ClearCanvas.Ris.Application.Services.Admin.FacilityAdmin
+{
+    [ExtensionOf(typeof(DataImporterExtensionPoint), Name = "Facility Importer")]
+    public class FacilityImporter : DataImporterBase
+    {
+        private List<Facility> _facilities = new List<Facility>();
+
+        public FacilityImporter()
+        {
+
+        }
+
+        public override void Import(List<string> rows, IUpdateContext context)
+        {
+            foreach (string line in rows)
+            {
+                // expect 2 fields in the row
+                string[] fields = ParseCsv(line, 2);
+
+                string id = fields[0];
+                string name = fields[1];
+
+                // first check if we have it in memory
+                Facility facility = CollectionUtils.SelectFirst<Facility>(_facilities,
+                    delegate(Facility f) { return f.Code == id && f.Name == name; });
+
+                // if not, check the database
+                if (facility == null)
+                {
+                    FacilitySearchCriteria where = new FacilitySearchCriteria();
+                    where.Code.EqualTo(id);
+                    where.Name.EqualTo(name);
+
+                    IFacilityBroker broker = context.GetBroker<IFacilityBroker>();
+                    facility = CollectionUtils.FirstElement<Facility>(broker.Find(where));
+
+                    // if not, create a new instance
+                    if (facility == null)
+                    {
+                        facility = new Facility(id, name);
+                        context.Lock(facility, DirtyState.New);
+                    }
+
+                    _facilities.Add(facility);
+                }
+            }
+        }
+    }
+}
