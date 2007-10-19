e60d7331f6cc67a3a7287ae52487ccd5d41daf3d
rob <devnull@localhost>
2007-10-19 16:26:15 +0000

    Requirement: #869
    - Added MPPS details pages for:
    -- Angiography / Interventional / Neuro Angio
    -- Gen Rad / GI/GU
    -- Breast Imaging / Mammography
    -- Nuclear Medicine
    -- Ultrasound / Prostate Centre / CEOU OB / Hydro Gyn

--- Ris/Client/Adt/PerformedProcedureStepDetailsPageUrlSelectorScript.js
+++ Ris/Client/Adt/PerformedProcedureStepDetailsPageUrlSelectorScript.js
@@ -12,4 +12,19 @@ if(modalityName == "MRI")
 if(modalityName == "PET")
     return "http://localhost/RIS/forms/technologist/petct-mpps.htm";
     
+if(modalityName == "Angiography" || modalityName == "Interventional" || modalityName == "Neuro Angiography")
+    return "http://localhost/RIS/forms/technologist/angio-interv-mpps.htm";
+
+if(modalityName == "General Radiography" || modalityName == "GI/GU")
+    return "http://localhost/RIS/forms/technologist/cr-mpps.htm";
+    
+if(modalityName == "Breast Imaging")
+    return "http://localhost/RIS/forms/technologist/mammo-mpps.htm";
+
+if(modalityName == "Ultrasound" || modalityName == "Prostate Centre" || modalityName == "CEOU OB" || modalityName == "Hydro Gyn")
+    return "http://localhost/RIS/forms/technologist/ultrasound-mpps.htm";
+    
+if(modalityName == "Nuclear Medicine")
+    return "http://localhost/RIS/forms/technologist/nucmed-mpps.htm";
+
 return "about:blank";
