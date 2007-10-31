f66778cd8c279b72bc699bf9fd6ba4fecc7c8bc1
jay <devnull@localhost>
2007-10-31 23:06:21 +0000

    Requirement: R1056 Implement the incoming fax queue
    - fix a few bugs, the Incoming-ToBeProcessed folder should work now
    - add a CreateFaxTool (create incoming fax)

---
+++ Ris/Application/Common/MimeDocumentSummary.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Runtime.Serialization;
+using ClearCanvas.Enterprise.Common;
+
+namespace ClearCanvas.Ris.Application.Common
+{
+    [DataContract]
+    public class MimeDocumentSummary
+    {
+        [DataMember]
+        public EntityRef DocumentRef;
+
+        [DataMember]
+        public DateTime? CreationTime;
+
+        [DataMember]
+        public string MimeType;
+
+        [DataMember]
+        public EntityRef BinaryDataRef;
+    }
+}
