addab4ece28e779130dd41530a03fbbbc5fa2b47
jonathan <devnull@localhost>
2007-11-22 22:27:32 +0000

    Bug: #971 Remove hard-code services URL
    Forgot the most important files in previous commit for this ticket.

---
+++ Ris/Client/WebServicesSettings.cs
@@ -0,0 +1,17 @@
+﻿using System.Configuration;
+
+namespace ClearCanvas.Ris.Client
+{
+
+    /// <summary>
+    /// Provides URL for RIS application web services.  These settings are stored locally in the app.config file.
+    /// </summary>
+    [SettingsGroupDescriptionAttribute("Provides location of the RIS application web services.")]
+    [SettingsProvider(typeof(LocalFileSettingsProvider))]
+    internal sealed partial class WebServicesSettings
+    {
+        private WebServicesSettings()
+        {
+        }
+    }
+}
---
+++ Ris/Server/WebServicesSettings.cs
@@ -0,0 +1,18 @@
+﻿using System.Configuration;
+
+namespace ClearCanvas.Ris.Server
+{
+    /// <summary>
+    /// Parameters for the web services exposed by this server. Note that these settings
+    /// are stored in the app.config file, not in the enterprise config store.
+    /// </summary>
+    [SettingsGroupDescription("Parameters for the web services exposed by this server.")]
+    internal sealed partial class WebServicesSettings
+    {
+        
+        public WebServicesSettings()
+        {
+        }
+        
+    }
+}
