4c8394e7799bcbe9a3eeeca50541ae37479f58fa
steve <devnull@localhost>
2008-01-24 15:52:56 +0000

    Bug: #1228
    On Jonathan's recommendation, removed the reference to ClearCanvas.log4net from ClearCanvas.Common, and changed the config file to reference the DLL name explicitly.  This removes the dependency on the ClearCanvas.log4net for the other projects.

--- Common/Platform.cs
+++ Common/Platform.cs
@@ -32,10 +32,7 @@
 using System;
 using System.Text;
 using ClearCanvas.Common.Auditing;
-using ClearCanvas.log4net;
 using log4net;
-
-//using log4net.spi;
 
 // Configure log4net using the .log4net file
 [assembly: log4net.Config.XmlConfigurator(ConfigFile = "Logging.config", Watch = true)]
@@ -138,11 +135,7 @@ namespace ClearCanvas.Common
 
 		private static object _syncRoot = new Object();
 
-        // This is a dummy variable to force the ClearCanvas.log4net assembly
-        // to be loaded before the log4net log manager is initialized.  This 
-        // allows us to configure our custom log manager in the Logging.config file.
-        private static readonly FileAppender _appender = new FileAppender();
-		private static readonly ILog _log = LogManager.GetLogger(typeof(Platform));
+    	private static readonly ILog _log = LogManager.GetLogger(typeof(Platform));
 
 		private static string _pluginSubFolder = "plugins";
 		private static string _logSubFolder = "logs";
