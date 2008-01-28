2132b6140e6227b0344ab6a8bc2f000565fc5ef6
norman <devnull@localhost>
2008-01-28 09:46:45 +0000

    Requirement: #700 - Turns out EllipseInteractiveGraphic.GetClosestPoint was implemented incorrectly; I had mistakenly thought the "t" in the parametric equation corresponded to the angle between the "radius" and the x-axis. - Proper solution involves solving a quadratic.

--- ImageViewer/InteractiveGraphics/EllipseInteractiveGraphic.cs
+++ ImageViewer/InteractiveGraphics/EllipseInteractiveGraphic.cs
@@ -4,6 +4,7 @@ using System.Drawing.Drawing2D;
 using System.Text;
 using ClearCanvas.ImageViewer.Graphics;
 using System.Drawing;
+using ClearCanvas.ImageViewer.Mathematics;
 
 namespace ClearCanvas.ImageViewer.InteractiveGraphics
 {
@@ -47,36 +48,79 @@ namespace ClearCanvas.ImageViewer.InteractiveGraphics
 
 		public override PointF GetClosestPoint(PointF point)
 		{
+			// Semi major/minor axes
 			float a = this.Width/2;
 			float b = this.Height/2;
 
-			float centerX = this.Left + a;
-			float centerY = this.Top + b;
+			// Center of ellipse
+			float x1 = this.Left + a;
+			float y1 = this.Top + b;
 
-			float rise = point.Y - centerY;
-			float run = point.X - centerX;
-			double theta;
+			PointF center = new PointF(x1,y1);
 
-			if (run != 0)
-			{
-				theta = Math.Atan(rise/run);
-
-				if (run < 0)
-					theta += Math.PI;
-
-			}
-			else
-			{
-				if (rise >= 0)
-					theta = Math.PI / 2;
-				else
-					theta = -Math.PI / 2;
-			}
-
-			float x = centerX + a*(float)Math.Cos(theta);
-			float y = centerY + b*(float)Math.Sin(theta);
-
-			return new PointF(x,y);
+			return IntersectEllipseAndLine(a, b, center, point);
 		}
+
+		/// <summary>
+		/// Finds the intersection between an ellipse and a line that starts at the
+		/// center of the ellipse and ends at an aribtrary point.
+		/// </summary>
+		/// <param name="a"></param>
+		/// <param name="b"></param>
+		/// <param name="center"></param>
+		/// <param name="point"></param>
+		/// <returns></returns>
+		internal static PointF IntersectEllipseAndLine(float a, float b, PointF center, PointF point)
+		{
+			// Test point
+			float x2 = point.X;
+			float y2 = point.Y;
+
+			float a2 = a * a;
+			float b2 = b * b;
+
+			// Center of ellipse
+			float x1 = center.X;
+			float y1 = center.Y;
+
+			if (a == 0 || b == 0)
+				return new PointF(x1, y1);
+
+			if (x2 == x1)
+			{
+				if (y2 > y1)
+					return new PointF(x1, y1 + b);
+				else
+					return new PointF(x1, y1 - b);
+			}
+
+			// Rise and run
+			float dy = y2 - y1;
+			float dx = x2 - x1;
+
+			// Slope
+			float m = dy / dx;
+			float m2 = m * m;
+
+			// y-intercept
+			float c = y1 - m * x1;
+			float c2 = c * c;
+
+			float A = (m2 / b2) + (1 / a2);
+			float B = 2 * ((m / b2) * (c - y1) - (x1 / a2));
+			float C = (c2 - (2 * c * y1) + (y1 * y1)) / b2 + ((x1 * x1) / a2) - 1;
+
+			float x;
+
+			if (x2 >= x1)
+				x = (-B + (float)Math.Sqrt(B * B - 4 * A * C)) / (2 * A);
+			else
+				x = (-B - (float)Math.Sqrt(B * B - 4 * A * C)) / (2 * A);
+
+			float y = m * x + c;
+
+			return new PointF(x, y);
+		}
+
 	}
 }
