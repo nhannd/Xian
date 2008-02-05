c3d0df4ac2895c74f8f1c27a2411eb6d80b25f62
rob <devnull@localhost>
2008-02-05 20:56:30 +0000

    Requirement: #1161 - Create & implement Structured Report
    - Changed DHTML tab control to work with IE6.  Specifically, modified CSS to not use multiple class selectors;  apparently it is a known issue that IE6 only respects the last class name specified.

--- Ris/web/js/tabs.js
+++ Ris/web/js/tabs.js
@@ -1,75 +1,78 @@
 function TabControl(tabControlId)
 {
-   this.currentTabPage = null;
-   this.tabControlId = tabControlId;
+	this.currentTabPage = null;
+	this.tabControlId = tabControlId;
 
-   var labelElements = document.getElementById(this.tabControlId).getElementsByTagName("LABEL");
-   for(var i=0; i<labelElements.length; i++)
-   {
-      if(labelElements[i].getAttribute("className") == "Tab")
-      {
-         pageElement = document.getElementById(labelElements[i].getAttribute("htmlFor"));
-         page = new TabPage(this, labelElements[i], pageElement);
-         if(this.currentTabPage == null)
-         {
-            page.Show();
-         }
-      }
-   }
+	var labelElements = document.getElementById(this.tabControlId).getElementsByTagName("LABEL");
+	for(var i=0; i<labelElements.length; i++)
+	{
+		if(labelElements[i].getAttribute(classAttribute) == "Tab")
+		{
+			pageElement = document.getElementById(labelElements[i].getAttribute(forAttribute));
+			page = new TabPage(this, labelElements[i], pageElement);
+			if(this.currentTabPage == null)
+			{
+				page.Show();
+			}
+		}
+	}
 }
 
 TabControl.prototype.HideCurrent = function ()
 {
-   if(this.currentTabPage != null)
-   {
-      this.currentTabPage.Hide();
-   }
+	if(this.currentTabPage != null)
+	{
+		this.currentTabPage.Hide();
+	}
 };
 
 function TabPage(tabControl, tabElement, pageElement)
 {
-   this.tabControl = tabControl;
-   this.tabElement = tabElement;
-   this.pageElement = pageElement;
-   
-   var oThis = this;
-   this.tabElement.onclick = function() { oThis.Show() };
+	this.tabControl = tabControl;
+	this.tabElement = tabElement;
+	this.pageElement = pageElement;
+	
+	var oThis = this;
+	oThis.tabElement.onclick = function() { oThis.Show() };
 }
 
 TabPage.prototype.PageClassName = "TabPage";
-TabPage.prototype.ActivePageClassName = "TabPage Active";
+TabPage.prototype.ActivePageClassName = "TabPage ActiveTabPage";
 TabPage.prototype.TabClassName = "Tab";
-TabPage.prototype.ActiveTabClassName = "Tab Active";
+TabPage.prototype.ActiveTabClassName = "Tab ActiveTab";
 
 TabPage.prototype.Show = function()
 {
-   if(this.tabControl.currentTabPage == this) return;
+	if(this.tabControl.currentTabPage == this) return;
 
-   this.tabControl.HideCurrent();
-   
-   this.pageElement.className = this.ActivePageClassName;
-   this.tabElement.className = this.ActiveTabClassName;
+	this.tabControl.HideCurrent();
+	
+	this.pageElement.className = this.ActivePageClassName;
+	this.tabElement.className = this.ActiveTabClassName;
 
-   this.tabControl.currentTabPage = this;
+	this.tabControl.currentTabPage = this;
 };
 
 TabPage.prototype.Hide = function()
 {
-   this.pageElement.className = this.PageClassName;
-   this.tabElement.className = this.TabClassName;
+	this.pageElement.className = this.PageClassName;
+	this.tabElement.className = this.TabClassName;
 };
 
 function initTabs()
 {
-   var divs = document.getElementsByTagName("DIV");
-   for(var i=0; i < divs.length; i++)
-   {
-      if(divs[i].getAttribute("className") == "TabControl")
-      {
-         new TabControl(divs[i].getAttribute("ID"));
-      }
-   }
-}         
+	var divs = document.getElementsByTagName("DIV");
+	for(var i=0; i < divs.length; i++)
+	{
+		if(divs[i].getAttribute(classAttribute) == "TabControl")
+		{
+			new TabControl(divs[i].getAttribute("ID"));
+		}
+	}
+}
+
+var classAttribute = "class";
+var forAttribute = "for";
 
 if ( typeof window.addEventListener != "undefined" )
 {
@@ -79,4 +82,6 @@ if ( typeof window.addEventListener != "undefined" )
 else if ( typeof window.attachEvent != "undefined" ) 
 {
   window.attachEvent( "onload", initTabs );
+  classAttribute = "className";
+  forAttribute = "htmlFor";
 }
