1c6a774c9599f2fe27811950836df9e465061315
jonathan <devnull@localhost>
2008-02-05 20:11:57 +0000

    Requirement: #1535, #1539, #1540 extended properties for staff and Practitioners
    More work on UHN StaffEditorAdditionalInfoPageComponent.  Could still use cosmetic improvements, but those can always be done later.  Right now it has only a single field, to capture the HMRI number for a staff person.  There is some validation to make sure what is entered is actually a number.  The validation icon shows up on the wrong side, but this is captured as a more general defect (#1596).
    
    Also, added javascript method Ris.setModified, which allows the page to inform the component that data has been modified, and improved the documentation in the ris.js file.

--- Ris/web/js/ris.js
+++ Ris/web/js/ris.js
@@ -1,5 +1,6 @@
 /*
-    Ris
+    Ris object
+	The Ris object is a singleton object representing the RIS client desktop application.
 */
 
 if(window.external)
@@ -15,6 +16,7 @@ if(window.external)
             return value;
         },
         
+		// equivalent of the window.confirm function, but routes the message through the RIS client desktop application
         // message - confirmation message to display
         // type - a string containing either "YesNo" or "OkCancel" (not case-sensitive)
         // returns true if the user pressed Yes or OK, false otherwise
@@ -23,16 +25,27 @@ if(window.external)
             return window.external.Confirm(message || "", type || "OkCancel");
         },
         
+		// equivalent of the window.alert function, but routes the message through the RIS client desktop application
         alert: function(message)
         {
             window.external.Alert(message || "");
         },
+		
+		// informs the application that the user has modified data on the page
+		// (sets the ApplicationComponent.Modified flag to the specified value)
+		setModified: function(modified)
+		{
+			window.external.Modified = modified;
+		},
         
 		getActionHtml: function(labelSearch, actionLabel)
 		{
 			return window.external.GetActionHtml(labelSearch, actionLabel);
 		},
 		
+		// attempts to resolve the specified query string to a staff person,
+		// returning a Staff object if successful, otherwise null.
+		// note: this method may interact with the user via a dialog box if the query is not specific enough
         resolveStaffName: function(query)
         {
             var staffSummary = JSML.parse(window.external.ResolveStaffName(query || ""));
@@ -45,66 +58,70 @@ if(window.external)
                 staffSummary.StaffType.Value);
         },
         
+		// gets the application format string for showing date only
+		// this format string is based on the .NET DateTime class format specifiers
         getDateFormat: function()
         {
             return window.external.DateFormat;
         },
         
+		// gets the application format string for showing time only
+		// this format string is based on the .NET DateTime class format specifiers
         getTimeFormat: function()
         {
             return window.external.TimeFormat;
         },
         
+		// gets the application format string for showing both date and time
+		// this format string is based on the .NET DateTime class format specifiers
         getDateTimeFormat: function()
         {
             return window.external.DateTimeFormat;
         },
         
-        getTag: function(tag)
-        {
-            return window.external.GetTag(tag);
-        },
-        
-        setTag: function(tag, data)
-        {
-            window.external.SetTag(tag, data);
-        },
-		
+		// formats the specified date object to a string showing only the date
 		formatDate: function(date)
 		{
 			return date ? window.external.FormatDate(date.toISOString()) : "";
 		},
 
+		// formats the specified date object to a string showing only the time
 		formatTime: function(date)
 		{
 			return date ? window.external.FormatTime(date.toISOString()) : "";
 		},
 
+		// formats the specified date object to a string showing both date and time
 		formatDateTime: function(date)
 		{
 			return date ? window.external.FormatDateTime(date.toISOString()) : "";
 		},
 
+		// formats the specified Address object
 		formatAddress: function(address)
 		{
 			return address ? window.external.FormatAddress(JSML.create(address, "Address")) : "";
 		},
 
+		// formats the specified HealthcardNumber object
 		formatHealthcard: function(healthcard)
 		{
 			return healthcard ? window.external.FormatHealthcard(JSML.create(healthcard, "Healthcard")) : "";
 		},
 
+		// formats the specified MRN object
 		formatMrn: function(mrn)
 		{
 			return mrn ? window.external.FormatMrn(JSML.create(mrn, "Mrn")) : "";
 		},
 
+		// formats the specified personName object
 		formatPersonName: function(personName)
 		{
 			return personName ? window.external.FormatPersonName(JSML.create(personName, "PersonName")) : "";
 		},
 		
+		// formats the specified telephone object
 		formatTelephone: function(telephone)
 		{
 			return telephone ? window.external.FormatTelephone(JSML.create(telephone, "Telephone")) : "";
@@ -133,6 +150,7 @@ if(window.external)
 		    return proxy;
 		},
 		
+		// convenience method to get the RIS BrowsePatientDataService, which provides access to a patient's data
 		getPatientDataService: function()
 		{
 		    return this.getService("ClearCanvas.Ris.Application.Common.BrowsePatientData.IBrowsePatientDataService, ClearCanvas.Ris.Application.Common");
@@ -140,12 +158,34 @@ if(window.external)
 		
 		// gets the healthcare context in which the page is running
 		// the healthcare context is an object that contains all entity-refs, etc., that the page
-		// can use as keys to load data
+		// can use as keys to load data.
+		// the healthcare context object is defined by the component that is hosting the page
+		// check that component's documentation
 		getHealthcareContext: function()
 		{
 		    return JSML.parse(window.external.GetHealthcareContext());
-		}
-    };
+		},
+		
+		// returns the value associated with the specified tag
+		// the hosting component may allow the page to store abritrary key-value pairs,
+		// in which case this function retrieves the value associated with the tag (key)
+		// the hosting component is responsible for determining the scope of the stored values -
+		// typically they are relative to the HealthcareContext (see getHealthcareContext).
+        getTag: function(tag)
+        {
+            return window.external.GetTag(tag);
+        },
+        
+		// sets the value associated with the specified tag
+		// the hosting component may allow the page to store abritrary key-value pairs,
+		// in which case this function stores the data (value) associated with the tag (key)
+		// the hosting component is responsible for determining the scope of the stored values -
+		// typically they are relative to the HealthcareContext (see getHealthcareContext).
+        setTag: function(tag, data)
+        {
+            window.external.SetTag(tag, data);
+        }
+   };
     
     // install global JSML parser filter
     JSML.setParseFilter(Ris._jsmlParserFilter);
