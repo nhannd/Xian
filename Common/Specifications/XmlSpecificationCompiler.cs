1870f67b4589492bf209141cccfb3b6223b9b622
steve <devnull@localhost>
2008-03-03 22:07:37 +0000

    Requirement: #1545
    Accidentally left logging of the specification schema on.  Disabled this.

--- Common/Specifications/XmlSpecificationCompiler.cs
+++ Common/Specifications/XmlSpecificationCompiler.cs
@@ -289,9 +289,9 @@ namespace ClearCanvas.Common.Specifications
                 compiledSchema = schema;
             }
 
-            StringWriter sw = new StringWriter();
-            compiledSchema.Write(sw);
-            Platform.Log(LogLevel.Info, sw);
+            //StringWriter sw = new StringWriter();
+            //compiledSchema.Write(sw);
+            //Platform.Log(LogLevel.Info, sw);
 
             return compiledSchema;
         }
