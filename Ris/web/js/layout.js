ce1a1ddfdb134ef71f5abcadc66672763742ba0f
clinton <devnull@localhost>
2008-03-14 21:42:26 +0000

    Ticket: #1546 - fix the inconsistent scaling of font sizes across the different pages.
    This is my first stab at it. Try it out and give me feedback.
    
    Approach: Forget about scaling relative to the "viewport" or width of the browser, since we want consistency of font-size which ever screen we might be looking at. Instead, make all the font sizing relative to the current default font size of the browser.
    
    - Effectively get rid of scaleWidth(), by making it a no-op function.
    - Changed the default font family to Arial, since Verdana has very large type. Used http://www.ampsoft.net/webdesign-l/WindowsMacFonts.html as a reference for browser fonts that are commonly available.
    - Change em-type font-sizing to percentages
    - Scale down the font size for the application to 80% of default size
    - Tweak the patient name and MRN classes, by creating two new classes specifically for those headings (left current pageheading and pageheading2 untouched)

--- Ris/web/js/layout.js
+++ Ris/web/js/layout.js
@@ -3,29 +3,5 @@ var _IE = document.all;
 
 function scaleWidth(numColumns)
 {
-    // if the numColumns arg was not provided, then default to 1
-    numColumns = numColumns || 1;
-
-/*
- * This is the pattern of the old function, which tries to format lines of
- * text into an optimal width, for readability. It doesn't apply to us, since
- * we use tables almost everywhare, but it'll have to do for now.
- *
- */
-	var windowWidth = document.body.clientWidth;
-	var minimumTextHeight = "9px";
-	var optimalLineLength = (numColumns*70) + "em";
-	var extraAccounting = "0em";
-	var optimalSize = windowWidth / (parseInt(optimalLineLength) + parseInt(extraAccounting));
-
-	if (optimalSize >= parseInt(minimumTextHeight))
-	{
-		document.body.style.fontSize = optimalSize + "px";
-	}
-	else
-	{
-		document.body.style.fontSize = parseInt(minimumTextHeight) + "px";
-	}
-	
 	return true;
 }
