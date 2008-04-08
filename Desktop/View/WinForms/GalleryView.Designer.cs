0daba430835ae8a444a1d0ae1a7ebdf6115f62ae
stewart <devnull@localhost>
2008-04-08 19:59:32 +0000

    Requirement: #691 - Add an image clipboard - Fixed a crash when the clipboard is opened, then closed, then an image is added to it

--- Desktop/View/WinForms/GalleryView.Designer.cs
+++ Desktop/View/WinForms/GalleryView.Designer.cs
@@ -13,9 +13,13 @@ namespace ClearCanvas.Desktop.View.WinForms
 		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
 		protected override void Dispose(bool disposing)
 		{
-			if (disposing && (components != null))
+			if (disposing)
 			{
-				components.Dispose();
+				if (_gallery != null)
+					_gallery.ListChanged -= OnListChanged;
+				
+				if (components != null)
+					components.Dispose();
 			}
 			base.Dispose(disposing);
 		}
