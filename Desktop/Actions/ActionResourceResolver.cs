a29a1e72ed10b18d6c038ad29039d48a56a30f04
rob <devnull@localhost>
2008-04-30 19:46:00 +0000

    Bug: #1271 - Icons duplicated in every project because no way for Tool to declare additional resource resolvers
    - ActionResourceResolver now adds all base-type assemblies to its list of assemblies in addition to the assembly of the action target (i.e. the Tool) itself.
    - Commit corrects need for duplicate icons, however, not all duplicate icons have been removed.

--- Desktop/Actions/ActionResourceResolver.cs
+++ Desktop/Actions/ActionResourceResolver.cs
@@ -29,27 +29,45 @@
 
 #endregion
 
+using System;
+using System.Collections.Generic;
 using System.Reflection;
 using ClearCanvas.Common.Utilities;
 
 namespace ClearCanvas.Desktop.Actions
 {
-    /// <summary>
-    /// A specialization of the <see cref="ResourceResolver"/> class for use in resolving resources
-    /// related to actions.
-    /// </summary>
-    public class ActionResourceResolver : ResourceResolver
-    {
-        /// <summary>
-        /// Constructs an instance of this object for the specified action target.
-        /// </summary>
-        /// <remarks>
+	/// <summary>
+	/// A specialization of the <see cref="ResourceResolver"/> class for use in resolving resources
+	/// related to actions.
+	/// </summary>
+	public class ActionResourceResolver : ResourceResolver
+	{
+		/// <summary>
+		/// Constructs an instance of this object for the specified action target.
+		/// </summary>
+		/// <remarks>
 		/// The class of the target object determines the primary assembly that will be used to resolve resources.
 		/// </remarks>
-        /// <param name="actionTarget">The action target for which resources will be resolved.</param>
+		/// <param name="actionTarget">The action target for which resources will be resolved.</param>
 		public ActionResourceResolver(object actionTarget)
-            :base(new Assembly[] { actionTarget.GetType().Assembly, typeof(Application).Assembly } )
-        {
-        }
-    }
+			: base(BaseTypeAssemblies(actionTarget.GetType()))
+		{
+		}
+
+		/// <summary>
+		/// Determines the assemblies containing the specified type and all of its base types.
+		/// </summary>
+		/// <param name="actionTargetType"></param>
+		/// <returns>An array of <see cref="Assembly"/>.  Excludes "System".</returns>
+		public static Assembly[] BaseTypeAssemblies(Type actionTargetType)
+		{
+			List<Assembly> assemblies = new List<Assembly>();
+			while (actionTargetType != typeof(object))
+			{
+				assemblies.Add(actionTargetType.Assembly);
+				actionTargetType = actionTargetType.BaseType;
+			}
+			return CollectionUtils.Unique(assemblies).ToArray();
+		}
+	}
 }
