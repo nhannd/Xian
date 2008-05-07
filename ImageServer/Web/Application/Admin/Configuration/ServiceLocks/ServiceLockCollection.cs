642fdbe8e364a8bec3ab510883272bc350f5e6e5
jonathanb <devnull@localhost>
2008-05-07 22:19:23 +0000

    Bug #1894 - Refactored Page Web controls, by globalizing references and creating standard labels.

--- ImageServer/Web/Application/Admin/Configuration/ServiceLocks/ServiceLockCollection.cs
+++
@@ -1,9 +0,0 @@
-using ClearCanvas.ImageServer.Model;
-using ClearCanvas.ImageServer.Web.Application.Common;
-
-namespace ClearCanvas.ImageServer.Web.Application.Admin.Configuration.ServiceLocks
-{
-    public class ServiceLockCollection:EntityCollection<ServiceLock>
-    {
-    }
-}
