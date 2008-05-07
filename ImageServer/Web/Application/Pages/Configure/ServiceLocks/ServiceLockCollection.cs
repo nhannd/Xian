fd7a2358cae669ed5422c34fc20c222f29ed8e77
jonathanb <devnull@localhost>
2008-05-07 22:10:24 +0000

    Added a folder remotely <Filling up space         >

---
+++ ImageServer/Web/Application/Pages/Configure/ServiceLocks/ServiceLockCollection.cs
@@ -0,0 +1,9 @@
+using ClearCanvas.ImageServer.Model;
+using ClearCanvas.ImageServer.Web.Application.Common;
+
+namespace ClearCanvas.ImageServer.Web.Application.Pages.Configure.ServiceLocks
+{
+    public class ServiceLockCollection:EntityCollection<ServiceLock>
+    {
+    }
+}
