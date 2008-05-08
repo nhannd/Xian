6d3e545cee6350c09618eaea4c1ae7c06917a088
jonathan <devnull@localhost>
2008-05-08 15:38:29 +0000

    Requirement #1924: import/export
    1. Added PatientNoteCategoryImex class, and a sample data file patient_note_categories.xml.
    2. Modified sample users.xml so that it includes a default set of authority group assignments - this means whenever we generate a new dev database, we do not need to go and manually add users to authority groups anymore.
    3. Added sample configuration/Configuration1.xml file, which has the authentication settings that we like for development (timeout disabled, 1 char passwords allowed) - so we no longer need to do this manually.
    
    This opens up the possibility of agreeing that the 'sa' password will always be changed to 'c' rather than 'clearcanvas1'.

---
+++ Healthcare/Imex/PatientNoteCategoryImex.cs
@@ -0,0 +1,79 @@
+using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.Text;
+using ClearCanvas.Common;
+using ClearCanvas.Enterprise.Common;
+using ClearCanvas.Enterprise.Core;
+using ClearCanvas.Enterprise.Core.Imex;
+using ClearCanvas.Healthcare.Brokers;
+
+namespace ClearCanvas.Healthcare.Imex
+{
+	[ExtensionOf(typeof(XmlDataImexExtensionPoint))]
+	[ImexDataClass("PatientNoteCategory")]
+	public class PatientNoteCategoryImex : XmlEntityImex<PatientNoteCategory, PatientNoteCategoryImex.PatientNoteCategoryData>
+	{
+		[DataContract]
+		public class PatientNoteCategoryData
+		{
+			[DataMember]
+			public string Name;
+
+			[DataMember]
+			public string Description;
+
+			[DataMember]
+			public string Severity;
+		}
+
+		#region Overrides
+
+		protected override IList<PatientNoteCategory> GetItemsForExport(IReadContext context, int firstRow, int maxRows)
+		{
+			PatientNoteCategorySearchCriteria where = new PatientNoteCategorySearchCriteria();
+			where.Name.SortAsc(0);
+
+			return context.GetBroker<IPatientNoteCategoryBroker>().Find(where, new SearchResultPage(firstRow, maxRows));
+		}
+
+		protected override PatientNoteCategoryData Export(PatientNoteCategory nc, IReadContext context)
+		{
+			PatientNoteCategoryData data = new PatientNoteCategoryData();
+			data.Name = nc.Name;
+			data.Description = nc.Description;
+			data.Severity = nc.Severity.ToString();
+
+			return data;
+		}
+
+		protected override void Import(PatientNoteCategoryData data, IUpdateContext context)
+		{
+			NoteSeverity severity = (NoteSeverity) Enum.Parse(typeof (NoteSeverity), data.Severity);
+			PatientNoteCategory nc = LoadOrCreatePatientNoteCategory(data.Name, severity, context);
+			nc.Description = data.Description;
+		}
+
+		#endregion
+
+		private PatientNoteCategory LoadOrCreatePatientNoteCategory(string name, NoteSeverity severity, IPersistenceContext context)
+		{
+			PatientNoteCategory nc;
+			try
+			{
+				// see if already exists in db
+				PatientNoteCategorySearchCriteria where = new PatientNoteCategorySearchCriteria();
+				where.Name.EqualTo(name);
+				nc = context.GetBroker<IPatientNoteCategoryBroker>().FindOne(where);
+			}
+			catch (EntityNotFoundException)
+			{
+				// create it
+				nc = new PatientNoteCategory(name, null, severity);
+				context.Lock(nc, DirtyState.New);
+			}
+
+			return nc;
+		}
+	}
+}
