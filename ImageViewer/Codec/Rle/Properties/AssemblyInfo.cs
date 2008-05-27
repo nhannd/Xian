d8292794a10bbf6b47ee043eb99356c1dd7b0fce
norman <devnull@localhost>
2008-05-27 19:16:56 +0000

    Requirements: #2016, #2017 - Added RLE, JPEG, JPEG2000 codecs. - Viewer should now allow the opening and display of JPEG and J2K images.  Network related actions such as send/receive of these images does not yet work.

---
+++ ImageViewer/Codec/Rle/Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+[assembly: AssemblyConfiguration("Release")]
+[assembly: AssemblyCompany("ClearCanvas Inc.")]
+[assembly: AssemblyProduct("ClearCanvas Workstation")]
+[assembly: AssemblyVersion("1.1.5170.21517")]
+[assembly: AssemblyFileVersion("1.1.5170.21517")]
+[assembly: ClearCanvas.Common.Plugin]
+[assembly: AssemblyTitle("Rle")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyCopyright("Copyright (c) 2006-2008")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
