36763f42668d45799c745a187f6ce3f328e96194
james <devnull@localhost>
2008-06-24 21:04:47 +0000

    Requirement: #1962 - Create admin screen for diagnostic services
    - Admin screen created for adding and editing diagnostic services
    - Added necessary files

---
+++ Ris/Client/Admin/DiagnosticServiceEditorComponent.cs
@@ -0,0 +1,229 @@
+#region License
+
+// Copyright (c) 2006-2008, ClearCanvas Inc.
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without modification, 
+// are permitted provided that the following conditions are met:
+//
+//    * Redistributions of source code must retain the above copyright notice, 
+//      this list of conditions and the following disclaimer.
+//    * Redistributions in binary form must reproduce the above copyright notice, 
+//      this list of conditions and the following disclaimer in the documentation 
+//      and/or other materials provided with the distribution.
+//    * Neither the name of ClearCanvas Inc. nor the names of its contributors 
+//      may be used to endorse or promote products derived from this software without 
+//      specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" 
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, 
+// THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR 
+// PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR 
+// CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, 
+// OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE 
+// GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) 
+// HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, 
+// STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN 
+// ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY 
+// OF SUCH DAMAGE.
+
+#endregion
+
+using System;
+using System.Collections.Generic;
+using ClearCanvas.Common;
+using ClearCanvas.Desktop;
+using ClearCanvas.Desktop.Tables;
+using ClearCanvas.Enterprise.Common;
+using ClearCanvas.Ris.Application.Common;
+using ClearCanvas.Ris.Application.Common.Admin;
+using ClearCanvas.Ris.Application.Common.Admin.DiagnosticServiceAdmin;
+using ClearCanvas.Desktop.Validation;
+
+namespace ClearCanvas.Ris.Client.Admin
+{
+
+	/// <summary>
+	/// Extension point for views onto <see cref="DiagnosticServiceEditorComponent"/>
+	/// </summary>
+	[ExtensionPoint]
+	public class DiagnosticServiceEditorComponentViewExtensionPoint : ExtensionPoint<IApplicationComponentView>
+	{
+	}
+
+	/// <summary>
+	/// DiagnosticServiceEditorComponent class
+	/// </summary>
+	[AssociateView(typeof(DiagnosticServiceEditorComponentViewExtensionPoint))]
+	public class DiagnosticServiceEditorComponent : ApplicationComponent
+	{
+		private readonly bool _isNew;
+
+		private EntityRef _editedItemEntityRef;
+		private DiagnosticServiceDetail _editedItemDetail;
+		private DiagnosticServiceSummary _editedItemSummary;
+
+		private ProcedureTypeSummaryTable _availableDiagnosticServices;
+		private ProcedureTypeSummaryTable _selectedDiagnosticServices;
+
+		public DiagnosticServiceEditorComponent()
+		{
+			_isNew = true;
+		}
+
+		public DiagnosticServiceEditorComponent(EntityRef entityRef)
+		{
+			_editedItemEntityRef = entityRef;
+			_isNew = false;
+		}
+
+		public override void Start()
+		{
+			_availableDiagnosticServices = new ProcedureTypeSummaryTable();
+			_selectedDiagnosticServices = new ProcedureTypeSummaryTable();
+
+			Platform.GetService<IDiagnosticServiceAdminService>(
+				delegate(IDiagnosticServiceAdminService service)
+				{
+					LoadDiagnosticServiceEditorFormDataResponse formDataResponse =
+						service.LoadDiagnosticServiceEditorFormData(new LoadDiagnosticServiceEditorFormDataRequest());
+					_availableDiagnosticServices.Items.AddRange(formDataResponse.ProcedureTypeChoices);
+
+					if (_isNew)
+					{
+						_editedItemDetail = new DiagnosticServiceDetail();
+					}
+					else
+					{
+						LoadDiagnosticServiceForEditResponse response =
+							service.LoadDiagnosticServiceForEdit(
+								new LoadDiagnosticServiceForEditRequest(_editedItemEntityRef));
+
+						_editedItemDetail = response.DiagnosticService;
+						_selectedDiagnosticServices.Items.AddRange(_editedItemDetail.ProcedureTypes);
+					}
+
+					foreach (ProcedureTypeSummary selectedSummary in _selectedDiagnosticServices.Items)
+					{
+						_availableDiagnosticServices.Items.Remove(selectedSummary);
+					}
+				});
+
+			base.Start();
+		}
+
+		public override void Stop()
+		{
+			base.Stop();
+		}
+
+		public DiagnosticServiceSummary DiagnosticServiceSummary
+		{
+			get { return _editedItemSummary; }
+		}
+
+		#region Presentation Model
+
+		[ValidateNotNull]
+		public string Name
+		{
+			get { return _editedItemDetail.Name; }
+			set
+			{
+				_editedItemDetail.Name = value;
+				this.Modified = true;
+			}
+		}
+
+		[ValidateNotNull]
+		public string ID
+		{
+			get { return _editedItemDetail.Id; }
+			set
+			{
+				_editedItemDetail.Id = value;
+				this.Modified = true;
+			}
+		}
+
+		public bool AcceptEnabled
+		{
+			get { return this.Modified; }
+		}
+
+		public ITable AvailableDiagnosticServices
+		{
+			get { return _availableDiagnosticServices; }
+		}
+
+		public ITable SelectedDiagnosticServices
+		{
+			get { return _selectedDiagnosticServices; }
+		}
+
+		#endregion
+
+		public void Accept()
+		{
+			if (this.HasValidationErrors)
+			{
+				this.ShowValidation(true);
+			}
+			else
+			{
+				try
+				{
+					Platform.GetService<IDiagnosticServiceAdminService>(
+						delegate(IDiagnosticServiceAdminService service)
+						{
+							if (_isNew)
+							{
+								_editedItemDetail.ProcedureTypes.AddRange(_selectedDiagnosticServices.Items);
+								AddDiagnosticServiceResponse response =
+									service.AddDiagnosticService(new AddDiagnosticServiceRequest(_editedItemDetail));
+								_editedItemEntityRef = response.DiagnosticService.DiagnosticServiceRef;
+								_editedItemSummary = response.DiagnosticService;
+							}
+							else
+							{
+								_editedItemDetail.ProcedureTypes.Clear();
+								_editedItemDetail.ProcedureTypes.AddRange(_selectedDiagnosticServices.Items);
+								UpdateDiagnosticServiceResponse response =
+									service.UpdateDiagnosticService(new UpdateDiagnosticServiceRequest(_editedItemDetail));
+								_editedItemEntityRef = response.DiagnosticService.DiagnosticServiceRef;
+								_editedItemSummary = response.DiagnosticService;
+							}
+						});
+
+					this.Exit(ApplicationComponentExitCode.Accepted);
+				}
+				catch (Exception e)
+				{
+					ExceptionHandler.Report(e, SR.ExceptionSaveDiagnosticService, this.Host.DesktopWindow,
+						delegate()
+						{
+							this.ExitCode = ApplicationComponentExitCode.Error;
+							this.Host.Exit();
+						});
+				}
+			}
+		}
+
+		public void Cancel()
+		{
+			this.ExitCode = ApplicationComponentExitCode.None;
+			Host.Exit();
+		}
+
+		public event EventHandler AcceptEnabledChanged
+		{
+			add { this.ModifiedChanged += value; }
+			remove { this.ModifiedChanged -= value; }
+		}
+
+		public void ItemsAddedOrRemoved()
+		{
+			this.Modified = true;
+		}
+	}
+}
---
+++ Ris/Client/Admin/DiagnosticServiceSummaryComponent.cs
@@ -0,0 +1,172 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+using ClearCanvas.Common;
+using ClearCanvas.Desktop;
+using ClearCanvas.Desktop.Actions;
+using ClearCanvas.Desktop.Tools;
+using ClearCanvas.Desktop.Tables;
+using ClearCanvas.Enterprise.Common;
+using ClearCanvas.Ris.Client;
+using ClearCanvas.Ris.Application.Common.Admin.DiagnosticServiceAdmin;
+using ClearCanvas.Ris.Application.Common;
+using ClearCanvas.Common.Utilities;
+
+namespace ClearCanvas.Ris.Client.Admin
+{
+	[MenuAction("launch", "global-menus/Admin/Diagnostic Services", "Launch")]
+	[ActionPermission("launch", ClearCanvas.Ris.Application.Common.AuthorityTokens.Admin.Data.DiagnosticService)]
+	[ExtensionOf(typeof(DesktopToolExtensionPoint))]
+	public class DiagnosticServiceAdminTool : Tool<IDesktopToolContext>
+	{
+		private IWorkspace _workspace;
+
+		public void Launch()
+		{
+			if (_workspace == null)
+			{
+				try
+				{
+					DiagnosticServiceSummaryComponent component = new DiagnosticServiceSummaryComponent();
+
+					_workspace = ApplicationComponent.LaunchAsWorkspace(
+						this.Context.DesktopWindow,
+						component,
+						"Diagnostic Services");
+					_workspace.Closed += delegate { _workspace = null; };
+
+				}
+				catch (Exception e)
+				{
+					// failed to launch component
+					ExceptionHandler.Report(e, this.Context.DesktopWindow);
+				}
+			}
+			else
+			{
+				_workspace.Activate();
+			}
+		}
+	}
+
+	public class DiagnosticServiceSummaryTable : Table<DiagnosticServiceSummary>
+	{
+		private readonly int columnSortIndex = 0;
+
+		public DiagnosticServiceSummaryTable()
+		{
+			this.Columns.Add(new TableColumn<DiagnosticServiceSummary, string>("ID",
+				delegate(DiagnosticServiceSummary rpt) { return rpt.Id; },
+				0.5f));
+
+			this.Columns.Add(new TableColumn<DiagnosticServiceSummary, string>("Name",
+				delegate(DiagnosticServiceSummary rpt) { return rpt.Name; },
+				0.5f));
+
+			this.Sort(new TableSortParams(this.Columns[columnSortIndex], true));
+		}
+	}
+
+	/// <summary>
+	/// DiagnosticServiceSummaryComponent class.
+	/// </summary>
+	public class DiagnosticServiceSummaryComponent : SummaryComponentBase<DiagnosticServiceSummary, DiagnosticServiceSummaryTable>
+	{
+		/// <summary>
+		/// Override this method to perform custom initialization of the action model,
+		/// such as adding permissions or adding custom actions.
+		/// </summary>
+		/// <param name="model"></param>
+		protected override void InitializeActionModel(CrudActionModel model)
+		{
+			base.InitializeActionModel(model);
+
+			model.Add.SetPermissibility(ClearCanvas.Ris.Application.Common.AuthorityTokens.Admin.Data.DiagnosticService);
+			model.Edit.SetPermissibility(ClearCanvas.Ris.Application.Common.AuthorityTokens.Admin.Data.DiagnosticService);
+		}
+
+		/// <summary>
+		/// Gets the list of items to show in the table, according to the specifed first and max items.
+		/// </summary>
+		/// <param name="firstItem"></param>
+		/// <param name="maxItems"></param>
+		/// <returns></returns>
+		protected override IList<DiagnosticServiceSummary> ListItems(int firstItem, int maxItems)
+		{
+			ListDiagnosticServicesResponse listResponse = null;
+			Platform.GetService<IDiagnosticServiceAdminService>(
+				delegate(IDiagnosticServiceAdminService service)
+				{
+					listResponse = service.ListDiagnosticServices(new ListDiagnosticServicesRequest(new SearchResultPage(firstItem, maxItems)));
+				});
+			return listResponse.DiagnosticServices;
+		}
+
+		/// <summary>
+		/// Called to handle the "add" action.
+		/// </summary>
+		/// <param name="addedItems"></param>
+		/// <returns>True if items were added, false otherwise.</returns>
+		protected override bool AddItems(out IList<DiagnosticServiceSummary> addedItems)
+		{
+			addedItems = new List<DiagnosticServiceSummary>();
+			DiagnosticServiceEditorComponent editor = new DiagnosticServiceEditorComponent();
+			ApplicationComponentExitCode exitCode = ApplicationComponent.LaunchAsDialog(
+				this.Host.DesktopWindow, editor, SR.TitleAddDiagnosticService);
+			if (exitCode == ApplicationComponentExitCode.Accepted)
+			{
+				addedItems.Add(editor.DiagnosticServiceSummary);
+				return true;
+			}
+			return false;
+		}
+
+		/// <summary>
+		/// Called to handle the "edit" action.
+		/// </summary>
+		/// <param name="items">A list of items to edit.</param>
+		/// <param name="editedItems">The list of items that were edited.</param>
+		/// <returns>True if items were edited, false otherwise.</returns>
+		protected override bool EditItems(IList<DiagnosticServiceSummary> items, out IList<DiagnosticServiceSummary> editedItems)
+		{
+			editedItems = new List<DiagnosticServiceSummary>();
+			DiagnosticServiceSummary item = CollectionUtils.FirstElement(items);
+
+			DiagnosticServiceEditorComponent editor = new DiagnosticServiceEditorComponent(item.DiagnosticServiceRef);
+			ApplicationComponentExitCode exitCode = LaunchAsDialog(
+				this.Host.DesktopWindow, editor, SR.TitleUpdateDiagnosticService + " - " + "(" + item.Id + ") " + item.Name);
+			if (exitCode == ApplicationComponentExitCode.Accepted)
+			{
+				editedItems.Add(editor.DiagnosticServiceSummary);
+				return true;
+			}
+			return false;
+		}
+
+		/// <summary>
+		/// Called to handle the "delete" action, if supported.
+		/// </summary>
+		/// <param name="items"></param>
+		/// <returns>True if items were deleted, false otherwise.</returns>
+		protected override bool DeleteItems(IList<DiagnosticServiceSummary> items)
+		{
+			throw new NotImplementedException();
+		}
+
+		/// <summary>
+		/// Compares two items to see if they represent the same item.
+		/// </summary>
+		/// <param name="x"></param>
+		/// <param name="y"></param>
+		/// <returns></returns>
+		protected override bool IsSameItem(DiagnosticServiceSummary x, DiagnosticServiceSummary y)
+		{
+			if (x != null && y != null)
+			{
+				return x.DiagnosticServiceRef.Equals(y.DiagnosticServiceRef, true);
+			}
+			return false;
+		}
+	}
+}
