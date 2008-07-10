bb02a7e33c133ceebbc4d65eaf7f62e9273e806b
rob <devnull@localhost>
2008-07-10 20:00:18 +0000

    Bug: #2380 - DHTML formulae should be isolated for testing purposes
    - Creatine calculation moved to separate file and class: CreatinineCalculator.
    - Simplified creatinine results table display.

---
+++ Ris/web/js/creatininecalculator.js
@@ -0,0 +1,32 @@
+var CreatinineCalculator = 
+{
+	_standardFactors :
+	{
+		"Male": {a:140, b:1.2, c:1.0}, 
+		"Female": {a:140, b:1.2, c:0.85},
+		"Unknown": {a:0, b:0, c:0},
+		"Other": {a:0, b:0, c:0}
+	},
+	
+	calculateClearance : function(age, weight, sex, level)
+	{
+		age = parseFloat(age);
+		weight = parseFloat(weight);
+		level = parseFloat(level);
+		
+		if (["Male", "Female", "Unknown", "Other"].indexOf(sex) == -1)
+		{
+			throw "Invalid sex";
+		}
+		if (weight <= 0.0) 
+		{
+			throw "Weight must be greater than 0";
+		}
+		if (level <= 0.0) 
+		{
+			throw "Level must be greater than 0";
+		}
+		
+		return ((this._standardFactors[sex]["a"] - age) * this._standardFactors[sex]["b"] * weight) / level * this._standardFactors[sex]["c"];
+	}
+}
