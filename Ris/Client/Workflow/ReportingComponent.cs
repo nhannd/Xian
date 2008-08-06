02822313fa3be145debb278528992251fabd5a8a
jonathan <devnull@localhost>
2008-08-06 16:50:47 +0000

    Bug: #2543 - User should not be able to verify if the report content is empty
    Reverting changes made to this file in Rev [6505], as these changes are not needed and are potentially damaging.  They should not have been committed.

--- Ris/Client/Workflow/ReportingComponent.cs
+++ Ris/Client/Workflow/ReportingComponent.cs
@@ -853,7 +853,7 @@ namespace ClearCanvas.Ris.Client.Workflow
 			get
 			{
 				 return (_canCompleteInterpretationAndVerify || _canCompleteVerification)
-					 && Thread.CurrentPrincipal.IsInRole(ClearCanvas.Ris.Application.Common.AuthorityTokens.Workflow.Report.Verify) && ReportContent != null;
+					 && Thread.CurrentPrincipal.IsInRole(ClearCanvas.Ris.Application.Common.AuthorityTokens.Workflow.Report.Verify);
 			}
 		}
 
@@ -876,7 +876,7 @@ namespace ClearCanvas.Ris.Client.Workflow
 
 		private bool CanSaveReport
 		{
-			get { return _canSaveReport && ReportContent != null; }
+			get { return _canSaveReport; }
 		}
 
 
