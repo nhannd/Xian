e133ecf426e92c874fd28862e0f66be935ff7da9
jonathan <devnull@localhost>
2008-08-12 17:20:39 +0000

    Bug: #2593 JsmlSerializer should handle properties as well as fields
    Completed unit tests for ObjectWalker.

--- Common/Utilities/Tests/ObjectWalkerTests.cs
+++ Common/Utilities/Tests/ObjectWalkerTests.cs
@@ -36,6 +36,7 @@
 using System;
 using System.Collections.Generic;
 using NUnit.Framework;
+using System.Reflection;
 
 namespace ClearCanvas.Common.Utilities.Tests
 {
@@ -64,6 +65,14 @@ namespace ClearCanvas.Common.Utilities.Tests
 				set { _publicPropertyField = value; }
 			}
 
+			public bool CheckConsistency(bool b, string s, int i, object o)
+			{
+				return PrivateField == b
+				       && PublicField == s
+				       && _privatePropertyField == i
+				       && _publicPropertyField == o;
+			}
+
 		}
 
 
@@ -74,7 +83,7 @@ namespace ClearCanvas.Common.Utilities.Tests
 		}
 
 		[Test]
-		public void TestPrivatePublic()
+		public void TestWalkPublicProperties()
 		{
 			ObjectWalker walker = new ObjectWalker();
 
@@ -84,9 +93,16 @@ namespace ClearCanvas.Common.Utilities.Tests
 			walker.IncludePublicFields = false;
 			walker.IncludePublicProperties = true;
 
-			List<IObjectMemberContext> members = new List<IObjectMemberContext>(walker.Walk(typeof (Foo)));
+			List<IObjectMemberContext> members = new List<IObjectMemberContext>(walker.Walk(new Foo()));
 			Assert.AreEqual(1, members.Count);
 			Assert.AreEqual("PublicProperty", members[0].Member.Name);
+			Assert.AreEqual(Foo.SomeObject, members[0].MemberValue);
+		}
+
+		[Test]
+		public void TestWalkPublicFields()
+		{
+			ObjectWalker walker = new ObjectWalker();
 
 			// public fields
 			walker.IncludeNonPublicFields = false;
@@ -94,9 +110,16 @@ namespace ClearCanvas.Common.Utilities.Tests
 			walker.IncludePublicFields = true;
 			walker.IncludePublicProperties = false;
 
-			members = new List<IObjectMemberContext>(walker.Walk(typeof(Foo)));
+			List<IObjectMemberContext> members = new List<IObjectMemberContext>(walker.Walk(new Foo()));
 			Assert.AreEqual(1, members.Count);
 			Assert.AreEqual("PublicField", members[0].Member.Name);
+			Assert.AreEqual("hello", members[0].MemberValue);
+		}
+
+		[Test]
+		public void TestWalkPrivateProperties()
+		{
+			ObjectWalker walker = new ObjectWalker();
 
 			// private property
 			walker.IncludeNonPublicFields = false;
@@ -104,9 +127,16 @@ namespace ClearCanvas.Common.Utilities.Tests
 			walker.IncludePublicFields = false;
 			walker.IncludePublicProperties = false;
 
-			members = new List<IObjectMemberContext>(walker.Walk(typeof(Foo)));
+			List<IObjectMemberContext> members = new List<IObjectMemberContext>(walker.Walk(new Foo()));
 			Assert.AreEqual(1, members.Count);
 			Assert.AreEqual("PrivateProperty", members[0].Member.Name);
+			Assert.AreEqual(10, members[0].MemberValue);
+		}
+
+		[Test]
+		public void TestWalkPrivateFields()
+		{
+			ObjectWalker walker = new ObjectWalker();
 
 			// private field
 			walker.IncludeNonPublicFields = true;
@@ -114,18 +144,97 @@ namespace ClearCanvas.Common.Utilities.Tests
 			walker.IncludePublicFields = false;
 			walker.IncludePublicProperties = false;
 
-			members = new List<IObjectMemberContext>(walker.Walk(typeof(Foo)));
-			Assert.AreEqual(1, members.Count);
-			Assert.AreEqual("PrivateField", members[0].Member.Name);
+			List<IObjectMemberContext> members = new List<IObjectMemberContext>(walker.Walk(new Foo()));
+			// there will be 3 private fields, becausing of the backing fields for the properties
+			Assert.AreEqual(3, members.Count);
+			foreach (IObjectMemberContext member in members)
+			{
+				if(member.Member.Name == "PrivateField")
+					Assert.AreEqual(true, member.MemberValue);
+			}
+			
+		}
 
-			// all
+		[Test]
+		public void TestWalkAllMembers()
+		{
+			ObjectWalker walker = new ObjectWalker();
+
+			// all members
 			walker.IncludeNonPublicFields = true;
 			walker.IncludeNonPublicProperties = true;
 			walker.IncludePublicFields = true;
 			walker.IncludePublicProperties = true;
 
-			members = new List<IObjectMemberContext>(walker.Walk(typeof(Foo)));
+			List<IObjectMemberContext> members = new List<IObjectMemberContext>(walker.Walk(new Foo()));
+			// there will be 6 members, becausing of the backing fields for the properties
+			Assert.AreEqual(6, members.Count);
+		}
+
+		[Test]
+		public void TestWalkAllMembersWithFilter()
+		{
+			// exclude members starting with an underscore
+			ObjectWalker walker = new ObjectWalker(delegate(MemberInfo member) { return !member.Name.StartsWith("_"); });
+
+			// all members
+			walker.IncludeNonPublicFields = true;
+			walker.IncludeNonPublicProperties = true;
+			walker.IncludePublicFields = true;
+			walker.IncludePublicProperties = true;
+
+			List<IObjectMemberContext> members = new List<IObjectMemberContext>(walker.Walk(new Foo()));
 			Assert.AreEqual(4, members.Count);
+		}
+
+		[Test]
+		public void TestWalkType()
+		{
+			// exclude members starting with an underscore
+			ObjectWalker walker = new ObjectWalker();
+
+			// all members
+			walker.IncludeNonPublicFields = true;
+			walker.IncludeNonPublicProperties = true;
+			walker.IncludePublicFields = true;
+			walker.IncludePublicProperties = true;
+
+			// in this case we walk typeof(Foo) rather than an instance of Foo
+			List<IObjectMemberContext> members = new List<IObjectMemberContext>(walker.Walk(typeof(Foo)));
+			// there will be 6 members, becausing of the backing fields for the properties
+			Assert.AreEqual(6, members.Count);
+		}
+
+		[Test]
+		public void TestMutation()
+		{
+			// exclude members starting with an underscore
+			ObjectWalker walker = new ObjectWalker(delegate(MemberInfo member) { return !member.Name.StartsWith("_"); });
+
+			// all members
+			walker.IncludeNonPublicFields = true;
+			walker.IncludeNonPublicProperties = true;
+			walker.IncludePublicFields = true;
+			walker.IncludePublicProperties = true;
+
+			Foo foo = new Foo();
+			Foo foo2 = new Foo();
+
+			// in this test we change all the values of foo via the IObjectMemberContext
+			foreach (IObjectMemberContext context in walker.Walk(foo))
+			{
+				if (context.MemberType == typeof(bool))
+					context.MemberValue = false;
+				if (context.MemberType == typeof(string))
+					context.MemberValue = "goodbye";
+				if (context.MemberType == typeof(int))
+					context.MemberValue = 1;
+				if (context.MemberType == typeof(object))
+					context.MemberValue = foo2;
+
+			}
+
+			Assert.AreEqual(true, foo.CheckConsistency(false, "goodbye", 1, foo2));
 		}
 
 	}
