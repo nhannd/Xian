c3721d83e262e20e69986d78418f28fb71861661
stewart <devnull@localhost>
2008-10-08 04:09:40 +0000

    Requirement: #2942 - Support ability to programmatically open studies using streaming - Added code to test the in-proc service clients.

--- ImageViewer/DesktopServices/Automation/ViewerAutomationTool.cs
+++ ImageViewer/DesktopServices/Automation/ViewerAutomationTool.cs
@@ -1,7 +1,9 @@
+using System;
 using System.ServiceModel;
 using System.ServiceModel.Description;
 using ClearCanvas.Common;
 using ClearCanvas.Desktop;
+using ClearCanvas.Desktop.Actions;
 using ClearCanvas.ImageViewer.Services.Automation;
 using System.Threading;
 
@@ -16,6 +18,8 @@ namespace ClearCanvas.ImageViewer.DesktopServices.Automation
 	/// automatically marshals all service request over to the thread on which the service host was
 	/// started.
 	/// </remarks>
+
+	//[ButtonAction("test", "global-menus/Test/Test Automation Client", "TestClient")]
 	[ExtensionOf(typeof(DesktopToolExtensionPoint))]
 	public class ViewerAutomationTool : DesktopHostTool
 	{
@@ -34,6 +38,37 @@ namespace ClearCanvas.ImageViewer.DesktopServices.Automation
 				endpoint.Binding.Namespace = AutomationNamespace.Value;
 
 			return host;
+		}
+
+		private void TestClient()
+		{
+			SynchronizationContext context = SynchronizationContext.Current;
+
+			//Have to test client on another thread, otherwise there is a deadlock b/c the service is hosted on the main thread.
+			ThreadPool.QueueUserWorkItem(delegate
+			                                {
+												try
+												{
+													using (ViewerAutomationServiceClient client = new ViewerAutomationServiceClient())
+													{
+														client.GetActiveViewerSessions();
+													}
+
+													context.Post(delegate
+											             			{
+											             				base.Context.DesktopWindow.ShowMessageBox("Success!", MessageBoxActions.Ok);
+											             			}, null);
+												}
+												catch (Exception e)
+												{
+													context.Post(delegate
+													{
+														base.Context.DesktopWindow.ShowMessageBox(e.Message, MessageBoxActions.Ok);
+													}, null);
+
+												}
+											});
+
 		}
 	}
 }
--- ImageViewer/DesktopServices/StudyLocator/StudyLocatorTool.cs
+++ ImageViewer/DesktopServices/StudyLocator/StudyLocatorTool.cs
@@ -4,12 +4,15 @@ using ClearCanvas.Common;
 using ClearCanvas.Desktop;
 using ClearCanvas.ImageViewer.Services.StudyLocator;
 using System.Threading;
+using ClearCanvas.Desktop.Actions;
+using System;
 
 namespace ClearCanvas.ImageViewer.DesktopServices.StudyLocator
 {
 	/// <summary>
 	/// For internal use only.
 	/// </summary>
+	//[ButtonAction("test", "global-menus/Test/Test Study Locator Client", "TestClient")]
 	[ExtensionOf(typeof(DesktopToolExtensionPoint))]
 	public class StudyLocatorTool : DesktopHostTool
 	{
@@ -28,6 +31,23 @@ namespace ClearCanvas.ImageViewer.DesktopServices.StudyLocator
 				endpoint.Binding.Namespace = StudyLocatorNamespace.Value;
 
 			return host;
+		}
+
+		private void TestClient()
+		{
+			try
+			{
+				using (StudyLocatorServiceClient client = new StudyLocatorServiceClient())
+				{
+					client.FindByAccessionNumber("test");
+				}
+
+				base.Context.DesktopWindow.ShowMessageBox("Success!", MessageBoxActions.Ok);
+			}
+			catch(Exception e)
+			{
+				base.Context.DesktopWindow.ShowMessageBox(e.Message, MessageBoxActions.Ok);
+			}
 		}
 	}
 }
