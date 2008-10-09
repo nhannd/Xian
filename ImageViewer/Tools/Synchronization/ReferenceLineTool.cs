603cbca7aa09cbd7ecb6ea615f7d2c3b77de7ceb
jasper <devnull@localhost>
2008-10-09 20:03:13 +0000

    Bug: #3008 - reference lines showing unexpectedly in completely coincident images * increased tolerance to 10 degrees

--- ImageViewer/Tools/Synchronization/ReferenceLineTool.cs
+++ ImageViewer/Tools/Synchronization/ReferenceLineTool.cs
@@ -240,7 +240,7 @@ namespace ClearCanvas.ImageViewer.Tools.Synchronization
 		private static ReferenceLine GetReferenceLine(DicomImagePlane referenceImagePlane, DicomImagePlane targetImagePlane)
 		{
 			// if planes are parallel within tolerance, then they do not intersect and thus no reference lines should be shown
-			const float parallelTolerance = (float)(Math.PI / 36); // 5 degrees of tolerance
+			const float parallelTolerance = (float)(Math.PI / 18); // 10 degrees of tolerance
 			if (referenceImagePlane.IsParallelTo(targetImagePlane, parallelTolerance))
 				return null;
 
