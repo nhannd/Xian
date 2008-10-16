f9191d6d4569062b7efd2dc2d4638820f5891e6f
thanh <devnull@localhost>
2008-10-16 15:11:55 +0000

    Defect #3046: Exception when comparing study tags for study integrity queue
    - Fixed the bug in the If statement which does not catch the case where first string is null but the 2nd string isn't.  Changes in the caller code may have masked the problem.

--- ImageServer/Common/Utilities/StringUtils.cs
+++ ImageServer/Common/Utilities/StringUtils.cs
@@ -6,12 +6,20 @@ namespace ClearCanvas.ImageServer.Common.Utilities
 {
     public static class StringUtils
     {
+        /// <summary>
+        /// Compares two strings, treat Null and Empty being the same.
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        /// <returns></returns>
         public static bool AreEqual(string x, string y)
         {
-            if (String.IsNullOrEmpty(x) && String.IsNullOrEmpty(y))
-                return true;
-
-            return x.Equals(y);
+            if (String.IsNullOrEmpty(x))
+            {
+                return String.IsNullOrEmpty(y);   
+            }
+            else
+                return x.Equals(y);
         }
     }
 }
