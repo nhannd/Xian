df8cfb569c0b48593a4583ceaebe956d777edac8
steve <devnull@localhost>
2008-12-11 19:00:08 +0000

    Ticket: #2078
    Added shell for QuerySCU, this will be used eventually in the sample app.

---
+++ Dicom/Samples/QuerySCU.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClearCanvas.Dicom.Samples
+{
+	public class QuerySCU
+	{
+		public void StudySearch()
+		{
+			
+		}
+	}
+}
