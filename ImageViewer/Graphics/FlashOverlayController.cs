7403c2d550bc2f46ac8b69686949788659a9eb0d
jasper <devnull@localhost>
2009-02-05 17:11:16 +0000

    Requirement: #3769 - Provide visual feedback when a key image is created * Modified flash time to 300ms, up from 150ms.

--- ImageViewer/Graphics/FlashOverlayController.cs
+++ ImageViewer/Graphics/FlashOverlayController.cs
@@ -21,7 +21,7 @@ namespace ClearCanvas.ImageViewer.Graphics
 	{
 		private readonly byte[] _pixelData;
 		private readonly int _rows, _columns;
-		private int _flashSpeed = 150;
+		private int _flashSpeed = 300;
 
 		private SynchronizationContext _syncContext;
 
