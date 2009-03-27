a2587598e01aba80f55dd944ecc9a475d2e4b32c
stewart <devnull@localhost>
2009-03-27 01:09:49 +0000

    Requirement: #3909 - Provide way to unify patient info in context menu and thumbnails - A couple of cleanup items. - Managed to get JScript to compile from inside viewer PostBuild_dist.proj file by first getting the path to the .NET framework.

--- ImageViewer/Layout/Basic/DefaultPatientReconciliationStrategy.cs
+++ ImageViewer/Layout/Basic/DefaultPatientReconciliationStrategy.cs
@@ -1,8 +1,68 @@
 using System;
 using System.Xml;
+using ClearCanvas.Common.Utilities;
+using ClearCanvas.ImageViewer.StudyManagement;
 
 namespace ClearCanvas.ImageViewer.Layout.Basic
 {
+	//TODO: at some point in the future, expand to a full blown auto reconciler that just wraps the Ris' reconciliation service.
+
+	[Cloneable(true)]
+	public class PatientInformation
+	{
+		private string _patientId;
+		//private string _patientsName;
+		//private string _patientsBirthDate;
+		//private string _patientsBirthTime;
+		//private string _patientsSex;
+
+		internal PatientInformation()
+		{
+		}
+
+		internal PatientInformation(Patient patient)
+		{
+			_patientId = patient.PatientId;
+			//_patientsName = patient.PatientsName;
+			//_patientsSex = patient.PatientsSex;
+		}
+
+		public string PatientId
+		{
+			get { return _patientId; }
+			set { _patientId = value; }
+		}
+
+		//public string PatientsName
+		//{
+		//    get { return _patientsName; }
+		//    set { _patientsName = value; }
+		//}
+
+		//public string PatientsBirthDate
+		//{
+		//    get { return _patientsBirthDate; }
+		//    set { _patientsBirthDate = value; }
+		//}
+
+		//public string PatientsBirthTime
+		//{
+		//    get { return _patientsBirthTime; }
+		//    set { _patientsBirthTime = value; }
+		//}
+
+		//public string PatientsSex
+		//{
+		//    get { return _patientsSex; }
+		//    set { _patientsSex = value; }
+		//}
+
+		public PatientInformation Clone()
+		{
+			return CloneBuilder.Clone(this) as PatientInformation;
+		}
+	}
+
 	internal interface IReconciliationAction : IXmlAction
 	{
 		void Apply(XmlElement context, PatientInformation item);
@@ -21,7 +81,7 @@ namespace ClearCanvas.ImageViewer.Layout.Basic
 		#endregion
 	}
 
-	internal class DefaultPatientReconciliationStrategy : IPatientAutoReconciliationService
+	internal class DefaultPatientReconciliationStrategy
 	{
 		private readonly XmlActionsApplicator _applicator;
 
