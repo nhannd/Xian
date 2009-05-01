9466c5aa32c63caab12b3498af0435b83cb234d3
norman <devnull@localhost>
2009-05-01 02:53:22 +0000

    Requirement: #4317 - Missing #region directive during copyright update

--- ImageViewer/Layout/Basic/ContextMenuLayoutTool.cs
+++ ImageViewer/Layout/Basic/ContextMenuLayoutTool.cs
@@ -1,4 +1,6 @@
-﻿// Copyright (c) 2009, ClearCanvas Inc.
+﻿#region License
+
+// Copyright (c) 2009, ClearCanvas Inc.
 // All rights reserved.
 //
 // Redistribution and use in source and binary forms, with or without modification, 
