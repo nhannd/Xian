8d688f86dcf4ca8b789f330bf6d46882cedd812b
henry <devnull@localhost>
2009-05-14 17:43:10 +0000

    Ticket #4063 - Build and Revision numbers are reversed in DatabaseVersion_ table following database upgrade Updated the method that gets the database version to reverse Revision and Build positions so that they are in line with the .NET order.

--- ImageServer/Model/DatabaseVersion.cs
+++ ImageServer/Model/DatabaseVersion.cs
@@ -53,7 +53,7 @@ namespace ClearCanvas.ImageServer.Model
 
 		public string GetVersionString()
 		{
-			return string.Format("{0}.{1}.{2}.{3}", Major ?? "0", Minor ?? "0", Revision ?? "0", Build ?? "0");
+			return string.Format("{0}.{1}.{2}.{3}", Major ?? "0", Minor ?? "0", Build ?? "0", Revision ?? "0");
 		}
 	}
 }
