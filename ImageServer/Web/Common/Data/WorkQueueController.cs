a1101272c012e513fd36f0f2df81b46a60b0fc4c
jonathanb <devnull@localhost>
2009-08-19 19:04:41 +0000

    Ticket: #4863 - WorkQueue should display the study information
    Forgot to commit a change to the WorkQueueController which gets the study from the work queue item.

--- ImageServer/Web/Common/Data/WorkQueueController.cs
+++ ImageServer/Web/Common/Data/WorkQueueController.cs
@@ -380,6 +380,13 @@ namespace ClearCanvas.ImageServer.Web.Common.Data
 			return false;
 		}
 
+        public Study GetWorkQueueItemStudy(WorkQueue item)
+        {
+            IPersistentStore store = PersistentStoreRegistry.GetDefaultStore();
+            IReadContext ctx = store.OpenReadContext();
+            return item.LoadStudy(ctx);
+        }
+
     	#endregion
     }
 }
