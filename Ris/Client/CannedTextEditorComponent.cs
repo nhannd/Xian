08cdb5802ce7953e8344d203a5790e492726f919
jay <devnull@localhost>
2009-08-20 21:21:04 +0000

    Ticket(s): #5087 Prevent using bad characters to name a canned text
    Schema/Configuration Changes: N/A
    
    - add a regex validation on the Name property
    - name can only be a combination of alphabets and space
    - if regex match failed, the error icon will say "The value contains invalid characters"

--- Ris/Client/CannedTextEditorComponent.cs
+++ Ris/Client/CannedTextEditorComponent.cs
@@ -40,171 +40,172 @@ using ClearCanvas.Desktop.Validation;
 using ClearCanvas.Enterprise.Common;
 using ClearCanvas.Ris.Application.Common.CannedTextService;
 using ClearCanvas.Ris.Application.Common;
+using System.Text.RegularExpressions;
 
 namespace ClearCanvas.Ris.Client
 {
-	/// <summary>
-	/// Extension point for views onto <see cref="CannedTextEditorComponent"/>
-	/// </summary>
-	[ExtensionPoint]
-	public class CannedTextEditorComponentViewExtensionPoint : ExtensionPoint<IApplicationComponentView>
-	{
-	}
+    /// <summary>
+    /// Extension point for views onto <see cref="CannedTextEditorComponent"/>
+    /// </summary>
+    [ExtensionPoint]
+    public class CannedTextEditorComponentViewExtensionPoint : ExtensionPoint<IApplicationComponentView>
+    {
+    }
 
-	/// <summary>
-	/// CannedTextEditorComponent class
-	/// </summary>
-	[AssociateView(typeof(CannedTextEditorComponentViewExtensionPoint))]
-	public class CannedTextEditorComponent : ApplicationComponent
-	{
-		class DummyItem
-		{
-			private readonly string _displayString;
+    /// <summary>
+    /// CannedTextEditorComponent class
+    /// </summary>
+    [AssociateView(typeof(CannedTextEditorComponentViewExtensionPoint))]
+    public class CannedTextEditorComponent : ApplicationComponent
+    {
+        class DummyItem
+        {
+            private readonly string _displayString;
 
-			public DummyItem(string displayString)
-			{
-				_displayString = displayString;
-			}
+            public DummyItem(string displayString)
+            {
+                _displayString = displayString;
+            }
 
-			public override string ToString()
-			{
-				return _displayString;
-			}
-		}
+            public override string ToString()
+            {
+                return _displayString;
+            }
+        }
 
-		private static readonly object _nullFilterItem = new DummyItem(SR.DummyItemNone);
+        private static readonly object _nullFilterItem = new DummyItem(SR.DummyItemNone);
 
-		private readonly EntityRef _cannedTextRef;
-		private readonly List<string> _categoryChoices;
+        private readonly EntityRef _cannedTextRef;
+        private readonly List<string> _categoryChoices;
 
-		private CannedTextDetail _cannedTextDetail;
-		private CannedTextSummary _cannedTextSummary;
+        private CannedTextDetail _cannedTextDetail;
+        private CannedTextSummary _cannedTextSummary;
 
-		private List<StaffGroupSummary> _staffGroupChoices;
+        private List<StaffGroupSummary> _staffGroupChoices;
 
-		private readonly bool _isDuplicate;
-		private readonly bool _isNew;
-		private readonly bool _canChangeType;
-		private bool _isEditingPersonal;
+        private readonly bool _isDuplicate;
+        private readonly bool _isNew;
+        private readonly bool _canChangeType;
+        private bool _isEditingPersonal;
 
-		/// <summary>
-		/// Constructor for creating a new canned text.
-		/// </summary>
-		public CannedTextEditorComponent(List<string> categoryChoices)
-		{
-			_isNew = true;
-			_canChangeType = HasPersonalAdminAuthority && HasGroupAdminAuthority;
-			_categoryChoices = categoryChoices;
-		}
+        /// <summary>
+        /// Constructor for creating a new canned text.
+        /// </summary>
+        public CannedTextEditorComponent(List<string> categoryChoices)
+        {
+            _isNew = true;
+            _canChangeType = HasPersonalAdminAuthority && HasGroupAdminAuthority;
+            _categoryChoices = categoryChoices;
+        }
 
-		/// <summary>
-		/// Constructor for editing an existing canned text.
-		/// </summary>
-		public CannedTextEditorComponent(List<string> categoryChoices, EntityRef cannedTextRef)
-		{
-			_isNew = false;
-			_cannedTextRef = cannedTextRef;
-			_categoryChoices = categoryChoices;
+        /// <summary>
+        /// Constructor for editing an existing canned text.
+        /// </summary>
+        public CannedTextEditorComponent(List<string> categoryChoices, EntityRef cannedTextRef)
+        {
+            _isNew = false;
+            _cannedTextRef = cannedTextRef;
+            _categoryChoices = categoryChoices;
 
-			_canChangeType = false;
-		}
+            _canChangeType = false;
+        }
 
-		/// <summary>
-		/// Constructor for duplicating an existing canned text.
-		/// </summary>
-		public CannedTextEditorComponent(List<string> categoryChoices, EntityRef cannedTextRef, bool duplicate)
-			: this(categoryChoices)
-		{
-			_cannedTextRef = cannedTextRef;
-			_isDuplicate = duplicate;
-		}
+        /// <summary>
+        /// Constructor for duplicating an existing canned text.
+        /// </summary>
+        public CannedTextEditorComponent(List<string> categoryChoices, EntityRef cannedTextRef, bool duplicate)
+            : this(categoryChoices)
+        {
+            _cannedTextRef = cannedTextRef;
+            _isDuplicate = duplicate;
+        }
 
-		public override void Start()
-		{
-			// Insert a blank choice as the first element
-			_categoryChoices.Insert(0, "");
+        public override void Start()
+        {
+            // Insert a blank choice as the first element
+            _categoryChoices.Insert(0, "");
 
-			Platform.GetService<ICannedTextService>(
-				delegate(ICannedTextService service)
-					{
-						GetCannedTextEditFormDataResponse formDataResponse =
-							service.GetCannedTextEditFormData(new GetCannedTextEditFormDataRequest());
-						_staffGroupChoices = formDataResponse.StaffGroups;
+            Platform.GetService<ICannedTextService>(
+                delegate(ICannedTextService service)
+                    {
+                        GetCannedTextEditFormDataResponse formDataResponse =
+                            service.GetCannedTextEditFormData(new GetCannedTextEditFormDataRequest());
+                        _staffGroupChoices = formDataResponse.StaffGroups;
 
 
-						if (_isNew && _isDuplicate == false)
-						{
-							_cannedTextDetail = new CannedTextDetail();
-							_isEditingPersonal = HasPersonalAdminAuthority;
-						}
-						else
-						{
-							LoadCannedTextForEditResponse response = service.LoadCannedTextForEdit(new LoadCannedTextForEditRequest(_cannedTextRef));
-							_cannedTextDetail = response.CannedTextDetail;
+                        if (_isNew && _isDuplicate == false)
+                        {
+                            _cannedTextDetail = new CannedTextDetail();
+                            _isEditingPersonal = HasPersonalAdminAuthority;
+                        }
+                        else
+                        {
+                            LoadCannedTextForEditResponse response = service.LoadCannedTextForEdit(new LoadCannedTextForEditRequest(_cannedTextRef));
+                            _cannedTextDetail = response.CannedTextDetail;
 
-							_isEditingPersonal = _cannedTextDetail.IsPersonal;
+                            _isEditingPersonal = _cannedTextDetail.IsPersonal;
 
-							if (_isDuplicate)
-								this.Name = "";
-						}
-					});
+                            if (_isDuplicate)
+                                this.Name = "";
+                        }
+                    });
 
-			// The selected staff groups should only contain entries in the selected group choices
-			if (_cannedTextDetail.StaffGroup == null)
-			{
-				_cannedTextDetail.StaffGroup = CollectionUtils.FirstElement(_staffGroupChoices);
-			}
-			else
-			{
-				_cannedTextDetail.StaffGroup = CollectionUtils.SelectFirst(_staffGroupChoices,
-					delegate(StaffGroupSummary choice)
-					{
-						return _cannedTextDetail.StaffGroup.StaffGroupRef.Equals(choice.StaffGroupRef, true);
-					});
-			}
+            // The selected staff groups should only contain entries in the selected group choices
+            if (_cannedTextDetail.StaffGroup == null)
+            {
+                _cannedTextDetail.StaffGroup = CollectionUtils.FirstElement(_staffGroupChoices);
+            }
+            else
+            {
+                _cannedTextDetail.StaffGroup = CollectionUtils.SelectFirst(_staffGroupChoices,
+                    delegate(StaffGroupSummary choice)
+                    {
+                        return _cannedTextDetail.StaffGroup.StaffGroupRef.Equals(choice.StaffGroupRef, true);
+                    });
+            }
 
-			// add validation rule to ensure the group must be populated when editing group
-			this.Validation.Add(new ValidationRule("StaffGroup",
-				delegate
-				{
-					bool ok = this.IsEditingPersonal || this.IsEditingGroup && this.StaffGroup != null;
-					return new ValidationResult(ok, Desktop.SR.MessageValueRequired);
-				}));
+            // add validation rule to ensure the group must be populated when editing group
+            this.Validation.Add(new ValidationRule("StaffGroup",
+                delegate
+                {
+                    bool ok = this.IsEditingPersonal || this.IsEditingGroup && this.StaffGroup != null;
+                    return new ValidationResult(ok, Desktop.SR.MessageValueRequired);
+                }));
 
-			base.Start();
-		}
+            base.Start();
+        }
 
-		/// <summary>
-		/// Returns the Canned Text summary for use by the caller of this component
-		/// </summary>
-		public CannedTextSummary UpdatedCannedTextSummary
-		{
-			get { return _cannedTextSummary; }
-		}
+        /// <summary>
+        /// Returns the Canned Text summary for use by the caller of this component
+        /// </summary>
+        public CannedTextSummary UpdatedCannedTextSummary
+        {
+            get { return _cannedTextSummary; }
+        }
 
-		#region Presentation Model
+        #region Presentation Model
 
-		public bool IsNew
-		{
-			get { return _isNew; }
-		}
+        public bool IsNew
+        {
+            get { return _isNew; }
+        }
 
         public bool CanChangeType
-		{
-			get { return _canChangeType; }
-		}
+        {
+            get { return _canChangeType; }
+        }
 
-		public bool IsEditingPersonal
-		{
-			get{ return _isEditingPersonal; }
-			set{ _isEditingPersonal = value; }
-		}
+        public bool IsEditingPersonal
+        {
+            get{ return _isEditingPersonal; }
+            set{ _isEditingPersonal = value; }
+        }
 
-		public bool IsEditingGroup
-		{
-			get { return !this.IsEditingPersonal; }
-			set { this.IsEditingPersonal = !value; }
-		}
+        public bool IsEditingGroup
+        {
+            get { return !this.IsEditingPersonal; }
+            set { this.IsEditingPersonal = !value; }
+        }
 
         public bool IsReadOnly
         {
@@ -216,146 +217,147 @@ namespace ClearCanvas.Ris.Client
         }
 
         public bool AcceptEnabled
-		{
-			get { return this.Modified; }
-		}
+        {
+            get { return this.Modified; }
+        }
 
-		public event EventHandler AcceptEnabledChanged
-		{
-			add { this.ModifiedChanged += value; }
-			remove { this.ModifiedChanged -= value; }
-		}
+        public event EventHandler AcceptEnabledChanged
+        {
+            add { this.ModifiedChanged += value; }
+            remove { this.ModifiedChanged -= value; }
+        }
 
-		public object NullFilterItem
-		{
-			get { return _nullFilterItem; }
-		}
+        public object NullFilterItem
+        {
+            get { return _nullFilterItem; }
+        }
 
-		[ValidateNotNull]
-		public string Name
-		{
-			get { return _cannedTextDetail.Name; }
-			set
-			{
-				_cannedTextDetail.Name = value;
-				this.Modified = true;
-			}
-		}
+        [ValidateNotNull]
+        [ValidateRegex(@"^[A-Za-z\s]+$", Message = "MessageNameContainsInvalidChars")]
+        public string Name
+        {
+            get { return _cannedTextDetail.Name; }
+            set
+            {
+                _cannedTextDetail.Name = value;
+                this.Modified = true;
+            }
+        }
 
-		[ValidateNotNull]
-		public string Category
-		{
-			get { return _cannedTextDetail.Category; }
-			set
-			{
-				_cannedTextDetail.Category = value;
-				this.Modified = true;
-			}
-		}
+        [ValidateNotNull]
+        public string Category
+        {
+            get { return _cannedTextDetail.Category; }
+            set
+            {
+                _cannedTextDetail.Category = value;
+                this.Modified = true;
+            }
+        }
 
-		public bool HasGroupAdminAuthority
-		{
-			get { return Thread.CurrentPrincipal.IsInRole(ClearCanvas.Ris.Application.Common.AuthorityTokens.Workflow.CannedText.Group); }
-		}
+        public bool HasGroupAdminAuthority
+        {
+            get { return Thread.CurrentPrincipal.IsInRole(ClearCanvas.Ris.Application.Common.AuthorityTokens.Workflow.CannedText.Group); }
+        }
 
-		public bool HasPersonalAdminAuthority
-		{
-			get { return Thread.CurrentPrincipal.IsInRole(ClearCanvas.Ris.Application.Common.AuthorityTokens.Workflow.CannedText.Personal); }
-		}
+        public bool HasPersonalAdminAuthority
+        {
+            get { return Thread.CurrentPrincipal.IsInRole(ClearCanvas.Ris.Application.Common.AuthorityTokens.Workflow.CannedText.Personal); }
+        }
 
-		[ValidateNotNull]
-		public string Text
-		{
-			get { return _cannedTextDetail.Text; }
-			set
-			{
-				_cannedTextDetail.Text = value;
-				this.Modified = true;
-			}
-		}
+        [ValidateNotNull]
+        public string Text
+        {
+            get { return _cannedTextDetail.Text; }
+            set
+            {
+                _cannedTextDetail.Text = value;
+                this.Modified = true;
+            }
+        }
 
-		// Dynamic Null Validation when IsEditingGroup
-		public StaffGroupSummary StaffGroup
-		{
-			get { return _cannedTextDetail.StaffGroup; }
-			set
-			{
-				_cannedTextDetail.StaffGroup = value;
-				this.Modified = true;
-			}
-		}
+        // Dynamic Null Validation when IsEditingGroup
+        public StaffGroupSummary StaffGroup
+        {
+            get { return _cannedTextDetail.StaffGroup; }
+            set
+            {
+                _cannedTextDetail.StaffGroup = value;
+                this.Modified = true;
+            }
+        }
 
-		public IList StaffGroupChoices
-		{
-			get { return _staffGroupChoices; }
-		}
+        public IList StaffGroupChoices
+        {
+            get { return _staffGroupChoices; }
+        }
 
-		public IList CategoryChoices
-		{
-			get { return _categoryChoices; }
-		}
+        public IList CategoryChoices
+        {
+            get { return _categoryChoices; }
+        }
 
-		public void Accept()
-		{
-			if (this.HasValidationErrors)
-			{
-				this.ShowValidation(true);
-				return;
-			}
+        public void Accept()
+        {
+            if (this.HasValidationErrors)
+            {
+                this.ShowValidation(true);
+                return;
+            }
 
-			try
-			{
-				// Depends on the editing mode, remove the unnecessary information
-				if (this.IsEditingPersonal)
-					_cannedTextDetail.StaffGroup = null;
+            try
+            {
+                // Depends on the editing mode, remove the unnecessary information
+                if (this.IsEditingPersonal)
+                    _cannedTextDetail.StaffGroup = null;
 
-				Platform.GetService<ICannedTextService>(
-					delegate(ICannedTextService service)
-					{
-						if (_isNew)
-						{
-							AddCannedTextResponse response = service.AddCannedText(new AddCannedTextRequest(_cannedTextDetail));
-							_cannedTextSummary = response.CannedTextSummary;
-						}
-						else
-						{
-							UpdateCannedTextResponse response = service.UpdateCannedText(new UpdateCannedTextRequest(_cannedTextRef, _cannedTextDetail));
-							_cannedTextSummary = response.CannedTextSummary;
-						}
-					});
+                Platform.GetService<ICannedTextService>(
+                    delegate(ICannedTextService service)
+                    {
+                        if (_isNew)
+                        {
+                            AddCannedTextResponse response = service.AddCannedText(new AddCannedTextRequest(_cannedTextDetail));
+                            _cannedTextSummary = response.CannedTextSummary;
+                        }
+                        else
+                        {
+                            UpdateCannedTextResponse response = service.UpdateCannedText(new UpdateCannedTextRequest(_cannedTextRef, _cannedTextDetail));
+                            _cannedTextSummary = response.CannedTextSummary;
+                        }
+                    });
 
-				this.Exit(ApplicationComponentExitCode.Accepted);
-			}
-			catch (Exception e)
-			{
-				ExceptionHandler.Report(e,
-					SR.ExceptionSaveCannedText,
-					this.Host.DesktopWindow,
-					delegate
-					{
-						this.ExitCode = ApplicationComponentExitCode.Error;
-						this.Host.Exit();
-					});
-			}
-		}
+                this.Exit(ApplicationComponentExitCode.Accepted);
+            }
+            catch (Exception e)
+            {
+                ExceptionHandler.Report(e,
+                    SR.ExceptionSaveCannedText,
+                    this.Host.DesktopWindow,
+                    delegate
+                    {
+                        this.ExitCode = ApplicationComponentExitCode.Error;
+                        this.Host.Exit();
+                    });
+            }
+        }
 
-		public void Cancel()
-		{
-			this.ExitCode = ApplicationComponentExitCode.None;
-			Host.Exit();
-		}
+        public void Cancel()
+        {
+            this.ExitCode = ApplicationComponentExitCode.None;
+            Host.Exit();
+        }
 
-		#endregion
+        #endregion
 
-		public string FormatStaffGroup(object item)
-		{
-			if (item is StaffGroupSummary)
-			{
-				StaffGroupSummary staffGroup = (StaffGroupSummary)item;
-				return staffGroup.Name;
-			}
-			else
-				return item.ToString(); // place-holder items
-		}
-	}
+        public string FormatStaffGroup(object item)
+        {
+            if (item is StaffGroupSummary)
+            {
+                StaffGroupSummary staffGroup = (StaffGroupSummary)item;
+                return staffGroup.Name;
+            }
+            else
+                return item.ToString(); // place-holder items
+        }
+    }
 }
