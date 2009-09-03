d11e3e0291b2ecddcf121702d2fdcb7073eeb5c1
rob <devnull@localhost>
2009-09-03 16:51:47 +0000

    Ticket(s): #5081 - Re-think document detachment tool
    Schema/Configuration Changes: N/A
    
    - Summary table toolbar/RCCM now disabled in AttachedDocumentDHtmlPreviewComponent readonly (i.e. Biography) context.
    - Operation enablement for various tools corrected for items in the "Detached" status.

--- Ris/Client/View/WinForms/AttachedDocumentPreviewComponentControl.cs
+++ Ris/Client/View/WinForms/AttachedDocumentPreviewComponentControl.cs
@@ -34,35 +34,40 @@ using ClearCanvas.Desktop.View.WinForms;
 
 namespace ClearCanvas.Ris.Client.View.WinForms
 {
-    /// <summary>
-    /// Provides a Windows Forms user-interface for <see cref="AttachedDocumentPreviewComponent"/>
-    /// </summary>
-    public partial class AttachedDocumentPreviewComponentControl : ApplicationComponentUserControl
-    {
-        private readonly AttachedDocumentPreviewComponent _component;
+	/// <summary>
+	/// Provides a Windows Forms user-interface for <see cref="AttachedDocumentPreviewComponent"/>
+	/// </summary>
+	public partial class AttachedDocumentPreviewComponentControl : ApplicationComponentUserControl
+	{
+		private readonly AttachedDocumentPreviewComponent _component;
 
-        public AttachedDocumentPreviewComponentControl(AttachedDocumentPreviewComponent component)
-            : base(component)
-        {
-            InitializeComponent();
-            _component = component;
+		public AttachedDocumentPreviewComponentControl(AttachedDocumentPreviewComponent component)
+			: base(component)
+		{
+			InitializeComponent();
+			_component = component;
 
-            _attachments.ShowToolbar = !_component.Readonly;
+			_attachments.ShowToolbar = !_component.Readonly;
 
-            _attachments.Table = _component.Attachments;
-            _attachments.MenuModel = _component.AttachmentActionModel;
-            _attachments.ToolbarModel = _component.AttachmentActionModel;
-            _attachments.DataBindings.Add("Selection", _component, "Selection", true, DataSourceUpdateMode.OnPropertyChanged);
+			_attachments.Table = _component.Attachments;
 
-            Control preview = (Control)_component.PreviewHost.ComponentView.GuiElement;
-            preview.Dock = DockStyle.Fill;
-            _splitContainer.Panel2.Controls.Add(preview);
-        }
+			if (!_component.Readonly)
+			{
+				_attachments.MenuModel = _component.AttachmentActionModel;
+				_attachments.ToolbarModel = _component.AttachmentActionModel;
 
-        private void AttachedDocumentPreviewComponentControl_Load(object sender, System.EventArgs e)
-        {
-            _component.OnControlLoad();
-        }
+			}
+			_attachments.DataBindings.Add("Selection", _component, "Selection", true, DataSourceUpdateMode.OnPropertyChanged);
 
-    }
+			var preview = (Control)_component.PreviewHost.ComponentView.GuiElement;
+			preview.Dock = DockStyle.Fill;
+			_splitContainer.Panel2.Controls.Add(preview);
+		}
+
+		private void AttachedDocumentPreviewComponentControl_Load(object sender, System.EventArgs e)
+		{
+			_component.OnControlLoad();
+		}
+
+	}
 }
