fba29ec172ce6a69dc8025870c8f4eb404d45534
jonathan <devnull@localhost>
2009-10-13 22:11:58 +0000

    Ticket: #5309 Support backwards compatibility with Enterprise Server 1.5
    Added a service provider that can connect to the old enterprise server.  It has a nonsense name so that we remember to remove it at some point in future.

---
+++ Enterprise/Common/TBoneSteakServiceProvider.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using ClearCanvas.Common;
+using ClearCanvas.Enterprise.Common.Configuration;
+
+namespace ClearCanvas.Enterprise.Common
+{
+	/// <summary>
+	/// Provides backwards compatability for Workstation 2.0 with Enterprise server 1.5 by mapping
+	/// IApplicationConfigurationReadService to IConfigurationService
+	/// </summary>
+	[ExtensionOf(typeof(ServiceProviderExtensionPoint), Enabled = false)]
+	public class TBoneSteakServiceProvider : IServiceProvider
+	{
+		class Proxy : IApplicationConfigurationReadService
+		{
+			public ListSettingsGroupsResponse ListSettingsGroups(ListSettingsGroupsRequest request)
+			{
+				ListSettingsGroupsResponse response = null;
+				Platform.GetService<IConfigurationService>(
+					service => response = service.ListSettingsGroups(request));
+				return response;
+			}
+
+			public ListSettingsPropertiesResponse ListSettingsProperties(ListSettingsPropertiesRequest request)
+			{
+				ListSettingsPropertiesResponse response = null;
+				Platform.GetService<IConfigurationService>(
+					service => response = service.ListSettingsProperties(request));
+				return response;
+			}
+
+			public GetConfigurationDocumentResponse GetConfigurationDocument(GetConfigurationDocumentRequest request)
+			{
+				GetConfigurationDocumentResponse response = null;
+				Platform.GetService<IConfigurationService>(
+					service => response = service.GetConfigurationDocument(request));
+				return response;
+			}
+		}
+
+
+
+
+		#region IServiceProvider Members
+
+		public object GetService(Type serviceType)
+		{
+			if(serviceType != typeof(IApplicationConfigurationReadService))
+				return null;
+
+			return new Proxy();
+		}
+
+		#endregion
+	}
+}
