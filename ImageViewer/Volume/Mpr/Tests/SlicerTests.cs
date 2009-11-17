93dd8d0999f487fbe2f7d0c7efc9fd6496ea0c20
jasper <devnull@localhost>
2009-11-17 15:28:22 +0000

    Ticket: #5312 - Write extensive MPR unit tests '''Schema/Configuration Changes: N/A'''  * The inverse rotation matrix now works as intended.

--- ImageViewer/Volume/Mpr/Tests/SlicerTests.cs
+++ ImageViewer/Volume/Mpr/Tests/SlicerTests.cs
@@ -33,7 +33,8 @@
 
 #pragma warning disable 1591,0419,1574,1587
 
-using System.Diagnostics;
+using System;
+using ClearCanvas.ImageViewer.Mathematics;
 using NUnit.Framework;
 
 namespace ClearCanvas.ImageViewer.Volume.Mpr.Tests
@@ -44,7 +45,6 @@ namespace ClearCanvas.ImageViewer.Volume.Mpr.Tests
 		public SlicerTests() {}
 
 		[Test]
-		[Ignore("This test case doesn't handle multiple solutions for the same rotation")]
 		public void TestInverseRotationMatrices()
 		{
 			for (int x = 0; x < 360; x += 15)
@@ -53,22 +53,49 @@ namespace ClearCanvas.ImageViewer.Volume.Mpr.Tests
 				{
 					for (int z = 0; z < 360; z += 15)
 					{
-						VolumeSlicerParams expected = new VolumeSlicerParams(x, y, z);
-						VolumeSlicerParams test = new VolumeSlicerParams(expected.SlicingPlaneRotation);
+						VolumeSlicerParams expected = new VolumeSlicerParams(x, y, z); // this constructor stores x, y, z separately from the matrix
+						Matrix mExpected = ComputeRotationMatrix(expected.RotateAboutX, expected.RotateAboutY, expected.RotateAboutZ);
 
-						// Within 1 one-thousandth of a degree is close enough, no?
-						Trace.WriteLine(string.Format("Expected: {0},{1},{2} Actual: {3},{4},{5}", expected.RotateAboutX, expected.RotateAboutY, expected.RotateAboutZ, test.RotateAboutX, test.RotateAboutY, test.RotateAboutZ));
-						Assert.AreEqual(expected.RotateAboutX, NormalizeDegrees(test.RotateAboutX), 1e-3, "Computation Error in X: Q={0},{1},{2}", x, y, z);
-						Assert.AreEqual(expected.RotateAboutY, NormalizeDegrees(test.RotateAboutY), 1e-3, "Computation Error in Y: Q={0},{1},{2}", x, y, z);
-						Assert.AreEqual(expected.RotateAboutZ, NormalizeDegrees(test.RotateAboutZ), 1e-3, "Computation Error in Z: Q={0},{1},{2}", x, y, z);
+						VolumeSlicerParams test = new VolumeSlicerParams(expected.SlicingPlaneRotation); // this constructor must infer x, y, z from the matrix
+						Matrix mTest = ComputeRotationMatrix(test.RotateAboutX, test.RotateAboutY, test.RotateAboutZ);
+
+						for (int r = 0; r < 3; r++)
+						{
+							for (int c = 0; c < 3; c++)
+							{
+								Assert.AreEqual(mExpected[r, c], mTest[r, c], 1e-6, "Rotation Matrices differ at R{3}C{4} for Q={0},{1},{2}", x, y, z, r, c);
+							}
+						}
 					}
 				}
 			}
 		}
 
-		private static float NormalizeDegrees(float degrees)
+		private static Matrix ComputeRotationMatrix(float degreesAboutX, float degreesAboutY, float degreesAboutZ)
 		{
-			return ((degrees%360) + 360)%360;
+			Matrix mTestX = Matrix.GetIdentity(3);
+			mTestX[1, 1] = mTestX[2, 2] = Cos(degreesAboutX);
+			mTestX[1, 2] = -(mTestX[2, 1] = Sin(degreesAboutX));
+
+			Matrix mTestY = Matrix.GetIdentity(3);
+			mTestY[0, 0] = mTestY[2, 2] = Cos(degreesAboutY);
+			mTestY[2, 0] = -(mTestY[0, 2] = Sin(degreesAboutY));
+
+			Matrix mTestZ = Matrix.GetIdentity(3);
+			mTestZ[0, 0] = mTestZ[1, 1] = Cos(degreesAboutZ);
+			mTestZ[0, 1] = -(mTestZ[1, 0] = Sin(degreesAboutZ));
+
+			return mTestX*mTestY*mTestZ;
+		}
+
+		private static float Cos(float degrees)
+		{
+			return (float) Math.Cos(degrees*Math.PI/180);
+		}
+
+		private static float Sin(float degrees)
+		{
+			return (float) Math.Sin(degrees*Math.PI/180);
 		}
 	}
 }
