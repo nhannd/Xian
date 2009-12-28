22dc7f5c03689927844063e7dce19543506218f1
steve <devnull@localhost>
2009-12-28 21:11:13 +0000

    Ticket: #5484 - Add DB Update Scripts for Performance milestone
    Schema/Configuration Changes: N/A
    
    Added upgrade scripts for 1.5 SP1.

--- ImageServer/Model/SqlServer2005/UpgradeScripts/UpgradeFrom_1_5_12104_35993 .cs
+++ ImageServer/Model/SqlServer2005/UpgradeScripts/UpgradeFrom_1_5_12104_35993 .cs
@@ -36,11 +36,11 @@ using ClearCanvas.Enterprise.Core.Upgrade;
 namespace ClearCanvas.ImageServer.Model.SqlServer2005.UpgradeScripts
 {
 	[ExtensionOf(typeof(PersistentStoreUpgradeScriptExtensionPoint))]
-	class UpgradeFrom_1_5_10852_32944 : BaseUpgradeScript
+	class UpgradeFrom_1_5_12104_35993 : BaseUpgradeScript
 	{
 		//In versions prior to 1.5 the use of Build and Revision were swapped and so it has to be swapped here in order for the utility to properly detect the older version
-		public UpgradeFrom_1_5_10852_32944()
-			: base(new Version(1, 5, 10852, 32944), new Version(1, 5, 12104, 35993), "UpgradeFrom_NoOp.sql")
+		public UpgradeFrom_1_5_12104_35993()
+			: base(new Version(1, 5, 12104, 35993), null, "UpgradeFrom_1_5_12104_35993.sql")
 		{
 		}
 	}
