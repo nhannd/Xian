6cc0875e3958100d2f93256a148f8fc3a43e8139
jonathan <devnull@localhost>
2010-01-21 00:18:28 +0000

    Ticket(s): #5884 - Browse patient data service's number of SQL queries should be reduced
    Disabled NH profiling by default.

--- Enterprise/Hibernate/PersistentStoreProfilerSettings.Designer.cs
+++ Enterprise/Hibernate/PersistentStoreProfilerSettings.Designer.cs
@@ -25,7 +25,7 @@ namespace ClearCanvas.Enterprise.Hibernate {
         
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("True")]
+        [global::System.Configuration.DefaultSettingValueAttribute("False")]
         public bool Enabled {
             get {
                 return ((bool)(this["Enabled"]));
