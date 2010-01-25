ffc1b024529f8c1f6648530e8352c90a7314bf91
jasper <devnull@localhost>
2010-01-25 19:48:36 +0000

    Ticket: #5901 - Modify MouseImageViewerTool to override IconSet of it's own actions '''Schema/Configuration Changes: N/A'''  * Left out the spatial locator tool last time.

--- ImageViewer/Tools/Synchronization/SpatialLocatorTool.cs
+++ ImageViewer/Tools/Synchronization/SpatialLocatorTool.cs
@@ -45,7 +45,7 @@ namespace ClearCanvas.ImageViewer.Tools.Synchronization
 	[ButtonAction("activate", "global-toolbars/ToolbarSynchronization/ToolbarSpatialLocator", "Select", Flags = ClickActionFlags.CheckAction)]
 	[CheckedStateObserver("activate", "Active", "ActivationChanged")]
 	[TooltipValueObserver("activate", "Tooltip", "TooltipChanged")]
-	[IconSet("activate", IconScheme.Colour, "Icons.SpatialLocatorToolSmall.png", "Icons.SpatialLocatorToolMedium.png", "Icons.SpatialLocatorToolLarge.png")]
+	[MouseButtonIconSet("activate", IconScheme.Colour, "Icons.SpatialLocatorToolSmall.png", "Icons.SpatialLocatorToolMedium.png", "Icons.SpatialLocatorToolLarge.png")]
 	[GroupHint("activate", "Tools.Image.Synchronization.SpatialLocator")]
 	
 	[MouseToolButton(XMouseButtons.Right, false)]
