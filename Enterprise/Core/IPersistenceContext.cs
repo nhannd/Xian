cc1cf0a0142eee3873a2b21d3d4ae6e72c4750a8
jonathan <devnull@localhost>
2010-04-06 21:09:04 +0000

    Ticket(s): #6211 - Scalability and Performance enhancements
    Fixed formatting and implemented resharper suggestions.

--- Enterprise/Core/IPersistenceContext.cs
+++ Enterprise/Core/IPersistenceContext.cs
@@ -30,60 +30,68 @@
 #endregion
 
 using System;
-using System.Collections;
 using ClearCanvas.Enterprise.Common;
 namespace ClearCanvas.Enterprise.Core
 {
-    /// <summary>
-    /// Base interface for a persistence context.  A persistence context is an implementation of the unit-of-work
-    /// and identity map patterns, and defines a scope in which the application can perform a set of operations on
-    /// a persistent store.  This interface is not implemented directly.
-    /// See <see cref="IReadContext"/> and <see cref="IUpdateContext"/>.
-    /// </summary>
-    /// <seealso cref="IReadContext"/>
-    /// <seealso cref="IUpdateContext"/>
-    public interface IPersistenceContext : IPersistenceBrokerFactory, IDisposable
-    {
-        /// <summary>
-        /// Locks the specified entity into the context.  If this is an update context, the entity will be
-        /// treated as "clean".  Use the other overload to specify that the entity is dirty.
-        /// </summary>
-        /// <param name="entity"></param>
-        void Lock(Entity entity);
+	/// <summary>
+	/// Base interface for a persistence context. See <see cref="IReadContext"/> and <see cref="IUpdateContext"/>.
+	/// </summary>
+	/// <remarks>
+	/// A persistence context is an implementation of the unit-of-work
+	/// and identity map patterns, and defines a scope in which the application can perform a set of operations on
+	/// a persistent store.  This interface is not implemented directly.
+	/// </remarks>
+	/// <seealso cref="IReadContext"/>
+	/// <seealso cref="IUpdateContext"/>
+	public interface IPersistenceContext : IPersistenceBrokerFactory, IDisposable
+	{
+		/// <summary>
+		/// Locks the specified entity into the context. 
+		/// </summary>
+		/// <remarks>
+		/// If this is an update context, the entity will be treated as "clean".
+		/// Use the other overload to specify that the entity is new or dirty.</remarks>
+		/// <param name="entity"></param>
+		void Lock(Entity entity);
 
-        /// <summary>
-        /// Locks the specified entity into the context with the specified <see cref="DirtyState"/>.
-        /// Note that it does not make sense to lock an entity into a read-context with <see cref="DirtyState.Dirty"/>,
-        /// and an exception will be thrown.  Nevertheless, this method exists on this interface for
-        /// the sake of convenience.
-        /// </summary>
-        /// <param name="entity"></param>
-        /// <param name="state"></param>
-        void Lock(Entity entity, DirtyState state);
+		/// <summary>
+		/// Locks the specified entity into the context with the specified <see cref="DirtyState"/>.
+		/// </summary>
+		/// <remarks>
+		/// Note that it does not make sense to lock an entity into a read context with <see cref="DirtyState.Dirty"/>,
+		/// and an exception will be thrown.
+		/// </remarks>
+		/// <param name="entity"></param>
+		/// <param name="state"></param>
+		void Lock(Entity entity, DirtyState state);
 
-        /// <summary>
-        /// Loads the specified entity into this context
-        /// </summary>
-        /// <param name="entityRef"></param>
-        /// <returns></returns>
-        TEntity Load<TEntity>(EntityRef entityRef)
-            where TEntity : Entity;
+		/// <summary>
+		/// Loads the specified entity into this context.
+		/// </summary>
+		/// <param name="entityRef"></param>
+		/// <returns></returns>
+		TEntity Load<TEntity>(EntityRef entityRef) where TEntity : Entity;
 
-        /// <summary>
-        /// Loads the specified entity into this context.
-        /// </summary>
-        /// <param name="entityRef"></param>
-        /// <param name="flags"></param>
-        /// <returns></returns>
-        TEntity Load<TEntity>(EntityRef entityRef, EntityLoadFlags flags)
-            where TEntity : Entity;
+		/// <summary>
+		/// Loads the specified entity into this context.
+		/// </summary>
+		/// <param name="entityRef"></param>
+		/// <param name="flags"></param>
+		/// <returns></returns>
+		TEntity Load<TEntity>(EntityRef entityRef, EntityLoadFlags flags) where TEntity : Entity;
 
 		/// <summary>
 		/// Loads the specified entity into this context.
 		/// </summary>
 		Entity Load(EntityRef entityRef, EntityLoadFlags flags);
 
-        void SynchState();
-
-    }
+		/// <summary>
+		/// Synchronizes the state of the persistent store (database) with the state of this context.
+		/// </summary>
+		/// <remarks>
+		/// This method will ensure that any pending writes to the persistent store are flushed, and that
+		/// any generated object identifiers for new persistent objects are generated and assigned to those objects. 
+		/// </remarks>
+		void SynchState();
+	}
 }
