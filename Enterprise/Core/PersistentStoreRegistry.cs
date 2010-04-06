76ce023b2fec8e2c1f1eee8c385d2e4c1c3562f6
jonathan <devnull@localhost>
2010-04-06 22:09:37 +0000

    Ticket(s): #6463 generate a large database for testing purposes
    Fixed formatting and implemented resharper suggestions.

--- Enterprise/Core/PersistentStoreRegistry.cs
+++ Enterprise/Core/PersistentStoreRegistry.cs
@@ -29,34 +29,31 @@
 
 #endregion
 
-using System;
-using System.Collections.Generic;
-using System.Text;
 using ClearCanvas.Common;
 
 namespace ClearCanvas.Enterprise.Core
 {
-    [ExtensionPoint]
-    public class PersistentStoreExtensionPoint : ExtensionPoint<IPersistentStore>
-    {
-    }
+	[ExtensionPoint]
+	public class PersistentStoreExtensionPoint : ExtensionPoint<IPersistentStore>
+	{
+	}
 
 
-    public class PersistentStoreRegistry
-    {
-        private static IPersistentStore _defaultStore;
+	public class PersistentStoreRegistry
+	{
+		private static IPersistentStore _defaultStore;
 
-        public static IPersistentStore GetDefaultStore()
-        {
-            if (_defaultStore == null)
-            {
-                // for now, just look for a single extension and treat it as the "default" store
-                // in future, there could conceivably be a number of different persistent stores,
-                // in which case we will need to use a different mechanism (config file or something)
-                _defaultStore = (IPersistentStore)(new PersistentStoreExtensionPoint()).CreateExtension();
-                _defaultStore.Initialize();
-            }
-            return _defaultStore;
-        }
-    }
+		public static IPersistentStore GetDefaultStore()
+		{
+			if (_defaultStore == null)
+			{
+				// for now, just look for a single extension and treat it as the "default" store
+				// in future, there could conceivably be a number of different persistent stores,
+				// in which case we will need to use a different mechanism (config file or something)
+				_defaultStore = (IPersistentStore)(new PersistentStoreExtensionPoint()).CreateExtension();
+				_defaultStore.Initialize();
+			}
+			return _defaultStore;
+		}
+	}
 }
