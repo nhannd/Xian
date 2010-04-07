a4776bd3733950d427a52c210cb1a7728222ef0d
jonathan <devnull@localhost>
2010-04-07 20:41:21 +0000

    Ticket(s): #6463 generate a large database for testing purposes
    Formatted file and implemented resharper suggestions.

--- Enterprise/Common/Caching/Cache.cs
+++ Enterprise/Common/Caching/Cache.cs
@@ -31,7 +31,6 @@
 
 using System;
 using System.Collections.Generic;
-using System.Text;
 using ClearCanvas.Common;
 using ClearCanvas.Common.Utilities;
 
@@ -41,7 +40,7 @@ namespace ClearCanvas.Enterprise.Common.Caching
 	/// Defines an extension point for implementations of <see cref="ICacheProvider"/>.
 	/// </summary>
 	[ExtensionPoint]
-	public class CacheProviderExtensionPoint: ExtensionPoint<ICacheProvider>
+	public class CacheProviderExtensionPoint : ExtensionPoint<ICacheProvider>
 	{
 	}
 
@@ -55,15 +54,15 @@ namespace ClearCanvas.Enterprise.Common.Caching
 		/// </summary>
 		private static readonly Dictionary<Type, ICacheProvider> _providers = new Dictionary<Type, ICacheProvider>();
 
-        /// <summary>
-        /// Gets a value indicating if the cache is supported in this environment.
-        /// </summary>
-        /// <returns></returns>
-        public static bool IsSupported()
-        {
-            CacheProviderExtensionPoint point = new CacheProviderExtensionPoint();
-            return point.ListExtensions().Length > 0;
-        }
+		/// <summary>
+		/// Gets a value indicating if the cache is supported in this environment.
+		/// </summary>
+		/// <returns></returns>
+		public static bool IsSupported()
+		{
+			var point = new CacheProviderExtensionPoint();
+			return point.ListExtensions().Length > 0;
+		}
 
 		/// <summary>
 		/// Creates a cache client for the specified logical cacheID.
@@ -71,21 +70,21 @@ namespace ClearCanvas.Enterprise.Common.Caching
 		/// <remarks>
 		/// This method is safe for concurrent use by multiple threads.
 		/// </remarks>
-		public static ICacheClient CreateClient(string cacheID)
+		public static ICacheClient CreateClient(string cacheId)
 		{
-            // a cacheID is required!
-            Platform.CheckForEmptyString(cacheID, "CacheID");
+			// a cacheID is required!
+			Platform.CheckForEmptyString(cacheId, "CacheID");
 
-            // TODO a more sophisticated delegate may be required here
+			// TODO a more sophisticated delegate may be required here
 			// if more than one cache provider extension exists, there will need to be mechanisms for choosing
 			// the appropriate provider, which may be influenced by a) the creation args,
 			// and b) potentially some external configuration settings
-			ICacheProvider provider = GetProvider(delegate { return true; });
+			var provider = GetProvider(delegate { return true; });
 
 			// create specified cache
-            // this call assumes the provider.CreateClient method is thread-safe, which
-            // is the responsibility of the provider!
-            ICacheClient client = provider.CreateClient(cacheID);
+			// this call assumes the provider.CreateClient method is thread-safe, which
+			// is the responsibility of the provider!
+			var client = provider.CreateClient(cacheId);
 
 			// if debug logging enabled, wrap client in a logging decorator set at Debug level
 			if (Platform.IsLogLevelEnabled(LogLevel.Debug))
@@ -95,31 +94,31 @@ namespace ClearCanvas.Enterprise.Common.Caching
 			return client;
 		}
 
-        /// <summary>
-        /// Thread-safe method to obtain singleton instance of <see cref="ICacheProvider"/>
-        /// matching specified filter.
-        /// </summary>
-        /// <param name="filter"></param>
-        /// <returns></returns>
+		/// <summary>
+		/// Thread-safe method to obtain singleton instance of <see cref="ICacheProvider"/>
+		/// matching specified filter.
+		/// </summary>
+		/// <param name="filter"></param>
+		/// <returns></returns>
 		private static ICacheProvider GetProvider(Predicate<ExtensionInfo> filter)
 		{
 			// determine the provider class
-			CacheProviderExtensionPoint point = new CacheProviderExtensionPoint();
-			ExtensionInfo extension = CollectionUtils.FirstElement(point.ListExtensions(filter));
+			var point = new CacheProviderExtensionPoint();
+			var extension = CollectionUtils.FirstElement(point.ListExtensions(filter));
 			if (extension == null)
 				throw new CacheException("No cache provider extension found, or those that exist do not support all required features.");
 
-			Type providerClass = extension.ExtensionClass;
+			var providerClass = extension.ExtensionClass;
 
 			// check if we already have an initialized instance of this provider class.
 			ICacheProvider provider;
 			if (!_providers.TryGetValue(providerClass, out provider))
 			{
 				// if not, create one
-				provider = (ICacheProvider) point.CreateExtension(
+				provider = (ICacheProvider)point.CreateExtension(
 					new ClassNameExtensionFilter(providerClass.FullName));
 
-				lock(_providers)
+				lock (_providers)
 				{
 					// ensure that another thread hasn't beat us to it
 					if (!_providers.ContainsKey(providerClass))
