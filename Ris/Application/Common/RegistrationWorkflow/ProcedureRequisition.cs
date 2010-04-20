2b872bbd56f319facfda02ac03e3d459eaed75d0
jonathan <devnull@localhost>
2010-04-20 14:16:15 +0000

    Ticket(s): #6531 The New Order, Modify Order dialogs should allow the entry by the user of a Department.
    Fixed formatting in this file

--- Ris/Application/Common/RegistrationWorkflow/ProcedureRequisition.cs
+++ Ris/Application/Common/RegistrationWorkflow/ProcedureRequisition.cs
@@ -35,103 +35,103 @@ using ClearCanvas.Enterprise.Common;
 
 namespace ClearCanvas.Ris.Application.Common.RegistrationWorkflow
 {
-    [DataContract]
-    public class ProcedureRequisition : DataContractBase
-    {
-        /// <summary>
-        /// Constructor for use by service to return requisition back to client for editing.
-        /// </summary>
-        public ProcedureRequisition(
-            ProcedureTypeSummary procedureType,
-            string procedureIndex,
-            DateTime? scheduledTime,
-            FacilitySummary performingFacility,
-            EnumValueInfo laterality,
-            bool portableModality,
-            bool checkedIn,
-            EnumValueInfo status,
-            bool canModify,
+	[DataContract]
+	public class ProcedureRequisition : DataContractBase
+	{
+		/// <summary>
+		/// Constructor for use by service to return requisition back to client for editing.
+		/// </summary>
+		public ProcedureRequisition(
+			ProcedureTypeSummary procedureType,
+			string procedureIndex,
+			DateTime? scheduledTime,
+			FacilitySummary performingFacility,
+			EnumValueInfo laterality,
+			bool portableModality,
+			bool checkedIn,
+			EnumValueInfo status,
+			bool canModify,
 			bool cancelled)
-        {
-            this.ProcedureType = procedureType;
-            this.ProcedureIndex = procedureIndex;
-            this.ScheduledTime = scheduledTime;
-            this.PerformingFacility = performingFacility;
-            this.Laterality = laterality;
-            this.PortableModality = portableModality;
-            this.CheckedIn = checkedIn;
-            this.Status = status;
-            this.CanModify = canModify;
-        	this.Cancelled = cancelled;
-        }
+		{
+			this.ProcedureType = procedureType;
+			this.ProcedureIndex = procedureIndex;
+			this.ScheduledTime = scheduledTime;
+			this.PerformingFacility = performingFacility;
+			this.Laterality = laterality;
+			this.PortableModality = portableModality;
+			this.CheckedIn = checkedIn;
+			this.Status = status;
+			this.CanModify = canModify;
+			this.Cancelled = cancelled;
+		}
 
-        /// <summary>
-        /// Constructor for use by client when initially creating a requisition.
-        /// </summary>
-        public ProcedureRequisition(ProcedureTypeSummary procedureType, FacilitySummary facility)
-        {
-            this.ProcedureType = procedureType;
-            this.PerformingFacility = facility;
-            this.CanModify = true;  // can modify a new requisition
-        }
+		/// <summary>
+		/// Constructor for use by client when initially creating a requisition.
+		/// </summary>
+		public ProcedureRequisition(ProcedureTypeSummary procedureType, FacilitySummary facility)
+		{
+			this.ProcedureType = procedureType;
+			this.PerformingFacility = facility;
+			this.CanModify = true;  // can modify a new requisition
+		}
 
 
-        /// <summary>
-        /// The procedure type. Required.
-        /// </summary>
-        [DataMember]
-        public ProcedureTypeSummary ProcedureType;
+		/// <summary>
+		/// The procedure type. Required.
+		/// </summary>
+		[DataMember]
+		public ProcedureTypeSummary ProcedureType;
 
-        /// <summary>
-        /// Procedure index.  Will be set by the server if this requisition represents an existing procedure.
-        /// Clients should not set or modify this field.
-        /// </summary>
-        [DataMember]
-        public string ProcedureIndex;
+		/// <summary>
+		/// Procedure index.  Will be set by the server if this requisition represents an existing procedure.
+		/// Clients should not set or modify this field.
+		/// </summary>
+		[DataMember]
+		public string ProcedureIndex;
 
-        /// <summary>
-        /// Time at which this procedure is scheduled to occur. May be null, indicating that
-        /// the procedure is not yet scheduled for a specific time.
-        /// </summary>
-        [DataMember]
-        public DateTime? ScheduledTime;
+		/// <summary>
+		/// Time at which this procedure is scheduled to occur. May be null, indicating that
+		/// the procedure is not yet scheduled for a specific time.
+		/// </summary>
+		[DataMember]
+		public DateTime? ScheduledTime;
 
-        /// <summary>
-        /// Status of this procedure, set by the server.
-        /// </summary>
-        [DataMember]
-        public EnumValueInfo Status;
+		/// <summary>
+		/// Status of this procedure, set by the server.
+		/// </summary>
+		[DataMember]
+		public EnumValueInfo Status;
 
-        /// <summary>
-        /// Facility at which this procedure will be performed. Required.
-        /// </summary>
-        [DataMember]
-        public FacilitySummary PerformingFacility;
+		/// <summary>
+		/// Facility at which this procedure will be performed. Required.
+		/// </summary>
+		[DataMember]
+		public FacilitySummary PerformingFacility;
 
-        /// <summary>
-        /// Indicates whether this procedure is to be performed on a portable modality.
-        /// </summary>
-        [DataMember]
-        public bool PortableModality;
+		/// <summary>
+		/// Indicates whether this procedure is to be performed on a portable modality.
+		/// </summary>
+		[DataMember]
+		public bool PortableModality;
 
-        /// <summary>
-        /// Laterality for this procedure.
-        /// </summary>
-        [DataMember]
-        public EnumValueInfo Laterality;
+		/// <summary>
+		/// Laterality for this procedure.
+		/// </summary>
+		[DataMember]
+		public EnumValueInfo Laterality;
 
-        /// <summary>
-        /// Set by the server to indicate whether this requested procedure can be modified
-        /// during an order modification (e.g. it cannot be modified if it is already in-progress).
-        /// </summary>
-        [DataMember]
-        public bool CanModify;
+		/// <summary>
+		/// Set by the server to indicate whether this requested procedure can be modified
+		/// during an order modification (e.g. it cannot be modified if it is already in-progress).
+		/// </summary>
+		[DataMember]
+		public bool CanModify;
 
-        /// <summary>
-        /// Indicates if an existing procedure is checked in or not, and if a new procedure should be checked in upon creation.
-        /// </summary>
-        [DataMember]
-        public bool CheckedIn;
+		/// <summary>
+		/// Indicates if an existing procedure is checked in or not, and if a new procedure should be checked in upon creation.
+		/// </summary>
+		[DataMember]
+		public bool CheckedIn;
 
 		/// <summary>
 		/// Set by the server if this procedure is cancelled, or by the client to indicate that the procedure should be cancelled.
