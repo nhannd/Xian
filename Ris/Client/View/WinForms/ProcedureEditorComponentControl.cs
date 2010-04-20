17e46fea8806e5955e0f8178a412f697d6394b46
jonathan <devnull@localhost>
2010-04-20 16:28:29 +0000

    Ticket(s): #6531 The New Order, Modify Order dialogs should allow the entry by the user of a Department.
    Fixed formatting in this file.

--- Ris/Client/View/WinForms/ProcedureEditorComponentControl.cs
+++ Ris/Client/View/WinForms/ProcedureEditorComponentControl.cs
@@ -36,38 +36,38 @@ using ClearCanvas.Desktop.View.WinForms;
 
 namespace ClearCanvas.Ris.Client.View.WinForms
 {
-    /// <summary>
-    /// Provides a Windows Forms user-interface for <see cref="ProcedureEditorComponent"/>
-    /// </summary>
-    public partial class ProcedureEditorComponentControl : ApplicationComponentUserControl
-    {
-        private readonly ProcedureEditorComponent _component;
+	/// <summary>
+	/// Provides a Windows Forms user-interface for <see cref="ProcedureEditorComponent"/>
+	/// </summary>
+	public partial class ProcedureEditorComponentControl : ApplicationComponentUserControl
+	{
+		private readonly ProcedureEditorComponent _component;
 
-        /// <summary>
-        /// Constructor
-        /// </summary>
-        public ProcedureEditorComponentControl(ProcedureEditorComponent component)
-            : base(component)
-        {
-            InitializeComponent();
+		/// <summary>
+		/// Constructor
+		/// </summary>
+		public ProcedureEditorComponentControl(ProcedureEditorComponent component)
+			: base(component)
+		{
+			InitializeComponent();
 
-            _component = component;
+			_component = component;
 
-            _procedureType.SuggestionProvider = _component.ProcedureTypeSuggestionProvider;
-            _procedureType.Format += delegate(object sender, ListControlConvertEventArgs e)
-                                         {
-                                             e.Value = _component.FormatProcedureType(e.ListItem);
-                                         };
-            _procedureType.DataBindings.Add("Enabled", _component, "IsProcedureTypeEditable");
-            _procedureType.DataBindings.Add("Value", _component, "SelectedProcedureType", true, DataSourceUpdateMode.OnPropertyChanged);
+			_procedureType.SuggestionProvider = _component.ProcedureTypeSuggestionProvider;
+			_procedureType.Format += delegate(object sender, ListControlConvertEventArgs e)
+										 {
+											 e.Value = _component.FormatProcedureType(e.ListItem);
+										 };
+			_procedureType.DataBindings.Add("Enabled", _component, "IsProcedureTypeEditable");
+			_procedureType.DataBindings.Add("Value", _component, "SelectedProcedureType", true, DataSourceUpdateMode.OnPropertyChanged);
 
-            _performingFacility.DataSource = _component.FacilityChoices;
-            _performingFacility.DataBindings.Add("Value", _component, "SelectedFacility", true, DataSourceUpdateMode.OnPropertyChanged);
-            _performingFacility.DataBindings.Add("Enabled", _component, "IsPerformingFacilityEditable");
-            _performingFacility.Format += delegate(object sender, ListControlConvertEventArgs e)
-                                         {
-                                             e.Value = _component.FormatFacility(e.ListItem);
-                                         };
+			_performingFacility.DataSource = _component.FacilityChoices;
+			_performingFacility.DataBindings.Add("Value", _component, "SelectedFacility", true, DataSourceUpdateMode.OnPropertyChanged);
+			_performingFacility.DataBindings.Add("Enabled", _component, "IsPerformingFacilityEditable");
+			_performingFacility.Format += delegate(object sender, ListControlConvertEventArgs e)
+										 {
+											 e.Value = _component.FormatFacility(e.ListItem);
+										 };
 
 			_performingDepartment.DataSource = _component.DepartmentChoices;
 			_performingDepartment.DataBindings.Add("Value", _component, "SelectedDepartment", true, DataSourceUpdateMode.OnPropertyChanged);
@@ -77,38 +77,38 @@ namespace ClearCanvas.Ris.Client.View.WinForms
 				e.Value = _component.FormatDepartment(e.ListItem);
 			};
 
-            _laterality.DataSource = _component.LateralityChoices;
-            _laterality.DataBindings.Add("Value", _component, "SelectedLaterality", true, DataSourceUpdateMode.OnPropertyChanged);
+			_laterality.DataSource = _component.LateralityChoices;
+			_laterality.DataBindings.Add("Value", _component, "SelectedLaterality", true, DataSourceUpdateMode.OnPropertyChanged);
 
-            _scheduledDate.DataBindings.Add("Value", _component, "ScheduledTime", true, DataSourceUpdateMode.OnPropertyChanged);
-            _scheduledDate.DataBindings.Add("Enabled", _component, "IsScheduledTimeEditable");
-            _scheduledTime.DataBindings.Add("Value", _component, "ScheduledTime", true, DataSourceUpdateMode.OnPropertyChanged);
-            _scheduledTime.DataBindings.Add("Enabled", _component, "IsScheduledTimeEditable");
+			_scheduledDate.DataBindings.Add("Value", _component, "ScheduledTime", true, DataSourceUpdateMode.OnPropertyChanged);
+			_scheduledDate.DataBindings.Add("Enabled", _component, "IsScheduledTimeEditable");
+			_scheduledTime.DataBindings.Add("Value", _component, "ScheduledTime", true, DataSourceUpdateMode.OnPropertyChanged);
+			_scheduledTime.DataBindings.Add("Enabled", _component, "IsScheduledTimeEditable");
 
-            _portable.DataBindings.Add("Checked", _component, "PortableModality", true, DataSourceUpdateMode.OnPropertyChanged);
+			_portable.DataBindings.Add("Checked", _component, "PortableModality", true, DataSourceUpdateMode.OnPropertyChanged);
 
-            _checkedIn.DataBindings.Add("Checked", _component, "CheckedIn", true, DataSourceUpdateMode.OnPropertyChanged);
-            _checkedIn.DataBindings.Add("Enabled", _component, "IsCheckedInEnabled", true, DataSourceUpdateMode.OnPropertyChanged);
+			_checkedIn.DataBindings.Add("Checked", _component, "CheckedIn", true, DataSourceUpdateMode.OnPropertyChanged);
+			_checkedIn.DataBindings.Add("Enabled", _component, "IsCheckedInEnabled", true, DataSourceUpdateMode.OnPropertyChanged);
 
 			_component.PropertyChanged += _component_PropertyChanged;
-        }
+		}
 
 		private void _component_PropertyChanged(object sender, PropertyChangedEventArgs e)
 		{
-			if(e.PropertyName == "DepartmentChoicesChanged")
+			if (e.PropertyName == "DepartmentChoicesChanged")
 			{
 				_performingDepartment.DataSource = _component.DepartmentChoices;
 			}
 		}
 
-        private void _okButton_Click(object sender, EventArgs e)
-        {
-            _component.Accept();
-        }
+		private void _okButton_Click(object sender, EventArgs e)
+		{
+			_component.Accept();
+		}
 
-        private void _cancelButton_Click(object sender, EventArgs e)
-        {
-            _component.Cancel();
-        }
-    }
+		private void _cancelButton_Click(object sender, EventArgs e)
+		{
+			_component.Cancel();
+		}
+	}
 }
