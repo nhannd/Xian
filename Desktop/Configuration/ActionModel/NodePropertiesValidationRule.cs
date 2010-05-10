0ebc02295ed2b9a04226a8b0176d817efcff646d
jasper <devnull@localhost>
2010-05-10 20:46:49 +0000

    Requirement: #2186 - User configurable toolbars/menus Requirement: #6021 - UI configurability (Phase 1) Ticket: #6586 - Mouse button assignments for toolbar and menu items Ticket: #6681 - Exclusive mouse button assignments '''Schema/Configuration Changes: YES'''  * Added intrinsic support for typed value validation rules.

--- Desktop/Configuration/ActionModel/NodePropertiesValidationRule.cs
+++ Desktop/Configuration/ActionModel/NodePropertiesValidationRule.cs
@@ -39,8 +39,6 @@ namespace ClearCanvas.Desktop.Configuration.ActionModel
 		bool Validate(AbstractActionModelTreeNode node, string propertyName, object value);
 	}
 
-	public delegate bool NodePropertiesValidationRuleDelegate<T>(T node, object value) where T : AbstractActionModelTreeNode;
-
 	public sealed class NodePropertiesValidationPolicy : INodePropertiesValidationRule
 	{
 		private readonly List<INodePropertiesValidationRule> _rules;
@@ -73,6 +71,13 @@ namespace ClearCanvas.Desktop.Configuration.ActionModel
 			this.AddRule(new NodePropertiesValidationRule<T>(propertyName, rule));
 		}
 
+		public void AddRule<TNode, TValue>(string propertyName, NodePropertiesValidationRuleDelegate<TNode, TValue> rule) where TNode : AbstractActionModelTreeNode
+		{
+			Platform.CheckForEmptyString(propertyName, "propertyName");
+			Platform.CheckForNullReference(rule, "rule");
+			this.AddRule(new NodePropertiesValidationRule<TNode, TValue>(propertyName, rule));
+		}
+
 		public bool Validate(AbstractActionModelTreeNode node, string propertyName, object value)
 		{
 			foreach (INodePropertiesValidationRule rule in _rules)
@@ -83,6 +88,10 @@ namespace ClearCanvas.Desktop.Configuration.ActionModel
 			}
 			return true;
 		}
+
+		public delegate bool NodePropertiesValidationRuleDelegate<T>(T node, object value) where T : AbstractActionModelTreeNode;
+
+		public delegate bool NodePropertiesValidationRuleDelegate<TNode, TValue>(TNode node, TValue value) where TNode : AbstractActionModelTreeNode;
 
 		private class NodePropertiesValidationRule<T> : INodePropertiesValidationRule where T : AbstractActionModelTreeNode
 		{
@@ -102,6 +111,29 @@ namespace ClearCanvas.Desktop.Configuration.ActionModel
 				if (!(node is T))
 					return true;
 				return _delegate.Invoke((T) node, value);
+			}
+		}
+
+		private class NodePropertiesValidationRule<TNode, TValue> : INodePropertiesValidationRule where TNode : AbstractActionModelTreeNode
+		{
+			private readonly NodePropertiesValidationRuleDelegate<TNode, TValue> _delegate;
+			private readonly string _propertyName;
+
+			public NodePropertiesValidationRule(string propertyName, NodePropertiesValidationRuleDelegate<TNode, TValue> @delegate)
+			{
+				_delegate = @delegate;
+				_propertyName = propertyName;
+			}
+
+			public bool Validate(AbstractActionModelTreeNode node, string propertyName, object value)
+			{
+				if (_propertyName != propertyName)
+					return true;
+				if (!(node is TNode))
+					return true;
+				if (!(value is TValue))
+					return true;
+				return _delegate.Invoke((TNode) node, (TValue) value);
 			}
 		}
 	}
