f58305d9aa9b56c492fc4c1f614b641f134db5c2
rob <devnull@localhost>
2010-05-31 21:10:37 +0000

    Ticket: #6188 - An external practitioner should always have exactly 1 default contact point
    Schema/Configuration Changes: N/A
    
     * Tabs/resharper cleanup.

--- Ris/Client/ExternalPractitionerContactPointDetailsEditorComponent.cs
+++ Ris/Client/ExternalPractitionerContactPointDetailsEditorComponent.cs
@@ -29,118 +29,115 @@
 
 #endregion
 
-using System;
+using System.Collections;
 using System.Collections.Generic;
-using System.Text;
-
 using ClearCanvas.Common;
 using ClearCanvas.Desktop;
-using ClearCanvas.Ris.Application.Common;
-using System.Collections;
 using ClearCanvas.Desktop.Validation;
+using ClearCanvas.Ris.Application.Common;
 
 namespace ClearCanvas.Ris.Client
 {
-    /// <summary>
-    /// Extension point for views onto <see cref="ExternalPractitionerContactPointDetailsEditorComponent"/>
-    /// </summary>
-    [ExtensionPoint]
-    public class ExternalPractitionerContactPointDetailsEditorComponentViewExtensionPoint : ExtensionPoint<IApplicationComponentView>
-    {
-    }
+	/// <summary>
+	/// Extension point for views onto <see cref="ExternalPractitionerContactPointDetailsEditorComponent"/>
+	/// </summary>
+	[ExtensionPoint]
+	public class ExternalPractitionerContactPointDetailsEditorComponentViewExtensionPoint : ExtensionPoint<IApplicationComponentView>
+	{
+	}
 
-    /// <summary>
-    /// ExternalPractitionerContactPointDetailsEditorComponent class
-    /// </summary>
-    [AssociateView(typeof(ExternalPractitionerContactPointDetailsEditorComponentViewExtensionPoint))]
-    public class ExternalPractitionerContactPointDetailsEditorComponent : ApplicationComponent
-    {
-        private ExternalPractitionerContactPointDetail _contactPointDetail;
-        private IList<EnumValueInfo> _resultCommunicationModeChoices;
+	/// <summary>
+	/// ExternalPractitionerContactPointDetailsEditorComponent class
+	/// </summary>
+	[AssociateView(typeof(ExternalPractitionerContactPointDetailsEditorComponentViewExtensionPoint))]
+	public class ExternalPractitionerContactPointDetailsEditorComponent : ApplicationComponent
+	{
+		private readonly ExternalPractitionerContactPointDetail _contactPointDetail;
+		private readonly IList<EnumValueInfo> _resultCommunicationModeChoices;
 
-        /// <summary>
-        /// Constructor
-        /// </summary>
-        public ExternalPractitionerContactPointDetailsEditorComponent(ExternalPractitionerContactPointDetail contactPointDetail, IList<EnumValueInfo> resultCommunicationModeChoices)
-        {
-            _contactPointDetail = contactPointDetail;
-            _resultCommunicationModeChoices = resultCommunicationModeChoices;
-        }
+		/// <summary>
+		/// Constructor
+		/// </summary>
+		public ExternalPractitionerContactPointDetailsEditorComponent(ExternalPractitionerContactPointDetail contactPointDetail, IList<EnumValueInfo> resultCommunicationModeChoices)
+		{
+			_contactPointDetail = contactPointDetail;
+			_resultCommunicationModeChoices = resultCommunicationModeChoices;
+		}
 
-        #region Presentation Model
+		#region Presentation Model
 
-        [ValidateNotNull]
-        public string ContactPointName
-        {
-            get { return _contactPointDetail.Name; }
-            set
-            {
-                _contactPointDetail.Name = value;
-                this.Modified = true;
-            }
-        }
+		[ValidateNotNull]
+		public string ContactPointName
+		{
+			get { return _contactPointDetail.Name; }
+			set
+			{
+				_contactPointDetail.Name = value;
+				this.Modified = true;
+			}
+		}
 
-        public string ContactPointDescription
-        {
-            get { return _contactPointDetail.Description; }
-            set
-            {
-                _contactPointDetail.Description = value;
-                this.Modified = true;
-            }
-        }
+		public string ContactPointDescription
+		{
+			get { return _contactPointDetail.Description; }
+			set
+			{
+				_contactPointDetail.Description = value;
+				this.Modified = true;
+			}
+		}
 
-        public bool IsDefaultContactPoint
-        {
-            get { return _contactPointDetail.IsDefaultContactPoint; }
-            set
-            {
-                if(_contactPointDetail.IsDefaultContactPoint == value)
-                    return;
+		public bool IsDefaultContactPoint
+		{
+			get { return _contactPointDetail.IsDefaultContactPoint; }
+			set
+			{
+				if (_contactPointDetail.IsDefaultContactPoint == value)
+					return;
 
-                if (value && _contactPointDetail.Deactivated && UserLeavesContactPointDeactivated())
-                {
-                    NotifyPropertyChanged("IsDefaultContactPoint");
-                    return;
-                }
+				if (value && _contactPointDetail.Deactivated && UserLeavesContactPointDeactivated())
+				{
+					NotifyPropertyChanged("IsDefaultContactPoint");
+					return;
+				}
 
-                _contactPointDetail.IsDefaultContactPoint = value;
-                this.Modified = true;
-            }
-        }
+				_contactPointDetail.IsDefaultContactPoint = value;
+				this.Modified = true;
+			}
+		}
 
-        public IList ResultCommunicationModeChoices
-        {
-            get { return (IList)_resultCommunicationModeChoices; }
-        }
+		public IList ResultCommunicationModeChoices
+		{
+			get { return (IList)_resultCommunicationModeChoices; }
+		}
 
-        [ValidateNotNull]
-        public EnumValueInfo SelectedResultCommunicationMode
-        {
-            get { return _contactPointDetail.PreferredResultCommunicationMode; }
-            set
-            {
-                _contactPointDetail.PreferredResultCommunicationMode = value;
-                this.Modified = true;
-            }
-        }
+		[ValidateNotNull]
+		public EnumValueInfo SelectedResultCommunicationMode
+		{
+			get { return _contactPointDetail.PreferredResultCommunicationMode; }
+			set
+			{
+				_contactPointDetail.PreferredResultCommunicationMode = value;
+				this.Modified = true;
+			}
+		}
 
-        #endregion
+		#endregion
 
-        private bool UserLeavesContactPointDeactivated()
-        {
-        	var activateResponse =
-                this.Host.ShowMessageBox(
-                    "This contact point is not currently active and must be active in order to set it as the default.  Would you like to make it active?",
-                    MessageBoxActions.YesNo);
+		private bool UserLeavesContactPointDeactivated()
+		{
+			var activateResponse =
+				this.Host.ShowMessageBox(
+					"This contact point is not currently active and must be active in order to set it as the default.  Would you like to make it active?",
+					MessageBoxActions.YesNo);
 
-            if (activateResponse != DialogBoxAction.Yes)
-            {
-                return true;
-            }
+			if (activateResponse != DialogBoxAction.Yes)
+			{
+				return true;
+			}
 
-            _contactPointDetail.Deactivated = false;
-            return false;
-        }
-    }
+			_contactPointDetail.Deactivated = false;
+			return false;
+		}
+	}
 }
--- Ris/Client/View/WinForms/ExternalPractitionerContactPointDetailsEditorComponentControl.cs
+++ Ris/Client/View/WinForms/ExternalPractitionerContactPointDetailsEditorComponentControl.cs
@@ -29,52 +29,47 @@
 
 #endregion
 
-using System;
-using System.Collections.Generic;
 using System.ComponentModel;
-using System.Drawing;
-using System.Data;
-using System.Text;
 using System.Windows.Forms;
 
 using ClearCanvas.Desktop.View.WinForms;
 
 namespace ClearCanvas.Ris.Client.View.WinForms
 {
-    /// <summary>
-    /// Provides a Windows Forms user-interface for <see cref="ExternalPractitionerContactPointDetailsEditorComponent"/>
-    /// </summary>
-    public partial class ExternalPractitionerContactPointDetailsEditorComponentControl : ApplicationComponentUserControl
-    {
-        private ExternalPractitionerContactPointDetailsEditorComponent _component;
+	/// <summary>
+	/// Provides a Windows Forms user-interface for <see cref="ExternalPractitionerContactPointDetailsEditorComponent"/>
+	/// </summary>
+	public partial class ExternalPractitionerContactPointDetailsEditorComponentControl : ApplicationComponentUserControl
+	{
+		private readonly ExternalPractitionerContactPointDetailsEditorComponent _component;
 
-        /// <summary>
-        /// Constructor
-        /// </summary>
-        public ExternalPractitionerContactPointDetailsEditorComponentControl(ExternalPractitionerContactPointDetailsEditorComponent component)
-            :base(component)
-        {
-            InitializeComponent();
+		/// <summary>
+		/// Constructor
+		/// </summary>
+		public ExternalPractitionerContactPointDetailsEditorComponentControl(ExternalPractitionerContactPointDetailsEditorComponent component)
+			: base(component)
+		{
+			InitializeComponent();
 
-            _component = component;
-            _component.PropertyChanged += _component_PropertyChanged;
+			_component = component;
+			_component.PropertyChanged += _component_PropertyChanged;
 
-            _name.DataBindings.Add("Value", _component, "ContactPointName", true, DataSourceUpdateMode.OnPropertyChanged);
-            _description.DataBindings.Add("Value", _component, "ContactPointDescription", true, DataSourceUpdateMode.OnPropertyChanged);
-            _isDefaultContactPoint.DataBindings.Add("Checked", _component, "IsDefaultContactPoint", true, DataSourceUpdateMode.OnPropertyChanged);
-            _resultCommunicationMode.DataBindings.Add("Value", _component, "SelectedResultCommunicationMode", true, DataSourceUpdateMode.OnPropertyChanged);
-            _resultCommunicationMode.DataSource = _component.ResultCommunicationModeChoices;
-        }
+			_name.DataBindings.Add("Value", _component, "ContactPointName", true, DataSourceUpdateMode.OnPropertyChanged);
+			_description.DataBindings.Add("Value", _component, "ContactPointDescription", true, DataSourceUpdateMode.OnPropertyChanged);
+			_isDefaultContactPoint.DataBindings.Add("Checked", _component, "IsDefaultContactPoint", true, DataSourceUpdateMode.OnPropertyChanged);
+			_resultCommunicationMode.DataBindings.Add("Value", _component, "SelectedResultCommunicationMode", true, DataSourceUpdateMode.OnPropertyChanged);
+			_resultCommunicationMode.DataSource = _component.ResultCommunicationModeChoices;
+		}
 
-        private void _component_PropertyChanged(object sender, PropertyChangedEventArgs e)
-        {
-            if (e.PropertyName == "IsDefaultContactPoint")
-            {
-                if (_isDefaultContactPoint.Checked != _component.IsDefaultContactPoint)
-                {
-                    _isDefaultContactPoint.Checked = _component.IsDefaultContactPoint;
-                }
-            }
-        }
-    }
+		private void _component_PropertyChanged(object sender, PropertyChangedEventArgs e)
+		{
+			if (e.PropertyName == "IsDefaultContactPoint")
+			{
+				if (_isDefaultContactPoint.Checked != _component.IsDefaultContactPoint)
+				{
+					_isDefaultContactPoint.Checked = _component.IsDefaultContactPoint;
+				}
+			}
+		}
+	}
 }
