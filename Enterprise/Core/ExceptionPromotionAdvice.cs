712e7efc1036f701ddde3b4e208291c11a526497
jonathan <devnull@localhost>
2010-06-07 17:04:15 +0000

    Defect: #6773 NullReferenceException thrown on Enterprise server when settings property/group is not found
    Formatting and resharper.

--- Enterprise/Core/ExceptionPromotionAdvice.cs
+++ Enterprise/Core/ExceptionPromotionAdvice.cs
@@ -30,14 +30,9 @@
 #endregion
 
 using System;
-using System.Collections.Generic;
-using System.Text;
 using System.ServiceModel;
-using System.Reflection;
 using ClearCanvas.Common.Utilities;
 using ClearCanvas.Enterprise.Common;
-using ClearCanvas.Enterprise.Core;
-using Castle.DynamicProxy;
 using Castle.Core.Interceptor;
 
 namespace ClearCanvas.Enterprise.Core
@@ -45,100 +40,95 @@ namespace ClearCanvas.Enterprise.Core
 	/// <summary>
 	/// Promotes certain types of exceptions to WCF faults (<see cref="FaultException{TDetail}"/>).
 	/// </summary>
-    public class ExceptionPromotionAdvice : IInterceptor
-    {
-        #region IMethodInterceptor Members
+	public class ExceptionPromotionAdvice : IInterceptor
+	{
+		#region IMethodInterceptor Members
 
-        public void Intercept(IInvocation invocation)
-        {
-            try
-            {
-                invocation.Proceed();
-            }
-            catch (Exception e)
-            {
-                // translate exception if necessary
-                Exception translated = TranslateException(e);
+		public void Intercept(IInvocation invocation)
+		{
+			try
+			{
+				invocation.Proceed();
+			}
+			catch (Exception e)
+			{
+				// translate exception if necessary
+				var translated = TranslateException(e);
 
-                // try promoting the exception to a fault
-                // if successful, throw the fault exception, otherwise throw the normal exception
-                FaultException fault = PromoteExceptionToFault(translated, invocation.InvocationTarget, invocation.Method.Name);
-                if (fault != null)
-                    throw fault;
-                else
-                    throw translated;
-            }
-        }
+				// try promoting the exception to a fault
+				// if successful, throw the fault exception, otherwise throw the normal exception
+				var fault = PromoteExceptionToFault(translated, invocation.InvocationTarget, invocation.Method.Name);
+				if (fault != null)
+					throw fault;
+				else
+					throw translated;
+			}
+		}
 
-        #endregion
+		#endregion
 
-        /// <summary>
-        /// Translate certain "special" classes of exception to classes that can be passed through the service boundary.
-        /// </summary>
-        /// <param name="e"></param>
-        /// <returns></returns>
-        private Exception TranslateException(Exception e)
-        {
-            // special handling of EntityVersionException
-            // assume all such exceptions occured because of concurrent modifications
-            // wrap in ConcurrentModificationException will be used in the fault contract
-            if (e is EntityVersionException)
-            {
-                return new ConcurrentModificationException(e.Message);
-            }
+		/// <summary>
+		/// Translate certain "special" classes of exception to classes that can be passed through the service boundary.
+		/// </summary>
+		/// <param name="e"></param>
+		/// <returns></returns>
+		private static Exception TranslateException(Exception e)
+		{
+			// special handling of EntityVersionException
+			// assume all such exceptions occured because of concurrent modifications
+			// wrap in ConcurrentModificationException will be used in the fault contract
+			if (e is EntityVersionException)
+			{
+				return new ConcurrentModificationException(e.Message);
+			}
 
-            // special handling of EntityValidationException
-            // convert to RequestValidationException
-            if (e is EntityValidationException)
-            {
-                return new RequestValidationException(e.Message);
-            }
+			// special handling of EntityValidationException
+			// convert to RequestValidationException
+			if (e is EntityValidationException)
+			{
+				return new RequestValidationException(e.Message);
+			}
 
-            // no translation
-            return e;
-        }
+			// no translation
+			return e;
+		}
 
-        /// <summary>
-        /// Promotes the exception to a fault, if the exception type has a corresponding fault contract.
-        /// Otherwise returns null.
-        /// </summary>
-        /// <param name="e"></param>
-        /// <param name="serviceInstance"></param>
-        /// <param name="operationMethodName"></param>
-        /// <returns></returns>
-        private FaultException PromoteExceptionToFault(Exception e, object serviceInstance, string operationMethodName)
-        {
-            // get the service contract
-            ServiceImplementsContractAttribute serviceContractAttr = CollectionUtils.FirstElement<ServiceImplementsContractAttribute>(
-                serviceInstance.GetType().GetCustomAttributes(typeof(ServiceImplementsContractAttribute), false));
+		/// <summary>
+		/// Promotes the exception to a fault, if the exception type has a corresponding fault contract.
+		/// Otherwise returns null.
+		/// </summary>
+		/// <param name="e"></param>
+		/// <param name="serviceInstance"></param>
+		/// <param name="operationMethodName"></param>
+		/// <returns></returns>
+		private static FaultException PromoteExceptionToFault(Exception e, object serviceInstance, string operationMethodName)
+		{
+			// get the service contract
+			var serviceContractAttr = CollectionUtils.FirstElement<ServiceImplementsContractAttribute>(
+				serviceInstance.GetType().GetCustomAttributes(typeof(ServiceImplementsContractAttribute), false));
 
-            if (serviceContractAttr == null)
-                return null;  // this should never happen, but if it does, there's nothing we can do
+			if (serviceContractAttr == null)
+				return null;  // this should never happen, but if it does, there's nothing we can do
 
-            // get the operation contract method
-            MethodInfo method = serviceContractAttr.ServiceContract.GetMethod(operationMethodName);
+			// get the operation contract method
+			var method = serviceContractAttr.ServiceContract.GetMethod(operationMethodName);
 
-            // find a fault contract for this exception type
-            object[] faultContracts = method.GetCustomAttributes(typeof(FaultContractAttribute), true);
-            FaultContractAttribute faultContract = CollectionUtils.SelectFirst<FaultContractAttribute>(
-                faultContracts,
-                delegate(FaultContractAttribute a)
-                {
-                    return a.DetailType.Equals(e.GetType());
-                });
+			// find a fault contract for this exception type
+			var faultContracts = method.GetCustomAttributes(typeof(FaultContractAttribute), true);
+			var faultContract = CollectionUtils.SelectFirst(faultContracts, (FaultContractAttribute a) => a.DetailType.Equals(e.GetType()));
 
-            if (faultContract != null)
-            {
-                // from Juval Lowey
-                Type faultUnboundedType = typeof(FaultException<>);
-                Type faultBoundedType = faultUnboundedType.MakeGenericType(e.GetType());
-                //Exception newException = (Exception)Activator.CreateInstance(error.GetType(), error.Message);
-                FaultException faultException = (FaultException)Activator.CreateInstance(faultBoundedType, e, new FaultReason(e.Message));
+			if (faultContract != null)
+			{
+				// from Juval Lowey
+				var faultUnboundedType = typeof(FaultException<>);
+				var faultBoundedType = faultUnboundedType.MakeGenericType(e.GetType());
+				//Exception newException = (Exception)Activator.CreateInstance(error.GetType(), error.Message);
+				var faultException = (FaultException)Activator.CreateInstance(faultBoundedType, e, new FaultReason(e.Message));
 
-                return faultException;
-            }
+				return faultException;
+			}
 
-            return null;    // no fault contract for this exception
-        }
-    }
+			return null;    // no fault contract for this exception
+		}
+	}
 }
