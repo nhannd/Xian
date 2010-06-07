f647186cbb29584d9fff93b6b74574636a8757d4
jonathan <devnull@localhost>
2010-06-07 16:08:27 +0000

    Ticket: #6774 Validation rules are tested at inappropriate time
    Part II
    
    Formatting and resharper suggestions.

--- Enterprise/Hibernate/ChangeTracker.cs
+++ Enterprise/Hibernate/ChangeTracker.cs
@@ -31,83 +31,80 @@
 
 using System;
 using System.Collections.Generic;
-using System.Text;
 using ClearCanvas.Enterprise.Common;
 using ClearCanvas.Enterprise.Core;
 using ClearCanvas.Common.Utilities;
-using Iesi.Collections;
 
 namespace ClearCanvas.Enterprise.Hibernate
 {
-    /// <summary>
-    /// Helper class used by <see cref="UpdateContextInterceptor"/> to track changes to entities during usage
-    /// of an update context.
-    /// </summary>
-    internal class ChangeTracker
-    {
+	/// <summary>
+	/// Helper class used by <see cref="UpdateContextInterceptor"/> to track changes to entities during usage
+	/// of an update context.
+	/// </summary>
+	internal class ChangeTracker
+	{
 
-        // keeps a record of changes made to entities
-        // a given entity instance will only appear once in this list
-        // (a dictionary is not used because we cannot control the implemention of the entity's Equals/GetHashCode methods,
-        // and we want to deal strictly with reference equality here)
-        private readonly List<ChangeRecord> _changeRecords = new List<ChangeRecord>();
+		// keeps a record of changes made to entities
+		// a given entity instance will only appear once in this list
+		// (a dictionary is not used because we cannot control the implemention of the entity's Equals/GetHashCode methods,
+		// and we want to deal strictly with reference equality here)
+		private readonly List<ChangeRecord> _changeRecords = new List<ChangeRecord>();
 
-        /// <summary>
-        /// Records that the specified change occured to the specified entity.
-        /// </summary>
-        /// <param name="entity"></param>
-        /// <param name="changeType"></param>
-        /// <param name="propertyDiffs"></param>
+		/// <summary>
+		/// Records that the specified change occured to the specified entity.
+		/// </summary>
+		/// <param name="entity"></param>
+		/// <param name="changeType"></param>
+		/// <param name="propertyDiffs"></param>
 		internal void RecordChange(Entity entity, EntityChangeType changeType, PropertyDiff[] propertyDiffs)
-        {
-            ChangeRecord thisChange = new ChangeRecord(entity, changeType, propertyDiffs);
+		{
+			var thisChange = new ChangeRecord(entity, changeType, propertyDiffs);
 
-            // check if this entity was already recorded in the list
-            // note that reference equality is used because we do not know how the Equals method of the entity may be implemented
-            // for example, it may define equality based on a property whose value has changed, which would break this logic
-            int prevRecordIndex = _changeRecords.FindIndex(delegate(ChangeRecord r) { return ReferenceEquals(r.Entity, entity); });
-            if (prevRecordIndex > -1)
-            {
-                // this entity was already marked as changed
-                ChangeRecord previousChange = _changeRecords[prevRecordIndex];
+			// check if this entity was already recorded in the list
+			// note that reference equality is used because we do not know how the Equals method of the entity may be implemented
+			// for example, it may define equality based on a property whose value has changed, which would break this logic
+			var prevRecordIndex = _changeRecords.FindIndex(r => ReferenceEquals(r.Entity, entity));
+			if (prevRecordIndex > -1)
+			{
+				// this entity was already marked as changed
+				var previousChange = _changeRecords[prevRecordIndex];
 
-                // compound the previous change record with this one
-                _changeRecords[prevRecordIndex] = thisChange.Compound(previousChange);
-            }
-            else
-            {
-                // record this change in the change set
-                _changeRecords.Add(thisChange);
-            }
-        }
+				// compound the previous change record with this one
+				_changeRecords[prevRecordIndex] = thisChange.Compound(previousChange);
+			}
+			else
+			{
+				// record this change in the change set
+				_changeRecords.Add(thisChange);
+			}
+		}
 
+		/// <summary>
+		/// Clears all change records.
+		/// </summary>
 		internal void Clear()
 		{
 			_changeRecords.Clear();
 		}
 
-        /// <summary>
-        /// Gets the set of <see cref="EntityChange"/> objects representing the cumulative changes made.
-        /// </summary>
+		/// <summary>
+		/// Gets the set of <see cref="EntityChange"/> objects representing the cumulative changes made.
+		/// </summary>
 		internal EntityChange[] EntityChangeSet
-        {
-            get
-            {
-                return CollectionUtils.Map<ChangeRecord, EntityChange>(_changeRecords,
-                    delegate(ChangeRecord changeRecord)
-                    {
-                        return changeRecord.AsEntityChange();
-                    }).ToArray();
-            }
-        }
+		{
+			get
+			{
+				return CollectionUtils.Map(_changeRecords, (ChangeRecord c) => c.AsEntityChange()).ToArray();
+			}
+		}
 
 		/// <summary>
 		/// Get the set of changed entities matching the specified filter.
 		/// </summary>
 		internal IList<Entity> GetChangedEntities(Predicate<EntityChangeType> filter)
-    	{
+		{
 			var filtered = CollectionUtils.Select(_changeRecords, record => filter(record.ChangeType));
 			return CollectionUtils.Map(filtered, (ChangeRecord cr) => cr.Entity);
-    	}
-    }
+		}
+	}
 }
