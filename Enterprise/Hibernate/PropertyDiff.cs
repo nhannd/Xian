cad39132061650c9fb36acfffda7a0d1d3e7ec94
jonathan <devnull@localhost>
2010-06-08 20:44:09 +0000

    Ticket(s): #6211 - Implement the OWLS module
    Formatting and resharper suggestions fixed...........

--- Enterprise/Hibernate/PropertyDiff.cs
+++ Enterprise/Hibernate/PropertyDiff.cs
@@ -37,15 +37,15 @@ using NHibernate;
 
 namespace ClearCanvas.Enterprise.Hibernate
 {
-    /// <summary>
-    /// Used by <see cref="ChangeRecord"/> to record changes to individual properties.
-    /// </summary>
-    class PropertyDiff
+	/// <summary>
+	/// Used by <see cref="ChangeRecord"/> to record changes to individual properties.
+	/// </summary>
+	class PropertyDiff
 	{
 		private readonly string _propertyName;
-        private readonly object _oldValue;
-        private readonly object _newValue;
-        private readonly IType _hibernateType;
+		private readonly object _oldValue;
+		private readonly object _newValue;
+		private readonly IType _hibernateType;
 
 		/// <summary>
 		/// Constructor
@@ -54,7 +54,7 @@ namespace ClearCanvas.Enterprise.Hibernate
 		/// <param name="hibernateType"></param>
 		/// <param name="oldValue"></param>
 		/// <param name="newValue"></param>
-		public PropertyDiff(string propertyName, IType hibernateType, object oldValue, object newValue)
+		internal PropertyDiff(string propertyName, IType hibernateType, object oldValue, object newValue)
 		{
 			_propertyName = propertyName;
 			_hibernateType = hibernateType;
@@ -62,54 +62,54 @@ namespace ClearCanvas.Enterprise.Hibernate
 			_newValue = newValue;
 		}
 
-        public string PropertyName
-        {
-            get { return _propertyName; }
-        }
+		public string PropertyName
+		{
+			get { return _propertyName; }
+		}
 
-        public bool IsCollectionProperty
-        {
-            get { return _hibernateType.IsCollectionType; }
-        }
+		public bool IsCollectionProperty
+		{
+			get { return _hibernateType.IsCollectionType; }
+		}
 
-        public bool IsChanged
-        {
-            get
-            {
+		public bool IsChanged
+		{
+			get
+			{
 				// if we're dealing with a collection property
-				if(IsCollectionProperty)
+				if (IsCollectionProperty)
 				{
-                    // an uninitialized collection cannot have changed
-                    if (!NHibernateUtil.IsInitialized(_newValue))
-                        return false;
+					// an uninitialized collection cannot have changed
+					if (!NHibernateUtil.IsInitialized(_newValue))
+						return false;
 
-                    //need to compare collection contents
+					//need to compare collection contents
 					//TODO: collections with list semantics should use order-sensitive comparisons, but how do we know??
 					//(e.g how do we differentiate a "bag" from a "list"?)
-					return !CollectionUtils.Equal((ICollection) _oldValue, (ICollection) _newValue, false);
+					return !CollectionUtils.Equal((ICollection)_oldValue, (ICollection)_newValue, false);
 				}
 
-            	return !Equals(_oldValue, _newValue);
-            }
-        }
+				return !Equals(_oldValue, _newValue);
+			}
+		}
 
-        public PropertyChange AsPropertyChange()
-        {
-            return new PropertyChange(_propertyName, _oldValue, _newValue);
-        }
+		public PropertyChange AsPropertyChange()
+		{
+			return new PropertyChange(_propertyName, _oldValue, _newValue);
+		}
 
-        /// <summary>
-        /// Returns a new <see cref="PropertyDiff"/> that is the result of adding this change
-        /// to <paramref name="previousChange"/>.
-        /// </summary>
-        /// <param name="previousChange"></param>
-        /// <returns></returns>
-        /// <remarks>
-        /// This operation is not commutative.
-        /// </remarks>
-        public PropertyDiff Compound(PropertyDiff previousChange)
-        {
+		/// <summary>
+		/// Returns a new <see cref="PropertyDiff"/> that is the result of adding this change
+		/// to <paramref name="previousChange"/>.
+		/// </summary>
+		/// <param name="previousChange"></param>
+		/// <returns></returns>
+		/// <remarks>
+		/// This operation is not commutative.
+		/// </remarks>
+		public PropertyDiff Compound(PropertyDiff previousChange)
+		{
 			return new PropertyDiff(_propertyName, _hibernateType, previousChange._oldValue, _newValue);
 		}
-    }
+	}
 }
