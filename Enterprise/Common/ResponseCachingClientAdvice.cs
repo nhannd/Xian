69ff7c9dc3ea0dcb76d269dbecb9e956d2c1a571
jonathan <devnull@localhost>
2010-06-10 21:41:23 +0000

    Task: #6783 Investigate use of caching on folder counts
    Formatted code and fixed resharper suggestions so that I can implement server-side caching in order to investigate this.

--- Enterprise/Common/ResponseCachingClientAdvice.cs
+++ Enterprise/Common/ResponseCachingClientAdvice.cs
@@ -30,7 +30,6 @@
 #endregion
 
 using System;
-using Castle.DynamicProxy;
 using Castle.Core.Interceptor;
 using System.ServiceModel;
 using ClearCanvas.Enterprise.Common.Caching;
@@ -41,42 +40,42 @@ namespace ClearCanvas.Enterprise.Common
 	/// Client-side advice to implement transparent caching on the client side,
 	/// where the server returns an appropriate <see cref="ResponseCachingDirective"/>.
 	/// </summary>
-    class ResponseCachingClientAdvice : IInterceptor
-    {
-        #region IInterceptor Members
+	class ResponseCachingClientAdvice : IInterceptor
+	{
+		#region IInterceptor Members
 
-        public void Intercept(IInvocation invocation)
-        {
-            object service = invocation.InvocationTarget;
-            string region = string.Format("{0}.{1}",
-                invocation.Method.DeclaringType.FullName, invocation.Method.Name);
-            object request = invocation.Arguments[0];
+		public void Intercept(IInvocation invocation)
+		{
+			var service = invocation.InvocationTarget;
+			var region = string.Format("{0}.{1}",
+				invocation.Method.DeclaringType.FullName, invocation.Method.Name);
+			var request = invocation.Arguments[0];
 
 			// check for a cached response
 			// must do this even if this operation does not support caching, because have no way of knowing
 			// whether it does or does not support caching
-            string cacheKey = GetCacheKey(request);
-			using (ICacheClient cacheClient = Cache.CreateClient("ResponseCache"))
+			var cacheKey = GetCacheKey(request);
+			using (var cacheClient = Cache.CreateClient("ResponseCache"))
 			{
 				if (cacheKey != null && cacheClient.RegionExists(region))
 				{
 					// check cache
-				    object response = cacheClient.Get(cacheKey, new CacheGetOptions(region));
-                    if(response != null)
-                    {
-                        invocation.ReturnValue = response;
-                        return;
-                    }
+					var response = cacheClient.Get(cacheKey, new CacheGetOptions(region));
+					if (response != null)
+					{
+						invocation.ReturnValue = response;
+						return;
+					}
 				}
 
 				// no cached response is available, so invoke service operation
-				using (new OperationContextScope(service as IContextChannel))
+				using (new OperationContextScope((IContextChannel)service))
 				{
 					// invoke the operation
-                    invocation.Proceed();
+					invocation.Proceed();
 
 					// read caching directive from headers
-					ResponseCachingDirective directive = ReadCachingDirectiveHeader();
+					var directive = ReadCachingDirectiveHeader();
 
 					// cache the response if directed
 					if (directive != null && directive.EnableCaching)
@@ -91,42 +90,42 @@ namespace ClearCanvas.Enterprise.Common
 					}
 				}
 			}
-        }
+		}
 
 		/// <summary>
 		/// Obtains the cache key for the specified request object.
 		/// </summary>
 		/// <param name="request"></param>
 		/// <returns></returns>
-        private static string GetCacheKey(object request)
-        {
-            if (request is IDefinesCacheKey)
-            {
-                return (request as IDefinesCacheKey).GetCacheKey();
-            }
-            // for now, the request must implement an interface to get a cache key
-            // in future, we could add some automatic serialization to turn it into a cache key
-            return null;
-        }
+		private static string GetCacheKey(object request)
+		{
+			if (request is IDefinesCacheKey)
+			{
+				return (request as IDefinesCacheKey).GetCacheKey();
+			}
+			// for now, the request must implement an interface to get a cache key
+			// in future, we could add some automatic serialization to turn it into a cache key
+			return null;
+		}
 
 		/// <summary>
 		/// Attempts to read the cache directive header from the message, returning null if the header doesn't exist.
 		/// </summary>
 		/// <returns></returns>
-        private static ResponseCachingDirective ReadCachingDirectiveHeader()
-        {
-            int h = OperationContext.Current.IncomingMessageHeaders.FindHeader(
-                        ResponseCachingDirective.HeaderName,
-                        ResponseCachingDirective.HeaderNamespace);
-            if (h > -1)
-            {
-                return OperationContext.Current.IncomingMessageHeaders
-                     .GetHeader<ResponseCachingDirective>(h);
-            }
+		private static ResponseCachingDirective ReadCachingDirectiveHeader()
+		{
+			var h = OperationContext.Current.IncomingMessageHeaders.FindHeader(
+						ResponseCachingDirective.HeaderName,
+						ResponseCachingDirective.HeaderNamespace);
+			if (h > -1)
+			{
+				return OperationContext.Current.IncomingMessageHeaders
+					 .GetHeader<ResponseCachingDirective>(h);
+			}
 
-            return null;
-        }
+			return null;
+		}
 
-        #endregion
-    }
+		#endregion
+	}
 }
--- Enterprise/Core/ResponseCachingAdvice.cs
+++ Enterprise/Core/ResponseCachingAdvice.cs
@@ -30,7 +30,6 @@
 #endregion
 
 using System;
-using Castle.DynamicProxy;
 using Castle.Core.Interceptor;
 using ClearCanvas.Common.Utilities;
 using System.ServiceModel;
@@ -44,17 +43,17 @@ namespace ClearCanvas.Enterprise.Core
 	/// Advice class responsible for honouring the <see cref="ResponseCachingAttribute"/> that may
 	/// be applied to service operations.
 	/// </summary>
-    public class ResponseCachingAdvice : ServiceOperationAdvice, IInterceptor
-    {
-        #region IInterceptor Members
+	public class ResponseCachingAdvice : ServiceOperationAdvice, IInterceptor
+	{
+		#region IInterceptor Members
 
-        public void Intercept(IInvocation invocation)
-        {
-            invocation.Proceed();
+		public void Intercept(IInvocation invocation)
+		{
+			invocation.Proceed();
 
-            // if the invocation succceeded, process the caching directive
-            ProcessCacheDirective(invocation.InvocationTarget, invocation.MethodInvocationTarget, invocation.Arguments);
-        }
+			// if the invocation succceeded, process the caching directive
+			ProcessCacheDirective(invocation.InvocationTarget, invocation.MethodInvocationTarget, invocation.Arguments);
+		}
 
 		/// <summary>
 		/// Processes any caching directive specified by the attribute.
@@ -62,26 +61,26 @@ namespace ClearCanvas.Enterprise.Core
 		/// <param name="service"></param>
 		/// <param name="operation"></param>
 		/// <param name="args"></param>
-        private static void ProcessCacheDirective(object service, MethodInfo operation, object[] args)
-        {
-            ResponseCachingDirective directive = GetCachingDirective(service, operation, args);
-            if (directive == null || Equals(directive, ResponseCachingDirective.DoNotCacheDirective))
-                return;
+		private static void ProcessCacheDirective(object service, MethodInfo operation, object[] args)
+		{
+			var directive = GetCachingDirective(service, operation, args);
+			if (directive == null || Equals(directive, ResponseCachingDirective.DoNotCacheDirective))
+				return;
 
-            if (directive.CacheSite == ResponseCachingSite.Server)
-                throw new NotSupportedException("Server-side caching not yet supported.");  // TODO implement this in future
+			if (directive.CacheSite == ResponseCachingSite.Server)
+				throw new NotSupportedException("Server-side caching not yet supported.");  // TODO implement this in future
 
-            // if cache site is client, and we have an op context (eg running as a WCF service),
-            // add caching directive to WCF message headers so that we send it to the client
-            if (directive.CacheSite == ResponseCachingSite.Client && OperationContext.Current != null)
-            {
-                MessageHeader header = MessageHeader.CreateHeader(
-                    ResponseCachingDirective.HeaderName,
-                    ResponseCachingDirective.HeaderNamespace,
-                    directive);
-                OperationContext.Current.OutgoingMessageHeaders.Add(header);
-            }
-        }
+			// if cache site is client, and we have an op context (eg running as a WCF service),
+			// add caching directive to WCF message headers so that we send it to the client
+			if (directive.CacheSite == ResponseCachingSite.Client && OperationContext.Current != null)
+			{
+				var header = MessageHeader.CreateHeader(
+					ResponseCachingDirective.HeaderName,
+					ResponseCachingDirective.HeaderNamespace,
+					directive);
+				OperationContext.Current.OutgoingMessageHeaders.Add(header);
+			}
+		}
 
 		/// <summary>
 		/// Dynamically invokes the specified method to obtain the caching directive.
@@ -90,20 +89,20 @@ namespace ClearCanvas.Enterprise.Core
 		/// <param name="operation"></param>
 		/// <param name="args"></param>
 		/// <returns></returns>
-        private static ResponseCachingDirective GetCachingDirective(object service, MethodInfo operation, object[] args)
-        {
-             // determine if the response is cacheable, and if so, obtain caching directive
-            ResponseCachingAttribute attr = AttributeUtils.GetAttribute<ResponseCachingAttribute>(operation, false);
-            if (attr != null)
-            {
-                Type serviceClass = operation.DeclaringType;
-                MethodInfo method = serviceClass.GetMethod(attr.DirectiveMethod,
-                    BindingFlags.Public|BindingFlags.NonPublic|BindingFlags.Instance|BindingFlags.Static);
-                return (ResponseCachingDirective)method.Invoke(service, args);
-            }
-            return null;
-       }
+		private static ResponseCachingDirective GetCachingDirective(object service, MethodInfo operation, object[] args)
+		{
+			// determine if the response is cacheable, and if so, obtain caching directive
+			var attr = AttributeUtils.GetAttribute<ResponseCachingAttribute>(operation, false);
+			if (attr != null)
+			{
+				var serviceClass = operation.DeclaringType;
+				var method = serviceClass.GetMethod(attr.DirectiveMethod,
+					BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.Static);
+				return (ResponseCachingDirective)method.Invoke(service, args);
+			}
+			return null;
+		}
 
-        #endregion
-    }
+		#endregion
+	}
 }
