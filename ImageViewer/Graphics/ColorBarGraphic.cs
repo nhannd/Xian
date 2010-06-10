31be5c492160d3462fc3fdb93b1a783d3270abdd
jasper <devnull@localhost>
2010-06-10 15:04:55 +0000

    Requirement: #6020 - Image Fusion (Phase 1) Ticket: #6787 - Color Bar graphic showing applied color map '''Schema/Configuration Changes: None'''  * Moved ColorBarGraphic from Fusion plugin to core viewer.  * Refactored ColorBarGraphic to cache and share gradient pixel data where possible (since the color map determines the actual color bar, the pixel data for each color bar is a black-to-white gradient, which will likely be the same across all instances of ColorBarGraphic.  * Added ColorBarTool to standard tools for toggling the visibility of a color bar layer.

---
+++ ImageViewer/Graphics/ColorBarGraphic.cs
@@ -0,0 +1,478 @@
+﻿#region License
+
+// Copyright (c) 2010, ClearCanvas Inc.
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without modification, 
+// are permitted provided that the following conditions are met:
+//
+//    * Redistributions of source code must retain the above copyright notice, 
+//      this list of conditions and the following disclaimer.
+//    * Redistributions in binary form must reproduce the above copyright notice, 
+//      this list of conditions and the following disclaimer in the documentation 
+//      and/or other materials provided with the distribution.
+//    * Neither the name of ClearCanvas Inc. nor the names of its contributors 
+//      may be used to endorse or promote products derived from this software without 
+//      specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" 
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, 
+// THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR 
+// PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR 
+// CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, 
+// OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE 
+// GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) 
+// HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, 
+// STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN 
+// ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY 
+// OF SUCH DAMAGE.
+
+#endregion
+
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using ClearCanvas.Common;
+using ClearCanvas.Common.Utilities;
+using ClearCanvas.ImageViewer.Imaging;
+
+namespace ClearCanvas.ImageViewer.Graphics
+{
+	[Cloneable]
+	public class ColorBarGraphic : CompositeGraphic, IColorMapProvider, IColorMapInstaller
+	{
+		[CloneIgnore]
+		private GrayscaleImageGraphic _colorBar;
+
+		[CloneIgnore]
+		private IGradientPixelData _gradientPixelData;
+
+		private ColorBarOrientation _orientation;
+		private Size _size;
+		private bool _reversed;
+
+		public ColorBarGraphic()
+			: this(ColorBarOrientation.Vertical) {}
+
+		public ColorBarGraphic(ColorBarOrientation orientation)
+			: this(125, 15, orientation) {}
+
+		public ColorBarGraphic(int length, int width, ColorBarOrientation orientation)
+			: this(orientation == ColorBarOrientation.Horizontal ? new Size(length, width) : new Size(width, length), orientation) {}
+
+		public ColorBarGraphic(Size size)
+			: this(size, size.Width > size.Height ? ColorBarOrientation.Horizontal : ColorBarOrientation.Vertical) {}
+
+		public ColorBarGraphic(Size size, ColorBarOrientation orientation)
+		{
+			_size = size;
+			_orientation = orientation;
+			_reversed = false;
+			_gradientPixelData = null;
+		}
+
+		/// <summary>
+		/// Cloning constructor.
+		/// </summary>
+		/// <param name="source">The source object from which to clone.</param>
+		/// <param name="context">The cloning context object.</param>
+		protected ColorBarGraphic(ColorBarGraphic source, ICloningContext context)
+		{
+			context.CloneFields(source, this);
+
+			if (source._gradientPixelData != null)
+				_gradientPixelData = source._gradientPixelData.Clone();
+		}
+
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing)
+			{
+				_colorBar = null;
+
+				if (_gradientPixelData != null)
+				{
+					_gradientPixelData.Dispose();
+					_gradientPixelData = null;
+				}
+			}
+
+			base.Dispose(disposing);
+		}
+
+		[OnCloneComplete]
+		private void OnCloneComplete()
+		{
+			_colorBar = (GrayscaleImageGraphic) CollectionUtils.SelectFirst(base.Graphics, g => g is GrayscaleImageGraphic);
+		}
+
+		public int Width
+		{
+			get { return _orientation == ColorBarOrientation.Horizontal ? _size.Height : _size.Width; }
+			set { this.Size = _orientation == ColorBarOrientation.Horizontal ? new Size(_size.Width, value) : new Size(value, _size.Height); }
+		}
+
+		public int Length
+		{
+			get { return _orientation == ColorBarOrientation.Horizontal ? _size.Width : _size.Height; }
+			set { this.Size = _orientation == ColorBarOrientation.Horizontal ? new Size(value, _size.Height) : new Size(_size.Width, value); }
+		}
+
+		public Size Size
+		{
+			get { return _size; }
+			set
+			{
+				if (_size != value)
+				{
+					_size = value;
+					this.OnSizeChanged(EventArgs.Empty);
+				}
+			}
+		}
+
+		public ColorBarOrientation Orientation
+		{
+			get { return _orientation; }
+			set
+			{
+				if (_orientation != value)
+				{
+					_orientation = value;
+					this.OnOrientationChanged(EventArgs.Empty);
+				}
+			}
+		}
+
+		public bool Reversed
+		{
+			get { return _reversed; }
+			set
+			{
+				if (_reversed != value)
+				{
+					_reversed = value;
+					this.OnReversedChanged(EventArgs.Empty);
+				}
+			}
+		}
+
+		protected GrayscaleImageGraphic ColorBar
+		{
+			get
+			{
+				if (_colorBar == null)
+				{
+					base.Graphics.Add(_colorBar = CreateVerticalGradient());
+					this.UpdateColorBar();
+				}
+				return _colorBar;
+			}
+		}
+
+		protected virtual void OnSizeChanged(EventArgs e)
+		{
+			this.UnloadColorBar();
+		}
+
+		protected virtual void OnReversedChanged(EventArgs e)
+		{
+			this.UpdateColorBar();
+		}
+
+		protected virtual void OnOrientationChanged(EventArgs e)
+		{
+			this.UpdateColorBar();
+		}
+
+		protected virtual GrayscaleImageGraphic CreateVerticalGradient()
+		{
+			if (_gradientPixelData != null)
+				_gradientPixelData.Dispose();
+			_gradientPixelData = GradientPixelData.GetGradient(this.Length, this.Width);
+			return new GrayscaleImageGraphic(_gradientPixelData.Length, _gradientPixelData.Width, 8, 8, 7, false, false, 1, 0, () => _gradientPixelData.Data);
+		}
+
+		protected virtual void UnloadColorBar()
+		{
+			if (_colorBar != null)
+			{
+				base.Graphics.Remove(_colorBar);
+				_colorBar.Dispose();
+			}
+
+			if (_gradientPixelData != null)
+			{
+				_gradientPixelData.Dispose();
+				_gradientPixelData = null;
+			}
+		}
+
+		private void UpdateColorBar()
+		{
+			if (_colorBar != null)
+			{
+				bool horizontal = _orientation == ColorBarOrientation.Horizontal;
+				_colorBar.SpatialTransform.RotationXY = horizontal ? -90 : 0;
+				_colorBar.SpatialTransform.TranslationX = horizontal ? -_colorBar.Columns : 0;
+				_colorBar.VoiLutManager.Invert = _reversed;
+			}
+		}
+
+		protected override SpatialTransform CreateSpatialTransform()
+		{
+			return new InvariantSpatialTransform(this);
+		}
+
+		public override void OnDrawing()
+		{
+			// ensure the colorbar is created
+			var x = this.ColorBar;
+
+			base.OnDrawing();
+		}
+
+		#region IColorMapProvider Members
+
+		public IColorMapManager ColorMapManager
+		{
+			get { return this.ColorBar.ColorMapManager; }
+		}
+
+		#endregion
+
+		#region IColorMapInstaller Members
+
+		public IDataLut ColorMap
+		{
+			get { return this.ColorMapManager.ColorMap; }
+		}
+
+		public void InstallColorMap(string name)
+		{
+			this.ColorMapManager.InstallColorMap(name);
+		}
+
+		public void InstallColorMap(ColorMapDescriptor descriptor)
+		{
+			this.ColorMapManager.InstallColorMap(descriptor);
+		}
+
+		public void InstallColorMap(IDataLut colorMap)
+		{
+			this.ColorMapManager.InstallColorMap(colorMap);
+		}
+
+		IEnumerable<ColorMapDescriptor> IColorMapInstaller.AvailableColorMaps
+		{
+			get { return this.ColorMapManager.AvailableColorMaps; }
+		}
+
+		#endregion
+
+		#region InvariantSpatialTransform Class
+
+		/// <summary>
+		/// Implements a <see cref="SpatialTransform"/> which is invariant in the destination coordinate system with respect to scale, flip and rotation.
+		/// </summary>
+		[Cloneable]
+		private sealed class InvariantSpatialTransform : SpatialTransform
+		{
+			public InvariantSpatialTransform(IGraphic ownerGraphic) : base(ownerGraphic) {}
+
+			/// <summary>
+			/// Cloning constructor.
+			/// </summary>
+			/// <param name="source">The source object from which to clone.</param>
+			/// <param name="context">The cloning context object.</param>
+			private InvariantSpatialTransform(InvariantSpatialTransform source, ICloningContext context)
+				: base(source, context)
+			{
+				context.CloneFields(source, this);
+			}
+
+			protected override void CalculatePostTransform(Matrix cumulativeTransform)
+			{
+				cumulativeTransform.Reset();
+				cumulativeTransform.Translate(this.TranslationX, this.TranslationY);
+			}
+
+			protected override void CalculatePreTransform(Matrix cumulativeTransform) {}
+		}
+
+		#endregion
+
+		#region GradientPixelData Class
+
+		private interface IGradientPixelData : IDisposable
+		{
+			int Width { get; }
+			int Length { get; }
+			byte[] Data { get; }
+			IGradientPixelData Clone();
+		}
+
+		private class GradientPixelData : IDisposable
+		{
+			private static readonly Dictionary<Size, GradientPixelData> _cachedGradients = new Dictionary<Size, GradientPixelData>();
+			private readonly Size _normalizedSize;
+			private byte[] _data;
+
+			public static IGradientPixelData GetGradient(int length, int width)
+			{
+				var normalizedSize = new Size(width, length);
+				if (_cachedGradients.ContainsKey(normalizedSize))
+					return _cachedGradients[normalizedSize].CreateTransientReference();
+				return new GradientPixelData(normalizedSize).CreateTransientReference();
+			}
+
+			private GradientPixelData(Size normalizedSize)
+			{
+				_normalizedSize = normalizedSize;
+				_cachedGradients.Add(normalizedSize, this);
+			}
+
+			protected void Dispose(bool disposing)
+			{
+				if (disposing)
+				{
+					_data = null;
+					_cachedGradients.Remove(_normalizedSize);
+				}
+			}
+
+			public byte[] Data
+			{
+				get
+				{
+					if (_data == null)
+					{
+						int bufferSize = _normalizedSize.Height*_normalizedSize.Width;
+						var buffer = new byte[bufferSize];
+						for (int n = 0; n < bufferSize; n++)
+							buffer[n] = (byte) (((int) (n/_normalizedSize.Width))*255f/(_normalizedSize.Height - 1));
+						_data = buffer;
+					}
+					return _data;
+				}
+			}
+
+			#region Transient Reference Support
+
+			private class GradientReference : IGradientPixelData
+			{
+				private GradientPixelData _gradientPixelData;
+
+				public GradientReference(GradientPixelData gradientPixelData)
+				{
+					_gradientPixelData = gradientPixelData;
+					_gradientPixelData.OnReferenceCreated();
+				}
+
+				public int Length
+				{
+					get { return _gradientPixelData._normalizedSize.Height; }
+				}
+
+				public int Width
+				{
+					get { return _gradientPixelData._normalizedSize.Width; }
+				}
+
+				public byte[] Data
+				{
+					get { return _gradientPixelData.Data; }
+				}
+
+				public IGradientPixelData Clone()
+				{
+					return _gradientPixelData.CreateTransientReference();
+				}
+
+				public void Dispose()
+				{
+					if (_gradientPixelData != null)
+					{
+						_gradientPixelData.OnReferenceDisposed();
+						_gradientPixelData = null;
+					}
+				}
+			}
+
+			private readonly object _syncLock = new object();
+			private int _transientReferenceCount = 0;
+			private bool _selfDisposed = false;
+
+			private void OnReferenceDisposed()
+			{
+				lock (_syncLock)
+				{
+					if (_transientReferenceCount > 0)
+						--_transientReferenceCount;
+
+					if (_transientReferenceCount == 0 && _selfDisposed)
+						DisposeInternal();
+				}
+			}
+
+			private void OnReferenceCreated()
+			{
+				lock (_syncLock)
+				{
+					if (_transientReferenceCount == 0 && _selfDisposed)
+						throw new ObjectDisposedException("");
+
+					++_transientReferenceCount;
+				}
+			}
+
+			private void DisposeInternal()
+			{
+				try
+				{
+					this.Dispose(true);
+					GC.SuppressFinalize(this);
+				}
+				catch (Exception e)
+				{
+					Platform.Log(LogLevel.Warn, e);
+				}
+			}
+
+			/// <summary>
+			/// Creates a new 'transient reference' to this <see cref="FusionOverlayFrameData"/>.
+			/// </summary>
+			/// <remarks>See <see cref="IFusionOverlayFrameDataReference"/> for a detailed explanation of 'transient references'.</remarks>
+			public IGradientPixelData CreateTransientReference()
+			{
+				return new GradientReference(this);
+			}
+
+			/// <summary>
+			/// Implementation of the <see cref="IDisposable"/> pattern.
+			/// </summary>
+			public void Dispose()
+			{
+				lock (_syncLock)
+				{
+					_selfDisposed = true;
+
+					//Only dispose for real when self has been disposed and all the transient references have been disposed.
+					if (_transientReferenceCount == 0)
+						DisposeInternal();
+				}
+			}
+
+			#endregion
+		}
+
+		#endregion
+	}
+
+	public enum ColorBarOrientation
+	{
+		Horizontal,
+		Vertical
+	}
+}
---
+++ ImageViewer/Tools/Standard/ColorBarTool.cs
@@ -0,0 +1,162 @@
+﻿#region License
+
+// Copyright (c) 2010, ClearCanvas Inc.
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without modification, 
+// are permitted provided that the following conditions are met:
+//
+//    * Redistributions of source code must retain the above copyright notice, 
+//      this list of conditions and the following disclaimer.
+//    * Redistributions in binary form must reproduce the above copyright notice, 
+//      this list of conditions and the following disclaimer in the documentation 
+//      and/or other materials provided with the distribution.
+//    * Neither the name of ClearCanvas Inc. nor the names of its contributors 
+//      may be used to endorse or promote products derived from this software without 
+//      specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" 
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, 
+// THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR 
+// PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR 
+// CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, 
+// OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE 
+// GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) 
+// HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, 
+// STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN 
+// ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY 
+// OF SUCH DAMAGE.
+
+#endregion
+
+using System;
+using ClearCanvas.Common;
+using ClearCanvas.Common.Utilities;
+using ClearCanvas.Desktop;
+using ClearCanvas.Desktop.Actions;
+using ClearCanvas.ImageViewer.Graphics;
+using ClearCanvas.ImageViewer.Imaging;
+
+namespace ClearCanvas.ImageViewer.Tools.Standard
+{
+	[MenuAction("showHide", "global-menus/MenuTools/MenuStandard/MenuShowHideColorBar", "ShowHide")]
+	[EnabledStateObserver("showHide", "Enabled", "EnabledChanged")]
+	[Tooltip("showHide", "TooltipShowHideColorBar")]
+	[GroupHint("showHide", "Tools.Image.Overlays.ColourBar.ShowHide")]
+	[IconSet("showHide", IconScheme.Colour, "Icons.ColorBarToolSmall.png", "Icons.ColorBarToolMedium.png", "Icons.ColorBarToolLarge.png")]
+	//
+	[MenuAction("toggle", "overlays-dropdown/ToolbarColorBar", "ShowHide")]
+	[EnabledStateObserver("toggle", "Enabled", "EnabledChanged")]
+	[CheckedStateObserver("toggle", "Checked", "CheckedChanged")]
+	[Tooltip("toggle", "TooltipColorBar")]
+	[GroupHint("toggle", "Tools.Image.Overlays.ColourBar.ShowHide")]
+	[IconSet("toggle", IconScheme.Colour, "Icons.ColorBarToolSmall.png", "Icons.ColorBarToolMedium.png", "Icons.ColorBarToolLarge.png")]
+	//
+	[ExtensionOf(typeof (ImageViewerToolExtensionPoint))]
+	public class ColorBarTool : OverlayToolBase
+	{
+		private bool _enabled;
+
+		public event EventHandler EnabledChanged;
+
+		public ColorBarTool()
+		{
+			this.Checked = false;
+		}
+
+		public bool Enabled
+		{
+			get { return _enabled; }
+			private set
+			{
+				if (_enabled != value)
+				{
+					_enabled = value;
+					EventsHelper.Fire(EnabledChanged, this, EventArgs.Empty);
+				}
+			}
+		}
+
+		protected override void UpdateVisibility(IPresentationImage image, bool visible)
+		{
+			ColorBarCompositeGraphic graphic = GetCompositeScaleGraphic(image, visible);
+			if (graphic != null)
+				graphic.Visible = visible;
+		}
+
+		private static ColorBarCompositeGraphic GetCompositeScaleGraphic(IPresentationImage image, bool createIfNull)
+		{
+			if (image is IColorMapProvider && image is IApplicationGraphicsProvider)
+			{
+				GraphicCollection applicationGraphics = ((IApplicationGraphicsProvider) image).ApplicationGraphics;
+				ColorBarCompositeGraphic graphic = (ColorBarCompositeGraphic) CollectionUtils.SelectFirst(applicationGraphics, g => g is ColorBarCompositeGraphic);
+
+				if (graphic == null && createIfNull)
+					applicationGraphics.Add(graphic = new ColorBarCompositeGraphic());
+
+				return graphic;
+			}
+
+			return null;
+		}
+
+		public override void Initialize()
+		{
+			base.Initialize();
+
+			base.Context.Viewer.EventBroker.PresentationImageSelected += OnPresentationImageSelected;
+		}
+
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing)
+			{
+				base.Context.Viewer.EventBroker.PresentationImageSelected -= OnPresentationImageSelected;
+			}
+
+			base.Dispose(disposing);
+		}
+
+		private void OnPresentationImageSelected(object sender, PresentationImageSelectedEventArgs e)
+		{
+			this.Enabled = e.SelectedPresentationImage is IColorMapProvider;
+		}
+
+		[Cloneable]
+		private class ColorBarCompositeGraphic : CompositeGraphic
+		{
+			[CloneIgnore]
+			private ColorBarGraphic _colorBarGraphic;
+
+			public ColorBarCompositeGraphic()
+			{
+				base.Graphics.Add(_colorBarGraphic = new ColorBarGraphic());
+			}
+
+			/// <summary>
+			/// Cloning constructor.
+			/// </summary>
+			/// <param name="source">The source object from which to clone.</param>
+			/// <param name="context">The cloning context object.</param>
+			protected ColorBarCompositeGraphic(ColorBarCompositeGraphic source, ICloningContext context)
+			{
+				context.CloneFields(source, this);
+			}
+
+			[OnCloneComplete]
+			private void OnCloneComplete()
+			{
+				_colorBarGraphic = (ColorBarGraphic) CollectionUtils.SelectFirst(base.Graphics, g => g is ColorBarGraphic);
+			}
+
+			public override void OnDrawing()
+			{
+				// ensure the color bar uses the same color map as the underlying image
+				if (base.ParentPresentationImage is IColorMapProvider)
+					_colorBarGraphic.ColorMapManager.SetMemento(((IColorMapProvider) base.ParentPresentationImage).ColorMapManager.CreateMemento());
+
+				base.OnDrawing();
+			}
+		}
+	}
+}
