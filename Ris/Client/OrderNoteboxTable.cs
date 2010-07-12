de21f1b8c20650b818150663a2f62ef88ec0e527
jay <devnull@localhost>
2010-07-12 17:01:38 +0000

    Ticket: #6920 - Using "Sort By" drop down menu in Order Notes and External Practitioners FS causes exception
    Schema/Configuration Changes: N/A
    
    - fix tab and suggestion

--- Ris/Client/OrderNoteboxTable.cs
+++ Ris/Client/OrderNoteboxTable.cs
@@ -43,7 +43,7 @@ namespace ClearCanvas.Ris.Client
 {
 	public class OrderNoteboxTable : Table<OrderNoteboxItemSummary>
 	{
-		private static readonly int NumRows = 4;
+		private const int NumRows = 4;
 
 		public OrderNoteboxTable()
 			: this(NumRows)
@@ -53,19 +53,14 @@ namespace ClearCanvas.Ris.Client
 		private OrderNoteboxTable(int cellRowCount)
 			: base(cellRowCount)
 		{
-			ResourceResolver resolver = new ResourceResolver(this.GetType().Assembly);
+			var resolver = new ResourceResolver(this.GetType().Assembly);
 
-			TableColumn<OrderNoteboxItemSummary, IconSet> urgentColumn =
-				new TableColumn<OrderNoteboxItemSummary, IconSet>(SR.ColumnUrgent,
-				delegate(OrderNoteboxItemSummary item)
+			var urgentColumn = new TableColumn<OrderNoteboxItemSummary, IconSet>(SR.ColumnUrgent,
+				item => item.Urgent ? new IconSet("SingleExclamation.png") : null, 0.3f)
 				{
-					return item.Urgent ? new IconSet("SingleExclamation.png") : null;
-				}, 0.3f);
-			urgentColumn.Comparison = delegate(OrderNoteboxItemSummary item1, OrderNoteboxItemSummary item2)
-										{
-											return item1.Urgent.CompareTo(item2.Urgent);
-										};
-			urgentColumn.ResourceResolver = resolver;
+					Comparison = (item1, item2) => item1.Urgent.CompareTo(item2.Urgent),
+					ResourceResolver = resolver
+				};
 			this.Columns.Add(urgentColumn);
 
 			/* JR: this isn't needed right now, because acknowledged notes are never shown.
@@ -79,48 +74,37 @@ namespace ClearCanvas.Ris.Client
 			this.Columns.Add(acknowledgedColumn);
 			 */
 
-			this.Columns.Add(new TableColumn<OrderNoteboxItemSummary, string>(SR.ColumnMRN,
-				delegate(OrderNoteboxItemSummary item) { return MrnFormat.Format(item.Mrn); },
-				1.0f));
-
-			this.Columns.Add(new TableColumn<OrderNoteboxItemSummary, string>(SR.ColumnPatientName,
-				delegate(OrderNoteboxItemSummary item) { return PersonNameFormat.Format(item.PatientName); },
-				1.0f));
-
-			this.Columns.Add(new TableColumn<OrderNoteboxItemSummary, string>(SR.ColumnDescription,
-				delegate(OrderNoteboxItemSummary item)
-				{
-					return string.Format("{0} {1}", AccessionFormat.Format(item.AccessionNumber), item.DiagnosticServiceName);
-				},
+			this.Columns.Add(new TableColumn<OrderNoteboxItemSummary, string>(SR.ColumnMRN, item => MrnFormat.Format(item.Mrn), 1.0f));
+			this.Columns.Add(new TableColumn<OrderNoteboxItemSummary, string>(SR.ColumnPatientName, item => PersonNameFormat.Format(item.PatientName), 1.0f));
+			this.Columns.Add(new TableColumn<OrderNoteboxItemSummary, string>(SR.ColumnDescription, 
+				item => string.Format("{0} {1}", AccessionFormat.Format(item.AccessionNumber), item.DiagnosticServiceName),
 				1.0f, 1));
 
 			this.Columns.Add(new TableColumn<OrderNoteboxItemSummary, string>(SR.ColumnFrom,
-				delegate(OrderNoteboxItemSummary item)
-				{
-					if (item.OnBehalfOfGroup != null)
-						return String.Format(SR.FormatFromOnBehalf, StaffNameAndRoleFormat.Format(item.Author), item.OnBehalfOfGroup.Name, item.PostTime);
-					else
-						return String.Format(SR.FormatFrom, StaffNameAndRoleFormat.Format(item.Author), item.PostTime);
-				},
+				item => item.OnBehalfOfGroup != null
+						? String.Format(SR.FormatFromOnBehalf, StaffNameAndRoleFormat.Format(item.Author), item.OnBehalfOfGroup.Name, item.PostTime)
+						: String.Format(SR.FormatFrom, StaffNameAndRoleFormat.Format(item.Author), item.PostTime),
 				1.0f, 2));
 
 			this.Columns.Add(new TableColumn<OrderNoteboxItemSummary, string>(SR.ColumnTo,
-				delegate(OrderNoteboxItemSummary item) { return String.Format(SR.FormatTo, RecipientsList(item.StaffRecipients, item.GroupRecipients)); },
+				item => String.Format(SR.FormatTo, RecipientsList(item.StaffRecipients, item.GroupRecipients)),
 				1.0f, 3));
 		}
 
+		/* this isn't needed right now, because acknowledged notes are never shown.
 		private static IconSet GetIsAcknowledgedIcon(bool isAcknowledged)
 		{
 			return isAcknowledged ? new IconSet("NoteRead.png") : new IconSet("NoteUnread.png");
 		}
+		*/
 
 		// Creates a semi-colon delimited list of the recipients
 		private static string RecipientsList(IEnumerable<StaffSummary> staffRecipients, IEnumerable<StaffGroupSummary> groupRecipients)
 		{
-			StringBuilder sb = new StringBuilder();
+			var sb = new StringBuilder();
 			const string itemSeparator = ";  ";
 
-			foreach (StaffSummary staffSummary in staffRecipients)
+			foreach (var staffSummary in staffRecipients)
 			{
 				if (String.Equals(PersonNameFormat.Format(staffSummary.Name), PersonNameFormat.Format(LoginSession.Current.FullName)))
 				{
@@ -133,7 +117,7 @@ namespace ClearCanvas.Ris.Client
 				}
 			}
 
-			foreach (StaffGroupSummary groupSummary in groupRecipients)
+			foreach (var groupSummary in groupRecipients)
 			{
 				sb.Append(groupSummary.Name);
 				sb.Append(itemSeparator);
