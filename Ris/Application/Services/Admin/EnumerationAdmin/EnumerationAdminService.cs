f5343b2911d9c5293e7a0e7fb4bf23289a20d10f
jay <devnull@localhost>
2010-07-19 15:17:17 +0000

    Ticket(s): #6834 - Attempting to delete an enumeration that's in use throws an exception
    Schema/Configuration Changes: N/A
    
    - tab and resharper suggestions

--- Ris/Application/Services/Admin/EnumerationAdmin/EnumerationAdminService.cs
+++ Ris/Application/Services/Admin/EnumerationAdmin/EnumerationAdminService.cs
@@ -31,92 +31,85 @@
 
 using System;
 using System.Collections.Generic;
-using System.Text;
+using System.Security.Permissions;
+using ClearCanvas.Common;
+using ClearCanvas.Common.Utilities;
 using ClearCanvas.Enterprise.Common;
+using ClearCanvas.Enterprise.Core;
 using ClearCanvas.Enterprise.Core.Modelling;
 using ClearCanvas.Ris.Application.Common.Admin.EnumerationAdmin;
-using ClearCanvas.Common;
-using ClearCanvas.Enterprise.Core;
-using ClearCanvas.Common.Utilities;
-using System.Security.Permissions;
 using AuthorityTokens=ClearCanvas.Ris.Application.Common.AuthorityTokens;
 
 namespace ClearCanvas.Ris.Application.Services.Admin.EnumerationAdmin
 {
-    [ExtensionOf(typeof(ApplicationServiceExtensionPoint))]
-    [ServiceImplementsContract(typeof(IEnumerationAdminService))]
-    public class EnumerationAdminService : ApplicationServiceBase, IEnumerationAdminService
-    {
-        #region IEnumerationAdminService Members
+	[ExtensionOf(typeof(ApplicationServiceExtensionPoint))]
+	[ServiceImplementsContract(typeof(IEnumerationAdminService))]
+	public class EnumerationAdminService : ApplicationServiceBase, IEnumerationAdminService
+	{
+		#region IEnumerationAdminService Members
 
-        [ReadOperation]
-        public ListEnumerationsResponse ListEnumerations(ListEnumerationsRequest request)
-        {
-            IMetadataBroker broker = PersistenceContext.GetBroker<IMetadataBroker>();
-            IList<Type> enumClasses = broker.ListEnumValueClasses();
+		[ReadOperation]
+		public ListEnumerationsResponse ListEnumerations(ListEnumerationsRequest request)
+		{
+			var broker = PersistenceContext.GetBroker<IMetadataBroker>();
+			var enumClasses = broker.ListEnumValueClasses();
 
-            List<EnumerationSummary> enumerations = CollectionUtils.Map<Type, EnumerationSummary, List<EnumerationSummary>>(enumClasses,
-                delegate(Type enumClass)
-                {
-                    return new EnumerationSummary(enumClass.AssemblyQualifiedName, enumClass.Name, IsSoftEnum(enumClass));
-                });
+			var enumerations = CollectionUtils.Map<Type, EnumerationSummary, List<EnumerationSummary>>(enumClasses,
+				enumClass => new EnumerationSummary(enumClass.AssemblyQualifiedName, enumClass.Name, IsSoftEnum(enumClass)));
 
-            return new ListEnumerationsResponse(enumerations);
-        }
+			return new ListEnumerationsResponse(enumerations);
+		}
 
-        [ReadOperation]
+		[ReadOperation]
 		public ListEnumerationValuesResponse ListEnumerationValues(ListEnumerationValuesRequest request)
-        {
-            IEnumBroker enumBroker = PersistenceContext.GetBroker<IEnumBroker>();
-            IList<EnumValue> enumValues = enumBroker.Load(GetEnumClass(request.AssemblyQualifiedClassName), request.IncludeDeactivated);
-            return new ListEnumerationValuesResponse(
-                CollectionUtils.Map<EnumValue, EnumValueAdminInfo>(enumValues,
-					delegate(EnumValue value)
-					{
-						return new EnumValueAdminInfo(value.Code, value.Value, value.Description, value.Deactivated);
-					}));
-        }
+		{
+			var enumBroker = PersistenceContext.GetBroker<IEnumBroker>();
+			var enumValues = enumBroker.Load(GetEnumClass(request.AssemblyQualifiedClassName), request.IncludeDeactivated);
+			return new ListEnumerationValuesResponse(
+				CollectionUtils.Map(enumValues, 
+				(EnumValue value) => new EnumValueAdminInfo(value.Code, value.Value, value.Description, value.Deactivated)));
+		}
 
-        [UpdateOperation]
+		[UpdateOperation]
 		[PrincipalPermission(SecurityAction.Demand, Role = AuthorityTokens.Admin.Data.Enumeration)]
 		public AddValueResponse AddValue(AddValueRequest request)
-        {
-            Platform.CheckForNullReference(request, "request");
-            Platform.CheckMemberIsSet(request.Value, "Value");
+		{
+			Platform.CheckForNullReference(request, "request");
+			Platform.CheckMemberIsSet(request.Value, "Value");
 
-            Type enumClass = GetEnumClass(request.AssemblyQualifiedClassName);
+			var enumClass = GetEnumClass(request.AssemblyQualifiedClassName);
 
-            // compute a display order for the new value
-            float displayOrder = ComputeDisplayOrderValue(enumClass, request.Value.Code,
-                request.InsertAfter == null ? null : request.InsertAfter.Code);
+			// compute a display order for the new value
+			var displayOrder = ComputeDisplayOrderValue(enumClass, request.Value.Code,
+				request.InsertAfter == null ? null : request.InsertAfter.Code);
 
-            // add the new value
-            IEnumBroker broker = PersistenceContext.GetBroker<IEnumBroker>();
-            broker.AddValue(enumClass, request.Value.Code, request.Value.Value, request.Value.Description, displayOrder,
+			// add the new value
+			var broker = PersistenceContext.GetBroker<IEnumBroker>();
+			broker.AddValue(enumClass, request.Value.Code, request.Value.Value, request.Value.Description, displayOrder,
 				IsSoftEnum(enumClass) ? request.Value.Deactivated : false);
 
-            return new AddValueResponse();
-        }
+			return new AddValueResponse();
+		}
 
-        [UpdateOperation]
+		[UpdateOperation]
 		[PrincipalPermission(SecurityAction.Demand, Role = AuthorityTokens.Admin.Data.Enumeration)]
 		public EditValueResponse EditValue(EditValueRequest request)
-        {
-            Platform.CheckForNullReference(request, "request");
-            Platform.CheckMemberIsSet(request.Value, "Value");
+		{
+			Platform.CheckForNullReference(request, "request");
+			Platform.CheckMemberIsSet(request.Value, "Value");
 
-            Type enumClass = GetEnumClass(request.AssemblyQualifiedClassName);
+			var enumClass = GetEnumClass(request.AssemblyQualifiedClassName);
 
-            // compute display order value
-            float displayOrder = ComputeDisplayOrderValue(enumClass, request.Value.Code,
-                request.InsertAfter == null ? null : request.InsertAfter.Code);
+			// compute display order value
+			var displayOrder = ComputeDisplayOrderValue(enumClass, request.Value.Code,
+				request.InsertAfter == null ? null : request.InsertAfter.Code);
 
-            IEnumBroker broker = PersistenceContext.GetBroker<IEnumBroker>();
-            broker.UpdateValue(enumClass, request.Value.Code, request.Value.Value, request.Value.Description, displayOrder,
+			var broker = PersistenceContext.GetBroker<IEnumBroker>();
+			broker.UpdateValue(enumClass, request.Value.Code, request.Value.Value, request.Value.Description, displayOrder,
 				IsSoftEnum(enumClass) ? request.Value.Deactivated : false);
 
-            return new EditValueResponse();
-        }
+			return new EditValueResponse();
+		}
 
 		[UpdateOperation]
 		[PrincipalPermission(SecurityAction.Demand, Role = AuthorityTokens.Admin.Data.Enumeration)]
@@ -132,7 +125,7 @@ namespace ClearCanvas.Ris.Application.Services.Admin.EnumerationAdmin
 			if (IsSoftEnum(enumClass) == false)
 				throw new RequestValidationException(SR.ExceptionUnableToDeleteHardEnumeration);
 
-				IEnumBroker broker = PersistenceContext.GetBroker<IEnumBroker>();
+				var broker = PersistenceContext.GetBroker<IEnumBroker>();
 				broker.RemoveValue(enumClass, request.Value.Code);
 				PersistenceContext.SynchState();
 				return new RemoveValueResponse();
@@ -143,49 +136,45 @@ namespace ClearCanvas.Ris.Application.Services.Admin.EnumerationAdmin
 			}
 		}
 
-        #endregion
+		#endregion
 
-        private Type GetEnumClass(string enumerationName)
-        {
-            Type enumClass = Type.GetType(enumerationName);
-            if (enumClass == null)
-                throw new RequestValidationException("Invalid enumeration name.");
+		private static Type GetEnumClass(string enumerationName)
+		{
+			var enumClass = Type.GetType(enumerationName);
+			if (enumClass == null)
+				throw new RequestValidationException("Invalid enumeration name.");
 
-            return enumClass;
-        }
+			return enumClass;
+		}
 
-        private bool IsSoftEnum(Type enumClass)
-        {
-            return !CollectionUtils.Contains<Type>(enumClass.Assembly.GetTypes(),
-                delegate(Type t)
-                {
-                    return t.IsEnum && AttributeUtils.HasAttribute<EnumValueClassAttribute>(t, false,
-                        delegate(EnumValueClassAttribute attr) { return attr.EnumValueClass.Equals(enumClass); });
-                });
-        }
+		private static bool IsSoftEnum(Type enumClass)
+		{
+			return !CollectionUtils.Contains(enumClass.Assembly.GetTypes(),
+				t => t.IsEnum && AttributeUtils.HasAttribute<EnumValueClassAttribute>(t, false, attr => attr.EnumValueClass.Equals(enumClass)));
+		}
 
-        private float ComputeDisplayOrderValue(Type enumValueClass, string code, string insertAfterCode)
-        {
-            IEnumBroker broker = PersistenceContext.GetBroker<IEnumBroker>();
+		private float ComputeDisplayOrderValue(Type enumValueClass, string code, string insertAfterCode)
+		{
+			var broker = PersistenceContext.GetBroker<IEnumBroker>();
 
-            // get insertAfter value, which may be null if the value is to be inserted at the beginning
-            EnumValue insertAfter = insertAfterCode == null ? null : broker.Find(enumValueClass, insertAfterCode);
-            if (insertAfter != null && insertAfter.Code == code)
-                throw new RequestValidationException("Value cannot be inserted after itself.");
+			// get insertAfter value, which may be null if the value is to be inserted at the beginning
+			var insertAfter = insertAfterCode == null ? null : broker.Find(enumValueClass, insertAfterCode);
+			if (insertAfter != null && insertAfter.Code == code)
+				throw new RequestValidationException("Value cannot be inserted after itself.");
 
-            // get the insertBefore value (the value immediately following insertAfter)
-            // this may be null if insertAfter is the last value in the set
-            IList<EnumValue> values = broker.Load(enumValueClass, true);
-            int insertAfterIndex = insertAfter == null ? -1 : values.IndexOf(insertAfter);
-            EnumValue insertBefore = (insertAfterIndex + 1 == values.Count) ? null : values[insertAfterIndex + 1];
+			// get the insertBefore value (the value immediately following insertAfter)
+			// this may be null if insertAfter is the last value in the set
+			var values = broker.Load(enumValueClass, true);
+			var insertAfterIndex = insertAfter == null ? -1 : values.IndexOf(insertAfter);
+			var insertBefore = (insertAfterIndex + 1 == values.Count) ? null : values[insertAfterIndex + 1];
 
-            // if the insertBefore value is the same as the value being edited, then there is no change in displayOrder
-            if (insertBefore != null && insertBefore.Code == code)
-                return insertBefore.DisplayOrder;
+			// if the insertBefore value is the same as the value being edited, then there is no change in displayOrder
+			if (insertBefore != null && insertBefore.Code == code)
+				return insertBefore.DisplayOrder;
 
-            // otherwise compute a new display order value
-            float lower = insertAfter == null ? 0 : insertAfter.DisplayOrder;
-            return insertBefore == null ? lower + 1 : (lower + insertBefore.DisplayOrder) / 2;
-        }
-    }
+			// otherwise compute a new display order value
+			var lower = insertAfter == null ? 0 : insertAfter.DisplayOrder;
+			return insertBefore == null ? lower + 1 : (lower + insertBefore.DisplayOrder) / 2;
+		}
+	}
 }
