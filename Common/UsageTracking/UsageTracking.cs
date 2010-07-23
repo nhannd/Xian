a85f4476021331e9506bd03a216bf34dce548b32
steve <devnull@localhost>
2010-07-23 22:02:14 +0000

    Ticket(s): #6966 - The core client side phone home mechanism shall be in a shared assembly
    Schema/Configuration Changes: N/A
    
    Defined the data contracts for the phone home mechanism, and added some preliminary code for the message transferred and a helper class.  Needs some work, but wanted to commit due to some instabilities with my system.

---
+++ Common/UsageTracking/UsageTracking.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Globalization;
+using System.IO;
+using System.Threading;
+using System.Xml.Serialization;
+
+namespace ClearCanvas.Common.UsageTracking
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class UsageTracking
+    {
+        private static void Send(object theMessage)
+        {
+            try
+            {
+                UsageMessage message = theMessage as UsageMessage;
+                if (message != null)
+                {
+                    message.Product = ProductInformation.Name;
+
+                    XmlSerializer serializer = new XmlSerializer(typeof(UsageMessage));
+
+                    using (MemoryStream ms = new MemoryStream())
+                    {
+                        serializer.Serialize(ms, message);
+
+                        using (UsageTrackingServiceClient client = new UsageTrackingServiceClient())
+                        {
+                            client.Register(ms.ToString());
+                        }
+                    }       
+                }
+            }
+            catch (Exception e)
+            {
+                Platform.Log(LogLevel.Debug, e);
+            }
+        }
+
+        /// <summary>
+        /// Register the usage of the application with a ClearCanvas server.
+        /// </summary>
+        /// <param name="message"></param>
+        public static void Register(UsageMessage message)
+        {
+            if (UsageTrackingSettings.Default.Enabled)
+                try
+                {
+                    UsageMessage theMessage = message;
+
+                    ThreadPool.QueueUserWorkItem(Send,theMessage);
+                }
+                catch (Exception e)
+                {
+                    // Fail silently
+                    Platform.Log(LogLevel.Debug, e);
+                }
+        }
+
+        /// <summary>
+        /// Get a <see cref="UsageMessage"/> for the application.
+        /// </summary>
+        /// <returns></returns>
+        public static UsageMessage GetUsageMessage()
+        {
+            UsageMessage msg = new UsageMessage
+                                   {
+                                       Version = ProductInformation.GetVersion(true, true),
+                                       Product = ProductInformation.Name,
+                                       Region = CultureInfo.CurrentCulture.Name,
+                                       Timestamp = Platform.Time,
+                                       OS = Environment.OSVersion.ToString(),
+                                       License = ProductInformation.License
+                                   };
+            return msg;
+        }
+    }
+}
