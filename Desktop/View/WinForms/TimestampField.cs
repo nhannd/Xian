cc6b3f7508bdfc2e670cf5e8c80a7bc516fabcde
thanh <devnull@localhost>
2010-07-28 20:00:41 +0000

    Ticket: #6947 - User interface shall support viewing and searching audit logs stored in the enterprise server
    Schema/Configuration Changes: N/A
    
    Added support for paging, and filter by time range.

---
+++ Desktop/View/WinForms/TimestampField.cs
@@ -0,0 +1,142 @@
+﻿#region License
+
+// Copyright (c) 2010, ClearCanvas Inc.
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without modification, 
+// are permitted provided that the following conditions are met:
+//
+//    * Redistributions of source code must retain the above copyright notice, 
+//      this list of conditions and the following disclaimer.
+//    * Redistributions in binary form must reproduce the above copyright notice, 
+//      this list of conditions and the following disclaimer in the documentation 
+//      and/or other materials provided with the distribution.
+//    * Neither the name of ClearCanvas Inc. nor the names of its contributors 
+//      may be used to endorse or promote products derived from this software without 
+//      specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" 
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, 
+// THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR 
+// PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR 
+// CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, 
+// OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE 
+// GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) 
+// HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, 
+// STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN 
+// ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY 
+// OF SUCH DAMAGE.
+
+#endregion
+
+using System;
+using System.ComponentModel;
+using System.Windows.Forms;
+using ClearCanvas.Common;
+using ClearCanvas.Common.Utilities;
+
+namespace ClearCanvas.Desktop.View.WinForms
+{
+    public partial class TimestampField : UserControl
+    {
+        public event EventHandler ValueChanged;
+
+        public TimestampField()
+        {
+            InitializeComponent();
+            _datePicker.ValueChanged += OnValueChanged;
+            _timePicker.ValueChanged += OnValueChanged;
+
+            if (!DesignMode)
+            {
+                try
+                {
+                    _datePicker.CustomFormat = Format.DateFormat;
+                    _timePicker.CustomFormat = Format.TimeFormat;
+                }
+                catch(Exception ex)
+                {
+                    Platform.Log(LogLevel.Error, ex, "Unable to set custom date/time format");
+                }
+            }
+        }
+
+        [Browsable(true)]
+        [Category("Label")]
+        public string TextLabel
+        {
+            get
+            {
+                return label.Text;
+            }
+            set
+            {
+                label.Text = value == string.Empty ? null : value;
+            }
+        }
+
+        public DateTime MaxValue
+        {
+            get { return _datePicker.MaxDate; }
+            set { _datePicker.MaxDate = value; }
+        }
+
+        public DateTime MinValue
+        {
+            get { return _datePicker.MinDate; }
+            set { _datePicker.MinDate = value; }
+        }
+
+        public DateTime? Value
+        {
+            get
+            {
+                if (!_datePicker.Checked)
+                    return null;
+
+                return _datePicker.Value.Date + _timePicker.Value.TimeOfDay;
+                
+            }
+
+            set
+            {
+                _datePicker.Checked = value != null;
+                if (value != null)
+                {
+                    _datePicker.Value = value.Value;
+                    _timePicker.Value = value.Value;
+                    _timePicker.Enabled = true;
+                }
+                else
+                {
+                    _timePicker.Enabled = false;
+                }
+
+                if (ValueChanged != null)
+                    EventsHelper.Fire(ValueChanged, this, EventArgs.Empty);
+            }
+        }
+
+        private void OnDatePickerMouseUp(object sender, MouseEventArgs e)
+        {
+            UpdateState();
+        }
+
+        private void UpdateState()
+        {
+            if (!_datePicker.Checked)
+            {
+                Value = null;
+            } 
+            else
+            {
+                Value = _datePicker.Value.Date + _timePicker.Value.TimeOfDay;
+            }
+        }
+
+        private void OnValueChanged(object sender, EventArgs e)
+        {
+            UpdateState();
+        }
+    }
+}
