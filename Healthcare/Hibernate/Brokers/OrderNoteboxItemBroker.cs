f00b8d9ab8c1f2cfd6b836b5bc9e73b3dd964506
jay <devnull@localhost>
2010-08-12 19:28:02 +0000

    Ticket: #7092 - OrderNote query time needs to be improved
    Schema/Configuration Changes: N/A
    
    - fix tab and resharper suggestion

--- Healthcare/Hibernate/Brokers/OrderNoteboxItemBroker.cs
+++ Healthcare/Hibernate/Brokers/OrderNoteboxItemBroker.cs
@@ -32,215 +32,214 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
-using System.Text;
+using ClearCanvas.Common;
 using ClearCanvas.Common.Utilities;
 using ClearCanvas.Enterprise.Hibernate;
 using ClearCanvas.Enterprise.Hibernate.Hql;
 using ClearCanvas.Healthcare.Brokers;
-using ClearCanvas.Common;
 
 namespace ClearCanvas.Healthcare.Hibernate.Brokers
 {
-    [ExtensionOf(typeof(BrokerExtensionPoint))]
-    public class OrderNoteboxItemBroker : Broker, IOrderNoteboxItemBroker
-    {
-        #region Hql Constants
+	[ExtensionOf(typeof(BrokerExtensionPoint))]
+	public class OrderNoteboxItemBroker : Broker, IOrderNoteboxItemBroker
+	{
+		#region Hql Constants
 
 		protected static readonly HqlSelect SelectNote = new HqlSelect("n");
 		protected static readonly HqlSelect SelectNotePostingAcknowledged = new HqlSelect("np.IsAcknowledged");
-        protected static readonly HqlSelect SelectNoteFullyAcknowledged = new HqlSelect("n.IsFullyAcknowledged");
-        protected static readonly HqlSelect SelectOrder = new HqlSelect("o");
-        protected static readonly HqlSelect SelectPatient = new HqlSelect("p");
-        protected static readonly HqlSelect SelectPatientProfile = new HqlSelect("pp");
+		protected static readonly HqlSelect SelectNoteFullyAcknowledged = new HqlSelect("n.IsFullyAcknowledged");
+		protected static readonly HqlSelect SelectOrder = new HqlSelect("o");
+		protected static readonly HqlSelect SelectPatient = new HqlSelect("p");
+		protected static readonly HqlSelect SelectPatientProfile = new HqlSelect("pp");
 
-        protected static readonly HqlJoin JoinOrder = new HqlJoin("n.Order", "o");
-        protected static readonly HqlJoin JoinPatient = new HqlJoin("o.Patient", "p");
-        protected static readonly HqlJoin JoinPatientProfile = new HqlJoin("p.Profiles", "pp");
-        protected static readonly HqlJoin JoinNotePostings = new HqlJoin("n.Postings", "np");
+		protected static readonly HqlJoin JoinOrder = new HqlJoin("n.Order", "o");
+		protected static readonly HqlJoin JoinPatient = new HqlJoin("o.Patient", "p");
+		protected static readonly HqlJoin JoinPatientProfile = new HqlJoin("p.Profiles", "pp");
+		protected static readonly HqlJoin JoinNotePostings = new HqlJoin("n.Postings", "np");
 
 
 		//protected static readonly HqlCondition ConditionMostRecentNote = new HqlCondition(
 		//    "(n.PostTime = (select max(n2.PostTime) from OrderNote n2 join n2.Postings np2 where np2 = np and n2.Order = n.Order and n2.Category = n.Category))");
 
-        protected static readonly HqlCondition ConditionConstrainPatientProfile =
-            new HqlCondition("pp.Mrn.AssigningAuthority = o.OrderingFacility.InformationAuthority");
+		protected static readonly HqlCondition ConditionConstrainPatientProfile =
+			new HqlCondition("pp.Mrn.AssigningAuthority = o.OrderingFacility.InformationAuthority");
 
-        private static readonly HqlSelect[] InboxItemProjection
-            = {
+		private static readonly HqlSelect[] InboxItemProjection
+			= {
 				SelectNote,
-                SelectOrder,
-                SelectPatient,
-                SelectPatientProfile,
-                SelectNotePostingAcknowledged
-              };
+				SelectOrder,
+				SelectPatient,
+				SelectPatientProfile,
+				SelectNotePostingAcknowledged
+			  };
 
-        private static readonly HqlSelect[] SentItemProjection
-            = {
+		private static readonly HqlSelect[] SentItemProjection
+			= {
 				SelectNote,
-                SelectOrder,
-                SelectPatient,
-                SelectPatientProfile,
-                SelectNoteFullyAcknowledged
-              };
+				SelectOrder,
+				SelectPatient,
+				SelectPatientProfile,
+				SelectNoteFullyAcknowledged
+			  };
 
-        private static readonly HqlSelect[] CountProjection
-            = {
-                  new HqlSelect("count(*)")
-              };
+		private static readonly HqlSelect[] CountProjection
+			= {
+				  new HqlSelect("count(*)")
+			  };
 
-        private static readonly HqlJoin[] InboxJoins
-            = {
-                JoinNotePostings,
-                JoinOrder,
-                JoinPatient,
-                JoinPatientProfile,
-              };
+		private static readonly HqlJoin[] InboxJoins
+			= {
+				JoinNotePostings,
+				JoinOrder,
+				JoinPatient,
+				JoinPatientProfile,
+			  };
 
-        private static readonly HqlJoin[] SentItemJoins
-            = {
-                JoinOrder,
-                JoinPatient,
-                JoinPatientProfile,
-              };
+		private static readonly HqlJoin[] SentItemJoins
+			= {
+				JoinOrder,
+				JoinPatient,
+				JoinPatientProfile,
+			  };
 
-        private static readonly HqlSort[] InboxItemOrdering
-           = {
-                    new HqlSort("np.IsAcknowledged", true, 0),
-                    new HqlSort("n.PostTime", false, 1)
-              };
+		private static readonly HqlSort[] InboxItemOrdering
+			= {
+					new HqlSort("np.IsAcknowledged", true, 0),
+					new HqlSort("n.PostTime", false, 1)
+			  };
 
-        private static readonly HqlSort[] SentItemOrdering
-           = {
-                    new HqlSort("n.IsFullyAcknowledged", true, 0),
-                    new HqlSort("n.PostTime", false, 1)
-              };
+		private static readonly HqlSort[] SentItemOrdering
+			= {
+				new HqlSort("n.IsFullyAcknowledged", true, 0),
+				new HqlSort("n.PostTime", false, 1)
+			  };
 
-        #endregion
+		#endregion
 
-        #region IOrderNoteboxItemBroker Members
+		#region IOrderNoteboxItemBroker Members
 
-        public IList GetInboxItems(Notebox notebox, INoteboxQueryContext nqc)
-        {
-            HqlProjectionQuery query = BuildInboxQuery(notebox, nqc, false);
-            return DoQuery(query);
-        }
+		public IList GetInboxItems(Notebox notebox, INoteboxQueryContext nqc)
+		{
+			var query = BuildInboxQuery(notebox, nqc, false);
+			return DoQuery(query);
+		}
 
-        public int CountInboxItems(Notebox notebox, INoteboxQueryContext nqc)
-        {
-            HqlProjectionQuery query = BuildInboxQuery(notebox, nqc, true);
-            return DoQueryCount(query);
-        }
+		public int CountInboxItems(Notebox notebox, INoteboxQueryContext nqc)
+		{
+			var query = BuildInboxQuery(notebox, nqc, true);
+			return DoQueryCount(query);
+		}
 
-        public IList GetSentItems(Notebox notebox, INoteboxQueryContext nqc)
-        {
-            HqlProjectionQuery query = BuildSentQuery(notebox, nqc, false);
-            return DoQuery(query);
-        }
+		public IList GetSentItems(Notebox notebox, INoteboxQueryContext nqc)
+		{
+			var query = BuildSentQuery(notebox, nqc, false);
+			return DoQuery(query);
+		}
 
-        public int CountSentItems(Notebox notebox, INoteboxQueryContext nqc)
-        {
-            HqlProjectionQuery query = BuildSentQuery(notebox, nqc, true);
-            return DoQueryCount(query);
-        }
+		public int CountSentItems(Notebox notebox, INoteboxQueryContext nqc)
+		{
+			var query = BuildSentQuery(notebox, nqc, true);
+			return DoQueryCount(query);
+		}
 
-        #endregion
+		#endregion
 
-        #region Helpers
+		#region Helpers
 
-        private HqlProjectionQuery GetBaseQuery(INoteboxQueryContext nqc, bool countQuery, HqlSelect[] itemProjection, HqlJoin[] joins)
-        {
-            HqlProjectionQuery query = new HqlProjectionQuery(new HqlFrom(typeof(OrderNote).Name, "n", joins));
-            if (countQuery)
-            {
-                query.Selects.AddRange(CountProjection);
-            }
-            else
-            {
-                query.Selects.AddRange(itemProjection);
+		private static HqlProjectionQuery GetBaseQuery(INoteboxQueryContext nqc, bool countQuery, HqlSelect[] itemProjection, HqlJoin[] joins)
+		{
+			var query = new HqlProjectionQuery(new HqlFrom(typeof(OrderNote).Name, "n", joins));
+			if (countQuery)
+			{
+				query.Selects.AddRange(CountProjection);
+			}
+			else
+			{
+				query.Selects.AddRange(itemProjection);
 
-                // need this in case note was sent to staff and staffgroup containing same staff
-                //query.SelectDistinct = true;
+				// need this in case orderNote was sent to staff and staffgroup containing same staff
+				//query.SelectDistinct = true;
 
-                // add paging if not a count query
-                query.Page = nqc.Page;
-            }
+				// add paging if not a count query
+				query.Page = nqc.Page;
+			}
 
-            // constrain patient profile by OrderingFacility (this is not ideal, but it is the easiest way to constrain the patient profile)
-            query.Conditions.Add(ConditionConstrainPatientProfile);
+			// constrain patient profile by OrderingFacility (this is not ideal, but it is the easiest way to constrain the patient profile)
+			query.Conditions.Add(ConditionConstrainPatientProfile);
 
-            return query;
-        }
+			return query;
+		}
 
 
-        private HqlProjectionQuery BuildInboxQuery(Notebox notebox, INoteboxQueryContext nqc, bool countQuery)
-        {
-            HqlProjectionQuery query = GetBaseQuery(nqc, countQuery, InboxItemProjection, InboxJoins);
+		private static HqlProjectionQuery BuildInboxQuery(Notebox notebox, INoteboxQueryContext nqc, bool countQuery)
+		{
+			var query = GetBaseQuery(nqc, countQuery, InboxItemProjection, InboxJoins);
 
-            HqlOr or = new HqlOr();
-            foreach (NoteboxItemSearchCriteria criteria in notebox.GetInvariantCriteria(nqc))
-            {
-                HqlAnd and = new HqlAnd();
-                and.Conditions.Add(new HqlCondition("np.IsAcknowledged = ?", criteria.IsAcknowledged));
-                if(criteria.SentToMe)
+			var or = new HqlOr();
+			foreach (var criteria in notebox.GetInvariantCriteria(nqc))
+			{
+				var and = new HqlAnd();
+				and.Conditions.Add(new HqlCondition("np.IsAcknowledged = ?", criteria.IsAcknowledged));
+				if(criteria.SentToMe)
 					and.Conditions.Add(new HqlCondition("np = (select np1 from StaffNotePosting np1 where np1 = np and np1.Recipient = ?)", nqc.Staff));
-                if(criteria.SentToGroupIncludingMe)
+				if(criteria.SentToGroupIncludingMe)
 					and.Conditions.Add(new HqlCondition("np = (select np1 from GroupNotePosting np1 where np1 = np and np1.Recipient = ?)", nqc.StaffGroup));
-                
-                or.Conditions.Add(and);
-            }
-            query.Conditions.Add(or);
+				
+				or.Conditions.Add(and);
+			}
+			query.Conditions.Add(or);
 			//query.Conditions.Add(ConditionMostRecentNote);
 
 			if(!countQuery)
-	            query.Sorts.AddRange(InboxItemOrdering);
+				query.Sorts.AddRange(InboxItemOrdering);
 
-            return query;
-        }
+			return query;
+		}
 
-        private HqlProjectionQuery BuildSentQuery(Notebox notebox, INoteboxQueryContext nqc, bool countQuery)
-        {
-            HqlProjectionQuery query = GetBaseQuery(nqc, countQuery, SentItemProjection, SentItemJoins);
+		private static HqlProjectionQuery BuildSentQuery(Notebox notebox, INoteboxQueryContext nqc, bool countQuery)
+		{
+			var query = GetBaseQuery(nqc, countQuery, SentItemProjection, SentItemJoins);
 
-            HqlOr or = new HqlOr();
-            foreach (NoteboxItemSearchCriteria criteria in notebox.GetInvariantCriteria(nqc))
-            {
-                HqlAnd and = new HqlAnd();
+			var or = new HqlOr();
+			foreach (var criteria in notebox.GetInvariantCriteria(nqc))
+			{
+				var and = new HqlAnd();
 
-                // for sent items, IsAcknowledged means fully acknowledged (all readers have acknowledged)
-                and.Conditions.Add(new HqlCondition("n.IsFullyAcknowledged = ?", criteria.IsAcknowledged));
+				// for sent items, IsAcknowledged means fully acknowledged (all readers have acknowledged)
+				and.Conditions.Add(new HqlCondition("n.IsFullyAcknowledged = ?", criteria.IsAcknowledged));
 
-                if (criteria.SentByMe)
-                    and.Conditions.Add(new HqlCondition("n.Author = ?", nqc.Staff));
+				if (criteria.SentByMe)
+					and.Conditions.Add(new HqlCondition("n.Author = ?", nqc.Staff));
 
-                or.Conditions.Add(and);
-            }
-            query.Conditions.Add(or);
+				or.Conditions.Add(and);
+			}
+			query.Conditions.Add(or);
 			query.Conditions.Add(new HqlCondition("size(n.Postings) > 0"));
 
 			if(!countQuery)
-	            query.Sorts.AddRange(SentItemOrdering);
+				query.Sorts.AddRange(SentItemOrdering);
 
-            return query;
-        }
+			return query;
+		}
 
-        protected List<OrderNoteboxItem> DoQuery(HqlProjectionQuery query)
-        {
-            IList<object[]> list = ExecuteHql<object[]>(query);
-            List<OrderNoteboxItem> results = new List<OrderNoteboxItem>();
-            foreach (object[] tuple in list)
-            {
-                OrderNoteboxItem item = (OrderNoteboxItem)Activator.CreateInstance(typeof(OrderNoteboxItem), tuple);
-                results.Add(item);
-            }
+		protected List<OrderNoteboxItem> DoQuery(HqlProjectionQuery query)
+		{
+			var list = ExecuteHql<object[]>(query);
+			var results = new List<OrderNoteboxItem>();
+			foreach (var tuple in list)
+			{
+				var item = (OrderNoteboxItem)Activator.CreateInstance(typeof(OrderNoteboxItem), tuple);
+				results.Add(item);
+			}
 
-            return results;
-        }
+			return results;
+		}
 
-        protected int DoQueryCount(HqlQuery query)
-        {
-            return (int)ExecuteHqlUnique<long>(query);
-        }
+		protected int DoQueryCount(HqlQuery query)
+		{
+			return (int)ExecuteHqlUnique<long>(query);
+		}
 
-        #endregion
-    }
+		#endregion
+	}
 }
