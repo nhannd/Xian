2ab0ddfdcf1aa5abd27105d5c232a716df8a4a49
jay <devnull@localhost>
2010-09-01 21:56:28 +0000

    Ticket: #7161 - DiagnosticServiceTree exporter should export taking into account the DisplayOrder of each child node
    Schema/Configuration Changes: N/A
    
    - fix tab and resharper suggestion prior to code changes

--- Healthcare/Imex/DiagnosticServiceImex.cs
+++ Healthcare/Imex/DiagnosticServiceImex.cs
@@ -29,111 +29,104 @@
 
 #endregion
 
-using System;
 using System.Collections.Generic;
-using System.Text;
-using ClearCanvas.Common;
-using ClearCanvas.Enterprise.Common;
-using ClearCanvas.Enterprise.Core.Imex;
 using System.Runtime.Serialization;
-using ClearCanvas.Enterprise.Core;
-using ClearCanvas.Healthcare.Brokers;
+using ClearCanvas.Common;
 using ClearCanvas.Common.Utilities;
+using ClearCanvas.Enterprise.Common;
+using ClearCanvas.Enterprise.Core;
+using ClearCanvas.Enterprise.Core.Imex;
+using ClearCanvas.Healthcare.Brokers;
 
 namespace ClearCanvas.Healthcare.Imex
 {
-    [ExtensionOf(typeof(XmlDataImexExtensionPoint))]
-    [ImexDataClass("DiagnosticService")]
-    public class DiagnosticServiceImex : XmlEntityImex<DiagnosticService, DiagnosticServiceImex.DiagnosticServiceData>
-    {
-        [DataContract]
+	[ExtensionOf(typeof(XmlDataImexExtensionPoint))]
+	[ImexDataClass("DiagnosticService")]
+	public class DiagnosticServiceImex : XmlEntityImex<DiagnosticService, DiagnosticServiceImex.DiagnosticServiceData>
+	{
+		[DataContract]
 		public class DiagnosticServiceData : ReferenceEntityDataBase
-        {
-            [DataMember]
-            public string Id;
+		{
+			[DataMember]
+			public string Id;
 
-            [DataMember]
-            public string Name;
+			[DataMember]
+			public string Name;
 
-            [DataMember]
-            public List<ProcedureTypeData> ProcedureTypes;
+			[DataMember]
+			public List<ProcedureTypeData> ProcedureTypes;
 
-        }
+		}
 
-        [DataContract]
-        public class ProcedureTypeData
-        {
-            [DataMember]
-            public string Id;
-        }
+		[DataContract]
+		public class ProcedureTypeData
+		{
+			[DataMember]
+			public string Id;
+		}
 
-        #region Overrides
+		#region Overrides
 
-        protected override IList<DiagnosticService> GetItemsForExport(IReadContext context, int firstRow, int maxRows)
-        {
-            DiagnosticServiceSearchCriteria where = new DiagnosticServiceSearchCriteria();
-            where.Id.SortAsc(0);
-            return context.GetBroker<IDiagnosticServiceBroker>().Find(where, new SearchResultPage(firstRow, maxRows));
-        }
+		protected override IList<DiagnosticService> GetItemsForExport(IReadContext context, int firstRow, int maxRows)
+		{
+			var where = new DiagnosticServiceSearchCriteria();
+			where.Id.SortAsc(0);
+			return context.GetBroker<IDiagnosticServiceBroker>().Find(where, new SearchResultPage(firstRow, maxRows));
+		}
 
-        protected override DiagnosticServiceData Export(DiagnosticService entity, IReadContext context)
-        {
-            DiagnosticServiceData data = new DiagnosticServiceData();
-        	data.Deactivated = entity.Deactivated;
-            data.Id = entity.Id;
-            data.Name = entity.Name;
-            data.ProcedureTypes = CollectionUtils.Map<ProcedureType,ProcedureTypeData>(
-                entity.ProcedureTypes,
-                delegate(ProcedureType pt)
-                {
-                    ProcedureTypeData ptdata = new ProcedureTypeData();
-                    ptdata.Id = pt.Id;
-                    return ptdata;
-                });
+		protected override DiagnosticServiceData Export(DiagnosticService entity, IReadContext context)
+		{
+			var data = new DiagnosticServiceData
+				{
+					Deactivated = entity.Deactivated,
+					Id = entity.Id,
+					Name = entity.Name,
+					ProcedureTypes = CollectionUtils.Map<ProcedureType, ProcedureTypeData>(entity.ProcedureTypes, pt => new ProcedureTypeData { Id = pt.Id })
+				};
 
-            return data;
-        }
+			return data;
+		}
 
-        protected override void Import(DiagnosticServiceData data, IUpdateContext context)
-        {
-            DiagnosticService ds = GetDiagnosticService(data.Id, data.Name, context);
-        	ds.Deactivated = data.Deactivated;
-            ds.Name = data.Name;
+		protected override void Import(DiagnosticServiceData data, IUpdateContext context)
+		{
+			var ds = GetDiagnosticService(data.Id, data.Name, context);
+			ds.Deactivated = data.Deactivated;
+			ds.Name = data.Name;
 
-            if (data.ProcedureTypes != null)
-            {
-                foreach (ProcedureTypeData s in data.ProcedureTypes)
-                {
-                    ProcedureTypeSearchCriteria where = new ProcedureTypeSearchCriteria();
-                    where.Id.EqualTo(s.Id);
-                    ProcedureType pt = CollectionUtils.FirstElement(context.GetBroker<IProcedureTypeBroker>().Find(where));
-                    if (pt != null)
-                        ds.ProcedureTypes.Add(pt);
-                }
-            }
-        }
+			if (data.ProcedureTypes != null)
+			{
+				foreach (var s in data.ProcedureTypes)
+				{
+					var where = new ProcedureTypeSearchCriteria();
+					where.Id.EqualTo(s.Id);
+					var pt = CollectionUtils.FirstElement(context.GetBroker<IProcedureTypeBroker>().Find(where));
+					if (pt != null)
+						ds.ProcedureTypes.Add(pt);
+				}
+			}
+		}
 
-        #endregion
+		#endregion
 
 
-        private DiagnosticService GetDiagnosticService(string id, string name, IPersistenceContext context)
-        {
-            DiagnosticService ds;
-            try
-            {
-                // see if already exists in db
-                DiagnosticServiceSearchCriteria where = new DiagnosticServiceSearchCriteria();
-                where.Id.EqualTo(id);
-                ds = context.GetBroker<IDiagnosticServiceBroker>().FindOne(where);
-            }
-            catch (EntityNotFoundException)
-            {
-                // create it
-                ds = new DiagnosticService(id, name);
-                context.Lock(ds, DirtyState.New);
-            }
+		private static DiagnosticService GetDiagnosticService(string id, string name, IPersistenceContext context)
+		{
+			DiagnosticService ds;
+			try
+			{
+				// see if already exists in db
+				var where = new DiagnosticServiceSearchCriteria();
+				where.Id.EqualTo(id);
+				ds = context.GetBroker<IDiagnosticServiceBroker>().FindOne(where);
+			}
+			catch (EntityNotFoundException)
+			{
+				// create it
+				ds = new DiagnosticService(id, name);
+				context.Lock(ds, DirtyState.New);
+			}
 
-            return ds;
-        }
-    }
+			return ds;
+		}
+	}
 }
