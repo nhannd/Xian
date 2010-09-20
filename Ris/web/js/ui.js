10be9d039bec11ff709b7403b585acea1ab40b45
jay <devnull@localhost>
2010-09-20 16:43:39 +0000

    Ticket(s): #7179 - Equipment missing in GI/GU procedure after Spring to Summer upgrade
    Schema/Configuration Changes: N/A
    
    - if user had specified a value that is not in the list of choices for a combobox, pre-append the value to the list of choices

--- Ris/web/js/ui.js
+++ Ris/web/js/ui.js
@@ -1,3 +1,30 @@
+// Copyright (c) 2010, ClearCanvas Inc.
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without modification, 
+// are permitted provided that the following conditions are met:
+//
+//    * Redistributions of source code must retain the above copyright notice, 
+//      this list of conditions and the following disclaimer.
+//    * Redistributions in binary form must reproduce the above copyright notice, 
+//      this list of conditions and the following disclaimer in the documentation 
+//      and/or other materials provided with the distribution.
+//    * Neither the name of ClearCanvas Inc. nor the names of its contributors 
+//      may be used to endorse or promote products derived from this software without 
+//      specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" 
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, 
+// THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR 
+// PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR 
+// CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, 
+// OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE 
+// GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) 
+// HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, 
+// STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN 
+// ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY 
+// OF SUCH DAMAGE.
+
 var _IE = document.all;
 
 /*
@@ -881,7 +908,12 @@ var Table = {
 
 				// choices may be an array, or a function that returns an array
 				var choices = (typeof(column.choices) == "function") ? column.choices(obj) : column.choices;
-				addOptions(input, choices);
+				var defaultChoice = column.getValue(obj);
+				var hasDefaultChoiceButNotInChoices = defaultChoice && defaultChoice != "" && choices.indexOf(defaultChoice) < 0;
+				if (hasDefaultChoiceButNotInChoices)
+					addOptions(input, [ defaultChoice ].concat(choices));
+				else
+					addOptions(input, choices);
 
 				input.onchange = function()
 				{
