ee20ac9c8eb3118addf23ad017eaf21b4ff3ce9d
jasper <devnull@localhost>
2010-11-08 20:01:40 +0000

    Bug: #6242 - Vector.LineSegmentIntersection computes unprecise floating point intersection ''Schema/Configuration Changes: N/A''  * Added unit tests for various static helper methods on Vector.  * Removed some overloaded helper methods on Vector that take Point instead of PointF on the justification that they were either entirely redundant or performed black-box rounding on the results.

--- ImageViewer/Mathematics/Tests/VectorTests.cs
+++ ImageViewer/Mathematics/Tests/VectorTests.cs
@@ -15,6 +15,7 @@
 
 using System;
 using System.Drawing;
+using ClearCanvas.Common;
 using NUnit.Framework;
 
 namespace ClearCanvas.ImageViewer.Mathematics.Tests
@@ -22,22 +23,8 @@ namespace ClearCanvas.ImageViewer.Mathematics.Tests
 	[TestFixture]
 	public class VectorTests
 	{
-		public VectorTests()
-		{
-		}
-
-		[TestFixtureSetUp]
-		public void Init()
-		{
-		}
-		
-		[TestFixtureTearDown]
-		public void Cleanup()
-		{
-		}
-
 		[Test]
-		public void SubtendedAngle()
+		public void TestSubtendedAngle()
 		{
 			double angle = Vector.SubtendedAngle(new PointF(10, 0), new PointF(0, 0), new PointF(1, 0));
 			Assert.AreEqual(0, angle);
@@ -53,7 +40,7 @@ namespace ClearCanvas.ImageViewer.Mathematics.Tests
 		}
 
 		[Test]
-		public void UnitVector()
+		public void TestUnitVector()
 		{
 			float oneOverSqrt2 = 1/(float) Math.Sqrt(2);
 			SizeF unitVector;
@@ -78,6 +65,221 @@ namespace ClearCanvas.ImageViewer.Mathematics.Tests
 			unitVector = new SizeF(Vector.CreateUnitVector(origin, origin + new SizeF(-2, 2)));
 			Assert.IsTrue(FloatComparer.AreEqual(new SizeF(-oneOverSqrt2, oneOverSqrt2), unitVector));
 			Assert.IsTrue(FloatComparer.AreEqual(unitVector.Width*unitVector.Width + unitVector.Height*unitVector.Height, 1), "Magnitude must be 1.");
+		}
+
+		[Test]
+		public void TestDistance()
+		{
+			var root2 = Math.Sqrt(2);
+			var p0 = new PointF(0, 0);
+			var p1 = new PointF(-1, 1);
+			var p2 = new PointF(1, -1);
+			var p3 = new PointF(-1, -1);
+			var p4 = new PointF(-2, -2);
+
+			// exercise zero computations
+			Assert.AreEqual(0, Vector.Distance(p0, p0), "P0 to P0 distance should be 0");
+			Assert.AreEqual(0, Vector.Distance(p1, p1), "P1 to P1 distance should be 0");
+			Assert.AreEqual(0, Vector.Distance(p2, p2), "P2 to P2 distance should be 0");
+			Assert.AreEqual(0, Vector.Distance(p3, p3), "P3 to P3 distance should be 0");
+
+			// exercise computation with origin
+			Assert.AreEqual(root2, Vector.Distance(p0, p1), "P0 to P1 distance should be \u221A2");
+			Assert.AreEqual(root2, Vector.Distance(p1, p0), "P1 to P0 distance should be \u221A2");
+			Assert.AreEqual(root2, Vector.Distance(p0, p2), "P0 to P2 distance should be \u221A2");
+			Assert.AreEqual(root2, Vector.Distance(p2, p0), "P2 to P0 distance should be \u221A2");
+			Assert.AreEqual(root2, Vector.Distance(p0, p3), "P0 to P3 distance should be \u221A2");
+			Assert.AreEqual(root2, Vector.Distance(p3, p0), "P3 to P0 distance should be \u221A2");
+
+			// exercise cross-quadrant computations
+			Assert.AreEqual(2*root2, Vector.Distance(p1, p2), "P1 to P2 distance should be 2\u221A2");
+			Assert.AreEqual(2*root2, Vector.Distance(p2, p1), "P2 to P1 distance should be 2\u221A2");
+			Assert.AreEqual(2, Vector.Distance(p1, p3), "P1 to P3 distance should be 2");
+			Assert.AreEqual(2, Vector.Distance(p3, p1), "P3 to P1 distance should be 2");
+			Assert.AreEqual(2, Vector.Distance(p2, p3), "P2 to P3 distance should be 2");
+			Assert.AreEqual(2, Vector.Distance(p3, p2), "P3 to P2 distance should be 2");
+
+			// exercise same-quadrant computation
+			Assert.AreEqual(root2, Vector.Distance(p3, p4), "P3 to P4 distance should be \u221A2");
+			Assert.AreEqual(root2, Vector.Distance(p4, p3), "P4 to P3 distance should be \u221A2");
+		}
+
+		[Test]
+		public void TestMidpoint()
+		{
+			var root2F = (float) Math.Sqrt(2);
+			var p0 = new PointF(0, 0);
+			var p1 = new PointF(-1, 1);
+			var p2 = new PointF(1, -1);
+			var p3 = new PointF(-1, -1);
+			var p4 = new PointF(-2, -2);
+
+			// exercise zero computations
+			Assert.AreEqual(p0, Vector.Midpoint(p0, p0), "P0 to P0 midpoint should be P0");
+			Assert.AreEqual(p1, Vector.Midpoint(p1, p1), "P1 to P1 midpoint should be P1");
+			Assert.AreEqual(p2, Vector.Midpoint(p2, p2), "P2 to P2 midpoint should be P2");
+			Assert.AreEqual(p3, Vector.Midpoint(p3, p3), "P3 to P3 midpoint should be P3");
+
+			// exercise computation with origin
+			Assert.AreEqual(new PointF(-1/2f, 1/2f), Vector.Midpoint(p0, p1), "P0 to P1 midpoint should be (-\u00BD,\u00BD)");
+			Assert.AreEqual(new PointF(-1/2f, 1/2f), Vector.Midpoint(p1, p0), "P1 to P0 midpoint should be (-\u00BD,\u00BD)");
+			Assert.AreEqual(new PointF(1/2f, -1/2f), Vector.Midpoint(p0, p2), "P0 to P2 midpoint should be (\u00BD,-\u00BD)");
+			Assert.AreEqual(new PointF(1/2f, -1/2f), Vector.Midpoint(p2, p0), "P2 to P0 midpoint should be (\u00BD,-\u00BD)");
+			Assert.AreEqual(new PointF(-1/2f, -1/2f), Vector.Midpoint(p0, p3), "P0 to P3 midpoint should be (-\u00BD,-\u00BD)");
+			Assert.AreEqual(new PointF(-1/2f, -1/2f), Vector.Midpoint(p3, p0), "P3 to P0 midpoint should be (-\u00BD,-\u00BD)");
+
+			// exercise cross-quadrant computations
+			Assert.AreEqual(new PointF(0, 0), Vector.Midpoint(p1, p2), "P1 to P2 midpoint should be (0,0)");
+			Assert.AreEqual(new PointF(0, 0), Vector.Midpoint(p2, p1), "P2 to P1 midpoint should be (0,0)");
+			Assert.AreEqual(new PointF(-1, 0), Vector.Midpoint(p1, p3), "P1 to P3 midpoint should be (-1,0)");
+			Assert.AreEqual(new PointF(-1, 0), Vector.Midpoint(p3, p1), "P3 to P1 midpoint should be (-1,0)");
+			Assert.AreEqual(new PointF(0, -1), Vector.Midpoint(p2, p3), "P2 to P3 midpoint should be (0,-1)");
+			Assert.AreEqual(new PointF(0, -1), Vector.Midpoint(p3, p2), "P3 to P2 midpoint should be (0,-1)");
+
+			// exercise same-quadrant computation
+			Assert.AreEqual(new PointF(root2F/2, -root2F/2), Vector.Midpoint(p0, new PointF(root2F, -root2F)), "P0 to (\u221A2,-\u221A2) midpoint should be (-\u221A2/2,-\u221A2/2)");
+			Assert.AreEqual(new PointF(root2F/2, -root2F/2), Vector.Midpoint(new PointF(root2F, -root2F), p0), "(\u221A2,-\u221A2) to P0 midpoint should be (-\u221A2/2,-\u221A2/2)");
+			Assert.AreEqual(new PointF(-1.5f, -1.5f), Vector.Midpoint(p3, p4), "P3 to P4 midpoint should be (-1\u00BD,-1\u00BD)");
+			Assert.AreEqual(new PointF(-1.5f, -1.5f), Vector.Midpoint(p4, p3), "P4 to P3 midpoint should be (-1\u00BD,-1\u00BD)");
+		}
+
+		[Test]
+		public void TestPointToLine()
+		{
+			var root2 = Math.Sqrt(2);
+			var root3 = Math.Sqrt(3);
+			var root5 = Math.Sqrt(5);
+			var p0 = new PointF(0, 0);
+			var p1 = new PointF(-1, -1);
+			var p2 = new PointF(2, -1);
+			var p3 = new PointF(1, 1);
+			var p4 = new PointF(1, (float) root3);
+
+			// exercise coincident point-point computations
+			AssertPointToLine(0, p0, p0, p0, p0, "Point P0 and line P0P0 (coincident points))");
+			AssertPointToLine(0, p1, p1, p1, p1, "Point P1 and line P1P1 (coincident points)");
+			AssertPointToLine(0, p2, p2, p2, p2, "Point P2 and line P2P2 (coincident points)");
+			AssertPointToLine(0, p3, p3, p3, p3, "Point P3 and line P3P3 (coincident points)");
+
+			// exercise point-point computations
+			AssertPointToLine(root2, p1, p0, p1, p1, "Point P0 and line P1P1 (point to point)");
+			AssertPointToLine(3, p2, p1, p2, p2, "Point P1 and line P2P2 (point to point)");
+			AssertPointToLine(root5, p3, p2, p3, p3, "Point P2 and line P3P3 (point to point)");
+			AssertPointToLine(root2, p0, p3, p0, p0, "Point P3 and line P0P0 (point to point)");
+
+			// exercise colinear point-line computations
+			AssertPointToLine(0, p0, p0, p1, p3, "Point P0 and line P1P3 (colinear point to line segment)");
+			AssertPointToLine(0, p0, p0, p3, p1, "Point P0 and line P3P1 (colinear point to line segment)");
+			AssertPointToLine(root2, p0, p1, p0, p3, "Point P1 and line P0P3 (colinear point to line segment)");
+			AssertPointToLine(root2, p0, p1, p3, p0, "Point P1 and line P3P0 (colinear point to line segment)");
+			AssertPointToLine(root2, p0, p3, p0, p1, "Point P3 and line P0P1 (colinear point to line segment)");
+			AssertPointToLine(root2, p0, p3, p1, p0, "Point P3 and line P1P0 (colinear point to line segment)");
+
+			// exercise point-line computations
+			AssertPointToLine(root2, p0, p1, p0, p2, "Point P1 and line P0P2 (point to line segment)");
+			AssertPointToLine(root2, p0, p1, p2, p0, "Point P1 and line P2P0 (point to line segment)");
+			AssertPointToLine(2, new PointF(1, -1), p3, p1, p2, "Point P3 and line P1P2 (point to line segment)");
+			AssertPointToLine(2, new PointF(1, -1), p3, p2, p1, "Point P3 and line P2P1 (point to line segment)");
+			AssertPointToLine(2*root3 - 1.5543624e-8, p4, new PointF(4, 0), p0, p4, "Point (4,0) and line P0P4 (point to line segment)");
+			AssertPointToLine(2*root3 - 1.5543624e-8, p4, new PointF(4, 0), p4, p0, "Point (4,0) and line P4P0 (point to line segment)");
+			// distance is off by that much here even when using Vector.Distance as a reference
+		}
+
+		private static void AssertPointToLine(double expectedDistance, PointF expectedPoint, PointF pT, PointF p1, PointF p2, string message, params object[] args)
+		{
+			var actualPoint = new PointF();
+			var actualDistance = Vector.DistanceFromPointToLine(pT, p1, p2, ref actualPoint);
+			Assert.AreEqual(expectedDistance, actualDistance, message, args);
+			Assert.AreEqual(expectedPoint, actualPoint, message, args);
+		}
+
+		[Test]
+		public void TestLineSegmentIntersection()
+		{
+			var p0 = new PointF(0, 0);
+			var p1 = new PointF(3, 0);
+			var p2 = new PointF(2, 1);
+			var p3 = new PointF(1.5f, 0);
+			var p4 = new PointF(1, -1);
+			var p5 = new PointF(4, 0);
+			var p6 = new PointF(2.5f, 2);
+			var p7 = new PointF(5, 1);
+
+			// exercise dual degenerate intersection computations
+			AssertLineSegmentIntersection(Vector.LineSegments.Colinear, null, p0, p0, p0, p0, "P0P0 and P0P0 (point and point)");
+			AssertLineSegmentIntersection(Vector.LineSegments.Colinear, null, p1, p1, p1, p1, "P1P1 and P1P1 (point and point)");
+			AssertLineSegmentIntersection(Vector.LineSegments.Colinear, null, p0, p0, p1, p1, "P0P0 and P1P1 (point and point)");
+			AssertLineSegmentIntersection(Vector.LineSegments.Colinear, null, p1, p1, p0, p0, "P1P1 and P0P0 (point and point)");
+
+			// exercise single degenerate intersection computations
+			AssertLineSegmentIntersection(Vector.LineSegments.DoNotIntersect, null, p2, p2, p0, p1, "P2P2 and P0P1 (point and line segment)");
+			AssertLineSegmentIntersection(Vector.LineSegments.DoNotIntersect, null, p2, p2, p1, p0, "P2P2 and P1P0 (point and line segment)");
+			AssertLineSegmentIntersection(Vector.LineSegments.Colinear, null, p3, p3, p0, p1, "P3P3 and P0P1 (point and line segment)");
+			AssertLineSegmentIntersection(Vector.LineSegments.Colinear, null, p3, p3, p1, p0, "P3P3 and P1P0 (point and line segment)");
+			AssertLineSegmentIntersection(Vector.LineSegments.Colinear, null, p5, p5, p0, p1, "P5P5 and P0P1 (point and line segment)");
+			AssertLineSegmentIntersection(Vector.LineSegments.Colinear, null, p5, p5, p1, p0, "P5P5 and P1P0 (point and line segment)");
+
+			// exercise two non-colinear line segments
+			AssertLineSegmentIntersection(Vector.LineSegments.Intersect, p3, p2, p4, p0, p1, "P2P4 and P0P1 (distinct line segments)");
+			AssertLineSegmentIntersection(Vector.LineSegments.Intersect, p3, p4, p2, p0, p1, "P4P2 and P0P1 (distinct line segments)");
+			AssertLineSegmentIntersection(Vector.LineSegments.DoNotIntersect, null, p2, p6, p0, p1, "P2P6 and P0P1 (distinct line segments)");
+			AssertLineSegmentIntersection(Vector.LineSegments.DoNotIntersect, null, p6, p2, p0, p1, "P6P2 and P0P1 (distinct line segments)");
+			AssertLineSegmentIntersection(Vector.LineSegments.DoNotIntersect, null, p2, p7, p0, p1, "P2P7 and P0P1 (distinct line segments)");
+			AssertLineSegmentIntersection(Vector.LineSegments.DoNotIntersect, null, p7, p2, p0, p1, "P7P2 and P0P1 (distinct line segments)");
+			AssertLineSegmentIntersection(Vector.LineSegments.DoNotIntersect, null, p6, p7, p0, p1, "P6P7 and P0P1 (distinct line segments)");
+			AssertLineSegmentIntersection(Vector.LineSegments.DoNotIntersect, null, p7, p6, p0, p1, "P7P6 and P0P1 (distinct line segments)");
+
+			// exercise two colinear line segments
+			AssertLineSegmentIntersection(Vector.LineSegments.Colinear, null, p0, p1, p3, p5, "P0P1 and P3P5 (partially overlapping line segments)");
+			AssertLineSegmentIntersection(Vector.LineSegments.Colinear, null, p0, p1, p5, p3, "P0P1 and P5P3 (partially overlapping line segments)");
+			AssertLineSegmentIntersection(Vector.LineSegments.Colinear, null, p1, p0, p3, p5, "P1P0 and P3P5 (partially overlapping line segments)");
+			AssertLineSegmentIntersection(Vector.LineSegments.Colinear, null, p1, p0, p5, p3, "P1P0 and P5P3 (partially overlapping line segments)");
+			AssertLineSegmentIntersection(Vector.LineSegments.Colinear, null, p0, p5, p3, p1, "P0P5 and P3P1 (overlapping line segments)");
+			AssertLineSegmentIntersection(Vector.LineSegments.Colinear, null, p0, p5, p1, p3, "P0P5 and P1P3 (overlapping line segments)");
+			AssertLineSegmentIntersection(Vector.LineSegments.Colinear, null, p3, p1, p5, p0, "P3P1 and P5P0 (overlapping line segments)");
+			AssertLineSegmentIntersection(Vector.LineSegments.Colinear, null, p1, p3, p5, p0, "P1P3 and P5P0 (overlapping line segments)");
+			AssertLineSegmentIntersection(Vector.LineSegments.Colinear, null, p0, p3, p1, p5, "P0P3 and P1P5 (distinct line segments)");
+			AssertLineSegmentIntersection(Vector.LineSegments.Colinear, null, p3, p0, p5, p1, "P3P0 and P5P1 (distinct line segments)");
+			AssertLineSegmentIntersection(Vector.LineSegments.Colinear, null, p2, p4, p2, p6, "P2P4 and P2P6 (common vertex line segments)");
+			AssertLineSegmentIntersection(Vector.LineSegments.Colinear, null, p2, p4, p6, p2, "P2P4 and P6P2 (common vertex line segments)");
+			AssertLineSegmentIntersection(Vector.LineSegments.Colinear, null, p4, p2, p2, p6, "P4P2 and P2P6 (common vertex line segments)");
+			AssertLineSegmentIntersection(Vector.LineSegments.Colinear, null, p4, p2, p6, p2, "P4P2 and P6P2 (common vertex line segments)");
+		}
+
+		private static void AssertLineSegmentIntersectionP(Vector.LineSegments expectedResult, PointF? expectedIntersection, PointF p1, PointF p2, PointF q1, PointF q2, string message, params object[] args)
+		{
+			AssertLineSegmentIntersection(expectedResult, expectedIntersection, p1, p2, q1, q2, string.Format(message, args) + " [Permutation {0}]", "p1p2,q1q2");
+			AssertLineSegmentIntersection(expectedResult, expectedIntersection, p2, p1, q1, q2, string.Format(message, args) + " [Permutation {0}]", "p2p1,q1q2");
+			AssertLineSegmentIntersection(expectedResult, expectedIntersection, p1, p2, q2, q1, string.Format(message, args) + " [Permutation {0}]", "p1p2,q2q1");
+			AssertLineSegmentIntersection(expectedResult, expectedIntersection, p2, p1, q2, q1, string.Format(message, args) + " [Permutation {0}]", "p2p1,q2q1");
+			AssertLineSegmentIntersection(expectedResult, expectedIntersection, q1, q2, p1, p2, string.Format(message, args) + " [Permutation {0}]", "q1q2,p1p2");
+			AssertLineSegmentIntersection(expectedResult, expectedIntersection, q2, q1, p1, p2, string.Format(message, args) + " [Permutation {0}]", "q2q1,p1p2");
+			AssertLineSegmentIntersection(expectedResult, expectedIntersection, q1, q2, p2, p1, string.Format(message, args) + " [Permutation {0}]", "q1q2,p2p1");
+			AssertLineSegmentIntersection(expectedResult, expectedIntersection, q2, q1, p2, p1, string.Format(message, args) + " [Permutation {0}]", "q2q1,p2p1");
+		}
+
+		private static void AssertLineSegmentIntersection(Vector.LineSegments expectedResult, PointF? expectedIntersection, PointF p1, PointF p2, PointF q1, PointF q2, string message, params object[] args)
+		{
+			// these checks protect against errors in the test case
+			Platform.CheckFalse(expectedResult != Vector.LineSegments.Intersect && expectedIntersection.HasValue, "Expected intersection point must be null if not expecting an intersection.");
+			Platform.CheckFalse(expectedResult == Vector.LineSegments.Intersect && !expectedIntersection.HasValue, "Expected intersection point must be provided if expecting an intersection.");
+
+			PointF actualIntersection;
+			var actualResult = Vector.LineSegmentIntersection(p1, p2, q1, q2, out actualIntersection);
+			Assert.AreEqual(expectedResult, actualResult, message, args);
+
+			if (actualResult == Vector.LineSegments.Intersect)
+			{
+				Console.WriteLine(" Expected: {0}", expectedIntersection.Value);
+				Console.WriteLine("   Actual: {0}", actualIntersection);
+				Assert.IsTrue(Math.Abs(expectedIntersection.Value.X - actualIntersection.X) <= 0.5001f, message, args);
+				Assert.IsTrue(Math.Abs(expectedIntersection.Value.Y - actualIntersection.Y) <= 0.5001f, message, args);
+				//Assert.AreEqual(expectedIntersection, actualIntersection, message, args);
+			}
+			else
+			{
+				Assert.AreEqual(actualIntersection, PointF.Empty, message, args);
+			}
 		}
 	}
 }
--- ImageViewer/Mathematics/Vector.cs
+++ ImageViewer/Mathematics/Vector.cs
@@ -57,23 +57,6 @@ namespace ClearCanvas.ImageViewer.Mathematics
 		/// <summary>
 		/// Calculates the distance between two points.
 		/// </summary>
-		/// <param name="pt1"></param>
-		/// <param name="pt2"></param>
-		/// <returns></returns>
-		public static double Distance(Point pt1, Point pt2)
-		{
-			int deltaX = pt2.X - pt1.X;
-			int deltaY = pt2.Y - pt1.Y;
-
-			return Math.Sqrt(deltaX * deltaX + deltaY * deltaY);
-		}
-
-		/// <summary>
-		/// Calculates the distance between two points.
-		/// </summary>
-		/// <param name="pt1"></param>
-		/// <param name="pt2"></param>
-		/// <returns></returns>
 		public static double Distance(PointF pt1, PointF pt2)
 		{
 			float deltaX = pt2.X - pt1.X;
@@ -85,30 +68,12 @@ namespace ClearCanvas.ImageViewer.Mathematics
 		/// <summary>
 		/// Finds the midpoint between two points.
 		/// </summary>
-		/// <param name="pt1"></param>
-		/// <param name="pt2"></param>
-		/// <returns></returns>
 		public static PointF Midpoint(PointF pt1, PointF pt2)
 		{
-			float x = (pt1.X + pt2.X) / 2;
-			float y = (pt1.Y + pt2.Y) / 2;
+			float x = (pt1.X + pt2.X) / 2f;
+			float y = (pt1.Y + pt2.Y) / 2f;
 
 			return new PointF(x, y);
-		}
-
-		//TODO (cr Feb 2010): Delete this, as it's inaccurate - if you want this, then do the rounding.
-		/// <summary>
-		/// Finds the midpoint between two points.
-		/// </summary>
-		/// <param name="pt1"></param>
-		/// <param name="pt2"></param>
-		/// <returns></returns>
-		public static Point Midpoint(Point pt1, Point pt2)
-		{
-			int x = (pt1.X + pt2.X) / 2;
-			int y = (pt1.Y + pt2.Y) / 2;
-
-			return new Point(x, y);
 		}
 
 		/// <summary>
@@ -132,10 +97,6 @@ namespace ClearCanvas.ImageViewer.Mathematics
 			return new PointF((float)(deltaX / magnitude), (float)(deltaY / magnitude));
 		}
 
-		//TODO (cr Feb 2010): Although we are pretty confident in this code, since
-		//it's been in use for a long time without change, there are no unit tests.
-		//The code is used in the Polygon, Linear and Protractor calculations, as well as the scale graphic.
-
 		/// <summary>
 		/// Calculates the shortest distance from a point to a line segment.
 		/// </summary>
@@ -146,7 +107,7 @@ namespace ClearCanvas.ImageViewer.Mathematics
 		/// closest to the test point.</param>
 		/// <returns>The distance from the test point to
 		/// the nearest point on the line segment.</returns>
-		static public double DistanceFromPointToLine(PointF ptTest, PointF pt1, PointF pt2, ref PointF ptNearest)
+		public static double DistanceFromPointToLine(PointF ptTest, PointF pt1, PointF pt2, ref PointF ptNearest)
 		{
 			float distanceX;
 			float distanceY;
@@ -210,115 +171,6 @@ namespace ClearCanvas.ImageViewer.Mathematics
 			/// The line segments are colinear.
 			/// </summary>
 			Colinear
-		}
-
-		/// <summary>
-		/// Determines whether two line segments intersect, do not intersect
-		/// or are colinear.
-		/// </summary>
-		/// <param name="p1"></param>
-		/// <param name="p2"></param>
-		/// <param name="q1"></param>
-		/// <param name="q2"></param>
-		/// <param name="intersectionPoint"></param>
-		/// <returns></returns>
-		public static LineSegments LineSegmentIntersection(
-			Point p1,
-			Point p2,
-			Point q1,
-			Point q2,
-			out Point intersectionPoint)
-		{
-			int x1 = p1.X;
-			int y1 = p1.Y;
-			int x2 = p2.X;
-			int y2 = p2.Y;
-			int x3 = q1.X;
-			int y3 = q1.Y;
-			int x4 = q2.X;
-			int y4 = q2.Y;
-
-			int a1, a2, b1, b2, c1, c2; /* Coefficients of line eqns. */
-			int r1, r2, r3, r4;         /* 'Sign' values */
-			int denom, offset, num;     /* Intermediate values */
-
-			/* Compute a1, b1, c1, where line joining points 1 and 2
-			 * is "a1 x  +  b1 y  +  c1  =  0".
-			 */
-
-			a1 = y2 - y1;
-			b1 = x1 - x2;
-			c1 = x2 * y1 - x1 * y2;
-
-			/* Compute r3 and r4.
-			 */
-
-
-			r3 = a1 * x3 + b1 * y3 + c1;
-			r4 = a1 * x4 + b1 * y4 + c1;
-
-			/* Check signs of r3 and r4.  If both point 3 and point 4 lie on
-			 * same side of line 1, the line segments do not intersect.
-			 */
-
-			if (r3 != 0 &&
-				r4 != 0 &&
-				Math.Sign(r3) == Math.Sign(r4))
-			{
-				intersectionPoint = new Point(0, 0);
-				return LineSegments.DoNotIntersect;
-			}
-
-			/* Compute a2, b2, c2 */
-
-			a2 = y4 - y3;
-			b2 = x3 - x4;
-			c2 = x4 * y3 - x3 * y4;
-
-			/* Compute r1 and r2 */
-
-			r1 = a2 * x1 + b2 * y1 + c2;
-			r2 = a2 * x2 + b2 * y2 + c2;
-
-			/* Check signs of r1 and r2.  If both point 1 and point 2 lie
-			 * on same side of second line segment, the line segments do
-			 * not intersect.
-			 */
-
-			if (r1 != 0 &&
-				r2 != 0 &&
-				Math.Sign(r1) == Math.Sign(r2))
-			{
-				intersectionPoint = new Point(0, 0);
-				return LineSegments.DoNotIntersect;
-			}
-
-			/* Line segments intersect: compute intersection point. 
-			 */
-
-			denom = a1 * b2 - a2 * b1;
-			if (denom == 0)
-			{
-				intersectionPoint = new Point(0, 0);
-				return LineSegments.Colinear;
-			}
-
-			offset = denom < 0 ? -denom / 2 : denom / 2;
-
-			/* The denom/2 is to get rounding instead of truncating.  It
-			 * is added or subtracted to the numerator, depending upon the
-			 * sign of the numerator.
-			 */
-
-			num = b1 * c2 - b2 * c1;
-			int x = (num < 0 ? num - offset : num + offset) / denom;
-
-			num = a2 * c1 - a1 * c2;
-			int y = (num < 0 ? num - offset : num + offset) / denom;
-
-			intersectionPoint = new Point(x, y);
-
-			return LineSegments.Intersect;
 		}
 
 		/// <summary>
