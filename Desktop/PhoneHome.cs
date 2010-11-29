30cc5d81ff96f13e82d8042df1719a68cd852b7d
thanh <devnull@localhost>
2010-11-29 20:27:13 +0000

    Ticket: #7489 - Workstation will transmit process up-time in shutdown message Ticket: #7488 - The service will store process up-time information Ticket: #7487 - The service must be capable of storing application specific data in custom ways, including additional tables
    Schema/Configuration Changes: YES
    
    - Update PhoneHome class to send process uptime on shutdown.
    - Added WorkstationDataProcessor to process workstation-specific data. A summary of the process uptime is stored in the Usage table.
    - Added UsageHistory table to store messages received for a given installation.
    - Updated usage tracking service to remove old messages (number of messages and max age are configurable)

--- Desktop/PhoneHome.cs
+++ Desktop/PhoneHome.cs
@@ -10,9 +10,12 @@
 #endregion
 
 using System;
+using System.Collections.Generic;
+using System.Globalization;
 using System.Threading;
 using ClearCanvas.Common;
 using ClearCanvas.Common.UsageTracking;
+using Timer=System.Threading.Timer;
 
 namespace ClearCanvas.Desktop
 {
@@ -23,7 +26,7 @@ namespace ClearCanvas.Desktop
         static private Timer _phoneHomeTimer;
         static private readonly object _sync = new object();
         static private bool _started;
-
+        static private DateTime _startTimestamp;
         #endregion
 
         #region Public Methods
@@ -33,11 +36,26 @@ namespace ClearCanvas.Desktop
         /// </summary>
         static internal void Startup()
         {
-            lock(_sync)
+            lock (_sync)
             {
-                OnStartUp();    
+                try
+                {
+                    OnStartUp();
+
+                    var msg = CreateUsageMessage(UsageType.Startup);
+                    UsageTracking.Register(msg, UsageTrackingThread.Background);
+
+                    _phoneHomeTimer = new Timer(ignore =>
+                                                UsageTracking.Register(UsageTracking.GetUsageMessage(), UsageTrackingThread.Background),
+                                                null, TimeSpan.FromHours(24), TimeSpan.FromHours(24));
+                }
+                catch (Exception ex)
+                {
+                    // Requirement says log must be in debug
+                    Platform.Log(LogLevel.Debug, ex, "Error occurred when shutting down phone home service");
+                }
             }
-            
+
         }
 
         /// <summary>
@@ -49,15 +67,28 @@ namespace ClearCanvas.Desktop
             {
                 try
                 {
-                    if (_phoneHomeTimer != null)
-                    {
-                        _phoneHomeTimer.Dispose();
-                        _phoneHomeTimer = null;
-                    }
+                    OnShutdown();
                     
+                    TimeSpan uptime = DateTime.Now - _startTimestamp;
+
                     // Message must be sent using the current thread instead of threadpool when the app is being shut down
                     Thread workerThread =
-                        new Thread(ignore => UsageTracking.Register(UsageTracking.GetUsageMessage(), UsageTrackingThread.Current));
+                        new Thread(ignore =>
+                        {
+                            try
+                            {
+                                var msg = CreateUsageMessage(UsageType.Shutdown);
+                                msg.AppData = new List<UsageApplicationData>();
+                                msg.AppData.Add(new UsageApplicationData
+                                                    {Key = "PROCESSUPTIME", Value = String.Format(CultureInfo.InvariantCulture, "{0}", uptime.TotalHours)});
+                                UsageTracking.Register(msg, UsageTrackingThread.Current);
+                            }
+                            catch (Exception ex)
+                            {
+                                // Requirement says log must be in debug
+                                Platform.Log(LogLevel.Debug, ex, "Error occurred when shutting down phone home service");
+                            }
+                        });
                     workerThread.Start();
 
                     // wait up to 10 seconds, this is a requirement.
@@ -66,7 +97,8 @@ namespace ClearCanvas.Desktop
                         Platform.Log(LogLevel.Debug,
                                      "PhoneHome.ShutDown(): web service does not return within 10 seconds. Continue shutting down.");
                     }
-                }catch(Exception ex)
+                }
+                catch (Exception ex)
                 {
                     // Requirement says log must be in debug
                     Platform.Log(LogLevel.Debug, ex, "Error occurred when shutting down phone home service");
@@ -74,32 +106,37 @@ namespace ClearCanvas.Desktop
             }
         }
 
+        private static void OnShutdown()
+        {
+            if (_phoneHomeTimer != null)
+            {
+                _phoneHomeTimer.Dispose();
+                _phoneHomeTimer = null;
+            }
+        }
+
         #endregion
 
         #region Helpers
+
+        static UsageMessage CreateUsageMessage(UsageType type)
+        {
+            var msg = UsageTracking.GetUsageMessage();
+            msg.MessageType = type;
+            return msg;
+        }
+
 
         static private void OnStartUp()
         {
             if (!_started)
             {
-                try
-                {
-                    UsageTracking.Register(UsageTracking.GetUsageMessage(), UsageTrackingThread.Background);
-
-                    _phoneHomeTimer = new Timer(ignore =>
-                                                UsageTracking.Register(UsageTracking.GetUsageMessage(),UsageTrackingThread.Background),
-                                                null, TimeSpan.FromHours(24), TimeSpan.FromHours(24));
-
-                    _started = true;
-                }
-                catch (Exception ex)
-                {
-                    // Requirement says log must be in debug
-                    Platform.Log(LogLevel.Debug, ex, "Error occurred when starting up phone home service");
-                }
+                _startTimestamp = DateTime.Now;
+                _started = true;
             }
         }
 
         #endregion
     }
+
 }
