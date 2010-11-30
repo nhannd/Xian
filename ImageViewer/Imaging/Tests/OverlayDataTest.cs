818722997348f89fa8a2ab1a2988d58f331871f4
jasper <devnull@localhost>
2010-11-30 21:45:30 +0000

    Bug: #7522 - Multiframe overlays not handled correctly by the viewer ''Schema/Configuration Changes: N/A''  * Added additional unit tests to OverlayData for asserting correct operation of bit unpacking algorithms.

--- ImageViewer/Imaging/Tests/OverlayDataTest.cs
+++ ImageViewer/Imaging/Tests/OverlayDataTest.cs
@@ -32,6 +32,7 @@
 #if	UNIT_TESTS
 #pragma warning disable 1591,0419,1574,1587
 
+using System.Text;
 using NUnit.Framework;
 
 namespace ClearCanvas.ImageViewer.Imaging.Tests
@@ -39,10 +40,189 @@ namespace ClearCanvas.ImageViewer.Imaging.Tests
 	[TestFixture]
 	public class OverlayDataTest
 	{
-		public OverlayDataTest() {}
+		#region OverlayData Tests
 
 		[Test]
-		public void TestBitUnpackingSingleFrame()
+		public void TestUnpack_SingleFrameA()
+		{
+			// 1 frame, 5x3
+			// 11111
+			// 10001
+			// 11111
+			// continuous bit stream: 11111100 0111111x
+			// continuous LE byte stream: 00111111 x1111110
+			// continuous BE word stream: x1111110 00111111
+
+			const string expectedResult = "111111000111111";
+			var packedBits = new byte[] {0x3f, 0x7e};
+
+			// little endian test
+			{
+				var unpackedBits = new OverlayData(3, 5, false, packedBits).Unpack();
+				var actualResult = FormatNonZeroBytes(unpackedBits);
+				Assert.AreEqual(expectedResult, actualResult, "Error in unpacked bits for single frame case A (little endian)");
+			}
+
+			// big endian test
+			{
+				var unpackedBits = new OverlayData(3, 5, true, SwapBytes(packedBits)).Unpack();
+				var actualResult = FormatNonZeroBytes(unpackedBits);
+				Assert.AreEqual(expectedResult, actualResult, "Error in unpacked bits for single frame case A (big endian)");
+			}
+		}
+
+		[Test]
+		public void TestUnpack_SingleFrameB()
+		{
+			// 1 frame, 5x3
+			// 11111
+			// 10010
+			// 00000
+			// continuous bit stream: 11111100 1000000x
+			// continuous LE byte stream: 00111111 x0000001
+
+			const string expectedResult = "111111001000000";
+			var packedBits = new byte[] {0x3f, 0x81};
+
+			// little endian test
+			{
+				var unpackedBits = new OverlayData(3, 5, false, packedBits).Unpack();
+				var actualResult = FormatNonZeroBytes(unpackedBits);
+				Assert.AreEqual(expectedResult, actualResult, "Error in unpacked bits for single frame case B (little endian)");
+			}
+
+			// big endian test
+			{
+				var unpackedBits = new OverlayData(3, 5, true, SwapBytes(packedBits)).Unpack();
+				var actualResult = FormatNonZeroBytes(unpackedBits);
+				Assert.AreEqual(expectedResult, actualResult, "Error in unpacked bits for single frame case B (big endian)");
+			}
+		}
+
+		[Test]
+		public void TestUnpack_Multiframe()
+		{
+			// 7 frames, each 3x2 (cols, rows) = 42 bits = 6 bytes
+			// 111 111 111 111 111 111 111
+			// 000 001 010 011 100 101 110
+			// continuous bit stream: 11100011 10011110 10111011 11110011 11011111 10xxxxxx
+			// continuous LE byte stream: 11000111 01111001 11011101 11001111 11111011 xxxxxx01
+			var expectedResult = new[] {"111000", "111001", "111010", "111011", "111100", "111101", "111110"};
+			var packedBits = new byte[] {0xc7, 0x79, 0xdd, 0xcf, 0xfb, 0xf1};
+
+			for (int frameIndex = 0; frameIndex < 7; frameIndex++)
+			{
+				var offset = 3*2*frameIndex;
+
+				// little endian test
+				{
+					var unpackedBits = new OverlayData(offset, 2, 3, false, packedBits).Unpack();
+					var actualResult = FormatNonZeroBytes(unpackedBits);
+					Assert.AreEqual(expectedResult[frameIndex], actualResult, "Error in unpacked bits for multiframe (fr#{0}) (little endian)", frameIndex);
+				}
+
+				// big endian test
+				{
+					var unpackedBits = new OverlayData(offset, 2, 3, true, SwapBytes(packedBits)).Unpack();
+					var actualResult = FormatNonZeroBytes(unpackedBits);
+					Assert.AreEqual(expectedResult[frameIndex], actualResult, "Error in unpacked bits for multiframe (fr#{0}) (big endian)", frameIndex);
+				}
+			}
+		}
+
+		[Test]
+		public void TestUnpackFromPixelData_8BitsAllocated()
+		{
+			// 1 frame, 5x3
+			// 11111
+			// 10010
+			// 00000
+			// continuous bit stream: 11111100 1000000x
+			// continuous LE byte stream: 00111111 x0000001
+			const string expectedResult = "111111001000000";
+
+			// 8 bits allocated
+
+			// some pixel data: 1st col is a random "pixel", 2nd col creates room for overlay bit, 3rd col is the overlay
+			var pixelData = new byte[]
+			                	{
+			                		(0xC5 & 0xFE) | 1,
+			                		(0x2D & 0xFE) | 1,
+			                		(0x5B & 0xFE) | 1,
+			                		(0xB3 & 0xFE) | 1,
+			                		(0xFC & 0xFE) | 1,
+			                		(0xBC & 0xFE) | 1,
+			                		(0x4d & 0xFE) | 0,
+			                		(0xbf & 0xFE) | 0,
+			                		(0x86 & 0xFE) | 1,
+			                		(0x75 & 0xFE) | 0,
+			                		(0xA8 & 0xFE) | 0,
+			                		(0x19 & 0xFE) | 0,
+			                		(0xAC & 0xFE) | 0,
+			                		(0xD4 & 0xFE) | 0,
+			                		(0x79 & 0xFE) | 0
+			                	};
+
+			var extractedBits = OverlayData.UnpackFromPixelData(0, 8, false, pixelData);
+			var actualResult = FormatNonZeroBytes(extractedBits);
+			Assert.AreEqual(expectedResult, actualResult, "Error in extracted bits from pixel data (8-bit pixels)");
+		}
+
+		[Test]
+		public void TestUnpackFromPixelData_16BitsAllocated()
+		{
+			// 1 frame, 5x3
+			// 11111
+			// 10010
+			// 00000
+			// continuous bit stream: 11111100 1000000x
+			// continuous LE byte stream: 00111111 x0000001
+			const string expectedResult = "111111001000000";
+
+			// 16 bits allocated
+
+			// some pixel data: 1st col is a random "pixel", 2nd col creates room for overlay bit, 3rd col is the overlay
+			//                  4th col moves the overlay to bit position 14, 5th col is the lower byte of the random pixel
+			var pixelData = SwapBytes(new byte[] // written in big endian form for ease of reading :P
+			                          	{
+			                          		(0xC5 & 0x0F) | (1 << 6), 0x93,
+			                          		(0x2D & 0x0F) | (1 << 6), 0x9C,
+			                          		(0x5B & 0x0F) | (1 << 6), 0x78,
+			                          		(0xB3 & 0x0F) | (1 << 6), 0x17,
+			                          		(0xFC & 0x0F) | (1 << 6), 0x0c,
+			                          		(0xBC & 0x0F) | (1 << 6), 0xc4,
+			                          		(0x4d & 0x0F) | (0 << 6), 0x45,
+			                          		(0xbf & 0x0F) | (0 << 6), 0xcd,
+			                          		(0x86 & 0x0F) | (1 << 6), 0xAE,
+			                          		(0x75 & 0x0F) | (0 << 6), 0xA9,
+			                          		(0xA8 & 0x0F) | (0 << 6), 0x29,
+			                          		(0x19 & 0x0F) | (0 << 6), 0x11,
+			                          		(0xAC & 0x0F) | (0 << 6), 0xDD,
+			                          		(0xD4 & 0x0F) | (0 << 6), 0x01,
+			                          		(0x79 & 0x0F) | (0 << 6), 0xF0
+			                          	});
+
+			// little endian
+			{
+				var extractedBits = OverlayData.UnpackFromPixelData(14, 16, false, pixelData);
+				var actualResult = FormatNonZeroBytes(extractedBits);
+				Assert.AreEqual(expectedResult, actualResult, "Error in extracted bits from pixel data (16-bit pixels, little endian)");
+			}
+
+			// big endian
+			{
+				var extractedBits = OverlayData.UnpackFromPixelData(14, 16, true, SwapBytes(pixelData));
+				var actualResult = FormatNonZeroBytes(extractedBits);
+				Assert.AreEqual(expectedResult, actualResult, "Error in extracted bits from pixel data (16-bit pixels, big endian)");
+			}
+		}
+
+		#endregion
+
+		#region Algorithm Tests
+
+		[Test]
+		public void TestBitUnpacking_SingleFrame()
 		{
 			byte[] testData;
 
@@ -50,7 +230,7 @@ namespace ClearCanvas.ImageViewer.Imaging.Tests
 			// 11111
 			// 10001
 			// 11111
-			// continuous stream: 11111100 0111111x (little end first)
+			// continuous bit stream: 11111100 0111111x
 			// continuous LE byte stream: 00111111 x1111110
 			// continuous BE word stream: x1111110 00111111
 			testData = new byte[] {0x3f, 0x7e};
@@ -60,21 +240,21 @@ namespace ClearCanvas.ImageViewer.Imaging.Tests
 			// 11111
 			// 10010
 			// 00000
-			// continuous stream: 11111100 1000000x (little end first)
+			// continuous bit stream: 11111100 1000000x
 			// continuous LE byte stream: 00111111 x0000001
 			testData = new byte[] {0x3f, 0x81};
 			TestFrame(testData, 5*3, 0, "111111001000000", "continuous stream: 11111100 1000000x");
 		}
 
 		[Test]
-		public void TestBitUnpackingMultiFrame()
+		public void TestBitUnpacking_MultiFrame()
 		{
 			byte[] testData;
 
 			// 7 frames, each 3x2 (cols, rows) = 42 bits = 6 bytes
 			// 111 111 111 111 111 111 111
 			// 000 001 010 011 100 101 110
-			// continuous stream: 11100011 10011110 10111011 11110011 11011111 10xxxxxx
+			// continuous bit stream: 11100011 10011110 10111011 11110011 11011111 10xxxxxx
 			// continuous LE byte stream: 11000111 01111001 11011101 11001111 11111011 xxxxxx01
 			testData = new byte[] {0xc7, 0x79, 0xdd, 0xcf, 0xfb, 0xf1};
 			TestFrame(testData, 3*2, 0, "111000", "continuous stream: 11100011 10011110 10111011 11110011 11011111 10xxxxxx");
@@ -86,18 +266,20 @@ namespace ClearCanvas.ImageViewer.Imaging.Tests
 			TestFrame(testData, 3*2, 6, "111110", "continuous stream: 11100011 10011110 10111011 11110011 11011111 10xxxxxx");
 		}
 
-		private void TestFrame(byte[] packedData, int outBufferSize, int frameNum, string expectedConcat, string datamsg)
+		private static void TestFrame(byte[] packedData, int outBufferSize, int frameNum, string expectedConcat, string datamsg)
 		{
 			byte[] result = new byte[outBufferSize];
-			OverlayData.TestUnpack(packedData, result, frameNum * outBufferSize, false);
-			Assert.AreEqual(expectedConcat, DumpNonZeroBytes(result), "LittleEndianWords Frame {0} of {1}", frameNum, datamsg);
+			OverlayData.TestUnpack(packedData, result, frameNum*outBufferSize, false);
+			Assert.AreEqual(expectedConcat, FormatNonZeroBytes(result), "LittleEndianWords Frame {0} of {1}", frameNum, datamsg);
 
 			// you should get the exact same frame data (scanning horizontally from top left to bottom right) if you had packed data in little endian or big endian
 			byte[] swappedPackedData = SwapBytes(packedData);
 			result = new byte[outBufferSize];
-			OverlayData.TestUnpack(swappedPackedData, result, frameNum * outBufferSize, true);
-			Assert.AreEqual(expectedConcat, DumpNonZeroBytes(result), "BigEndianWords Frame {0} of {1}", frameNum, datamsg);
+			OverlayData.TestUnpack(swappedPackedData, result, frameNum*outBufferSize, true);
+			Assert.AreEqual(expectedConcat, FormatNonZeroBytes(result), "BigEndianWords Frame {0} of {1}", frameNum, datamsg);
 		}
+
+		#endregion
 
 		private static byte[] SwapBytes(byte[] swapBytes)
 		{
@@ -110,19 +292,45 @@ namespace ClearCanvas.ImageViewer.Imaging.Tests
 			return output;
 		}
 
-		private static string DumpNonZeroBytes(byte[] unpackedBits)
+		/// <summary>
+		/// Formats the bits of a byte array as a sequence of 1s and 0s.
+		/// </summary>
+		private static string FormatBits(byte[] data, bool bigEndianWords)
 		{
-			string s = "";
-
-			for (int n = 0; n < unpackedBits.Length; n++)
+			var sb = new StringBuilder(data.Length*8);
+			if (bigEndianWords)
 			{
-				if (unpackedBits[n] > 0)
-					s += "1";
-				else
-					s += "0";
+				for (int n = 0; n < data.Length; n += 2)
+				{
+					// the lsb is the 2nd byte in each big endian word
+					// list each bit of the byte from lsb to msb
+					for (int bit = 0; bit < 8; bit++)
+						sb.Append((data[n + 1] >> bit)%2 == 1 ? '1' : '0');
+					for (int bit = 0; bit < 8; bit++)
+						sb.Append((data[n] >> bit)%2 == 1 ? '1' : '0');
+				}
 			}
+			else
+			{
+				for (int n = 0; n < data.Length; n++)
+				{
+					// list each bit of the byte from lsb to msb
+					for (int bit = 0; bit < 8; bit++)
+						sb.Append((data[n] >> bit)%2 == 1 ? '1' : '0');
+				}
+			}
+			return sb.ToString();
+		}
 
-			return s;
+		/// <summary>
+		/// Formats a byte array as a sequence of 1s and 0s representing non-zero and zero bytes respectively.
+		/// </summary>
+		private static string FormatNonZeroBytes(byte[] data)
+		{
+			var sb = new StringBuilder(data.Length);
+			for (int n = 0; n < data.Length; n++)
+				sb.Append(data[n] > 0 ? '1' : '0');
+			return sb.ToString();
 		}
 	}
 }
