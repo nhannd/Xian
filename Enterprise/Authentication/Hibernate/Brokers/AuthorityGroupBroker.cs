40fc444f265e6039eec56de0149283df9879a12e
thanh <devnull@localhost>
2011-04-15 18:21:55 +0000

    Ticket: #8294 - ImageServer WebUI and Workstation allow user groups to be deleted when associated with a user
    Schema/Configuration Changes: N/A
    
    - Missing files in prev commit.

---
+++ Enterprise/Authentication/Hibernate/Brokers/AuthorityGroupBroker.cs
@@ -0,0 +1,30 @@
+﻿#region License
+
+// Copyright (c) 2011, ClearCanvas Inc.
+// All rights reserved.
+// http://www.clearcanvas.ca
+//
+// This software is licensed under the Open Software License v3.0.
+// For the complete license, see http://www.clearcanvas.ca/OSLv3.0
+
+#endregion
+
+using System.Collections.Generic;
+using ClearCanvas.Enterprise.Hibernate.Hql;
+
+namespace ClearCanvas.Enterprise.Authentication.Hibernate.Brokers
+{
+    public partial class AuthorityGroupBroker
+    {
+        public int GetUserCountForGroup(AuthorityGroup group)
+        {
+            HqlQuery query = new HqlQuery("select count(*) from User u join u.AuthorityGroups g");
+
+            AuthorityGroupSearchCriteria whereGroup = new AuthorityGroupSearchCriteria();
+            whereGroup.OID.EqualTo(group.OID);
+            query.Conditions.AddRange(HqlCondition.FromSearchCriteria("g", whereGroup));
+            IList<long> results = ExecuteHql<long>(query);
+            return results.Count == 0 ? 0 : (int)results[0];
+        }
+    }
+}
