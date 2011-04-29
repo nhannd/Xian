5610cbc2974f1fd5e0b32791f2f16672119533ee
jasper <devnull@localhost>
2011-04-29 18:59:36 +0000

    Requirement: #8312 - Integrate localization satellite resources and assemblies into build process ''Schema/Configuration Changes: N/A''  * Added new custom MSBuild tasks for XML file manipulation using XPaths, and a task for splitting a string by separator characters into an item group.

---
+++ Utilities/BuildTasks/XmlTaskBase.cs
@@ -0,0 +1,215 @@
+﻿#region License
+
+// Copyright (c) 2011, ClearCanvas Inc.
+// All rights reserved.
+// http://www.clearcanvas.ca
+//
+// This software is licensed under the Open Software License v3.0.
+// For the complete license, see http://www.clearcanvas.ca/OSLv3.0
+
+#endregion
+
+using System;
+using System.Collections;
+using System.Xml;
+using System.Xml.XPath;
+using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+
+namespace ClearCanvas.Utilities.BuildTasks
+{
+	/// <summary>
+	/// Base class for MSBuild <see cref="Task"/>s that operate on XML documents.
+	/// </summary>
+	public abstract class XmlTaskBase : Task
+	{
+		/// <summary>
+		/// Initializes an <see cref="XmlTaskBase"/>.
+		/// </summary>
+		protected XmlTaskBase()
+		{
+			ErrorIfNotExists = true;
+		}
+
+		/// <summary>
+		/// Gets or sets the path to the XML document file.
+		/// </summary>
+		[Required]
+		public string File { get; set; }
+
+		/// <summary>
+		/// Gets or sets the XPath that refers to the XML nodes on which the operation will be performed.
+		/// </summary>
+		[Required]
+		public string XPath { get; set; }
+
+		/// <summary>
+		/// Gets or sets a value indicating whether or not it is an error if the nodes selected by <see cref="XPath"/> do not exist. The default value is True.
+		/// </summary>
+		public bool ErrorIfNotExists { get; set; }
+
+		/// <summary>
+		/// Gets the current state of the loaded <see cref="XmlDocument"/>.
+		/// </summary>
+		/// <remarks>
+		/// This property is only available while an <see cref="XmlTaskBase"/> is being executed.
+		/// </remarks>
+		protected XmlDocument XmlDocument { get; private set; }
+
+		/// <summary>
+		/// Gets or sets a value indicating whether or not any changes were made to <see cref="XmlDocument"/>.
+		/// </summary>
+		protected bool Modified { get; set; }
+
+		/// <summary>
+		/// Gets a value indicating whether or not it is an error if the XML document is empty or the file does not exist.
+		/// </summary>
+		protected virtual bool AllowEmptyDocument
+		{
+			get { return false; }
+		}
+
+		/// <summary>
+		/// Flags the <see cref="Modified"/> flag if it is not already set.
+		/// </summary>
+		/// <param name="modified">A value indicating whether or not the flag should be set if it is not already set.</param>
+		protected void FlagModified(bool modified)
+		{
+			Modified = Modified || modified;
+		}
+
+		/// <summary>
+		/// Executes the task.
+		/// </summary>
+		/// <returns>True if the task succeeded; False otherwise.</returns>
+		public override sealed bool Execute()
+		{
+			// validate parameters
+			string validationMessage;
+			if (!ValidateParameters(out validationMessage))
+			{
+				Log.LogError(validationMessage);
+				return false;
+			}
+
+			XmlDocument = new XmlDocument();
+			Modified = false;
+			try
+			{
+				if (!string.IsNullOrEmpty(File) && System.IO.File.Exists(File))
+				{
+					// load the file
+					try
+					{
+						XmlDocument.Load(File);
+					}
+					catch (XmlException ex)
+					{
+						Log.LogError("Invalid XML near line {0}, position {1}", ex.LineNumber, ex.LinePosition);
+						return false;
+					}
+				}
+				else if (!AllowEmptyDocument)
+				{
+					// if empty documents are not allowed, error out now
+					Log.LogError("File not found: {0}", File);
+					return false;
+				}
+
+				// perform the task
+				var result = PerformTask();
+				if (result && Modified)
+				{
+					// save output if task was successful and changes were made
+					if (!string.IsNullOrEmpty(File))
+						XmlDocument.Save(File);
+				}
+				return result;
+			}
+#if !DEBUG
+			catch (Exception ex)
+			{
+				Log.LogErrorFromException(ex);
+				return false;
+			}		
+#endif
+			finally
+			{
+				XmlDocument = null;
+				Modified = false;
+			}
+		}
+
+		/// <summary>
+		/// Called to validate task parameters before the task is actually executed.
+		/// </summary>
+		/// <param name="message">A message to be logged if validation did not succeed.</param>
+		/// <returns>True if validation succeeded; False otherwise.</returns>
+		protected virtual bool ValidateParameters(out string message)
+		{
+			if (string.IsNullOrEmpty(File))
+			{
+				message = "File is a required parameter";
+				return false;
+			}
+
+			if (string.IsNullOrEmpty(XPath))
+			{
+				message = "XPath is a required parameter";
+				return false;
+			}
+
+			message = null;
+			return true;
+		}
+
+		/// <summary>
+		/// Called to execute the task.
+		/// </summary>
+		/// <param name="xmlNodes">A list of <see cref="XmlNode"/>s on which the operation is to be performed.</param>
+		/// <returns>True if the task succeeded; False otherwise.</returns>
+		protected abstract bool PerformTask(XmlNodeList xmlNodes);
+
+		private bool PerformTask()
+		{
+			try
+			{
+				var xmlNodes = XmlDocument.SelectNodes(XPath) ?? new EmptyXmlNodeList();
+				if (ErrorIfNotExists && xmlNodes.Count == 0)
+				{
+					Log.LogError("No results for XPath expression {0}", XPath);
+					return false;
+				}
+				return PerformTask(xmlNodes);
+			}
+			catch (XPathException)
+			{
+				Log.LogError("Invalid XPath expression {0}", XPath);
+				return false;
+			}
+		}
+
+		/// <summary>
+		/// Implementation of an empty <see cref="XmlNodeList"/>.
+		/// </summary>
+		private class EmptyXmlNodeList : XmlNodeList
+		{
+			private static readonly object[] _empty = new object[0];
+
+			public override int Count
+			{
+				get { return 0; }
+			}
+
+			public override IEnumerator GetEnumerator()
+			{
+				return _empty.GetEnumerator();
+			}
+
+			public override XmlNode Item(int index)
+			{
+				throw new IndexOutOfRangeException();
+			}
+		}
+	}
+}
---
+++ Utilities/BuildTasks/XmlUpdate.cs
@@ -0,0 +1,263 @@
+#region License
+
+// Copyright (c) 2011, ClearCanvas Inc.
+// All rights reserved.
+// http://www.clearcanvas.ca
+//
+// This software is licensed under the Open Software License v3.0.
+// For the complete license, see http://www.clearcanvas.ca/OSLv3.0
+
+#endregion
+
+using System;
+using System.Xml;
+using Microsoft.Build.Framework;
+
+namespace ClearCanvas.Utilities.BuildTasks
+{
+	/// <summary>
+	/// A task for updating nodes in an XML document.
+	/// </summary>
+	/// <remarks>
+	/// This task updates the nodes selected by the <see cref="XmlTaskBase.XPath"/> expression.
+	/// </remarks>
+	public class XmlUpdate : XmlUpdateTaskBase
+	{
+		/// <summary>
+		/// Outputs whether or not the selected nodes were updated.
+		/// </summary>
+		[Output]
+		public bool Updated { get; set; }
+
+		protected override bool PerformTask(XmlNodeList xmlNodes)
+		{
+			var success = Cross(xmlNodes, GetOperations(), (node, op) => op.Update(node));
+			if (!success)
+			{
+				Log.LogError("Inputs must have only 1 item or be in 1:1 correspondance with selected nodes");
+				return false;
+			}
+
+			FlagModified(Updated = xmlNodes.Count > 0);
+			return true;
+		}
+	}
+
+	/// <summary>
+	/// Base class for MSBuild <see cref="Task"/>s that update nodes in XML documents.
+	/// </summary>
+	public abstract class XmlUpdateTaskBase : XmlTaskBase
+	{
+		/// <summary>
+		/// The inner XML contents with which to update the nodes. May not be used in conjunction with <see cref="Value"/>.
+		/// </summary>
+		public string[] InnerXml { get; set; }
+
+		/// <summary>
+		/// The value contents with which to update the nodes. May not be used in conjunction with <see cref="InnerXml"/>.
+		/// </summary>
+		public string[] Value { get; set; }
+
+		/// <summary>
+		/// The attribute names with which to update the nodes. Must be used in conjunction with <see cref="Value"/>.
+		/// </summary>
+		public string[] Attribute { get; set; }
+
+		/// <summary>
+		/// A value indicating whether or not empty elements should be condensed into the empty tag format.
+		/// </summary>
+		public bool UseEmptyTag { get; set; }
+
+		/// <summary>
+		/// Parses the values of <see cref="InnerXml"/>, <see cref="Value"/> and <see cref="Attribute"/> into <see cref="IUpdateOperation"/>s.
+		/// </summary>
+		protected IUpdateOperation[] GetOperations()
+		{
+			if (!IsNullOrEmpty(InnerXml))
+			{
+				var ops = new IUpdateOperation[InnerXml.Length];
+				for (int n = 0; n < InnerXml.Length; n++)
+					ops[n] = new SetInnerXmlOperation(InnerXml[n], UseEmptyTag);
+				return ops;
+			}
+			else if (!IsNullOrEmpty(Value))
+			{
+				var ops = new IUpdateOperation[Value.Length];
+				if (!IsNullOrEmpty(Attribute))
+				{
+					if (Attribute.Length > 1 && Attribute.Length != Value.Length)
+						throw new ArgumentException("Attribute must have only 1 item or be in 1:1 correspondance with Value");
+					for (int n = 0; n < Value.Length; n++)
+						ops[n] = new SetAttributeOperation(Value[n], Attribute.Length == 1 ? Attribute[0] : Attribute[n]);
+				}
+				else
+				{
+					for (int n = 0; n < Value.Length; n++)
+						ops[n] = new SetValueOperation(Value[n], UseEmptyTag);
+				}
+				return ops;
+			}
+			else
+			{
+				return new[] {new SetValueOperation(string.Empty, UseEmptyTag)};
+			}
+		}
+
+		protected override bool ValidateParameters(out string message)
+		{
+			if (!IsNullOrEmpty(InnerXml) && !IsNullOrEmpty(Value))
+			{
+				message = "Only one of InnerXml and Value can be set";
+				return false;
+			}
+
+			try
+			{
+				GetOperations();
+			}
+			catch (ArgumentException ex)
+			{
+				message = ex.Message;
+				return false;
+			}
+
+			return base.ValidateParameters(out message);
+		}
+
+		/// <summary>
+		/// Determines whether or not a <see cref="string"/> <paramref name="array"/> is null, empty, or contains a single null or empty string.
+		/// </summary>
+		private static bool IsNullOrEmpty(string[] array)
+		{
+			return array == null || array.Length == 0 || (array.Length == 1 && string.IsNullOrEmpty(array[0]));
+		}
+
+		/// <summary>
+		/// Callback function for <see cref="Cross{T}"/>.
+		/// </summary>
+		protected delegate void CrossCallback<T>(XmlNode xmlNode, T item);
+
+		/// <summary>
+		/// Performs a <paramref name="callback"/> after matching each node in <paramref name="xmlNodeList"/> to an appropriate element in <paramref name="items"/>.
+		/// </summary>
+		/// <returns>Returns False if there is no 1:1, 1:N or N:1 correspondance between nodes and items.</returns>
+		protected static bool Cross<T>(XmlNodeList xmlNodeList, T[] items, CrossCallback<T> callback)
+		{
+			var nodeCount = xmlNodeList.Count;
+			var inputCount = items.Length;
+
+			if (nodeCount > 1 && inputCount > 1 && nodeCount != inputCount)
+				return false;
+
+			if (nodeCount == inputCount)
+			{
+				for (int n = 0; n < items.Length; n++)
+					callback.Invoke(xmlNodeList[n], items[n]);
+			}
+			else if (inputCount == 1)
+			{
+				for (int n = 0; n < xmlNodeList.Count; n++)
+					callback.Invoke(xmlNodeList[n], items[0]);
+			}
+			else if (nodeCount == 1)
+			{
+				for (int n = 0; n < items.Length; n++)
+					callback.Invoke(xmlNodeList[0], items[n]);
+			}
+			return true;
+		}
+
+		/// <summary>
+		/// Represents an operation that updates an <see cref="XmlNode"/>.
+		/// </summary>
+		protected interface IUpdateOperation
+		{
+			/// <summary>
+			/// Updates the <paramref name="xmlNode"/>.
+			/// </summary>
+			void Update(XmlNode xmlNode);
+		}
+
+		/// <summary>
+		/// An <see cref="IUpdateOperation"/> that updates the <see cref="XmlNode.InnerXml"/> of an <see cref="XmlNode"/>.
+		/// </summary>
+		private class SetInnerXmlOperation : IUpdateOperation
+		{
+			private readonly string _innerXml;
+			private readonly bool _useEmptyTag;
+
+			public SetInnerXmlOperation(string innerXml, bool useEmptyTag)
+			{
+				_innerXml = innerXml ?? string.Empty;
+				_useEmptyTag = useEmptyTag;
+			}
+
+			public void Update(XmlNode xmlNode)
+			{
+				xmlNode.InnerXml = _innerXml;
+				if (xmlNode is XmlElement)
+					((XmlElement) xmlNode).IsEmpty = string.IsNullOrEmpty(_innerXml) && _useEmptyTag;
+			}
+		}
+
+		/// <summary>
+		/// An <see cref="IUpdateOperation"/> that updates the <see cref="XmlNode.InnerText"/> of an <see cref="XmlElement"/> or the <see cref="XmlNode.Value"/> of any other <see cref="XmlNode"/>.
+		/// </summary>
+		private class SetValueOperation : IUpdateOperation
+		{
+			private readonly string _value;
+			private readonly bool _useEmptyTag;
+
+			public SetValueOperation(string value, bool useEmptyTag)
+			{
+				_value = value ?? string.Empty;
+				_useEmptyTag = useEmptyTag;
+			}
+
+			public void Update(XmlNode xmlNode)
+			{
+				if (xmlNode is XmlElement)
+				{
+					// the Value property is invalid on an XmlElement - use InnerText instead
+					xmlNode.InnerText = _value;
+					((XmlElement) xmlNode).IsEmpty = string.IsNullOrEmpty(_value) && _useEmptyTag;
+				}
+				else
+				{
+					xmlNode.Value = _value;
+				}
+			}
+		}
+
+		/// <summary>
+		/// An <see cref="IUpdateOperation"/> that updates the <see cref="XmlNode.Attributes"/> of an <see cref="XmlNode"/>.
+		/// </summary>
+		private class SetAttributeOperation : IUpdateOperation
+		{
+			private readonly string _value;
+			private readonly string _attribute;
+
+			public SetAttributeOperation(string value, string attribute)
+			{
+				_value = value ?? string.Empty;
+				_attribute = attribute;
+			}
+
+			public void Update(XmlNode xmlNode)
+			{
+				if (xmlNode.Attributes != null)
+				{
+					var attribute = xmlNode.Attributes[_attribute];
+					if (attribute == null && xmlNode.OwnerDocument != null)
+					{
+						attribute = xmlNode.OwnerDocument.CreateAttribute(_attribute);
+						xmlNode.Attributes.Append(attribute);
+					}
+
+					if (attribute != null)
+						attribute.Value = _value;
+				}
+			}
+		}
+	}
+}
