25be8cced0679ee92047c4b3f7dda136909d5ea0
rob <rob@clearcanvas.ca>
2011-05-25 16:30:02 -0400

    Changes to get post-SummerWS code to build:  * DialogBox.cs: removed duplicate Size property  * ClearCanvas.Enterprise.Desktop.csproj: Added reference to System.ServiceModel  * Ris/Client/SessionManager.cs: Non-functional implementation of ISessionManager.SessionStatus and SessionStatusChanged added.

---
+++ Enterprise/Authentication/SessionStatistics.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections;
+using System.Text;
+
+using ClearCanvas.Enterprise.Core;
+
+namespace ClearCanvas.Enterprise.Authentication {
+
+
+    /// <summary>
+    /// SessionStatistics entity
+    /// </summary>
+	public partial class SessionStatistics : ClearCanvas.Enterprise.Core.Entity
+	{
+	
+		/// <summary>
+		/// This method is called from the constructor.  Use this method to implement any custom
+		/// object initialization.
+		/// </summary>
+		private void CustomInitialize()
+		{
+		}
+	}
+}
