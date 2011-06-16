1d81593f134946b236d07fed12e67638ecc95f23
jasper <devnull@localhost>
2011-06-16 16:38:30 +0000

    [Requirement: #8801] - Text overlay cannot be turned off ''Schema/Configuration Changes: N/A''  * Fixed. The MammogramAnnotationLayoutProxy now caches the annotation layout results so that a consistent annotation box visibility state can be maintained. When the layout results change, the state is persisted across to the new layout via matching on annotation item identifier. When the proxy is cloned, the cached annotation layout results are also cloned so that the visibility state is maintained across clone lines as well.

--- ImageViewer/Annotations/MammogramAnnotationLayoutProxy.cs
+++ ImageViewer/Annotations/MammogramAnnotationLayoutProxy.cs
@@ -16,13 +16,44 @@ using ClearCanvas.ImageViewer.Graphics;
 
 namespace ClearCanvas.ImageViewer.Annotations
 {
-	[Cloneable(true)]
+	[Cloneable(false)]
 	internal class MammogramAnnotationLayoutProxy : IAnnotationLayout
 	{
 		[CloneIgnore]
 		private DicomGrayscalePresentationImage _ownerImage;
 
+		[CloneIgnore]
+		private Dictionary<object, AnnotationBox> _annotationBoxes;
+
+		private string _layoutId = null;
 		private bool _visible = true;
+
+		public MammogramAnnotationLayoutProxy() {}
+
+		/// <summary>
+		/// Cloning constructor.
+		/// </summary>
+		/// <param name="source">The source object from which to clone.</param>
+		/// <param name="context">The cloning context object.</param>
+		protected MammogramAnnotationLayoutProxy(MammogramAnnotationLayoutProxy source, ICloningContext context)
+		{
+			context.CloneFields(source, this);
+
+			// clone the annotation boxes with their state!
+			if (source._annotationBoxes != null)
+			{
+				var annotationBoxes = new Dictionary<object, AnnotationBox>();
+				foreach (var sourceBox in source._annotationBoxes.Values)
+				{
+					var cloneBox = (AnnotationBox) CloneBuilder.Clone(sourceBox);
+					if (cloneBox.AnnotationItem != null)
+						annotationBoxes.Add(cloneBox.AnnotationItem.GetIdentifier(), cloneBox);
+					else
+						annotationBoxes.Add(cloneBox, cloneBox);
+				}
+				_annotationBoxes = annotationBoxes;
+			}
+		}
 
 		public DicomGrayscalePresentationImage OwnerImage
 		{
@@ -40,23 +71,57 @@ namespace ClearCanvas.ImageViewer.Annotations
 		{
 			get
 			{
-				if (_ownerImage != null && _ownerImage.SpatialTransform is MammographyImageSpatialTransform)
+				// we only need to rebuild annotation boxes if the layout ID has changed
+				var layoutId = GetCurrentLayoutId();
+				if (layoutId != _layoutId || _annotationBoxes == null)
 				{
-					string effectiveRowOrientation, effectiveColumnOrientation;
-					((MammographyImageSpatialTransform) _ownerImage.SpatialTransform).GetEffectivePosteriorPatientOrientation(out effectiveRowOrientation, out effectiveColumnOrientation);
-					var filterCandidates = new List<KeyValuePair<string, string>>();
-					filterCandidates.Add(new KeyValuePair<string, string>("Modality", _ownerImage.ImageSop.Modality));
-					filterCandidates.Add(new KeyValuePair<string, string>("PatientOrientation_Row", effectiveRowOrientation));
-					filterCandidates.Add(new KeyValuePair<string, string>("PatientOrientation_Col", effectiveColumnOrientation));
-					return DicomAnnotationLayoutFactory.CreateLayout(filterCandidates).AnnotationBoxes;
+					// index the annotation boxes in the new layout
+					var annotationBoxes = new Dictionary<object, AnnotationBox>();
+					foreach (var annotationBox in AnnotationLayoutFactory.CreateLayout(layoutId).AnnotationBoxes)
+					{
+						// if the box has an item, use the item's identifier as the key
+						object key = annotationBox;
+						if (annotationBox.AnnotationItem != null)
+						{
+							key = annotationBox.AnnotationItem.GetIdentifier();
+
+							// if for some reason the key is a duplicate, use the box as the key (but keep it as part of the layout!)
+							if (annotationBoxes.ContainsKey(key))
+								key = annotationBox;
+						}
+						annotationBoxes.Add(key, annotationBox);
+
+						// if the old layout has an item with the same key, copy its visibility state over
+						if (_annotationBoxes != null && _annotationBoxes.ContainsKey(key))
+							annotationBox.Visible = _annotationBoxes[key].Visible;
+					}
+
+					// update variables
+					_annotationBoxes = annotationBoxes;
+					_layoutId = layoutId;
 				}
-				return DicomAnnotationLayoutFactory.CreateLayout(_ownerImage).AnnotationBoxes;
+				return _annotationBoxes.Values;
 			}
 		}
 
 		public IAnnotationLayout Clone()
 		{
-			return new MammogramAnnotationLayoutProxy();
+			return (IAnnotationLayout) CloneBuilder.Clone(this);
+		}
+
+		private string GetCurrentLayoutId()
+		{
+			if (_ownerImage != null && _ownerImage.SpatialTransform is MammographyImageSpatialTransform)
+			{
+				string effectiveRowOrientation, effectiveColumnOrientation;
+				((MammographyImageSpatialTransform) _ownerImage.SpatialTransform).GetEffectivePosteriorPatientOrientation(out effectiveRowOrientation, out effectiveColumnOrientation);
+				var filterCandidates = new List<KeyValuePair<string, string>>();
+				filterCandidates.Add(new KeyValuePair<string, string>("Modality", _ownerImage.ImageSop.Modality));
+				filterCandidates.Add(new KeyValuePair<string, string>("PatientOrientation_Row", effectiveRowOrientation));
+				filterCandidates.Add(new KeyValuePair<string, string>("PatientOrientation_Col", effectiveColumnOrientation));
+				return DicomFilteredAnnotationLayoutStore.Instance.GetMatchingStoredLayoutId(filterCandidates);
+			}
+			return string.Empty;
 		}
 	}
 }
