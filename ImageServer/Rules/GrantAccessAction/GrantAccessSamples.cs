6d7536cfdda30008a2dc75db440e7d1ddea65590
steve <devnull@localhost>
2011-06-23 10:07:05 -0500

    [Requirement: #8750] - Support for assigning Authority Groups to Studies
    Added rules engine Action for granting accessing to studies.

---
+++ ImageServer/Rules/GrantAccessAction/GrantAccessSamples.cs
@@ -0,0 +1,29 @@
+﻿#region License
+
+// Copyright (c) 2011, ClearCanvas Inc.
+// All rights reserved.
+// http://www.clearcanvas.ca
+//
+// This software is licensed under the Open Software License v3.0.
+// For the complete license, see http://www.clearcanvas.ca/OSLv3.0
+
+#endregion
+
+using ClearCanvas.Common;
+using ClearCanvas.ImageServer.Model;
+
+namespace ClearCanvas.ImageServer.Rules.GrantAccessAction
+{
+    [ExtensionOf(typeof (SampleRuleExtensionPoint))]
+    public class GrantAccessSamples : SampleRuleBase
+    {
+        public GrantAccessSamples()
+            : base("GrantAccessSample",
+                   "Simple Grant Access",
+                   ServerRuleTypeEnum.DataAccess,
+                   "Sample_GrantAccess.xml")
+        {
+            ApplyTimeList.Add(ServerRuleApplyTimeEnum.StudyProcessed);
+		}
+    }
+}
