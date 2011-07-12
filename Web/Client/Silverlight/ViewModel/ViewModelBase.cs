65e0697bc0cd5b653bf6efc2f9b8c2f194e37a48
steve <devnull@localhost>
2011-07-11 22:54:55 -0500

    [Requirement: #8759] - WebPortal Search for Studies
    Introduced some Model-View-ViewModel related classes for the search screen
    and started introducing support.

---
+++ Web/Client/Silverlight/ViewModel/ViewModelBase.cs
@@ -0,0 +1,192 @@
+﻿#region License
+
+// Copyright (c) 2011, ClearCanvas Inc.
+// All rights reserved.
+// http://www.clearcanvas.ca
+// 
+// For information about the licensing and copyright of this software please
+// contact ClearCanvas, Inc. at info@clearcanvas.ca
+
+// Note, some of this code was taken from the MVVM Lite Toolkit, which is licensed here:   http://www.galasoft.ch/license_MIT.txt
+#endregion
+
+using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using System.Diagnostics.CodeAnalysis;
+using System.ComponentModel;
+using System.Linq.Expressions;
+using System.Diagnostics;
+using System.Reflection;
+
+namespace ClearCanvas.Web.Client.Silverlight.ViewModel
+{
+    public class ViewModelBase : INotifyPropertyChanged
+    {
+        #region Design Mode
+        private static bool? _isInDesignMode;
+
+
+        /// <summary>
+        /// Gets a value indicating whether the control is in design mode
+        /// (running under Blend or Visual Studio).
+        /// </summary>
+        [SuppressMessage("Microsoft.Performance", "CA1822:MarkMembersAsStatic", Justification = "Non static member needed for data binding")]
+        public bool IsInDesignMode
+        {
+            get
+            {
+                return IsInDesignModeStatic;
+            }
+        }
+
+     
+        /// <summary>
+        /// Gets a value indicating whether the control is in design mode
+        /// (running in Blend or Visual Studio).
+        /// </summary>
+        [SuppressMessage(
+            "Microsoft.Security",
+            "CA2122:DoNotIndirectlyExposeMethodsWithLinkDemands",
+            Justification = "The security risk here is neglectible.")]
+        public static bool IsInDesignModeStatic
+        {
+            get
+            {
+                if (!_isInDesignMode.HasValue)
+                {
+                    _isInDesignMode = DesignerProperties.IsInDesignTool;
+
+                }
+
+                return _isInDesignMode.Value;
+            }
+        }
+
+        #endregion
+
+        #region Dependency Property Related
+
+        /// <summary>
+        /// Occurs when a property value changes.
+        /// </summary>
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        /// <summary>
+        /// Provides access to the PropertyChanged event handler to derived classes.
+        /// </summary>
+        protected PropertyChangedEventHandler PropertyChangedHandler
+        {
+            get
+            {
+                return PropertyChanged;
+            }
+        }
+
+        /// <summary>
+        /// Verifies that a property name exists in this ViewModel. This method
+        /// can be called before the property is used, for instance before
+        /// calling RaisePropertyChanged. It avoids errors when a property name
+        /// is changed but some places are missed.
+        /// <para>This method is only active in DEBUG mode.</para>
+        /// </summary>
+        /// <param name="propertyName"></param>
+        [Conditional("DEBUG")]
+        [DebuggerStepThrough]
+        public void VerifyPropertyName(string propertyName)
+        {
+            var myType = this.GetType();
+            if (myType.GetProperty(propertyName) == null)
+            {
+                throw new ArgumentException("Property not found", propertyName);
+            }
+        }
+
+        /// <summary>
+        /// Raises the PropertyChanged event if needed.
+        /// </summary>
+        /// <remarks>If the propertyName parameter
+        /// does not correspond to an existing property on the current class, an
+        /// exception is thrown in DEBUG configuration only.</remarks>
+        /// <param name="propertyName">The name of the property that
+        /// changed.</param>
+        [SuppressMessage("Microsoft.Design", "CA1030:UseEventsWhereAppropriate",
+            Justification = "This cannot be an event")]
+        protected virtual void RaisePropertyChanged(string propertyName)
+        {
+            VerifyPropertyName(propertyName);
+
+            var handler = PropertyChanged;
+
+            if (handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+        /// <summary>
+        /// Raises the PropertyChanged event if needed.
+        /// </summary>
+        /// <typeparam name="T">The type of the property that
+        /// changed.</typeparam>
+        /// <param name="propertyExpression">An expression identifying the property
+        /// that changed.</param>
+        [SuppressMessage("Microsoft.Design", "CA1030:UseEventsWhereAppropriate",
+            Justification = "This cannot be an event")]
+        [SuppressMessage(
+            "Microsoft.Design",
+            "CA1006:GenericMethodsShouldProvideTypeParameter",
+           Justification = "This syntax is more convenient than other alternatives.")]
+        protected virtual void RaisePropertyChanged<T>(Expression<Func<T>> propertyExpression)
+        {
+            if (propertyExpression == null)
+            {
+                return;
+            }
+
+            var handler = PropertyChanged;
+
+            if (handler != null)
+            {
+                var body = propertyExpression.Body as MemberExpression;
+                var expression = body.Expression as ConstantExpression;
+                handler(expression.Value, new PropertyChangedEventArgs(body.Member.Name));
+            }
+        }
+
+        /// <summary>
+        /// When called in a property setter, raises the PropertyChanged event for 
+        /// the current property.
+        /// </summary>
+        /// <exception cref="InvalidOperationException">If this method is called outside
+        /// of a property setter.</exception>
+        [SuppressMessage("Microsoft.Design", "CA1030:UseEventsWhereAppropriate",
+            Justification = "This cannot be an event")]
+        protected virtual void RaisePropertyChanged()
+        {
+            var frames = new StackTrace();
+
+            for (var i = 0; i < frames.FrameCount; i++)
+            {
+                var frame = frames.GetFrame(i).GetMethod() as MethodInfo;
+                if (frame != null)
+                    if (frame.IsSpecialName && frame.Name.StartsWith("set_", StringComparison.OrdinalIgnoreCase))
+                    {
+                        RaisePropertyChanged(frame.Name.Substring(4));
+                        return;
+                    }
+            }
+
+            throw new InvalidOperationException("This method can only by invoked within a property setter.");
+        }
+
+        #endregion
+    }
+}
