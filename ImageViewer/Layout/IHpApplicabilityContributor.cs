966686f4dea434e195b7ae7acbec39812c094f80
jonathan <jonathan@zeus>
2011-08-04 13:54:49 -0400

    [Requirement #8983] - The user will be able to control whether a given layout is applicable to a given study
    1. Renamed MatchCriteria to Applicability, and introduced two subclasses - ProtocolApplicability and LayoutApplicability.
    
    2. Renamed all code elements to reflect the conceptual shift from "match criteria" to the more general "applicability".  UI will be covered in a subsequent commit.

---
+++ ImageViewer/Layout/IHpApplicabilityContributor.cs
@@ -0,0 +1,59 @@
+﻿#region License
+
+// Copyright (c) 2011, ClearCanvas Inc.
+// All rights reserved.
+// http://www.clearcanvas.ca
+
+// This software is licensed under the Open Software License v3.0.
+// For the complete license, see http://www.clearcanvas.ca/OSLv3.0
+
+#endregion
+
+using ClearCanvas.ImageViewer.StudyManagement;
+
+namespace ClearCanvas.ImageViewer.Layout
+{
+	/// <summary>
+	/// Defines the interface to a "match criteria" contributor.
+	/// </summary>
+	public interface IHpApplicabilityContributor<TContext> : IHpContributor
+	{
+		/// <summary>
+		/// Captures the state from the specified context.
+		/// </summary>
+		/// <param name="context"></param>
+		void Capture(TContext context);
+
+		/// <summary>
+		/// Tests the applicability based on the specified context.
+		/// </summary>
+		/// <param name="context"></param>
+		/// <returns></returns>
+		HpMatchResult Test(TContext context);
+	}
+
+	public interface IHpProtocolApplicabilityContext
+	{
+		Study Study { get; }
+	}
+
+
+	/// <summary>
+	/// Defines the interface to a "protocol applicability" contributor.
+	/// </summary>
+	public interface IHpProtocolApplicabilityContributor : IHpApplicabilityContributor<IHpProtocolApplicabilityContext>
+	{
+	}
+
+	public interface IHpLayoutApplicabilityContext
+	{
+	}
+
+
+	/// <summary>
+	/// Defines the interface to a "layout applicability" contributor.
+	/// </summary>
+	public interface IHpLayoutApplicabilityContributor : IHpApplicabilityContributor<IHpLayoutApplicabilityContext>
+	{
+	}
+}
