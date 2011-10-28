da8f02a5dd2aac643c6bb7305d69b15c1026353e
thanh <devnull@localhost>
2011-10-28 11:26:55 -0400

    [Defect #8866] - Filesystme Reapply Rules causes Rearchive
    - Fixed. Updated InsertArchiveQueue stored procedure to not insert duplicate entries for the same study. If one already exists and is in failed state, it will be reset to pending.

--- ImageServer/Model/SqlServer/UpgradeScripts/UpgradeFrom_4_0_5014_9549.sql
+++ ImageServer/Model/SqlServer/UpgradeScripts/UpgradeFrom_4_0_5014_9549.sql
@@ -13,6 +13,130 @@ GO
 BEGIN TRANSACTION
 GO
 
+
+set ANSI_NULLS ON
+set QUOTED_IDENTIFIER ON
+go
+
+
+PRINT N'Updating InsertArchiveQueue stored procedure'
+GO
+-- =============================================
+-- Author:		Steve Wranovsky
+-- Create date: July 11, 2008
+-- Update date: Oct 28, 2011
+-- Description:	Insert and/or update the appropriate ArchiveQueue records
+-- 
+-- Oct 28, 2011:	(#8866) Ensured existing entries are reset (if failed) and only insert new ones if no entries are found for the studies
+-- Oct 15, 2008:	Removed Update parameter and insert new entry if the study has been archive so that edit can trigger rearchive
+--
+-- =============================================
+ALTER PROCEDURE [dbo].[InsertArchiveQueue] 
+	-- Add the parameters for the stored procedure here
+	@ServerPartitionGUID uniqueidentifier, 
+	@StudyStorageGUID uniqueidentifier
+AS
+BEGIN
+	-- SET NOCOUNT ON added to prevent extra result sets from
+	-- interfering with SELECT statements.
+	SET NOCOUNT ON;
+
+	DECLARE @StudyDeleteTypeEnum smallint
+	DECLARE @StudyPurgeTypeEnum smallint
+	DECLARE @PendingArchiveQueueStatus smallint
+	DECLARE @FailedArchiveQueueStatus smallint
+	DECLARE @StudyDeleteCount int
+	DECLARE @StudyPurgeCount int
+	DECLARE @ArchiveStudyStorageCount int
+	DECLARE @ArchiveDelayHours int
+	DECLARE	@PartitionArchiveGUID uniqueidentifier
+
+	SELECT @StudyDeleteTypeEnum = Enum from FilesystemQueueTypeEnum where Lookup = 'DeleteStudy'
+	SELECT @StudyPurgeTypeEnum = Enum from FilesystemQueueTypeEnum where Lookup = 'PurgeStudy'
+	SELECT @PendingArchiveQueueStatus = Enum from ArchiveQueueStatusEnum where Lookup = 'Pending'
+	SELECT @FailedArchiveQueueStatus = Enum from ArchiveQueueStatusEnum where Lookup = 'Failed'
+
+	BEGIN TRANSACTION
+
+	-- Check if there's any DeleteStudy records in the db
+	SELECT @StudyDeleteCount=count(*) FROM FilesystemQueue 
+		WHERE FilesystemQueueTypeEnum=@StudyDeleteTypeEnum
+			AND StudyStorageGUID=@StudyStorageGUID
+
+	-- Check if there's any PurgeStudy records in the db
+	SELECT @StudyPurgeCount=count(*) FROM FilesystemQueue 
+		WHERE FilesystemQueueTypeEnum=@StudyPurgeTypeEnum
+			AND StudyStorageGUID=@StudyStorageGUID
+
+
+	IF @StudyDeleteCount = 0
+	BEGIN
+		-- Use a cursor to find all the configured ArchiveQueue entries
+		DECLARE PartitionArchiveCursor Cursor FOR
+			SELECT GUID, ArchiveDelayHours from dbo.PartitionArchive WHERE ServerPartitionGUID=@ServerPartitionGUID AND Enabled=1 AND ReadOnly=0
+		Open PartitionArchiveCursor
+		Fetch NEXT FROM PartitionArchiveCursor INTO @PartitionArchiveGUID, @ArchiveDelayHours
+		While (@@FETCH_STATUS <> -1)
+		BEGIN
+			-- Check if the study's been already archived
+			SELECT @ArchiveStudyStorageCount=count(*) FROM ArchiveStudyStorage 
+				WHERE StudyStorageGUID=@StudyStorageGUID
+				AND PartitionArchiveGUID = @PartitionArchiveGUID
+			
+			DECLARE @ArchiveQueueGUID uniqueidentifier
+			DECLARE @ScheduledTime datetime
+
+			set @ScheduledTime = getdate()
+			set @ScheduledTime = dateadd(hour, @ArchiveDelayHours, @ScheduledTime)
+
+			SELECT @ArchiveQueueGUID = GUID from ArchiveQueue 
+			WHERE StudyStorageGUID = @StudyStorageGUID
+				AND PartitionArchiveGUID = @PartitionArchiveGUID
+			if @@ROWCOUNT = 0
+			BEGIN
+				-- There's no archive entry, insert one
+				SET @ArchiveQueueGUID = NEWID();
+
+				INSERT into ArchiveQueue (GUID, PartitionArchiveGUID, StudyStorageGUID, ArchiveQueueStatusEnum, ScheduledTime)
+				values  (@ArchiveQueueGUID, @PartitionArchiveGUID, @StudyStorageGUID, @PendingArchiveQueueStatus, @ScheduledTime)
+			END
+			ELSE
+			BEGIN
+				-- Reset/reschedule existing entries, make sure the failed ones are reset to pending
+				UPDATE ArchiveQueue 
+				SET ScheduledTime = @ScheduledTime,
+					ArchiveQueueStatusEnum=@PendingArchiveQueueStatus
+				WHERE StudyStorageGUID = @StudyStorageGUID
+					AND PartitionArchiveGUID = @PartitionArchiveGUID
+					AND (ArchiveQueueStatusEnum in (@PendingArchiveQueueStatus, @FailedArchiveQueueStatus))
+			END
+
+			Fetch NEXT FROM PartitionArchiveCursor INTO @PartitionArchiveGUID, @ArchiveDelayHours
+		END
+		CLOSE PartitionArchiveCursor
+		DEALLOCATE PartitionArchiveCursor	
+		
+		IF @StudyPurgeCount > 0
+		BEGIN
+			DELETE FROM FilesystemQueue 
+			WHERE FilesystemQueueTypeEnum=@StudyPurgeTypeEnum
+			AND StudyStorageGUID=@StudyStorageGUID
+		END
+	END
+	ELSE
+	BEGIN
+		-- Delete from the ArchiveQueue, the study is scheduled for deletion
+		-- Only delete entries that are Pending or Failed. In most cases this should delete no rows
+		DELETE FROM ArchiveQueue
+			WHERE StudyStorageGUID = @StudyStorageGUID
+				AND ArchiveQueueStatusEnum = @PendingArchiveQueueStatus
+	END
+
+	COMMIT TRANSACTION
+END
+GO
+
+
 PRINT N'Adding new ServiceLockTypeEnum, SyncDataAccess'
 GO
 INSERT INTO [ImageServer].[dbo].[ServiceLockTypeEnum]
