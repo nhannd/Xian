640308932514ebe648e4e13fe9058f3362282a6e
jonathan <devnull@localhost>
2012-03-14 18:30:41 -0400

    [Requirement #9819] - Refactored ORM for database
    Created ImageViewer.StudyManagement.Storage plugin, and committing some initial files that capture the database schema, including:
    - a SQL script for generating the tables
    - an example .sdf file
    - L2S dbml file

---
+++ ImageViewer/StudyManagement/Storage/WorkItem.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ClearCanvas.ImageViewer.Common.WorkItem;
+
+namespace ClearCanvas.ImageViewer.StudyManagement.Storage
+{
+	partial class WorkItem
+	{
+		public WorkItemRequest Request
+		{
+			get
+			{
+				return Deserialize<WorkItemRequest>(this.SerializedRequest);
+			}
+		}
+
+		private T Deserialize<T>(object value)
+		{
+			// need to figure out exactly which serialization format we're using
+			throw new NotImplementedException();
+		}
+	}
+}
