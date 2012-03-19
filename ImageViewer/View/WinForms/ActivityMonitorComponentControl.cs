7437a7f3b968a3f7d7e59a12b404dac465c532c0
jonathan <devnull@localhost>
2012-03-19 16:42:05 -0400

    [Requirement #9798] - Activity Monitor [Requirement #9810] - Rules Editor
    Created all the boilerplate stuff for these two UI pieces: plugins, launch tools, application components and views.

---
+++ ImageViewer/View/WinForms/ActivityMonitorComponentControl.cs
@@ -0,0 +1,28 @@
+﻿#region License
+
+// Copyright (c) 2011, ClearCanvas Inc.
+// All rights reserved.
+// http://www.clearcanvas.ca
+//
+// This software is licensed under the Open Software License v3.0.
+// For the complete license, see http://www.clearcanvas.ca/OSLv3.0
+
+#endregion
+
+using ClearCanvas.Desktop.View.WinForms;
+using ClearCanvas.ImageViewer.StudyManagement;
+
+namespace ClearCanvas.ImageViewer.View.WinForms
+{
+	public partial class ActivityMonitorComponentControl : ApplicationComponentUserControl
+	{
+		private ActivityMonitorComponent _component;
+
+		public ActivityMonitorComponentControl(ActivityMonitorComponent component)
+			:base(component)
+		{
+			_component = component;
+			InitializeComponent();
+		}
+	}
+}
