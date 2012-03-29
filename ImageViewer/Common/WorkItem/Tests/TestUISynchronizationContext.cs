1c46d866f942232e0433c81694a58ca8422a3611
stewart <devnull@localhost>
2012-03-29 13:05:48 -0400

    [Requirement #9794] - Workstation local services will be refactored to use a WorkQueue [Requirement #9819] - Refactored ORM for database - Renamed StudyFinders/Loaders projects from LocalDataStore to just Local. - Fixed a race condition in the activity monitor that caused the UI thread to hang sometimes. - Changed activity monitor to fire events on the "connection" thread, rather than using the .NET thread pool. I recall this was a source of problems in the LDS with events firing out of order before.

---
+++ ImageViewer/Common/WorkItem/Tests/TestUISynchronizationContext.cs
@@ -0,0 +1,38 @@
+﻿#if UNIT_TESTS
+
+using System.Threading;
+
+namespace ClearCanvas.ImageViewer.Common.WorkItem.Tests
+{
+    //TODO (Marmot): should really test this scenario, too.
+    internal class TestUISynchronizationContext : SynchronizationContext
+    {
+        //private readonly object _syncLock = new object();
+        //private Thread _thread;
+        //private readonly Queue<SendOrPostCallback> _callbacks = new Queue<SendOrPostCallback>();
+
+        //public TestUISynchronizationContext()
+        //{
+        //    _thread = new Thread(RunThread);
+        //    _thread.Name = String.Format("Simulated UI Thread [{0}]", _thread.ManagedThreadId);
+        //    _thread.Start();
+        //    SynchronizationContext.Current.Post()
+        //}
+
+        //private void RunThread(object ignore)
+        //{
+
+        //}
+
+        //public void RunPump()
+        //{
+        //    lock(_syncLock)
+        //    {
+
+        //    }
+
+        //}
+    }
+}
+
+#endif
