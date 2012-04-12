fc29f70aa2819493e844d8c53a0ce09eb608f4ad
jonathan <devnull@localhost>
2012-04-12 18:17:32 -0400

    [Requirement #9817] - Auto-route rules will be applied once a Work Item has completed processing
     * Added a RuleTransformer class, which is responsible for transforming rules stored in Marmot-style to the Rules Engine format that is used by the IS.
     * Also added unit tests, but they don't pass yet.

---
+++ ImageViewer/Common/StudyManagement/Rules/RuleTransformer.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Linq;
+using ClearCanvas.Dicom;
+
+namespace ClearCanvas.ImageViewer.Common.StudyManagement.Rules
+{
+	internal class RuleTransformer
+	{
+#if UNIT_TESTS
+#endif
+
+
+		public XElement Rule(RuleData input)
+		{
+			return new XElement(XName.Get("rule"), Conditions(input.Conditions, input.Junction));
+		}
+
+		public XElement Conditions(IList<RuleCondition> conditions, JunctionType junction)
+		{
+			var node = new XElement(XName.Get("condition"));
+			node.SetAttributeValue(XName.Get("expressionLanguage"), "dicom");
+			if (conditions.Count > 0)
+			{
+				return Junction(junction == JunctionType.MatchAllConditions ? "and" : "or", conditions.Select(Condition));
+			}
+
+			// TODO: this is a dummy expr which should return true for all studies - is there a better way to do this??
+			node.Add(Element("not-null", DicomTagDictionary.GetDicomTag("StudyInstanceUid"), new Dictionary<string, string>()));
+			return node;
+		}
+
+		public XElement Condition(RuleCondition input)
+		{
+			switch(input.Operator)
+			{
+				case ComparisonOperator.EqualTo:
+					return Equality("equal", input.Tag, input.Value);
+				case ComparisonOperator.NotEqualTo:
+					return Equality("not-equal", input.Tag, input.Value);
+				case ComparisonOperator.Contains:
+					return Regex(input.Tag, string.Format("{0}", input.Value));
+				case ComparisonOperator.DoesNotContain:
+					return Not(Regex(input.Tag, string.Format("{0}", input.Value)));
+				case ComparisonOperator.StartsWith:
+					return Regex(input.Tag, string.Format("^{0}", input.Value));
+				case ComparisonOperator.EndsWith:
+					return Regex(input.Tag, string.Format("{0}$", input.Value));
+			}
+			throw new ArgumentOutOfRangeException();
+		}
+
+		private static XElement Junction(string op, IEnumerable<XElement> childNodes)
+		{
+			var node = new XElement(XName.Get(op));
+			node.Add(childNodes);
+			return node;
+		}
+
+		private static XElement Equality(string op, DicomTag tag, string value)
+		{
+			return Element(op, tag, new Dictionary<string, string> {{"refValue", value}});
+		}
+
+		private static XElement Regex(DicomTag tag, string pattern)
+		{
+			return Element("regex", tag, new Dictionary<string, string> { { "pattern", pattern}, {"ignoreCase", "true" } });
+		}
+
+		private static XElement Not(XElement spec)
+		{
+			return new XElement(XName.Get("not"), spec);
+		}
+
+		private static XElement Element(string op, DicomTag tag, Dictionary<string, string> attributes)
+		{
+			var node = new XElement(XName.Get(op));
+			node.SetAttributeValue(XName.Get("test"), string.Format("${0}", tag.VariableName));
+			foreach (var kvp in attributes)
+			{
+				node.SetAttributeValue(XName.Get(kvp.Key), kvp.Value);
+			}
+			return node;
+		}
+	}
+}
