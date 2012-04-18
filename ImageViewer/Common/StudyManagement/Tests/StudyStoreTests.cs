c19aca07c4015419ea98b4952663259252a6f7d9
stewart <devnull@localhost>
2012-04-18 16:07:01 -0400

    [Requirement #9954] - The user must be able to see which rules a particular study will be matched by - Added a way to convert a StudyEntry to a DicomAttributeCollection, including the extended data that is DICOM in nature. - Note that BitsAllocatedInStudy and BitsStoredInStudy are both put into custom attribute with VR=IS, even though that's not the correct VR, so that we can get multiple values in the attribute.

---
+++ ImageViewer/Common/StudyManagement/Tests/StudyStoreTests.cs
@@ -0,0 +1,62 @@
+﻿#region License
+
+// Copyright (c) 2012, ClearCanvas Inc.
+// All rights reserved.
+// http://www.clearcanvas.ca
+//
+// This software is licensed under the Open Software License v3.0.
+// For the complete license, see http://www.clearcanvas.ca/OSLv3.0
+
+#endregion
+
+using ClearCanvas.Dicom;
+using ClearCanvas.Dicom.ServiceModel.Query;
+using NUnit.Framework;
+
+#if UNIT_TESTS
+
+namespace ClearCanvas.ImageViewer.Common.StudyManagement.Tests
+{
+    [TestFixture]
+    public class StudyStoreTests
+    {
+        [Test]
+        public void TestStudyEntry_ToDicomAttributeCollection()
+        {
+            var entry = new StudyEntry
+                                   {
+                                       Study = new StudyRootStudyIdentifier
+                                                   {
+                                                       PatientId = "123"
+                                                   },
+                                       Data = new StudyEntryData
+                                                  {
+                                                      TransferSyntaxesInStudy = new string[]{TransferSyntax.ExplicitVrLittleEndian.UidString, TransferSyntax.ImplicitVrLittleEndian.UidString},
+                                                      SourceAETitlesInStudy = new string[]{"PACS1", "PACS2"},
+                                                      StationNamesInStudy = new string[] { "STN1", "STN2" },
+                                                      InstitutionNamesInStudy = new string[] { "INST1", "INST2" },
+
+                                                      PhotometricInterpretationsInStudy = new string[] { "MONOCHROME1", "MONOCHROME2" },
+                                                      BitsAllocatedInStudy = new int[] { 16, 8 },
+                                                      BitsStoredInStudy = new int[] { 16, 15, 11 }
+                                                  }
+                                   };
+
+            var collection = entry.ToDicomAttributeCollection();
+
+            string ts = string.Format("{0}\\{1}", TransferSyntax.ExplicitVrLittleEndian.UidString, TransferSyntax.ImplicitVrLittleEndian.UidString);
+            Assert.AreEqual(ts, collection[DicomTags.TransferSyntaxUid].ToString());
+
+            Assert.AreEqual("PACS1\\PACS2", collection[DicomTags.SourceApplicationEntityTitle].ToString());
+            Assert.AreEqual("STN1\\STN2", collection[DicomTags.StationName].ToString());
+            Assert.AreEqual("INST1\\INST2", collection[DicomTags.InstitutionName].ToString());
+
+            Assert.AreEqual("MONOCHROME1\\MONOCHROME2", collection[DicomTags.PhotometricInterpretation].ToString());
+            Assert.AreEqual("16\\8", collection[DicomTags.BitsAllocated].ToString());
+            Assert.AreEqual("16\\15\\11", collection[DicomTags.BitsStored].ToString());
+        }
+    }
+}
+
+
+#endif
