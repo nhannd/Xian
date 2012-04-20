9588a1af2851dc1c3216dc2fc3a98c4760a60a18
jasper <devnull@localhost>
2012-04-20 14:29:35 -0400

    [Requirement #9909] - Premium features shall be available only when a valid, active license exists  * Implemented framework mechanism for actions to be controlled by product licensing.   * The ActionPermission attribute now supports an optional property to specify a feature identification token.   * The LicenseInformation will be consulted to indicate whether or not a specific action is authorized for use by the license.   * The feature token check must succeed in addition to all standard authority tokens specified in the same attribute.   * The feature token check in one given ActionPermission attribute instance is independent of any other ActionPermission attributes specified for the same action.

--- Desktop/Actions/ActionPermissionAttribute.cs
+++ Desktop/Actions/ActionPermissionAttribute.cs
@@ -13,55 +13,82 @@ using ClearCanvas.Common.Specifications;
 
 namespace ClearCanvas.Desktop.Actions
 {
-    /// <summary>
-    /// Associates authority tokens with an action.
-    /// </summary>
-    /// <remarks>
-    /// This attribute sets the action permissibility via the <see cref="Action.SetPermissibility(ISpecification)"/> method.
-    /// If multiple authority tokens are supplied in an array to a single instance of the attribute, those tokens will be combined using AND.  If
-    /// multiple instances of this attribute are specified, the tokens associated with each instance are combined
-    /// using OR logic.  This allows for the possibility of constructing a permission specification based on a complex boolean
-    /// combination of authority tokens.
-    /// </remarks>
-    public class ActionPermissionAttribute : ActionDecoratorAttribute
-    {
-        private string[] _authorityTokens;
+	/// <summary>
+	/// Associates authority tokens with an action.
+	/// </summary>
+	/// <remarks>
+	/// This attribute sets the action permissibility via the <see cref="Action.SetPermissibility(ISpecification)"/> method.
+	/// If multiple authority tokens are supplied in an array to a single instance of the attribute, those tokens will be combined using AND.  If
+	/// multiple instances of this attribute are specified, the tokens associated with each instance are combined
+	/// using OR logic.  This allows for the possibility of constructing a permission specification based on a complex boolean
+	/// combination of authority tokens.
+	/// </remarks>
+	public class ActionPermissionAttribute : ActionDecoratorAttribute
+	{
+		private readonly string[] _authorityTokens;
 
-        /// <summary>
-        /// Constructor - the specified authority token will be associated with the specified action ID.
-        /// </summary>
-        public ActionPermissionAttribute(string actionID, string authorityToken)
-            : this(actionID, new string[] { authorityToken })
-        {
-        }
+		/// <summary>
+		/// Constructor - the specified authority token will be associated with the specified action ID.
+		/// </summary>
+		public ActionPermissionAttribute(string actionId, string authorityToken)
+			: this(actionId, new[] {authorityToken}) {}
 
-        /// <summary>
-        /// Constructor - all of the specified tokens will combined using AND and associated with the specified action ID.
-        /// </summary>
-        public ActionPermissionAttribute(string actionID, params string[] authorityTokens)
-            :base(actionID)
-        {
-            _authorityTokens = authorityTokens;
-        }
+		/// <summary>
+		/// Constructor - all of the specified tokens will combined using AND and associated with the specified action ID.
+		/// </summary>
+		public ActionPermissionAttribute(string actionId, params string[] authorityTokens)
+			: base(actionId)
+		{
+			_authorityTokens = authorityTokens;
+		}
+
+		/// <summary>
+		/// Feature identification token to be checked against application licensing.
+		/// </summary>
+		public string FeatureToken { get; set; }
+
+		/// <summary>
+		/// Gets the associated authority tokens.
+		/// </summary>
+		protected string[] AuthorityTokens
+		{
+			get { return _authorityTokens; }
+		}
 
 		/// <summary>
 		/// Applies permissions represented by this attribute to an action instance, via the specified <see cref="IActionBuildingContext"/>.
 		/// </summary>
 		public override void Apply(IActionBuildingContext builder)
-        {
-            // if this is the first occurence of this attribute, create the parent spec
-            if (builder.Action.PermissionSpecification == null)
-                builder.Action.PermissionSpecification = new OrSpecification();
+		{
+			// if this is the first occurence of this attribute, create the parent spec
+			if (builder.Action.PermissionSpecification == null)
+				builder.Action.PermissionSpecification = new OrSpecification();
 
-            // combine the specified tokens with AND logic
-            AndSpecification and = new AndSpecification();
-            foreach (string token in _authorityTokens)
-            {
-                and.Add(new PrincipalPermissionSpecification(token));
-            }
+			var specification = CreateSpecification();
 
-            // combine this spec with any previous occurence of this attribute using OR logic
-            ((OrSpecification)builder.Action.PermissionSpecification).Add(and);
-        }
-    }
+			// combine this spec with any previous occurence of this attribute using OR logic
+			((OrSpecification) builder.Action.PermissionSpecification).Add(specification);
+		}
+
+		/// <summary>
+		/// Called to create the permission specification.
+		/// </summary>
+		protected virtual ISpecification CreateSpecification()
+		{
+			// combine the specified tokens with AND logic
+			var specification = new AndSpecification();
+
+			foreach (string token in _authorityTokens)
+			{
+				specification.Add(new PrincipalPermissionSpecification(token));
+			}
+
+			if (!string.IsNullOrEmpty(FeatureToken))
+			{
+				specification.Add(new FeatureAuthorizationSpecification(FeatureToken));
+			}
+
+			return specification;
+		}
+	}
 }
---
+++ Desktop/FeatureAuthorizationSpecification.cs
@@ -0,0 +1,38 @@
+﻿#region License
+
+// Copyright (c) 2012, ClearCanvas Inc.
+// All rights reserved.
+// http://www.clearcanvas.ca
+//
+// This software is licensed under the Open Software License v3.0.
+// For the complete license, see http://www.clearcanvas.ca/OSLv3.0
+
+#endregion
+
+using ClearCanvas.Common;
+using ClearCanvas.Common.Specifications;
+
+namespace ClearCanvas.Desktop
+{
+	/// <summary>
+	/// An implementation of <see cref="ISpecification"/> that tests if the identified feature is authorized by application licensing.
+	/// </summary>
+	public class FeatureAuthorizationSpecification : ISpecification
+	{
+		private readonly string _featureToken;
+
+		/// <summary>
+		/// Constructor.
+		/// </summary>
+		/// <param name="featureToken">The feature identification token to be checked.</param>
+		public FeatureAuthorizationSpecification(string featureToken)
+		{
+			_featureToken = featureToken;
+		}
+
+		public TestResult Test(object obj)
+		{
+			return new TestResult(LicenseInformation.IsFeatureAuthorized(_featureToken));
+		}
+	}
+}
