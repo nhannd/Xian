c871fd00f8afbc874f2ffa903c81e639744fe8c0
jonathan <devnull@localhost>
2012-04-24 16:50:25 -0400

    [Requirement #9802] - Disk space manager will scan studies and insert Delete Study items in the WorkQueue for processing
     * first pass at writing the Filestore Management shred.
     * needs a lot more testing, once we get the rules engine going

---
+++ ImageViewer/Shreds/FilestoreManagement/FilestoreManagementProcessor.cs
@@ -0,0 +1,97 @@
+﻿#region License
+
+// Copyright (c) 2011, ClearCanvas Inc.
+// All rights reserved.
+// http://www.clearcanvas.ca
+//
+// This software is licensed under the Open Software License v3.0.
+// For the complete license, see http://www.clearcanvas.ca/OSLv3.0
+
+#endregion
+
+using System;
+using System.Collections.Generic;
+using ClearCanvas.Common.Shreds;
+using ClearCanvas.ImageViewer.Common.Auditing;
+using ClearCanvas.ImageViewer.Common.WorkItem;
+using ClearCanvas.ImageViewer.Shreds.WorkItemService;
+using ClearCanvas.ImageViewer.StudyManagement.Storage;
+using ClearCanvas.Common;
+
+namespace ClearCanvas.ImageViewer.Shreds.FilestoreManagement
+{
+	internal sealed class FilestoreManagementProcessor : QueueProcessor<Study>
+	{
+		public FilestoreManagementProcessor(int batchSize, TimeSpan sleepTime)
+			: base(batchSize, sleepTime)
+		{
+		}
+
+		protected override IList<Study> GetNextBatch(int batchSize)
+		{
+			using(var context = new DataAccessContext())
+			{
+				var broker = context.GetStudyBroker();
+				return broker.GetStudiesForDeletion(Platform.Time, batchSize);
+			}
+		}
+
+		protected override void ProcessItem(Study study)
+		{
+			var result = EventResult.Success;
+			try
+			{
+				using (var context = new DataAccessContext(DataAccessContext.WorkItemMutex))
+				{
+					ProcessStudy(study.Oid, context);
+					context.Commit();
+				}
+			}
+			catch (Exception)
+			{
+				result = EventResult.MajorFailure;
+				throw;
+			}
+			finally
+			{
+				var instances = new AuditedInstances();
+				instances.AddInstance(study.PatientId, study.PatientsName, study.StudyInstanceUid);
+
+				AuditHelper.LogDeleteStudies(AuditHelper.LocalAETitle, instances, EventSource.CurrentUser, result);
+			}
+		}
+
+		private static void ProcessStudy(long studyOid, DataAccessContext context)
+		{
+			var studyBroker = context.GetStudyBroker();
+			var study = studyBroker.GetStudy(studyOid);
+
+			// on the off chance it was deleted by another thread/process, there's nothing to do here
+			if (study.Deleted)
+				return;
+
+			// TODO (Marmot): this code was duplicated from DeleteClient.DeleteStudy
+			var now = Platform.Time;
+			var item = new WorkItem
+			           	{
+			           		Request = new DeleteStudyRequest
+			           		          	{
+			           		          		Study = new WorkItemStudy(study),
+			           		          		Patient = new WorkItemPatient(study)
+			           		          	},
+			           		Type = WorkItemTypeEnum.DeleteStudy,
+			           		Priority = WorkItemPriorityEnum.Normal,
+			           		InsertTime = now,
+			           		ScheduledTime = now.AddSeconds(5),
+			           		DeleteTime = now.AddMinutes(WorkItemServiceSettings.Instance.DeleteDelayMinutes),
+			           		ExpirationTime = now.AddSeconds(WorkItemServiceSettings.Instance.ExpireDelaySeconds),
+			           		Status = WorkItemStatusEnum.Pending,
+			           		StudyInstanceUid = study.StudyInstanceUid
+			           	};
+
+			var workItemBroker = context.GetWorkItemBroker();
+			workItemBroker.AddWorkItem(item);
+			study.Deleted = true;
+		}
+	}
+}
---
+++ ImageViewer/Shreds/FilestoreManagement/FilestoreManagementShred.cs
@@ -0,0 +1,43 @@
+﻿#region License
+
+// Copyright (c) 2011, ClearCanvas Inc.
+// All rights reserved.
+// http://www.clearcanvas.ca
+//
+// This software is licensed under the Open Software License v3.0.
+// For the complete license, see http://www.clearcanvas.ca/OSLv3.0
+
+#endregion
+
+using System;
+using System.Collections.Generic;
+using ClearCanvas.Common;
+using ClearCanvas.Common.Shreds;
+
+namespace ClearCanvas.ImageViewer.Shreds.FilestoreManagement
+{
+	[ExtensionOf(typeof(ShredExtensionPoint))]
+	public class FilestoreManagementShred : QueueProcessorShred
+	{
+		public FilestoreManagementShred()
+		{
+			
+		}
+
+		public override string GetDisplayName()
+		{
+			return SR.FilestoreManagementShred;
+		}
+
+		public override string GetDescription()
+		{
+			return SR.FilestoreManagementShred;
+		}
+
+		protected override IList<QueueProcessor> GetProcessors()
+		{
+			// todo externalize these constants?
+			return new QueueProcessor[] {new FilestoreManagementProcessor(100, TimeSpan.FromSeconds(10))};
+		}
+	}
+}
