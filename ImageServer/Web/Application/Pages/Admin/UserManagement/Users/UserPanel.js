b64dc3952654c598db38c8361dbbe7a644a1e8ee
steve <devnull@localhost>
2012-07-21 00:54:41 -0500

    [Requirement #10273] - ImageServer shall function with .NET 4.
    Updates to make the IS work with .NET 4.0.  Managed to update the AJAX toolkit
    to use the 4.0 version included with .NET.  We no longer are dependent on
    that external project!  Also, had to make soem changes to support the new
    licensing being stored through the enterprise server.  Finally, updates
    needed to be done to all the javascript files that created classes to match
    server side classes, some code to check if the javascript was already created
    was no longer needed after the ajax upgrade, in fact it caused the code
    to not work.  As part of this, cleaned up the js files also.  Finally, upgraded
    to the latest version of the AjaxControlToolkit.

--- ImageServer/Web/Application/Pages/Admin/UserManagement/Users/UserPanel.js
+++ ImageServer/Web/Application/Pages/Admin/UserManagement/Users/UserPanel.js
@@ -1,6 +1,6 @@
 /* License
  *
- * Copyright (c) 2011, ClearCanvas Inc.
+ * Copyright (c) 2012, ClearCanvas Inc.
  * All rights reserved.
  * http://www.clearcanvas.ca
  *
@@ -10,179 +10,137 @@
  */
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////
-///
 /// This script contains the javascript component class for the study search panel
-/// 
-/////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 // Define and register the control type.
-//
-// Only define and register the type if it doens't exist. Otherwise "... does not derive from Sys.Component" error 
-// will show up if multiple instance of the controls must be created. The error is misleading. It looks like the type 
-// is RE-define for the 2nd instance but registerClass() will fail so the type will be essential undefined when the object
-// is instantiated.
-//
+Type.registerNamespace('ClearCanvas.ImageServer.Web.Application.Pages.Admin.UserManagement.Users');
 
-if (window.__registeredTypes['ClearCanvas.ImageServer.Web.Application.Pages.Admin.UserManagement.Users.UserPanel']==null)
-{
-
-    Type.registerNamespace('ClearCanvas.ImageServer.Web.Application.Pages.Admin.UserManagement.Users');
-
-    /////////////////////////////////////////////////////////////////////////////////////////////////////////
-    //
-    // Constructor
-    //
-    /////////////////////////////////////////////////////////////////////////////////////////////////////////
-    ClearCanvas.ImageServer.Web.Application.Pages.Admin.UserManagement.Users.UserPanel = function(element) { 
-        ClearCanvas.ImageServer.Web.Application.Pages.Admin.UserManagement.Users.UserPanel.initializeBase(this, [element]);
+/////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Constructor
+ClearCanvas.ImageServer.Web.Application.Pages.Admin.UserManagement.Users.UserPanel = function(element) { 
+    ClearCanvas.ImageServer.Web.Application.Pages.Admin.UserManagement.Users.UserPanel.initializeBase(this, [element]);
        
-    }
+},
 
-    /////////////////////////////////////////////////////////////////////////////////////////////////////////
-    //
-    // Create the prototype for the control.
-    //
-    /////////////////////////////////////////////////////////////////////////////////////////////////////////
-    ClearCanvas.ImageServer.Web.Application.Pages.Admin.UserManagement.Users.UserPanel.prototype = 
-    {
-        initialize : function() {
-            ClearCanvas.ImageServer.Web.Application.Pages.Admin.UserManagement.Users.UserPanel.callBaseMethod(this, 'initialize');        
+/////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Create the prototype for the control.
+ClearCanvas.ImageServer.Web.Application.Pages.Admin.UserManagement.Users.UserPanel.prototype = 
+{
+    initialize : function() {
+        ClearCanvas.ImageServer.Web.Application.Pages.Admin.UserManagement.Users.UserPanel.callBaseMethod(this, 'initialize');        
             
-            this._OnUserListRowClickedHandler = Function.createDelegate(this,this._OnUserListRowClicked);
+        this._OnUserListRowClickedHandler = Function.createDelegate(this,this._OnUserListRowClicked);
             
-            this._OnLoadHandler = Function.createDelegate(this,this._OnLoad);
-            Sys.Application.add_load(this._OnLoadHandler);
+        this._OnLoadHandler = Function.createDelegate(this,this._OnLoad);
+        Sys.Application.add_load(this._OnLoadHandler);
                  
-        },
+    },
         
-        dispose : function() {
-            $clearHandlers(this.get_element());
+    dispose : function() {
+        $clearHandlers(this.get_element());
 
-            ClearCanvas.ImageServer.Web.Application.Pages.Admin.UserManagement.Users.UserPanel.callBaseMethod(this, 'dispose');
+        ClearCanvas.ImageServer.Web.Application.Pages.Admin.UserManagement.Users.UserPanel.callBaseMethod(this, 'dispose');
             
-            Sys.Application.remove_load(this._OnLoadHandler);
-        },
+        Sys.Application.remove_load(this._OnLoadHandler);
+    },
         
         
-        /////////////////////////////////////////////////////////////////////////////////////////////////////////
-        //
-        // Events
-        //
-        /////////////////////////////////////////////////////////////////////////////////////////////////////////
-        
-        _OnLoad : function()
-        {                    
-            var userlist = $find(this._UserListClientID);
-            userlist.add_onClientRowClick(this._OnUserListRowClickedHandler);
+    /////////////////////////////////////////////////////////////////////////////////////////////////////////
+    // Events
+    _OnLoad : function()
+    {                    
+        var userlist = $find(this._UserListClientID);
+        userlist.add_onClientRowClick(this._OnUserListRowClickedHandler);
                  
-            this._updateToolbarButtonStates();
-        },
+        this._updateToolbarButtonStates();
+    },
         
-        // called when user clicked on a row in the study list
-        _OnUserListRowClicked : function(sender, event)
-        {    
-            this._updateToolbarButtonStates();        
-        },
+    // called when user clicked on a row in the study list
+    _OnUserListRowClicked : function(sender, event)
+    {    
+        this._updateToolbarButtonStates();        
+    },
                        
-        /////////////////////////////////////////////////////////////////////////////////////////////////////////
-        //
-        // Private Methods
-        //
-        /////////////////////////////////////////////////////////////////////////////////////////////////////////
-        
-        _updateToolbarButtonStates : function()
-        {
-            var userlist = $find(this._UserListClientID);
+    /////////////////////////////////////////////////////////////////////////////////////////////////////////
+    // Private Methods
+    _updateToolbarButtonStates : function()
+    {
+        var userlist = $find(this._UserListClientID);
                       
-            this._enableEditButton(false);
-            this._enableDeleteButton(false);
-            this._enableResetPasswordButton(false);
+        this._enableEditButton(false);
+        this._enableDeleteButton(false);
+        this._enableResetPasswordButton(false);
                                
-            if (userlist!=null )
-            {
-                var rows = userlist.getSelectedRowElements();
+        if (userlist!=null )
+        {
+            var rows = userlist.getSelectedRowElements();
 
-                if(rows != null && rows.length > 0) {
-                    this._enableEditButton(true);
-                    this._enableDeleteButton(true);
-                    this._enableResetPasswordButton(true);
-                }
+            if(rows != null && rows.length > 0) {
+                this._enableEditButton(true);
+                this._enableDeleteButton(true);
+                this._enableResetPasswordButton(true);
             }
-        },
-        
-        _enableDeleteButton : function(en)
-        {
-            var deleteButton = $find(this._DeleteButtonClientID);
-            deleteButton.set_enable(en);
-        },
-        
-        _enableEditButton : function(en)
-        {
-            var editButton = $find(this._EditButtonClientID);
-            editButton.set_enable(en);
-        },       
-
-        _enableResetPasswordButton : function(en)
-        {
-            var resetPasswordButton = $find(this._ResetPasswordButtonClientID);
-            resetPasswordButton.set_enable(en);
-        },               
-
-        /////////////////////////////////////////////////////////////////////////////////////////////////////////
-        //
-        // Public methods
-        //
-        /////////////////////////////////////////////////////////////////////////////////////////////////////////
-        
-        
-
-        /////////////////////////////////////////////////////////////////////////////////////////////////////////
-        //
-        // Properties
-        //
-        /////////////////////////////////////////////////////////////////////////////////////////////////////////
-               
-        get_DeleteButtonClientID : function() {
-            return this._DeleteButtonClientID;
-        },
-
-        set_DeleteButtonClientID : function(value) {
-            this._DeleteButtonClientID = value;
-            this.raisePropertyChanged('DeleteButtonClientID');
-        },
-        
-        get_EditButtonClientID : function() {
-            return this._EditButtonClientID;
-        },
-
-        set_EditButtonClientID : function(value) {
-            this._EditButtonClientID = value;
-            this.raisePropertyChanged('EditButtonClientID');
-        },
-        
-        get_ResetPasswordButtonClientID : function() {
-            return this._ResetPasswordButtonClientID;
-        },
-
-        set_ResetPasswordButtonClientID : function(value) {
-            this._ResetPasswordButtonClientID = value;
-            this.raisePropertyChanged('ResetPasswordButtonClientID');
-        },
-        
-        get_UserListClientID : function() {
-            return this._UserListClientID;
-        },
-
-        set_UserListClientID : function(value) {
-            this._UserListClientID = value;
-            this.raisePropertyChanged('UserListClientID');
         }
+    },
+        
+    _enableDeleteButton : function(en)
+    {
+        var deleteButton = $find(this._DeleteButtonClientID);
+        deleteButton.set_enable(en);
+    },
+        
+    _enableEditButton : function(en)
+    {
+        var editButton = $find(this._EditButtonClientID);
+        editButton.set_enable(en);
+    },       
+
+    _enableResetPasswordButton : function(en)
+    {
+        var resetPasswordButton = $find(this._ResetPasswordButtonClientID);
+        resetPasswordButton.set_enable(en);
+    },               
+
+    /////////////////////////////////////////////////////////////////////////////////////////////////////////
+    // Properties
+    get_DeleteButtonClientID : function() {
+        return this._DeleteButtonClientID;
+    },
+
+    set_DeleteButtonClientID : function(value) {
+        this._DeleteButtonClientID = value;
+        this.raisePropertyChanged('DeleteButtonClientID');
+    },
+        
+    get_EditButtonClientID : function() {
+        return this._EditButtonClientID;
+    },
+
+    set_EditButtonClientID : function(value) {
+        this._EditButtonClientID = value;
+        this.raisePropertyChanged('EditButtonClientID');
+    },
+        
+    get_ResetPasswordButtonClientID : function() {
+        return this._ResetPasswordButtonClientID;
+    },
+
+    set_ResetPasswordButtonClientID : function(value) {
+        this._ResetPasswordButtonClientID = value;
+        this.raisePropertyChanged('ResetPasswordButtonClientID');
+    },
+        
+    get_UserListClientID : function() {
+        return this._UserListClientID;
+    },
+
+    set_UserListClientID : function(value) {
+        this._UserListClientID = value;
+        this.raisePropertyChanged('UserListClientID');
     }
+},
 
-    // Register the class as a type that inherits from Sys.UI.Control.
-    ClearCanvas.ImageServer.Web.Application.Pages.Admin.UserManagement.Users.UserPanel.registerClass('ClearCanvas.ImageServer.Web.Application.Pages.Admin.UserManagement.Users.UserPanel', Sys.UI.Control);
-     
+// Register the class as a type that inherits from Sys.UI.Control.
+ClearCanvas.ImageServer.Web.Application.Pages.Admin.UserManagement.Users.UserPanel.registerClass('ClearCanvas.ImageServer.Web.Application.Pages.Admin.UserManagement.Users.UserPanel', Sys.UI.Control);     
 
-    if (typeof(Sys) !== 'undefined') Sys.Application.notifyScriptLoaded();
-
-}
+if (typeof(Sys) !== 'undefined') Sys.Application.notifyScriptLoaded();
--- ImageServer/Web/Application/Pages/Studies/StudyDetails/Scripts/FileSystemQueueGridView.js
+++ ImageServer/Web/Application/Pages/Studies/StudyDetails/Scripts/FileSystemQueueGridView.js
@@ -10,159 +10,109 @@
  */
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////
-///
 /// This script contains the javascript component class for the FileSystemQueueGridView
-/// 
-/////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 // Define and register the control type.
-//
-// Only define and register the type if it doens't exist. Otherwise "... does not derive from Sys.Component" error 
-// will show up if multiple instance of the controls must be created. The error is misleading. It looks like the type 
-// is RE-define for the 2nd instance but registerClass() will fail so the type will be essential undefined when the object
-// is instantiated.
-//
-if (window.__registeredTypes['ClearCanvas.ImageServer.Web.Application.Pages.Studies.StudyDetails.Controls.FileSystemQueueGridView']==null)
+Type.registerNamespace('ClearCanvas.ImageServer.Web.Application.Pages.Studies.StudyDetails.Controls');
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Constructor
+ClearCanvas.ImageServer.Web.Application.Pages.Studies.StudyDetails.Controls.FileSystemQueueGridView = function(element) { 
+    ClearCanvas.ImageServer.Web.Application.Pages.Studies.StudyDetails.Controls.FileSystemQueueGridView.initializeBase(this, [element]);
+},
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Create the prototype for the control.
+ClearCanvas.ImageServer.Web.Application.Pages.Studies.StudyDetails.Controls.FileSystemQueueGridView.prototype = 
 {
-    Type.registerNamespace('ClearCanvas.ImageServer.Web.Application.Pages.Studies.StudyDetails.Controls');
+    initialize : function() {
+        ClearCanvas.ImageServer.Web.Application.Pages.Studies.StudyDetails.Controls.FileSystemQueueGridView.callBaseMethod(this, 'initialize');        
+            
+        //this._OnLoadHandler = Function.createDelegate(this,this._OnLoad);
+        //this._OnSeriesListDoubleClickedHandler = Function.createDelegate(this,this._OnSeriesListDoubleClicked);
+            
+        //Sys.Application.add_load(this._OnLoadHandler);
+    },
+        
+    dispose : function() {
+        $clearHandlers(this.get_element());
 
+        ClearCanvas.ImageServer.Web.Application.Pages.Studies.StudyDetails.Controls.FileSystemQueueGridView.callBaseMethod(this, 'dispose');
+            
+        //Sys.Application.remove_load(this._OnLoadHandler);
+    },
+        
     /////////////////////////////////////////////////////////////////////////////////////////////////////////
-    //
-    // Constructor
-    //
-    /////////////////////////////////////////////////////////////////////////////////////////////////////////
-    ClearCanvas.ImageServer.Web.Application.Pages.Studies.StudyDetails.Controls.FileSystemQueueGridView = function(element) { 
-        ClearCanvas.ImageServer.Web.Application.Pages.Studies.StudyDetails.Controls.FileSystemQueueGridView.initializeBase(this, [element]);
-       
-    }
-
-    /////////////////////////////////////////////////////////////////////////////////////////////////////////
-    //
-    // Create the prototype for the control.
-    //
-    /////////////////////////////////////////////////////////////////////////////////////////////////////////
-    ClearCanvas.ImageServer.Web.Application.Pages.Studies.StudyDetails.Controls.FileSystemQueueGridView.prototype = 
+    // Events
+        
+    /// called whenever the page is reloaded or partially reloaded
+    _OnLoad : function()
     {
-        initialize : function() {
-            ClearCanvas.ImageServer.Web.Application.Pages.Studies.StudyDetails.Controls.FileSystemQueueGridView.callBaseMethod(this, 'initialize');        
-            
-            //this._OnLoadHandler = Function.createDelegate(this,this._OnLoad);
-            //this._OnSeriesListDoubleClickedHandler = Function.createDelegate(this,this._OnSeriesListDoubleClicked);
-            
-            //Sys.Application.add_load(this._OnLoadHandler);
-                 
-        },
-        
-        dispose : function() {
-            $clearHandlers(this.get_element());
-
-            ClearCanvas.ImageServer.Web.Application.Pages.Studies.StudyDetails.Controls.FileSystemQueueGridView.callBaseMethod(this, 'dispose');
-            
-            //Sys.Application.remove_load(this._OnLoadHandler);
-        },
-        
-        
-        /////////////////////////////////////////////////////////////////////////////////////////////////////////
-        //
-        // Events
-        //
-        /////////////////////////////////////////////////////////////////////////////////////////////////////////
-        
-        
-        /// called whenever the page is reloaded or partially reloaded
-        _OnLoad : function()
+        var serieslist = $find(this._SeriesListClientID);
+        if (serieslist!=null)
         {
-            var serieslist = $find(this._SeriesListClientID);
-            if (serieslist!=null)
-            {
-                serieslist.add_onClientRowDblClick(this._OnSeriesListDoubleClickedHandler);
-            }
-        },
-        
-        // called when the user double click on the series list
-        _OnSeriesDoubleClicked : function(src, event)
-        {
-            var serieslist = $find(this._SeriesListClientID);
-            if (serieslist!=null)
-            {
-                var rows = serieslist.getSelectedRowElements();
-                for(i=0; i<rows.length; i++)
-                {
-                    var url = String.format('{0}?serverae={1}&studyuid={2}&seriesuid={3}', 
-                                this._OpenSeriesPageUrl, 
-                                this._getServerAE(rows[i]), 
-                                this._getStudyUid(rows[i]), 
-                                this._getSeriesUid(rows[i]));
-                    window.open(url);
-                }    
-            }
-        },
-        
-        
-        
-        /////////////////////////////////////////////////////////////////////////////////////////////////////////
-        //
-        // Private Methods
-        //
-        /////////////////////////////////////////////////////////////////////////////////////////////////////////
-        
-        _getServerAE : function (row)
-        {
-            return row.getAttribute('serverae');
-        },
-        
-        _getStudyUid : function (row)
-        {
-            return row.getAttribute('studyuid');
-        },
-        
-        _getSeriesUid: function (row)
-        {
-            return row.getAttribute('seriesuid');
-        },
-
-        /////////////////////////////////////////////////////////////////////////////////////////////////////////
-        //
-        // Public methods
-        //
-        /////////////////////////////////////////////////////////////////////////////////////////////////////////
-        
-        
-
-        /////////////////////////////////////////////////////////////////////////////////////////////////////////
-        //
-        // Properties
-        //
-        /////////////////////////////////////////////////////////////////////////////////////////////////////////
-        
-        get_SeriesListClientID : function() {
-            return this._SeriesListClientID;
-        },
-
-        set_SeriesListClientID: function(value) {
-            this._SeriesListClientID = value;
-            this.raisePropertyChanged('SeriesListClientID');
-        },
-        
-        
-        
-        get_OpenSeriesPageUrl : function() {
-            return this._OpenSeriesPageUrl;
-        },
-       
-        set_OpenSeriesPageUrl : function(value) {
-            this._OpenSeriesPageUrl = value;
-            this.raisePropertyChanged('OpenSeriesPageUrl');
+            serieslist.add_onClientRowDblClick(this._OnSeriesListDoubleClickedHandler);
         }
+    },
         
+    // called when the user double click on the series list
+    _OnSeriesDoubleClicked : function(src, event)
+    {
+        var serieslist = $find(this._SeriesListClientID);
+        if (serieslist!=null)
+        {
+            var rows = serieslist.getSelectedRowElements();
+            for(i=0; i<rows.length; i++)
+            {
+                var url = String.format('{0}?serverae={1}&studyuid={2}&seriesuid={3}', 
+                            this._OpenSeriesPageUrl, 
+                            this._getServerAE(rows[i]), 
+                            this._getStudyUid(rows[i]), 
+                            this._getSeriesUid(rows[i]));
+                window.open(url);
+            }    
+        }
+    },
+        
+    /////////////////////////////////////////////////////////////////////////////////////////////////////////
+    // Private Methods
+    _getServerAE : function (row)
+    {
+        return row.getAttribute('serverae');
+    },
+        
+    _getStudyUid : function (row)
+    {
+        return row.getAttribute('studyuid');
+    },
+        
+    _getSeriesUid: function (row)
+    {
+        return row.getAttribute('seriesuid');
+    },
 
+    /////////////////////////////////////////////////////////////////////////////////////////////////////////
+    // Properties
+    get_SeriesListClientID : function() {
+        return this._SeriesListClientID;
+    },
+
+    set_SeriesListClientID: function(value) {
+        this._SeriesListClientID = value;
+        this.raisePropertyChanged('SeriesListClientID');
+    },
+        
+    get_OpenSeriesPageUrl : function() {
+        return this._OpenSeriesPageUrl;
+    },
+       
+    set_OpenSeriesPageUrl : function(value) {
+        this._OpenSeriesPageUrl = value;
+        this.raisePropertyChanged('OpenSeriesPageUrl');
     }
+},
 
-    // Register the class as a type that inherits from Sys.UI.Control.
+// Register the class as a type that inherits from Sys.UI.Control.
+ClearCanvas.ImageServer.Web.Application.Pages.Studies.StudyDetails.Controls.FileSystemQueueGridView.registerClass('ClearCanvas.ImageServer.Web.Application.Pages.Studies.StudyDetails.Controls.FileSystemQueueGridView', Sys.UI.Control);
 
-        ClearCanvas.ImageServer.Web.Application.Pages.Studies.StudyDetails.Controls.FileSystemQueueGridView.registerClass('ClearCanvas.ImageServer.Web.Application.Pages.Studies.StudyDetails.Controls.FileSystemQueueGridView', Sys.UI.Control);
-     
-
-    if (typeof(Sys) !== 'undefined') Sys.Application.notifyScriptLoaded();
-
-}
+if (typeof(Sys) !== 'undefined') Sys.Application.notifyScriptLoaded();
