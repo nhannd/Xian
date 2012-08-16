9ff8b17b846800b77bf990a33cf5d66091d93ecd
jonathan <devnull@localhost>
2012-08-16 16:10:58 -0400

    [Requirement #10305] - Simplify the Performing workspace
    Implemented the simple documentation - just a rich text field with canned text support.  For some reason there are cosmetic issues - will have to return to those later.

---
+++ Ris/Client/Workflow/Extended/MppsStructuredDocumentationComponent.cs
@@ -0,0 +1,121 @@
+﻿#region License
+
+// Copyright (c) 2011, ClearCanvas Inc.
+// All rights reserved.
+// http://www.clearcanvas.ca
+//
+// This software is licensed under the Open Software License v3.0.
+// For the complete license, see http://www.clearcanvas.ca/OSLv3.0
+
+#endregion
+
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using ClearCanvas.Common;
+using ClearCanvas.Common.Serialization;
+using ClearCanvas.Common.Utilities;
+using ClearCanvas.Desktop;
+using ClearCanvas.Enterprise.Common;
+using ClearCanvas.Ris.Application.Common.ModalityWorkflow;
+
+namespace ClearCanvas.Ris.Client.Workflow.Extended
+{
+	[ExtensionOf(typeof(PerformedStepEditorPageProviderExtensionPoint))]
+	public class MppsStructuredDocumentationComponentProvider : IPerformedStepEditorPageProvider
+	{
+		public IPerformedStepEditorPage[] GetPages(IPerformedStepEditorContext context)
+		{
+			var component = new MppsStructuredDocumentationComponent(context);
+			return new IPerformedStepEditorPage[] {component};
+		}
+	}
+
+	class MppsStructuredDocumentationComponent : DHtmlComponent, IPerformedStepEditorPage
+	{
+		#region HealthcareContext
+
+		[DataContract]
+		class HealthcareContext : DataContractBase
+		{
+			private readonly MppsStructuredDocumentationComponent _owner;
+
+			public HealthcareContext(MppsStructuredDocumentationComponent owner)
+			{
+				_owner = owner;
+			}
+
+			[DataMember]
+			public EntityRef OrderRef
+			{
+				get { return _owner._context.OrderRef; }
+			}
+
+			[DataMember]
+			public EntityRef PatientRef
+			{
+				get { return _owner._context.PatientRef; }
+			}
+
+			[DataMember]
+			public EntityRef PatientProfileRef
+			{
+				get { return _owner._context.PatientProfileRef; }
+			}
+
+			[DataMember]
+			public ModalityPerformedProcedureStepDetail ModalityPerformedProcedureStep
+			{
+				get { return _owner._context.SelectedPerformedStep; }
+			}
+		}
+
+		#endregion
+
+		private readonly IPerformedStepEditorContext _context;
+
+		public MppsStructuredDocumentationComponent(IPerformedStepEditorContext context)
+		{
+			_context = context;
+		}
+
+		public override void Start()
+		{
+			// when the selected step changes, refresh the browser
+			_context.SelectedPerformedStepChanged += delegate
+			{
+				SetUrl(WebResourcesSettings.Default.DetailsPageUrl);
+			};
+
+			base.Start();
+		}
+
+		protected override DataContractBase GetHealthcareContext()
+		{
+			return new HealthcareContext(this);
+		}
+
+		protected override IDictionary<string, string> TagData
+		{
+			get { return _context.SelectedPerformedStepExtendedProperties; }
+		}
+
+		#region IPerformedStepEditorPage Members
+
+		Path IExtensionPage.Path
+		{
+			get { return new Path("Details", new ResourceResolver(this.GetType().Assembly)); }
+		}
+
+		IApplicationComponent IExtensionPage.GetComponent()
+		{
+			return this;
+		}
+
+		void IPerformedStepEditorPage.Save()
+		{
+			SaveData();
+		}
+
+		#endregion
+	}
+}
---
+++ Ris/Client/Workflow/MppsDocumentationComponent.cs
@@ -0,0 +1,96 @@
+﻿#region License
+
+// Copyright (c) 2011, ClearCanvas Inc.
+// All rights reserved.
+// http://www.clearcanvas.ca
+//
+// This software is licensed under the Open Software License v3.0.
+// For the complete license, see http://www.clearcanvas.ca/OSLv3.0
+
+#endregion
+
+using ClearCanvas.Common;
+using ClearCanvas.Common.Utilities;
+using ClearCanvas.Desktop;
+
+namespace ClearCanvas.Ris.Client.Workflow
+{
+	/// <summary>
+	/// Extension point for views onto <see cref="MppsDocumentationComponent"/>
+	/// </summary>
+	[ExtensionPoint]
+	public class MppsDocumentationComponentViewExtensionPoint : ExtensionPoint<IApplicationComponentView>
+	{
+	}
+
+	/// <summary>
+	/// MppsDocumentationComponent class
+	/// </summary>
+	[AssociateView(typeof(MppsDocumentationComponentViewExtensionPoint))]
+	public class MppsDocumentationComponent : ApplicationComponent, IPerformedStepEditorPage
+	{
+		private readonly IPerformedStepEditorContext _context;
+		private ICannedTextLookupHandler _cannedTextLookupHandler;
+
+		public MppsDocumentationComponent(IPerformedStepEditorContext context)
+		{
+			_context = context;
+		}
+
+		public override void Start()
+		{
+			_cannedTextLookupHandler = new CannedTextLookupHandler(this.Host.DesktopWindow);
+
+			// when the selected step changes, refresh the browser
+			_context.SelectedPerformedStepChanged += delegate
+			{
+				NotifyPropertyChanged("Comments");
+			};
+
+			base.Start();
+		}
+
+		#region Presentation Model
+
+		public string Comments
+		{
+			get
+			{
+				return _context.SelectedPerformedStepExtendedProperties == null ? null : _context.SelectedPerformedStepExtendedProperties["Comments"];
+			}
+			set
+			{
+				if (_context.SelectedPerformedStepExtendedProperties != null)
+				{
+					_context.SelectedPerformedStepExtendedProperties["Comments"] = value;
+				}
+			}
+		}
+
+		public ICannedTextLookupHandler CannedTextLookupHandler
+		{
+			get { return _cannedTextLookupHandler; }
+		}
+
+		#endregion
+
+		#region IPerformedStepEditorPage Members
+
+		Path IExtensionPage.Path
+		{
+			//todo: loc
+			get { return new Path("MPPS Documentation", new ResourceResolver(this.GetType().Assembly)); }
+		}
+
+		IApplicationComponent IExtensionPage.GetComponent()
+		{
+			return this;
+		}
+
+		void IPerformedStepEditorPage.Save()
+		{
+		}
+
+		#endregion
+	}
+}
