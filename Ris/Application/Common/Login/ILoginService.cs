57f02bf393a605ac1d6001e2ee52cbe7ece8c643
jonathan <devnull@localhost>
2012-08-16 21:25:56 -0400

    [Requirement #10416] - The RIS will be integrated into the Team Edition workstation
    Refactored the RIS Client's login mechanism so that it no longer proxies through the RIS server, but rather talks directly to the ES.  This gets us one step closer to unification, as the RIS Client is now doing essentially the same thing as the Team EnterpriseDesktop plugin.

--- Ris/Application/Common/Login/ILoginService.cs
+++ Ris/Application/Common/Login/ILoginService.cs
@@ -9,59 +9,25 @@
 
 #endregion
 
-using System;
-using System.Collections.Generic;
-using System.Text;
 using System.ServiceModel;
 using ClearCanvas.Enterprise.Common;
 
 namespace ClearCanvas.Ris.Application.Common.Login
 {
-    /// <summary>
-    /// Provides application login operations
-    /// </summary>
-    [RisApplicationService]
-    [ServiceContract]
-    [Authentication(false)]
-    public interface ILoginService
-    {
-        /// <summary>
-        /// Allows a client application to validate user credentials and obtain a set of authority tokens
-        /// specifying what privileges the user has.
-        /// </summary>
-        /// <param name="request"></param>
-        /// <returns></returns>
-        [OperationContract]
-        [FaultContract(typeof(UserAccessDeniedException))]
-        [FaultContract(typeof(PasswordExpiredException))]
-        LoginResponse Login(LoginRequest request);
-
-        /// <summary>
-        /// Allows a client application to inform the server explicitly that the user has logged out
-        /// of the application.
-        /// </summary>
-        /// <param name="request"></param>
-        /// <returns></returns>
-        [OperationContract]
-        [FaultContract(typeof(InvalidUserSessionException))]
-        LogoutResponse Logout(LogoutRequest request);
-
-        /// <summary>
-        /// Allows the user of a client application to change his password.
-        /// </summary>
-        /// <param name="request"></param>
-        /// <returns></returns>
-        [OperationContract]
-        [FaultContract(typeof(RequestValidationException))]
-        [FaultContract(typeof(UserAccessDeniedException))]
-        ChangePasswordResponse ChangePassword(ChangePasswordRequest request);
-
-        /// <summary>
-        /// Gets the list of facilities so that the user can choose their current working facility.
-        /// </summary>
-        /// <param name="request"></param>
-        /// <returns></returns>
-        [OperationContract]
-        GetWorkingFacilityChoicesResponse GetWorkingFacilityChoices(GetWorkingFacilityChoicesRequest request);
-    }
+	/// <summary>
+	/// Provides application login operations
+	/// </summary>
+	[RisApplicationService]
+	[ServiceContract]
+	[Authentication(false)]
+	public interface ILoginService
+	{
+		/// <summary>
+		/// Gets the list of facilities so that the user can choose their current working facility.
+		/// </summary>
+		/// <param name="request"></param>
+		/// <returns></returns>
+		[OperationContract]
+		GetWorkingFacilityChoicesResponse GetWorkingFacilityChoices(GetWorkingFacilityChoicesRequest request);
+	}
 }
--- Ris/Application/Services/Login/LoginService.cs
+++ Ris/Application/Services/Login/LoginService.cs
@@ -9,19 +9,13 @@
 
 #endregion
 
-using System.Security.Principal;
-using System.Threading;
+using System.Linq;
 using ClearCanvas.Common;
 using ClearCanvas.Common.Utilities;
-using ClearCanvas.Enterprise.Common;
-using ClearCanvas.Enterprise.Common.Authentication;
 using ClearCanvas.Enterprise.Core;
 using ClearCanvas.Healthcare;
 using ClearCanvas.Healthcare.Brokers;
 using ClearCanvas.Ris.Application.Common.Login;
-using ChangePasswordRequest = ClearCanvas.Ris.Application.Common.Login.ChangePasswordRequest;
-using ChangePasswordResponse = ClearCanvas.Ris.Application.Common.Login.ChangePasswordResponse;
-using System.ServiceModel;
 
 namespace ClearCanvas.Ris.Application.Services.Login
 {
@@ -36,7 +30,7 @@ namespace ClearCanvas.Ris.Application.Services.Login
 		{
 			// load all facilities and sort by code
 			var facilities = PersistenceContext.GetBroker<IFacilityBroker>().FindAll(false);
-			facilities = CollectionUtils.Sort(facilities, (x, y) => x.Code.CompareTo(y.Code));
+			facilities = facilities.OrderBy(x => x.Code).ToList();
 
 			var facilityAssembler = new FacilityAssembler();
 			return new GetWorkingFacilityChoicesResponse(
@@ -44,136 +38,6 @@ namespace ClearCanvas.Ris.Application.Services.Login
 					(Facility input) => facilityAssembler.CreateFacilitySummary(input)));
 		}
 
-		[UpdateOperation]
-		[Audit(typeof(LoginServiceRecorder))]
-		public LoginResponse Login(LoginRequest request)
-		{
-			Platform.CheckForNullReference(request, "request");
-			Platform.CheckMemberIsSet(request.UserName, "UserName");
-
-			var user = request.UserName;
-			var password = StringUtilities.EmptyIfNull(request.Password);
-			var hostName = StringUtilities.NullIfEmpty(request.HostName) ?? StringUtilities.NullIfEmpty(request.ClientIP);
-
-			try
-			{
-				// initiate session and obtain authority tokens
-				string[] authorityTokens;
-				var token = InitiateSession(user, password, hostName, out authorityTokens);
-
-				// load staff for user
-				var staff = FindStaffForUser(user);
-
-				return new LoginResponse(
-					token,
-					authorityTokens,
-					staff == null ? null : new StaffAssembler().CreateStaffSummary(staff, this.PersistenceContext));
-
-			}
-			// for some reason, we need to catch and rethrow these to get the client
-			// to see a strongly typed fault - otherwise it just gets a general FaultException
-			catch (FaultException<UserAccessDeniedException> e)
-			{
-				throw e.Detail;
-			}
-			catch (FaultException<PasswordExpiredException> e)
-			{
-				throw e.Detail;
-			}
-		}
-
-
-		[UpdateOperation]
-		[Audit(typeof(LoginServiceRecorder))]
-		public LogoutResponse Logout(LogoutRequest request)
-		{
-			Platform.CheckForNullReference(request, "request");
-			Platform.CheckMemberIsSet(request.UserName, "UserName");
-			Platform.CheckMemberIsSet(request.SessionToken, "SessionToken");
-
-			try
-			{
-				Platform.GetService<IAuthenticationService>(
-					service => service.TerminateSession(new TerminateSessionRequest(request.UserName, request.SessionToken)));
-
-				return new LogoutResponse();
-			}
-			catch (FaultException<InvalidUserSessionException> e)
-			{
-				throw e.Detail;
-			}
-		}
-
-		[UpdateOperation]
-		[Audit(typeof(LoginServiceRecorder))]
-		public ChangePasswordResponse ChangePassword(ChangePasswordRequest request)
-		{
-			Platform.CheckForNullReference(request, "request");
-			Platform.CheckMemberIsSet(request.UserName, "UserName");
-
-			var user = request.UserName;
-			var password = StringUtilities.EmptyIfNull(request.Password);
-			var newPassword = StringUtilities.EmptyIfNull(request.NewPassword);
-
-			try
-			{
-				Platform.GetService<IAuthenticationService>(
-					service =>
-					service.ChangePassword(new Enterprise.Common.Authentication.ChangePasswordRequest(user, password, newPassword)));
-
-				return new ChangePasswordResponse();
-			}
-			// for some reason, we need to catch and rethrow these to get the client
-			// to see a strongly typed fault - otherwise it just gets a general FaultException
-			catch (FaultException<RequestValidationException> e)
-			{
-				throw e.Detail;
-			}
-			catch (FaultException<UserAccessDeniedException> e)
-			{
-				throw e.Detail;
-			}
-		}
-
 		#endregion
-
-		/// <summary>
-		/// Initiates a session for the specified user, and establishes a principal on this thread.
-		/// </summary>
-		/// <param name="userName"></param>
-		/// <param name="password"></param>
-		/// <param name="hostname"></param>
-		/// <param name="authorityTokens"></param>
-		/// <returns></returns>
-		private static SessionToken InitiateSession(string userName, string password, string hostname, out string[] authorityTokens)
-		{
-			InitiateSessionResponse initSessionResponse = null;
-			Platform.GetService<IAuthenticationService>(
-				service =>
-				{
-					// TODO: app name shouldn't be hardcoded
-					initSessionResponse = service.InitiateSession(
-						new InitiateSessionRequest(userName, "RIS", hostname, password, true));
-
-					// setup a principal on this thread for the duration of this request
-					// (this is necessary in order to load the WorkingFacilitySettings, etc)
-					Thread.CurrentPrincipal = DefaultPrincipal.CreatePrincipal(new GenericIdentity(userName), initSessionResponse.SessionToken);
-				});
-			authorityTokens = initSessionResponse.AuthorityTokens;
-			return initSessionResponse.SessionToken;
-		}
-
-		/// <summary>
-		/// Gets the staff associated with specified user, or null if no staff associated.
-		/// </summary>
-		/// <param name="userName"></param>
-		/// <returns></returns>
-		private Staff FindStaffForUser(string userName)
-		{
-			var where = new StaffSearchCriteria();
-			where.UserName.EqualTo(userName);
-			return CollectionUtils.FirstElement(PersistenceContext.GetBroker<IStaffBroker>().Find(where));
-		}
-
 	}
 }
