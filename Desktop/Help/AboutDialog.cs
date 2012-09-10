bcdeab3b8dbc54ca4dad78336394917b4d951ee2
jasper <devnull@localhost>
2012-09-10 14:29:44 -0400

    [Requirement #10679] - CE mark in all Team Edition components [Requirement #10680] - CE mark in the Workstation Personal Edition  * Patched in changes from the [3495a395fb552a25e06fe189c8f93c9821a65876/xian] changeset.   * Added an extension point to allow customization of the about dialog.  * Patched in changes from the [8f528e32b5e98664892f3b7639dcd83d453c7904/xian] changeset.   * Added a property to the OEM configuration for specifying a colour for overlay glyphs on desktop branding screens, such as regulatory marks.   * The SplashScreen class now provides a mechanism for dynamically recolouring a source image based on a specified colour.

---
+++ Desktop/Help/AboutDialog.cs
@@ -0,0 +1,38 @@
+﻿#region License
+
+// Copyright (c) 2012, ClearCanvas Inc.
+// All rights reserved.
+// http://www.clearcanvas.ca
+//
+// This software is licensed under the Open Software License v3.0.
+// For the complete license, see http://www.clearcanvas.ca/OSLv3.0
+
+#endregion
+
+using System;
+using System.Windows.Forms;
+using ClearCanvas.Common;
+
+namespace ClearCanvas.Desktop.Help
+{
+	[ExtensionPoint]
+	public sealed class AboutDialogExtensionPoint : ExtensionPoint<IAboutDialog>
+	{
+		public static IAboutDialog CreateDialog()
+		{
+			try
+			{
+				return (IAboutDialog) new AboutDialogExtensionPoint().CreateExtension();
+			}
+			catch (Exception)
+			{
+				return new AboutForm();
+			}
+		}
+	}
+
+	public interface IAboutDialog
+	{
+		DialogResult ShowDialog();
+	}
+}
