71d143c4dac0c4d451d982f34fff829abe963094
thanh <devnull@localhost>
2012-09-09 20:37:06 -0400

    [Requirement #10454] - ImageServer and Webportal need to be intergrated with new licensing
    * Updated EnterpriseLicenseProvider to return the license key in the local license file if it fails to retrieve the key from the Admin Services.
    It no longer throws an exception.
    * Updated DefaultLicenseChecker to ping the Admin Services if the license key is empty (the only time this happens is when the application has never
    been able to contact the Admin Services for the license. Otherwise, the license key would have been retrieved and stored on local cache).
    * Moved WorkQueueManagerExtension to CC.IS.Common to allow the DefaultLicenseChecker to be used in the WebPortal.

---
+++ ImageServer/Common/WorkQueueManagerExtensionPoint.cs
@@ -0,0 +1,33 @@
+﻿#region License
+
+// Copyright (c) 2011, ClearCanvas Inc.
+// All rights reserved.
+// http://www.clearcanvas.ca
+//
+// This software is licensed under the Open Software License v3.0.
+// For the complete license, see http://www.clearcanvas.ca/OSLv3.0
+
+#endregion
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ClearCanvas.Common;
+
+namespace ClearCanvas.ImageServer.Common
+{
+    public interface IWorkQueueManagerExtensionPoint
+    {
+        /// <summary>
+        /// Called when work queue processor is being initialized
+        /// </summary>
+        /// <exception cref="WorkQueueInitializationException"></exception>
+        void OnInitializing(ThreadedService service);
+    }
+
+    [ExtensionPoint]
+    public class WorkQueueManagerExtensionPoint : ExtensionPoint<IWorkQueueManagerExtensionPoint>
+    {
+    }
+}
