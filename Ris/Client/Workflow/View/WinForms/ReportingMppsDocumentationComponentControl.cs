925500a916d920e290cfa199f35baf6b7fd836bf
jonathan <devnull@localhost>
2012-09-18 17:09:03 -0400

    [Requirement #10546] - Exam documentation notes are not available to the radiologist reporting on the procedure
    Introduced ReportingMppsDocumentationComponent to display the documentation within the reporting workspace.

---
+++ Ris/Client/Workflow/View/WinForms/ReportingMppsDocumentationComponentControl.cs
@@ -0,0 +1,33 @@
+﻿#region License
+
+// Copyright (c) 2011, ClearCanvas Inc.
+// All rights reserved.
+// http://www.clearcanvas.ca
+//
+// This software is licensed under the Open Software License v3.0.
+// For the complete license, see http://www.clearcanvas.ca/OSLv3.0
+
+#endregion
+
+using System.Windows.Forms;
+using ClearCanvas.Desktop.View.WinForms;
+
+namespace ClearCanvas.Ris.Client.Workflow.View.WinForms
+{
+	public partial class ReportingMppsDocumentationComponentControl : ApplicationComponentUserControl
+	{
+		private readonly ReportingMppsDocumentationComponent _component;
+
+		public ReportingMppsDocumentationComponentControl(ReportingMppsDocumentationComponent component)
+			: base(component)
+		{
+			InitializeComponent();
+
+			_component = component;
+			_mppsTableView.Table = _component.MppsTable;
+			_mppsTableView.DataBindings.Add("Selection", _component, "SelectedMpps", true, DataSourceUpdateMode.OnPropertyChanged);
+
+			_comments.DataBindings.Add("Text", _component, "MppsComments");
+		}
+	}
+}
---
+++ Ris/Client/Workflow/View/WinForms/ReportingMppsDocumentationComponentView.cs
@@ -0,0 +1,49 @@
+﻿#region License
+
+// Copyright (c) 2011, ClearCanvas Inc.
+// All rights reserved.
+// http://www.clearcanvas.ca
+//
+// This software is licensed under the Open Software License v3.0.
+// For the complete license, see http://www.clearcanvas.ca/OSLv3.0
+
+#endregion
+
+using ClearCanvas.Common;
+using ClearCanvas.Desktop;
+using ClearCanvas.Desktop.View.WinForms;
+
+namespace ClearCanvas.Ris.Client.Workflow.View.WinForms
+{
+	/// <summary>
+	/// Provides a Windows Forms view onto <see cref="ReportingMppsDocumentationComponent"/>
+	/// </summary>
+	[ExtensionOf(typeof(ReportingMppsDocumentationComponentViewExtensionPoint))]
+	public class ReportingMppsDocumentationComponentView : WinFormsView, IApplicationComponentView
+	{
+		private ReportingMppsDocumentationComponent _component;
+		private ReportingMppsDocumentationComponentControl _control;
+
+
+		#region IApplicationComponentView Members
+
+		public void SetComponent(IApplicationComponent component)
+		{
+			_component = (ReportingMppsDocumentationComponent)component;
+		}
+
+		#endregion
+
+		public override object GuiElement
+		{
+			get
+			{
+				if (_control == null)
+				{
+					_control = new ReportingMppsDocumentationComponentControl(_component);
+				}
+				return _control;
+			}
+		}
+	}
+}
