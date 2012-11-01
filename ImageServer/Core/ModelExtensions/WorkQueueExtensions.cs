8e604641c4999c003198898d069d73132608ceac
thanh <devnull@localhost>
2012-10-31 22:38:06 -0400

    [Defect #10716] - ImageServer should warn users of potential incomplete study if they delete a failed StudyProcess (Copy of #10621)
    * Applied patches from #10621 (dbb35c8236da2c07a4443747d6874a3a26167965/xian and e395dbba1a2b05ee509b506143e7d69cf5669bdb/jin)

---
+++ ImageServer/Core/ModelExtensions/WorkQueueExtensions.cs
@@ -0,0 +1,39 @@
+﻿#region License
+
+// Copyright (c) 2012, ClearCanvas Inc.
+// All rights reserved.
+// http://www.clearcanvas.ca
+//
+// This software is licensed under the Open Software License v3.0.
+// For the complete license, see http://www.clearcanvas.ca/OSLv3.0
+
+#endregion
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ClearCanvas.ImageServer.Model;
+
+namespace ClearCanvas.ImageServer.Core.ModelExtensions
+{
+    public static class WorkQueueExtensions
+    {
+        /// <summary>
+        /// Indicates whether or not this WQI will result in patient/study information change.
+        /// This usually indicates if the operation can be safely deleted from the system without any major consequences.
+        /// </summary>
+        public static bool WillResultInDataChanged(this WorkQueue item)
+        {
+            var harmlessWQITypes = new[]{
+                WorkQueueTypeEnum.AutoRoute,
+                WorkQueueTypeEnum.CompressStudy, // not changing patient/study info 
+                WorkQueueTypeEnum.PurgeStudy, // nearline or online
+                WorkQueueTypeEnum.MigrateStudy,
+                WorkQueueTypeEnum.WebMoveStudy
+            };
+
+            return !harmlessWQITypes.Contains(item.WorkQueueTypeEnum);
+        }
+    }
+}
